00562eeb554cc9b2611c174d9eae1fac383cfd59
gabrieldeori <deori.pessoal@gmail.com>
2020-09-11 18:29:09 -0300

    Paleta de cores black red green blue

---
+++ script.js
@@ -0,0 +1,6 @@
+// Random color function
+const arrayPalette = document.getElementsByClassName("color");
+
+arrayPalette[1].style.backgroundColor = "red";
+arrayPalette[2].style.backgroundColor = "green";
+arrayPalette[3].style.backgroundColor = "blue";
