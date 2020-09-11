376605719ace40ef3c85299d3d42632f2b55bff8
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-09-11 19:39:41 -0300

    button

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const black = document.getElementById('black');
+const blue =  document.getElementById('blue');
+const green = document.getElementById('green');
 
+
+blue.addEventListener('click', select => 
+  blue.className = 'color blue selected',
+);
