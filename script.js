f58d93b979e05fa95c657d5ddeac1d0b73701383
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-09-12 14:20:42 -0300

    colorindo e limpando o quadro

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 const clearButton = document.querySelector('#clear-board');
 const coloredPixels = document.querySelectorAll('.pixel');
 const pixelsBoard = document.querySelector('#pixel-board');
-const paleta = document.querySelectorAll('.color');
+const paletas = document.querySelectorAll('.color');
+let paletaID = 'black';
 
-
-function clearBoard() {
-  pixelsBoard.classList.add('white');  
+function defineColor() {
+  let paleta = event.target;
+  paletaID = paleta.getAttribute('id');
+  console.log(paletaID)
 }
 
-clearButton.addEventListener('click', clearBoard);
+
+for( let paleta of paletas) {
+  paleta.addEventListener('click', defineColor);
+}
+function paintBoard() {
+  console.log(paletaID)
+  if (paletaID === 'red') {
+    event.target.classList.add('red');
+  } else if (paletaID === 'blue') {
+    event.target.classList.add('blue');
+  } else if (paletaID === 'green') {
+    event.target.classList.add('green');
+  } else {
+  event.target.classList.add('black');
+  }
+}
+for (let coloredPixel of coloredPixels) {
+  coloredPixel.addEventListener('click', paintBoard)
+}
+
+
+
+clearButton.addEventListener('click', function clearBoard() {
+  for (let i = 0; i < coloredPixels.length; i += 1) {
+    coloredPixels[i].className = 'pixel white';
+  }  
+});
