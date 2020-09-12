18034bc532333dd62c5d29477ea6450e87ead18d
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-09-12 19:03:14 -0300

    selected itens

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const black = document.getElementById('black');
 const blue =  document.getElementById('blue');
 const green = document.getElementById('green');
+const pixels = document.getElementsByClassName('pixel')
 
+blue.addEventListener('click', () => selectColor(blue, 'blue'));
+black.addEventListener('click', () => selectColor(black, 'black'));
+green.addEventListener('click', () => selectColor(green, 'green'));
+red.addEventListener('click', () => selectColor(red, 'red'));
 
-blue.addEventListener('click', select => 
-  blue.className = 'color blue selected',
-);
+function selectColor (element, color) {
+  removeAll();
+  element.className = `color ${color} selected`
+}
+
+function removeAll () {
+  black.classList.remove('selected');
+  blue.classList.remove('selected');
+  green.classList.remove('selected');
+  red.classList.remove('selected');
+}
