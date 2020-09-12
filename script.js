fe2259929083be7dd41048df9b92d1504e2c02b6
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-12 17:39:35 -0300

    Arrumando os classList

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 let color = 'black';
 
-function resetSelector() {
+let resetSelector = () => {
   document.querySelectorAll('.color').forEach((item) => {
-    item.classList.remove = 'selected';
+    item.classList.remove('selected');
   });
-}
+};
 
 const blackPicker = document.querySelector('.black');
 blackPicker.addEventListener('click', function () {
   color = 'black';
   resetSelector();
-  document.querySelector('.black').classList.add = 'selected';
+  document.querySelector('.black').classList.add('selected');
 });
 
 const greenPicker = document.querySelector('.green');
 greenPicker.addEventListener('click', function () {
   color = 'green';
   resetSelector();
-  document.querySelector('.green').classList.add = 'selected';
+  document.querySelector('.green').classList.add('selected');
 });
 
 const bluePicker = document.querySelector('.blue');
 bluePicker.addEventListener('click', function () {
   color = 'blue';
   resetSelector();
-  document.querySelector('.blue').classList.add = 'selected';
+  document.querySelector('.blue').classList.add('selected');
 });
 
 const redPicker = document.querySelector('.red');
 redPicker.addEventListener('click', function () {
   color = 'red';
   resetSelector();
-  document.querySelector('.red').classList.add = 'selected';
+  document.querySelector('.red').classList.add('selected');
 });
 
 // https://flaviocopes.com/how-to-add-event-listener-multiple-elements-javascript/
