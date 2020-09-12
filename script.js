17271bb4939f74fb9bd272a588e7ac1bc3fa84f0
Lorena Góes <goesloren@gmail.com>
2020-09-12 13:14:05 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ let black = document.getElementsByClassName('color')[0];
 let red = document.getElementsByClassName('color')[1];
 let green = document.getElementsByClassName('color')[2];
 let purple = document.getElementsByClassName('color')[3];
+let pixel = document.querySelectorAll('pixel')[0];
+let pixelBoard = document.querySelector('.pixel-board');
