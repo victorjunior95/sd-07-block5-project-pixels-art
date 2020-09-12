82c21d72d4e08d7919027a1c83cbdfbfd47ffb3f
vanderson-henrique <vhbenedito@gmail.com>
2020-09-12 15:32:11 -0300

    Requsiito 7

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 window.onload = function (){
     corInicial.classList.add('selected');
 }*/
+
+let paleta = document.querySelectorAll('.color');
+for (index = 0; index < paleta.length; index += 1){
+    let corAtual = paleta[index];
+    corAtual.addEventListener('click', function(){
+        for (index = 0; index < paleta.length; index += 1){
+            let removeClasse = paleta[index];
+            removeClasse.classList.remove('selected');
+        }
+        corAtual.classList.add('selected');
+    });
+}
