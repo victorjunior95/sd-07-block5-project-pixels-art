ebb29c24671fa30a60c8aefef498acbde9a9e913
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-09-12 13:37:50 -0300

    Requisito 4 tamanho do quadro

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const colors = document.getElementsByClassName('color')
 
-colors[0].style.backgroundColor="red"
+colors[0].style.backgroundColor="black"
 colors[1].style.backgroundColor="blue"
 colors[2].style.backgroundColor="green"
-colors[3].style.backgroundColor="black"
+colors[3].style.backgroundColor="red"
