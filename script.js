f8d9f531f105be8b8615d059ad60f25c242a3661
danielmlduarte <danielduarte.tryber@gmail.com>
2020-09-11 22:37:24 -0300

    Adicionando a classe selected ao elemento quando recebe o clique

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 const color2 = document.querySelector('#color-2');
 const color3 = document.querySelector('#color-3');
 const color4 = document.querySelector('#color-4');
-
+const colorPalette = document.querySelector('#color-palette');
+let selectedColor = 'black';
 
 function colorGeneration() {
   const red = Math.floor(Math.random() * 255);
@@ -12,6 +13,21 @@ function colorGeneration() {
   return rgb;
 }
 
+function removeSelectedClass(stringClass) {
+  const classArray = stringClass.split(' ');
+  classArray.splice(2,1);
+  return classArray.join(' ');
+}
+
 color2.style.backgroundColor = colorGeneration();
 color3.style.backgroundColor = colorGeneration();
 color4.style.backgroundColor = colorGeneration();
+
+colorPalette.addEventListener('click', function (event) {
+  if (event.target.className.includes('color')) {
+    const oldSelectedElement = document.querySelector('.selected');
+    const oldClassName = removeSelectedClass(oldSelectedElement.className);
+    oldSelectedElement.className = oldClassName;
+    event.target.className = event.target.className+' selected';
+  }
+});
