950edb50bb90b8bd7b3b336fb4be8b8084d09289
Lorena Góes <goesloren@gmail.com>
2020-09-12 00:21:40 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // 1) Declare the variables:
 
-let red = document.getElementsByClassName('color')[0];
-let green = document.getElementsByClassName('color')[1];
-let black = document.getElementsByClassName('color')[2];
+let black = document.getElementsByClassName('color')[0];
+let red = document.getElementsByClassName('color')[1];
+let green = document.getElementsByClassName('color')[2];
 let purple = document.getElementsByClassName('color')[3];
