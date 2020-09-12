ab63cfc73ed00e4f71c1316f921baaa920771db2
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-12 16:40:06 -0300

    Refatorando o js, modificada a função que chamava item por item e sobrescrevia todas as classes dos botões por forEach e classListadd e remove

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 let color = "black";
-function resetSelector (){
-    document.querySelector('.black').className = 'color black box';
-    document.querySelector('.green').className = 'color green box';
-    document.querySelector('.blue').className = 'color blue box';
-    document.querySelector('.red').className = 'color red box';
-}
-const blackPicker = document.querySelector('.black');
-blackPicker.addEventListener('click', function (){
-    color = "black";
-    resetSelector();
-    document.querySelector('.black').className = 'color black box selected';
+
+function resetSelector() {
+  document.querySelectorAll('.color').forEach(item) =>{
+      item.classList.remove = 'selected';
+  };
+};
+
+const blackPicker = document.querySelector(".black");
+blackPicker.addEventListener("click", function () {
+  color = "black";
+  resetSelector();
+  document.querySelector(".black").classList.add = 'selected';
 });
 
-const greenPicker = document.querySelector('.green');
-greenPicker.addEventListener('click', function (){
-    color = "green";
-    resetSelector();
-    document.querySelector('.green').className = 'color green box selected';
+const greenPicker = document.querySelector(".green");
+greenPicker.addEventListener("click", function () {
+  color = "green";
+  resetSelector();
+  document.querySelector(".green").classList.add = 'selected';
 });
 
-const bluePicker = document.querySelector('.blue');
-bluePicker.addEventListener('click', function (){
-    color = "blue";
-    resetSelector();
-    document.querySelector('.blue').className = 'color blue box selected';
+const bluePicker = document.querySelector(".blue");
+bluePicker.addEventListener("click", function () {
+  color = "blue";
+  resetSelector();
+  document.querySelector(".blue").classList.add = 'selected';
 });
 
-const redPicker = document.querySelector('.red');
-redPicker.addEventListener('click', function (){
-    color = 'red';
-    resetSelector();
-    document.querySelector('.red').className = 'color red box selected';
+const redPicker = document.querySelector(".red");
+redPicker.addEventListener("click", function () {
+  color = "red";
+  resetSelector();
+  document.querySelector(".red").classList.add = 'selected';
 });
 
 // https://flaviocopes.com/how-to-add-event-listener-multiple-elements-javascript/
-document.querySelectorAll('.pixel').forEach(item => {
-    item.addEventListener('click', event => {
-      item.style.backgroundColor = color;
-    })
-  })
+document.querySelectorAll(".pixel").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.style.backgroundColor = color;
+  });
+});
