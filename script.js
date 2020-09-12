fe977a92fe89a7086e000a87d6151d6edda3a295
vanderson-henrique <vhbenedito@gmail.com>
2020-09-12 16:31:44 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-/*let corInicial = document.querySelector('.color-1');
-window.onload = function (){
-    corInicial.classList.add('selected');
-}*/
-
 let paleta = document.querySelectorAll('.color');
 let corSelecionada = document.querySelector('.color-1');
 
-for (index = 0; index < paleta.length; index += 1){
+for (index = 0; index < paleta.length; index += 1) {
   let corAtual = paleta[index];
     corAtual.addEventListener('click', function() {
-      for (index = 0; index < paleta.length; index += 1){
+      for (index = 0; index < paleta.length; index += 1) {
         let removeClasse = paleta[index];
           removeClasse.classList.remove('selected');
         }
@@ -19,9 +14,18 @@ for (index = 0; index < paleta.length; index += 1){
 }
 
 let quadro = document.querySelectorAll('.pixel');
-for (index = 0; index < quadro.length; index += 1){
+for (index = 0; index < quadro.length; index += 1) {
     let pixelSelecionado = quadro[index];
     pixelSelecionado.addEventListener('click', function() {
         pixelSelecionado.classList.add(corSelecionada.classList[1]);
     }) 
 }
+
+let botaoLimpar = document.getElementById('clear-board');
+botaoLimpar.addEventListener('click', function() {
+    for (index = 0; index < quadro.length; index += 1) {
+        let limpaQuadro = quadro[index];
+        limpaQuadro.className = '';
+        limpaQuadro.classList.add('pixel');
+    }
+})
