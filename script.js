d01dc1d41783532564a47a40d3bfb1acef148001
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-12 16:07:36 -0300

    Resolvido o exercicio com for each

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
 let color = "black";
+function resetSelector (){
+    document.querySelector('.black').className = 'color black box';
+    document.querySelector('.green').className = 'color green box';
+    document.querySelector('.blue').className = 'color blue box';
+    document.querySelector('.red').className = 'color red box';
+}
 const blackPicker = document.querySelector('.black');
 blackPicker.addEventListener('click', function (){
     color = "black";
+    resetSelector();
+    document.querySelector('.black').className = 'color black box selected';
 });
 
 const greenPicker = document.querySelector('.green');
 greenPicker.addEventListener('click', function (){
     color = "green";
+    resetSelector();
+    document.querySelector('.green').className = 'color green box selected';
 });
 
 const bluePicker = document.querySelector('.blue');
 bluePicker.addEventListener('click', function (){
     color = "blue";
+    resetSelector();
+    document.querySelector('.blue').className = 'color blue box selected';
 });
 
 const redPicker = document.querySelector('.red');
 redPicker.addEventListener('click', function (){
     color = 'red';
-})
-for(index = 0; index < document.querySelectorAll('.pixel').length; index += 1){
-    let pixel = document.querySelectorAll('.pixel')[1];
-    pixel.addEventListener('click', function (){
-        document.querySelectorAll('.pixel')[1].style.backgroundColor = color;
+    resetSelector();
+    document.querySelector('.red').className = 'color red box selected';
+});
+
+document.querySelectorAll('.pixel').forEach(item => {
+    item.addEventListener('click', event => {
+      item.style.backgroundColor = color;
     })
-}
+  })
