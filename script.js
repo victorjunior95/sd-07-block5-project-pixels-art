cdd4a740636bcc5269629170272de7111a697f9c
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-09-12 18:35:05 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
-const pixels = document.getElementsByClassName('pixel');
-let pixelSelectedCurrent;
-
-function addSelectedCurrent(pixel) {
-  pixel.className += ' selected';
-  pixelSelectedCurrent = pixel;
-}
-
-function emptySelectedCurrent() {
-  pixelSelectedCurrent.className = 'pixel';
-}
-
-for(let index = 0; index < pixels.length; index += 1){
-  pixels[index].addEventListener('click', function(event){
-    if (pixelSelectedCurrent) emptySelectedCurrent();
-    addSelectedCurrent(pixels[index]);
-  });
-}
-
-const colors = document.getElementsByClassName('color');
-
-const colorsConfig = {
+const colorsPallet = {
   color1: 'black',
   color2: 'red',
   color3: 'green',
   color4: 'blue',
 }
 
-for(let index = 0; index < colors.length; index += 1){
-  colors[index].addEventListener('click', function(event){
-    let pixel = document.getElementsByClassName('pixel selected')[0];
-    pixel.style.backgroundColor = colorsConfig[`${event.target.id}`];
+let colorSelected = colorsPallet.color1;
+
+const colors = document.getElementsByClassName('color');
+
+for (let index = 0; index < colors.length; index += 1) {
+  colors[index].addEventListener('click', function (event) {
+    colorSelected = colorsPallet[`${event.target.id}`];
   });
 }
 
+const pixels = document.getElementsByClassName('pixel');
+
+let pixelCurrent;
+
+function pixelDefault() {
+  pixelCurrent.className = 'pixel';
+}
+
+function addPixelCurrent(pixel) {
+  pixel.className += ' selected';
+  pixelCurrent = pixel;
+}
+
+for (let index = 0; index < pixels.length; index += 1) {
+  pixels[index].addEventListener('click', function (event) {
+    if (pixelCurrent) pixelDefault();
+    addPixelCurrent(event.target);
+    if (pixelCurrent) pixelCurrent.style.backgroundColor = colorSelected;
+  });
+}
