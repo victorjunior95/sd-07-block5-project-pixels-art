813cd67eb74ce92454001fbf71f6892821418aa9
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-12 18:03:55 -0300

    estiliza botão de cor selecionado

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const colorPalette = document.getElementById('color-palette');
-const clearBoard = document.getElementById('clear-board');
 const pixelBoard = document.getElementById('pixel-board');
 
 const divPixel = document.getElementsByClassName('pixel');
+const buttonColor = document.getElementsByClassName('color');
+const buttonClear = document.getElementById('clear-board');
 
 const buttonBlack = document.querySelector('.black');
 const buttonRed = document.querySelector('.red');
 const buttonGreen = document.querySelector('.green');
 const buttonBlue = document.querySelector('.blue');
 
-let currentColor = '';
+let currentColor = 'black';
 
 buttonBlack.addEventListener('click', selectColor);
 buttonRed.addEventListener('click', selectColor);
@@ -17,6 +18,12 @@ buttonGreen.addEventListener('click', selectColor);
 buttonBlue.addEventListener('click', selectColor);
 
 function selectColor(event) {
+  for (let element of buttonColor) {
+    if (element.classList[2] === 'selected') {
+      element.classList.toggle('selected');
+    }
+  }
+  event.target.classList.toggle('selected');
   currentColor = event.target.classList[1];
 }
 
