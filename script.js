0bc6b58d8bf1569ebca006fa0849e1be5205eb34
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-09-11 22:31:04 -0300

    adicionando classe pra bg branco requisito9

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const clearButton = document.querySelector('#clear-board');
 const coloredPixels = document.querySelectorAll('.pixel');
 const pixelsBoard = document.querySelector('#pixel-board');
+const paleta = document.querySelectorAll('.color');
+
 
 function clearBoard() {
-  for (let i = 0; i < coloredPixels.length; i += 1){
-    coloredPixels.style.backgroundColor = 'white';
+  for (let i = 0; i < coloredPixels.length; i += 1) {
+    coloredPixels[i].classList.add('white');
   }
 }
 
