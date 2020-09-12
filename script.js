18ddb088f9d31f651fdf2a7e6c240d0d063a1938
vanderson-henrique <vhbenedito@gmail.com>
2020-09-12 16:06:06 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -4,13 +4,24 @@ window.onload = function (){
 }*/
 
 let paleta = document.querySelectorAll('.color');
+let corSelecionada = document.querySelector('.color-1');
+
 for (index = 0; index < paleta.length; index += 1){
-    let corAtual = paleta[index];
-    corAtual.addEventListener('click', function(){
-        for (index = 0; index < paleta.length; index += 1){
-            let removeClasse = paleta[index];
-            removeClasse.classList.remove('selected');
+  let corAtual = paleta[index];
+    corAtual.addEventListener('click', function() {
+      for (index = 0; index < paleta.length; index += 1){
+        let removeClasse = paleta[index];
+          removeClasse.classList.remove('selected');
         }
         corAtual.classList.add('selected');
+        corSelecionada = corAtual;
     });
+}
+
+let quadro = document.querySelectorAll('.pixel');
+for (index = 0; index < quadro.length; index += 1){
+    let pixelSelecionado = quadro[index];
+    pixelSelecionado.addEventListener('click', function() {
+        pixelSelecionado.classList.add(corSelecionada.classList[1]);
+    }) 
 }
