192f54109528425839803998221c67d83cacac91
Carol Bezerra <carol.arbe@gmail.com>
2020-09-12 20:38:09 -0300

    Requisito 9 tentativa 2

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ window.onload = function () {
   colors[0].className += ' selected';
 };
 
-document.querySelector('.clear-board').addEventListener('click', function () {
-  const clear = document.querySelectorAll('.pixel');
-  for (let i = 0; i < clear.length; i += 1) {
-    clear[i].style.backgroundColor = 'white';
+document.querySelector('#clear-board').addEventListener('click', function () {
+  const tableData = document.querySelectorAll('.pixel');
+  for (let i = 0; i < tableData.length; i += 1) {
+    tableData[i].style.backgroundColor = 'white';
   }
-
 });
