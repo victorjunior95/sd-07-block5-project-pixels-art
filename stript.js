0d1aaa7019cf0d141d9179dbe7039e3d09bbcf48
Danwhat <daanwhat@gmail.com>
2020-09-12 14:40:37 -0300

    100% vai! 2.0

--- stript.js
+++ stript.js
@@ -64,9 +64,34 @@ function tamanho(){
     return printSize
     
 }
+
+function limparTela(){
+    let tabela = document.querySelector("#pixel-board")
+    let printSize = tabela.rows.length
+    if (printSize != "") {
+        let tabela = document.querySelector("#pixel-board")
+        tabela.innerHTML = ""
+        for (let i = 0; i< printSize; i+=1){
+            let linha = document.createElement("tr")
+                for (let i = 0; i< printSize; i+=1) {
+                    let coluna = document.createElement("td")
+                    coluna.className = "pixel"
+                    coluna.style.height = "40px"
+                    coluna.style.width = "40px"
+                    coluna.style.backgroundColor = "branco"
+                    coluna.style.border = "black 1px solid"
+                    coluna.addEventListener("click", pintar)
+                    linha.append(coluna)
+                }
+            tabela.append(linha)
+       }
+    }
+}
+
 /* Botão Limpar */
+
 let btnLimpar = document.querySelector("#clear-board")
-btnLimpar.addEventListener("click", criarTela)
+btnLimpar.addEventListener("click", limparTela)
 
 /* Botão VQV */
 
