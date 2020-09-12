a0753b217141af276f23b8aec13e4bd03f021812
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-09-12 13:19:52 -0300

    Requisito 2 adicionando paleta de cores

---
+++ script.js
@@ -0,0 +1,6 @@
+const colors = document.getElementsByClassName('color')
+
+colors[0].style.backgroundColor="red"
+colors[1].style.backgroundColor="blue"
+colors[2].style.backgroundColor="green"
+colors[3].style.backgroundColor="black"
