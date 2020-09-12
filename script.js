0b897abb2e87948f7b6511e3c8729e228d5f06d7
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-09-12 19:31:47 -0300

    adicionando funcionalidade que pinta os pixels

--- script.js
+++ script.js
@@ -7,7 +7,19 @@ function selectingColor(event) {
   event.target.classList.add('selected');
 }
 
+function paintingPixel(event) {
+  if (event.target.classList.length > 1) {
+      event.target.classList.remove(event.target.classList[1]);
+  }
+  const selectedColor = document.querySelector('.selected').classList[1];
+  event.target.classList.add(selectedColor);
+}
+
 const colorPixelList = document.getElementsByClassName('color');
 for (let i = 0; i < 4; i += 1) {
   colorPixelList[i].addEventListener('click', selectingColor);
+}
+const pixelList = document.getElementsByClassName('pixel');
+for (let i = 0; i < pixelList.length; i += 1) {
+    pixelList[i].addEventListener('click', paintingPixel);
 }
