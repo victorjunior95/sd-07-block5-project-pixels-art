571380cea28fc0d98f74ac9a9a6ba6a91ab6ede6
Fernando <nando.neves@gmail.com>
2020-09-12 18:37:29 -0300

    trocando as cores

--- script.js
+++ script.js
@@ -4,49 +4,50 @@ let black = document.querySelector("#black");
 let azul = document.querySelector("#blue");
 let red = document.querySelector("#red");
 let purple = document.querySelector("#purple");
+let cor = "black";
 
-let ultima = document.querySelector(".selected")
-
+let ultima = document.querySelector(".selected");
 
 
 black.addEventListener("click",function(){
-    console.log(document.getElementsByClassName("selected").style.backgroundColor);
-    // ultima.class = "color";
-    // selecao.class = "color selected";
     document.getElementsByClassName("selected")[0].classList = "color";
     document.getElementById("black").classList.add("selected");
-
+    cor = "black"
 })
 
 azul.addEventListener("click",function(){
-    console.log(document.querySelector(".selected").style.backgroundColor);
-    // ultima.class = "color";
-    // selecao.class = "color selected";
     document.getElementsByClassName("selected")[0].classList = "color";
     document.getElementById("blue").classList.add("selected");
-
+    cor = "blue"
 })
 
 red.addEventListener("click",function(){
-    console.log(document.querySelector(".selected").style.backgroundColor);
-    // ultima.class = "color";
-    // selecao.class = "color selected";
     document.getElementsByClassName("selected")[0].classList = "color";
     document.getElementById("red").classList.add("selected");
-
+    cor = "red"
 })
 
 
 purple.addEventListener("click",function(){
-    console.log(document.querySelector(".selected").style.backgroundColor);
-    // ultima.class = "color";
-    // selecao.class = "color selected";
     document.getElementsByClassName("selected")[0].classList = "color";
     document.getElementById("purple").classList.add("selected");
-
+    cor = "purple"
 })
 
 //Mudar a cor dos pixels
+
+let qualPixel = document.querySelectorAll(".pixel")
+
+
+for (let i = 0; i < qualPixel.length; i++) {
+    (function(i) {
+         qualPixel[i].addEventListener("click", function() {
+           
+             qualPixel[i].style.backgroundColor = cor;
+            
+        })
+    })(i);
+ }
 
 
 
