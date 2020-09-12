224d55c9d844b1d586ae559cb173771bab7ae328
Carol Bezerra <carol.arbe@gmail.com>
2020-09-12 19:57:05 -0300

    Adiantando o Requisito 9

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+const colors = document.querySelectorAll('.color');
+
 window.onload = function () {
-    colors[0].className += ' selected';
+  colors[0].className += ' selected';
 };
 
-const colors = document.querySelectorAll('.color');
+document.querySelector('.clear-board').addEventListener('click', function () {
+  const clear = document.querySelectorAll('.pixel');
+  for (let i = 0; i < clear.length; i += 1) {
+    clear[i].style.backgroundColor = 'white';
+  }
+
+});
