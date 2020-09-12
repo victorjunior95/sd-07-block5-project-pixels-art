21e9333abfbe61cfd02332839f3b6d5bcf6e2dd7
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-09-12 14:14:45 -0300

    Requirement Bonus 10

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let selectedColorRGB = '';
 // function to apply selected class to color pallet
 function applySelectedClass(colorElement) {
     // Apply the class selected
-    colorElement.className = colorElement.className + " selected";
+    colorElement.className = colorElement.className + ' selected';
 
     // Save color selected
     selectedColorRGB = getComputedStyle(colorElement).getPropertyValue('background-color');
@@ -15,7 +15,7 @@ function removeSelectedClass() {
     // For each color Pallet 
     document.querySelectorAll('.color').forEach(item => {
         // remove class selected
-        item.className = item.className.replace('selected','');
+        item.className = item.className.replace(' selected','');
     });
 }
 
@@ -42,8 +42,41 @@ function clearBoard(){
     });
 }
 
+// Function that generate the pixel board
+function generateBoard() {
+    // Get input value
+    let sizeBoard = document.getElementById('board-size').value;
+    // if size is null, alert the user
+    if (sizeBoard == '' || sizeBoard == 0) {
+        alert('Board inválido!');
+    } else {
+        // Get the pixel board
+        const pixelBoard = document.getElementById('pixel-board');
+        // Clear the board
+        pixelBoard.innerHTML = '';
+        // Create the board
+        for (let indexLine = 0; indexLine < sizeBoard; indexLine += 1) {
+            // Create the line <div class="board-line">
+            let boardLine = document.createElement('div');
+            boardLine.className = 'board-line';
+            // Create the pixels
+            for (let indexPixel = 0; indexPixel < sizeBoard; indexPixel += 1) {
+                // Create pixel element  <div class="pixel"></div>
+                let pixel = document.createElement('div');
+                pixel.className = 'pixel';
+                // Assign event applyBackgroundColor
+                pixel.addEventListener('click',applyBackgroundColor);
+                // Append the pixel to board line
+                boardLine.appendChild(pixel);
+            }
+            // Append the board line to board pixel
+            pixelBoard.appendChild(boardLine); 
+        }
+    }
+}
+
 // Execute function to select the first color pallet
-applySelectedClass(document.querySelector(".color"));
+applySelectedClass(document.querySelector('.color'));
 
 // Assign the function changeSelectedColorPallet on all colors Pallet
 // For each color Pallet 
@@ -62,4 +95,5 @@ document.querySelectorAll('.pixel').forEach(item => {
 // Assign the function clearBoard to button
 document.getElementById('clear-board').addEventListener('click',clearBoard);
 
-
+// Assign the function generateBoard to button
+document.getElementById('generate-board').addEventListener('click',generateBoard);
