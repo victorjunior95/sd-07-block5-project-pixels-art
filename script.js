bdfe21ce13d9e1d43637f87f75e9a0bff6f02c50
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-12 17:13:43 -0300

    Arrumando codeclimate

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
-let color = "black";
+let color = 'black';
 
 function resetSelector() {
-  document.querySelectorAll('.color').forEach(item); {
-      item.classList.remove = 'selected'
+  document.querySelectorAll('.color').forEach(item);
+  {
+    item.classList.remove = 'selected';
   }
 }
 
-const blackPicker = document.querySelector(".black");
-blackPicker.addEventListener("click", function () {
-  color = "black";
+const blackPicker = document.querySelector('.black');
+blackPicker.addEventListener('click', function () {
+  color = 'black';
   resetSelector();
-  document.querySelector(".black").classList.add = 'selected';
+  document.querySelector('.black').classList.add = 'selected';
 });
 
-const greenPicker = document.querySelector(".green");
-greenPicker.addEventListener("click", function () {
-  color = "green";
+const greenPicker = document.querySelector('.green');
+greenPicker.addEventListener('click', function () {
+  color = 'green';
   resetSelector();
-  document.querySelector(".green").classList.add = 'selected';
+  document.querySelector('.green').classList.add = 'selected';
 });
 
-const bluePicker = document.querySelector(".blue");
-bluePicker.addEventListener("click", function () {
-  color = "blue";
+const bluePicker = document.querySelector('.blue');
+bluePicker.addEventListener('click', function () {
+  color = 'blue';
   resetSelector();
-  document.querySelector(".blue").classList.add = 'selected';
+  document.querySelector('.blue').classList.add = 'selected';
 });
 
-const redPicker = document.querySelector(".red");
-redPicker.addEventListener("click", function () {
-  color = "red";
+const redPicker = document.querySelector('.red');
+redPicker.addEventListener('click', function () {
+  color = 'red';
   resetSelector();
-  document.querySelector(".red").classList.add = 'selected';
+  document.querySelector('.red').classList.add = 'selected';
 });
 
 // https://flaviocopes.com/how-to-add-event-listener-multiple-elements-javascript/
-document.querySelectorAll(".pixel").forEach((item) => {
-  item.addEventListener("click", (event) => {
+document.querySelectorAll('.pixel').forEach((item) => {
+  item.addEventListener('click', (event) => {
     item.style.backgroundColor = color;
   });
 });
