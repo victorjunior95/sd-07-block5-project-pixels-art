25a5240a362d5efc712d6fa2172ad9767e2d0014
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-09-12 20:22:53 -0300

    fi

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+let boardSizeLoaded =  5;
+
+function removeBoard() {
+  const pixelBoard = document.getElementsByClassName('pixel-board')[0];
+  let boards = document.getElementsByClassName('pixel');
+
+  for(let index = 0; index < boards.length; index += 1){
+    pixelBoard.removeChild(boards[index]);
+  }
+}
+
+// generate board
+function generateBoard() {
+  let boardSize = boardSizeLoaded * boardSizeLoaded;
+
+  const pixelBoard = document.getElementsByClassName('pixel-board')[0];
+  pixelBoard.style.width = `${40 * boardSizeLoaded}px`;
+  pixelBoard.style.height = `${40 * boardSizeLoaded}px`;
+
+  for(let index = 1; index <= boardSize; index += 1){
+    let div = document.createElement('div');
+    div.id = 'pixel';
+    div.className = 'pixel';
+    pixelBoard.appendChild(div);
+  }
+}
+
+generateBoard();
+
+const inputBoard = document.getElementById('board-size');
+const btn = document.getElementById('generate-board');
+btn.addEventListener('click', function(){
+  boardSizeLoaded = inputBoard.value ? inputBoard.value : 5;
+  removeBoard();
+  generateBoard();
+})
+
+
 const colorsPallet = {
   color1: 'black',
   color2: 'red',
