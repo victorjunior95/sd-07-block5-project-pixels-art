8fde43d830f3f523b84d788781d4cef14dedd720
josielvs <josielvs@gmail.com>
2020-09-13 18:01:49 -0300

    Requisito 1. - Adicionando título a page com id.

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function createReader() {
   const headerPage = document.createElement('header');
   headerPage.className = 'head-class';
   document.body.appendChild(headerPage);
-  
   const titleProject = document.createElement('h1');
   titleProject.innerHTML = 'Paleta de Cores';
   titleProject.className = 'title';
+  titleProject.setAttribute('id', 'title')
   headerPage.appendChild(titleProject);
 }
 createReader();
