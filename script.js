8c95a423f99678bd51cacfcfe18bbe5af99c067e
Raphael Caputo <faelelf2@gmail.com>
2020-09-13 20:37:00 -0300

    Adicionando botao White all

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-let colorEnter = document.getElementById('black-index');
-document.querySelectorAll('.color').forEach(clickToConfigurePallete);
-
-function removeAll(item) {
-  item.classList.remove('selected');
-}
-
-function clickToConfigurePallete(item) {
-  item.addevenListener("click", function () {
-    document.querySelectorAll(".color").forEach(removeAll);
-    item.classList.add("selected");
-  });
-}
-
-const button = querySelector("#clear-board");
+let button = document.getElementById('clear-board');
 button.addevenListener("click", function () {
   document.querySelectorAll(".pixel").forEach(allToWhite);
 });
