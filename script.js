1f554531eed0c2bfd8dd1317842952ed8b2b96ce
Lucas Ribeiro <martins7ribeiro@gmail.com>
2020-09-13 00:23:52 -0300

    requirement 2

---
+++ script.js
@@ -0,0 +1,10 @@
+//Defining the colors
+const black = document.querySelector('#black');
+const red = document.querySelector('#red');
+const green = document.querySelector('#green');
+const blue = document.querySelector('#blue');
+
+black.style.backgroundColor = 'black';
+red.style.backgroundColor = 'red';
+green.style.backgroundColor = 'green';
+blue.style.backgroundColor = 'blue';
