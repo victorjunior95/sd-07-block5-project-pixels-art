bc4eafa87e255726a8c21acd75851ae37da23711
Lorena Góes <goesloren@gmail.com>
2020-09-13 17:20:59 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,61 +1,51 @@
 // 1) Declare the variables:
 
-let black = document.getElementsByClassName('color')[0];
-let red = document.getElementsByClassName('color')[1];
-let green = document.getElementsByClassName('color')[2];
-let purple = document.getElementsByClassName('color')[3];
+let black = document.querySelectorAll('.color')[0];
+let red = document.querySelectorAll('.color')[1];
+let green = document.querySelectorAll('.color')[2];
+let purple = document.querySelectorAll('.color')[3];
 let pixel = document.querySelectorAll('.pixel')[0];
 let pixelBoard = document.querySelector('.pixel-board');
-let selected = document.querySelector('.selected')
 
 // 2) Define the functions:
 
+// To define the black color:
+black.style.backgroundColor = 'black';
+window.addEventListener('load', function () {
+  red.style.backgroundColor = 'red';
+  green.style.backgroundColor = 'green';
+  purple.style.backgroundColor = 'purple';
+})
+
 // To select the black color:
-function selectBlack (selected) {
+function selectBlack() {
+  let selected = document.querySelector('.selected')
   selected.classList.remove('selected');
   black.classList.add('selected');
 }
 
 // To select the red color:
-function selectRed (selected) {
+function selectRed () {
+  let selected = document.querySelector('.selected')
   selected.classList.remove('selected');
   red.classList.add('selected');
 }
 
 // To select the green color:
-function selectGreen (selected) {
+function selectGreen () {
+  let selected = document.querySelector('.selected')
   selected.classList.remove('selected');
   green.classList.add('selected');
 }
 
 // To select the purple color:
-function selectPurple (selected) {
+function selectPurple () {
+  let selected = document.querySelector('.selected')
   selected.classList.remove('selected');
   purple.classList.add('selected');
 }
 
-// To set a color:
-function setColor () {
-  if (black) {
-    selectBlack;
-  }
-  else if (red) {
-    selectRed;
-  }
-  else if (green) {
-    selectGreen;
-  }
-  else if (purple) {
-    selectPurple;
-  }
-  else {
-    console.log("Error! Please choose a valid color");
-  }
-}
-
-// To click on the color squares:
-document.querySelectorAll('.color').forEach(square => {
-  square.addEventListener('click', event => {
-    setColor (square);  
-  })
-})
+// To paint the blank squares:
+pixelBoard.addEventListener('click', function(pixel) {
+  pixel.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+});
