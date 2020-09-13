85a1b5b18803816863251bdb0b00b1daed924c1a
kramersilva <kramerscs@gmail.com>
2020-09-13 10:34:57 -0300

    requisito 9

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
+// Define color-pallete variables
 let colorOne = document.getElementsByClassName('color')[0];
 let colorTwo= document.getElementsByClassName('color')[1];
 let colorThree = document.getElementsByClassName('color')[2];
 let colorFour = document.getElementsByClassName('color')[3];
-let selectedColor = document.getElementsByClassName('color selected')
+let clearButton = document.getElementById('clear-board')
 
-let paletteSelection = document.getElementsByClassName('color')
+// Define selected color with color selected class
+let selectedColor = document.getElementsByClassName('color selected');
 
-let pixelSelection = document.getElementsByClassName('pixel')
+// Array of all elements with class color
+let paletteSelection = document.getElementsByClassName('color');
 
+// Array of all elements in the pixel board
+let pixelSelection = document.getElementsByClassName('pixel');
 
+// Loop to AddEventListener to all elements in color palette
 for (let item = 0; item < paletteSelection.length; item += 1) {
-  paletteSelection[item].addEventListener('click', selectColor)
+  paletteSelection[item].addEventListener('click', selectColor);
 }
-
+// Loop to AddEventListener to all elements in the pixel board
 for (let pixel = 0; pixel < pixelSelection.length; pixel += 1) {
-  let pixelBox = pixelSelection[pixel]
-  console.log(pixelBox)
-  pixelBox.addEventListener('click', fillColor)
+  let pixelBox = pixelSelection[pixel];
+  pixelBox.addEventListener('click', fillColor);
 }
 
+clearButton.addEventListener('click', function () {
+  for (let box = 0; box < pixelSelection.length; box += 1) {
+    pixelSelection[box].style.backgroundColor = 'white'
+  }
+})
+
+// Function that pick a color (chosen element is tagged with a color selected class while previous selected color is changed to a color class name)
 function selectColor(event) {
-  selectedColor[0].className = 'color'
-  event.target.className = 'color selected'
+  selectedColor[0].className = 'color';
+  event.target.className = 'color selected';
 }
 
+// function that fills a pixel and changes its background color to the selected one.
 function fillColor(event) {
-  let color = selectedColor[0].id
-  event.target.style.backgroundColor = color
+  let color = selectedColor[0].id;
+  event.target.style.backgroundColor = color;
 }
