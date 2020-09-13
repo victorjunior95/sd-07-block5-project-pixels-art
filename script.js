644ea717432d53263a6affa74e8faba8327b35bb
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-09-13 12:35:37 -0300

    Requisite 7.1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//document.getElementsByClassName('black').class = 'color black selected';
-
 function black() {
   document.getElementsByClassName('black').class = 'color black selected';
   document.getElementsByClassName('blue').class = 'color blue';
@@ -8,22 +6,22 @@ function black() {
 }
 
 function blue() {
-  document.getElementsByClassName('black').class = 'color black';
   document.getElementsByClassName('blue').class = 'color blue selected';
+  document.getElementsByClassName('black').class = 'color black';
   document.getElementsByClassName('yellow').class = 'color yellow';
   document.getElementsByClassName('green').class = 'color green';
 }
 
 function yellow() {
+  document.getElementsByClassName('yellow').class = 'color yellow selected';
   document.getElementsByClassName('black').class = 'color black';
   document.getElementsByClassName('blue').class = 'color blue';
-  document.getElementsByClassName('yellow').class = 'color yellow selected';
   document.getElementsByClassName('green').class = 'color green';
 }
 
 function green() {
+  document.getElementsByClassName('green').class = 'color green selected';
   document.getElementsByClassName('black').class = 'color black';
   document.getElementsByClassName('blue').class = 'color blue';
   document.getElementsByClassName('yellow').class = 'color yellow';
-  document.getElementsByClassName('green').class = 'color green selected';
 }
