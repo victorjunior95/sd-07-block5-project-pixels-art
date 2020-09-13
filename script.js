fc10c8c0c5e09cd59d717cdf6676c10045a8ec55
samantabelow <samanta.below@gmail.com>
2020-09-12 22:06:18 -0300

    Ajustando quesitos do code climate

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
-let selectedColor = document.getElementById("first-color");
-document.querySelectorAll(".color").forEach(configureColorPaletteItemsAsClickable);
+let selectedColor = document.getElementById('first-color');
+document.querySelectorAll('.color').forEach(configureColorPaletteItemsAsClickable);
 
-function removeClass(item)
-{
-    item.classList.remove("selected");
+function removeClass(item) {
+    item.classList.remove('selected');
 }
 
-function configureColorPaletteItemsAsClickable(item)
-{
-    item.addEventListener("click", function(){
-        document.querySelectorAll(".color").forEach(removeClass);
-        item.classList.add("selected");
+function configureColorPaletteItemsAsClickable(item) {
+    item.addEventListener("click", function () {
+        document.querySelectorAll('.color').forEach(removeClass);
+        item.classList.add('selected');
         selectedColor = item;
-    })
+    });
 }
 
-document.querySelectorAll(".pixel").forEach(changePixelColor);
+document.querySelectorAll('.pixel').forEach(changePixelColor);
 
-function changePixelColor(item)
-{
-    item.addEventListener("click", function(){
-        color = window.getComputedStyle(selectedColor, null).getPropertyValue("background-color");
+function changePixelColor(item) {
+    item.addEventListener("click", function () {
+        let color = window.getComputedStyle(selectedColor, null).getPropertyValue("background-color");
         item.style.backgroundColor = color;
-    })
+    });
 }
 
-let button = document.getElementById("clear-board");
-button.addEventListener("click", function(){
+let button = document.getElementById('clear-board');
+button.addEventListener("click", function () {
     document.querySelectorAll(".pixel").forEach(changeBackgroundColorToWhite);
-})
+});
 
-function changeBackgroundColorToWhite(item)
-{
+function changeBackgroundColorToWhite(item) {
     item.style.backgroundColor = "white";
 }
