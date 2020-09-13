3957dd92a0a73411aed11c099d2cc51ee5f3f0dd
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-09-13 12:25:12 -0300

    Requisite 7 - Adding the class selected in the color palletes

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-document.getElementsByClassName('black').class = 'color black selected'
+//document.getElementsByClassName('black').class = 'color black selected';
 
+function black() {
+  document.getElementsByClassName('black').class = 'color black selected';
+  document.getElementsByClassName('blue').class = 'color blue';
+  document.getElementsByClassName('yellow').class = 'color yellow';
+  document.getElementsByClassName('green').class = 'color green';
+}
 
+function blue() {
+  document.getElementsByClassName('black').class = 'color black';
+  document.getElementsByClassName('blue').class = 'color blue selected';
+  document.getElementsByClassName('yellow').class = 'color yellow';
+  document.getElementsByClassName('green').class = 'color green';
+}
 
+function yellow() {
+  document.getElementsByClassName('black').class = 'color black';
+  document.getElementsByClassName('blue').class = 'color blue';
+  document.getElementsByClassName('yellow').class = 'color yellow selected';
+  document.getElementsByClassName('green').class = 'color green';
+}
+
+function green() {
+  document.getElementsByClassName('black').class = 'color black';
+  document.getElementsByClassName('blue').class = 'color blue';
+  document.getElementsByClassName('yellow').class = 'color yellow';
+  document.getElementsByClassName('green').class = 'color green selected';
+}
