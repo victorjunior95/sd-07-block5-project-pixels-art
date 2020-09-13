c7789c19f343adfa49ab5c3e5502904947d22a4a
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-09-13 20:08:09 -0300

    refazendo js

--- script.js
+++
@@ -1,9 +0,0 @@
-let selected = document.getElementById('selected')
-
-let pixel = document.querySelector('td');
-
-pixel.addEventListener('click', coloring);
-
-function coloring() {
-  pixel.style.backgroundColor = selected.style.backgroundColor;
-}
