bd4858dff16cf32c04733a2e892da86b122ca4c4
vanderson-henrique <vhbenedito@gmail.com>
2020-09-13 07:57:03 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -1,65 +1,77 @@
-let paleta = document.querySelectorAll('.color'); // seleciona a paleta de cores
+const paleta = document.querySelectorAll('.color'); // seleciona a paleta de cores
 let corSelecionada = document.querySelector('.color-1'); // define a variável com a cor preta inicial
 
-for (index = 0; index < paleta.length; index += 1) { // insere a classe selected onde for clicado
-  let corAtual = paleta[index]; // define a cor atual de acordo com o indice do array "paleta"
-    corAtual.addEventListener('click', function() { 
-      for (index = 0; index < paleta.length; index += 1) { // apaga a classe selected de todos
-        let removeClasse = paleta[index];
-          removeClasse.classList.remove('selected');
-        }
-        corAtual.classList.add('selected');
-        corSelecionada = corAtual; // armazena a cor atual numa variável global
+for (let index = 0; index < paleta.length; index += 1) { // insere a classe selected onde for clicado
+  const corAtual = paleta[index]; // define a cor atual de acordo com o indice do array "paleta"
+  corAtual.addEventListener('click', function () {
+      for (let index = 0; index < paleta.length; index += 1) { // apaga a classe selected de todos
+        const removeClasse = paleta[index];
+        removeClasse.classList.remove('selected');
+      }
+      corAtual.classList.add('selected');
+      corSelecionada = corAtual; // armazena a cor atual numa variável global
     });
 }
 
-let quadro = document.querySelectorAll('.pixel'); // armazena todos os pixels num array
-for (index = 0; index < quadro.length; index += 1) {
-    let pixelSelecionado = quadro[index];
-    pixelSelecionado.addEventListener('click', function() {
-        pixelSelecionado.classList.add(corSelecionada.classList[1]); 
-    }) // add a segunda classe da div selecionada ao pixel clicado
+const quadro = document.querySelectorAll('.pixel'); // armazena todos os pixels num array
+for (let index = 0; index < quadro.length; index += 1) {
+  const pixelSelecionado = quadro[index];
+  pixelSelecionado.addEventListener('click', function () {
+      pixelSelecionado.classList.add(corSelecionada.classList[1]);
+        }); // add a segunda classe da div selecionada ao pixel clicado
 }
 
-let botaoLimpar = document.getElementById('clear-board');
-botaoLimpar.addEventListener('click', function() {
-    for (index = 0; index < quadro.length; index += 1) { // passa por todo o quadro
-        let limpaQuadro = quadro[index];
-        limpaQuadro.className = ''; // apaga todas as classes nela
-        limpaQuadro.classList.add('pixel'); // insere a classe padrão de volta
+const botaoLimpar = document.getElementById('clear-board');
+botaoLimpar.addEventListener('click', function () {
+  for (let index = 0; index < quadro.length; index += 1) { // passa por todo o quadro
+        const limpaQuadro = quadro[index];
+      limpaQuadro.className = ''; // apaga todas as classes nela
+      limpaQuadro.classList.add('pixel'); // insere a classe padrão de volta
     }
-})
+});
 
-let boardSize = document.getElementById('board-size');
-let buttonGenerateBoard = document.getElementById('generate-board');
-let pixelBoard = document.getElementById('pixel-board');
-let removeLinhas = document.getElementsByClassName('linha');
-let celula;
+const boardSize = document.getElementById('board-size');
+const buttonGenerateBoard = document.getElementById('generate-board');
+const pixelBoard = document.getElementById('pixel-board');
+const removeLinhas = document.getElementsByClassName('linha');
+//let celula;
 let linha;
 
-buttonGenerateBoard.addEventListener('click', function() {
-    if (boardSize.value == "") {
-        alert('Board inválido!');
+buttonGenerateBoard.addEventListener('click', function () {
+    if (boardSize.value === '') {
+      alert('Board inválido!');
     }
     // remover quadro antigo
-    for (let index = removeLinhas.length - 1; index >= 0 ; index -= 1){
-        removeLinhas[index].remove(removeLinhas);
+    for (let index = removeLinhas.length - 1; index >= 0; index -= 1) {
+      removeLinhas[index].remove(removeLinhas);
     }
     // inserir quadro novo
-    if (boardSize.value < 5){
-        boardSize.value = 5;
+  if (boardSize.value < 5) {
+    boardSize.value = 5;
     }
-    if (boardSize.value > 50){
-        boardSize.value = 50;
+  if (boardSize.value > 50) {
+    boardSize.value = 50;
     }
-    for (let index = 0; index < boardSize.value; index += 1){
-        let novaLinha = document.createElement('div');
-        linha = pixelBoard.appendChild(novaLinha);
+    for (let index = 0; index < boardSize.value; index += 1) {
+      const novaLinha = document.createElement('div');
+      linha = pixelBoard.appendChild(novaLinha);
         linha.className = 'linha';
-        for (let index = 0; index < boardSize.value; index += 1) {
-            let novaCelula = document.createElement('div');
-            linha.appendChild(novaCelula).className = 'pixel';
+      for (let index1 = 0; index1 < boardSize.value; index1 += 1) {
+          const novaCelula = document.createElement('div');
+          linha.appendChild(novaCelula).className = 'pixel';
         }
     }
-})
+});
 
+function numberRGB () {
+    let number = Math.floor(Math.random() * 255);
+    return number;
+}
+
+window.onload = function () {
+    // let paletaInicial = document.querySelectorAll('.color');
+    for (let index = 1; index < paleta.length; index += 1){
+        paleta[index].style.backgroundColor = `rgb(${numberRGB()} ,${numberRGB()} , ${numberRGB()})`;
+        
+    }
+}
