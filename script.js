9423142fbf055b409fca4112f1b7197d05a9cf99
marcelosoliveira <msbobsk8@gmail.com>
2020-09-12 22:50:40 -0300

    Requisitos obrigatórios estão funcionando corretamente

--- script.js
+++ script.js
@@ -12,3 +12,50 @@ for(let aux = 0; aux < 25; aux += 1) {
     document.querySelectorAll('#palete div')[aux].setAttribute('id', 'pixel-board');
     document.querySelectorAll('#palete div')[aux].setAttribute('class', 'pixel');
 }
+
+let palete = document.querySelectorAll('#palete div');
+window.onload = initial;
+function initial() {
+    paletteColor[0].className = 'selected';
+    for(let index = 0; index < 25; index += 1) {
+        palete[index].addEventListener('click', function () {
+            palete[index].style.backgroundColor = paletteColor[0].style.backgroundColor;
+        });
+    }
+}
+
+paletteColor[0].addEventListener('click', function () {
+    paletteColor[0].className = 'selected';
+    for(let index = 0; index < 25; index += 1) {
+        palete[index].addEventListener('click', function () {
+            palete[index].style.backgroundColor = paletteColor[0].style.backgroundColor;
+        });
+    }
+});
+
+paletteColor[1].addEventListener('click', function () {
+    paletteColor[1].className = 'selected';
+    for(let index = 0; index < 25; index += 1) {
+        palete[index].addEventListener('click', function () {
+            palete[index].style.backgroundColor = paletteColor[1].style.backgroundColor;
+        });
+    }
+});
+
+paletteColor[2].addEventListener('click', function () {
+    paletteColor[2].className = 'selected';
+    for(let index = 0; index < 25; index += 1) {
+        palete[index].addEventListener('click', function () {
+            palete[index].style.backgroundColor = paletteColor[2].style.backgroundColor;
+        });
+    }
+});
+
+paletteColor[3].addEventListener('click', function () {
+    paletteColor[3].className = 'selected';
+    for(let index = 0; index < 25; index += 1) {
+        palete[index].addEventListener('click', function () {
+            palete[index].style.backgroundColor = paletteColor[3].style.backgroundColor;
+        });
+    }
+});
