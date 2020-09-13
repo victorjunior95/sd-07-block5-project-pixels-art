c86c1e7f117aa53153a0a8cfb60645b834e0f6f9
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-09-12 22:11:55 -0300

    adicionando funcionalidade que permite usuario escolher tamanho do grid

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+window.onload
 function selectingColor(event) {
   if (event.target.className.match(/selected/g)) {
     return
@@ -16,10 +17,38 @@ function paintingPixel(event) {
 }
 
 function clearColors() {
-    const pixelList = document.getElementsByClassName('pixel');
-    for (let i = 0; i < pixelList.length; i += 1) {
-        pixelList[i].style.backgroundColor = 'white';
+  const pixelList = document.getElementsByClassName('pixel');
+  for (let i = 0; i < pixelList.length; i += 1) {
+    const currentColor = pixelList[i].classList[1];
+    pixelList[i].classList.remove(currentColor);
+    pixelList[i].classList.add('white');
+  }
+}
+
+function generateGrid() {
+  const gridSize = document.getElementById('board-size').value;
+  if (gridSize > 0) {  const grid = document.getElementById('pixel-board');
+    document.body.removeChild(grid);
+    let newDivGrid = document.createElement('div');
+    newDivGrid.className = 'new-grid';
+    document.body.appendChild(newDivGrid);
+    for (let i = 0; i < gridSize; i += 1) {
+        const rowDiv = document.createElement('div');
+        newDivGrid.appendChild(rowDiv);
+        rowDiv.className = 'div-row';
+        for (let i = 0; i < gridSize; i += 1) {
+            const newPixel = document.createElement('div');
+            newPixel.className = 'pixel';
+            newPixel.classList.add('white');
+            rowDiv.appendChild(newPixel);
+        }
     }
+
+    newDivGrid.style.columnFill = "balance";
+  } else {
+      alert('Board inválido!');
+  }
+
 }
 
 const colorPixelList = document.getElementsByClassName('color');
@@ -32,3 +61,5 @@ for (let i = 0; i < pixelList.length; i += 1) {
 }
 const clearButton = document.getElementById('clear-board');
 clearButton.addEventListener('click', clearColors);
+const generateGridButton = document.getElementById('generate-board');
+generateGridButton.addEventListener('click', generateGrid);
