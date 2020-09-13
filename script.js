db239730ed87762ce587eaf396f65a958ed56ec3
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2020-09-13 19:46:44 -0300

    requisito 4 envio 1

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 const colorPalette = document.querySelector('#color-palette');
+const pixelboard = document.querySelectorAll('.pixel');
+
+
+makePixelBoard(pixelboard);
+
+function makePixelBoard(boradLines) {
+ for (let index = 0; index < pixelboard.length; index += 1) {
+    printLine(boradLines[index]);
+ }
+}
+
+function printLine(lineToPrint) {
+  for(let index = 0; index < pixelboard.length; index += 1) {
+      let pixelLine = createPixel('perPixel')
+      lineToPrint.appendChild(pixelLine);
+  }
+} 
+
+function createPixel(className) {
+  let pixel = document.createElement('div');
+  pixel.className = className
+  return pixel;
+}
