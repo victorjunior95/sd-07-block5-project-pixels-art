0a949554d97f6bb7ae4780b7116b24d88b793ccc
Lucas Andrade <lafdesouza2002@outlook.com>
2020-09-13 19:58:24 -0300

    Uma classe selecionada por vez

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let palette = document.querySelector("#color-palette")
 let listOfColors = palette.childNodes
+
 palette.addEventListener("click", (event)=>{
+    let selected = document.querySelector(".selected")
+    selected.classList.remove("selected")
     color = event.target
     color.classList.add("selected")
-    console.log(color)
+    console.log(palette)
 })
