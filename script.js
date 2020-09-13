4154a8dec63e2f589ab3bc7b56f7e67ba145f36f
Carol Bezerra <carol.arbe@gmail.com>
2020-09-13 13:37:04 -0300

    Requisito 8 e 9

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 const colors = document.querySelectorAll('.color');
-const tableData = document.querySelectorAll('.pixel');
 
 for (let i = 0; i < 4; i += 1) {
   colors[i].addEventListener('click', function () {
     if (i === 0) {
       colors[0].className += ' selected';
+      colors[0].style.backgroundColor = 'black';
       colors[1].className = 'color red';
       colors[2].className = 'color blue';
       colors[3].className = 'color green';
     } else if (i === 1) {
       colors[0].className = 'color black';
       colors[1].className += ' selected';
+      colors[1].style.backgroundColor = 'red';
       colors[2].className = 'color blue';
       colors[3].className = 'color green';
     } else if (i === 2) {
       colors[0].className = 'color black';
       colors[1].className = 'color red';
       colors[2].className += ' selected';
+      colors[2].style.backgroundColor = 'blue';
       colors[3].className = 'color green';
     } else if (i === 3) {
       colors[0].className = 'color black';
       colors[1].className = 'color red';
       colors[2].className = 'color blue';
       colors[3].className += ' selected';
+      colors[3].style.backgroundColor = 'green';
     }
-  })
-};
+  });
+}
+
+const tableData = document.querySelectorAll('.pixel');
+
+for (let i = 0; i < tableData.length; i += 1) {
+  tableData[i].addEventListener('click', function() {
+    tableData[i].style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+    })
+  document.querySelector('#clear-board').addEventListener('click', function () {
+    tableData[i].style.backgroundColor = 'white';
+  });
+}
+
