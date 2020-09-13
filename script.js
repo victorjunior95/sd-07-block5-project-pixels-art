0e359f887e291727a4f211b08b291a25c753bf34
Bruno L Nogueira <nugobruno@gmail.com>
2020-09-13 15:58:45 -0300

    Pixel board adicionado

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 const colorPalette = document.querySelectorAll('.color');
 const pixelBoard = document.querySelector('#pixel-board');
+let sizeOfBoard = [5, 5];
+let numberOfPixels = sizeOfBoard[0]*sizeOfBoard[1];
+pixelBoard.style.heigth = (sizeOfBoard[0]*40) + "px";
+pixelBoard.style.width = (sizeOfBoard[1]*40) + "px";
+
+window.onload = function() {
+  //pixels generator
+  for ( yIndex = 0; yIndex < sizeOfBoard[1]; yIndex += 1) {
+    for ( xIndex = 0; xIndex < sizeOfBoard[0]; xIndex += 1) {
+      let setClass = "pixel-" + (yIndex + 1).toString() + "-" + (xIndex + 1).toString();
+      let pixels = document.createElement("div");
+      pixels.className = "pixel";
+      pixels.className += " " + setClass;
+      pixelBoard.appendChild(pixels);
+  }
+  }
+}
