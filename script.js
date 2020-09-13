d912f3fde4861418590bf1d2ce9bf8077112ff42
Brune Miguel <bstfreire@gmail.com>
2020-09-13 10:18:55 -0300

    Requisitos do 1 ao 5

---
+++ script.js
@@ -0,0 +1,13 @@
+paletaCores = document.querySelector("#title");
+corUm = document.querySelector("#cor1");
+corDois = document.querySelector("#cor2");
+corTres = document.querySelector("#cor3");
+corQuatro = document.querySelector("#cor4");
+quadroPixel = document.querySelector("#pixel-board");
+
+
+
+
+
+ 
+ 
