45689bcf04a794b0786e7dd22dc770a818b1b3be
josielvs <josielvs@gmail.com>
2020-09-13 17:45:28 -0300

    Requisito 1. - Adicionando título a page.

---
+++ script.js
@@ -0,0 +1,11 @@
+function createReader() {
+  const headerPage = document.createElement('header');
+  headerPage.className = 'head-class';
+  document.body.appendChild(headerPage);
+  
+  const titleProject = document.createElement('h1');
+  titleProject.innerHTML = 'Paleta de Cores';
+  titleProject.className = 'title';
+  headerPage.appendChild(titleProject);
+}
+createReader();
