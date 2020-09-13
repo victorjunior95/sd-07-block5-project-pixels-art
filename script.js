c5e99ee89114b443145b24522bcd5ba4b3a6f412
Lucas Andrade <lafdesouza2002@outlook.com>
2020-09-13 20:08:14 -0300

    Pintando os quadros

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let palette = document.querySelector("#color-palette")
 let listOfColors = palette.childNodes
+let fields = document.querySelectorAll("td")
 
 palette.addEventListener("click", (event)=>{
     let selected = document.querySelector(".selected")
     selected.classList.remove("selected")
     color = event.target
     color.classList.add("selected")
-    console.log(palette)
 })
+
+for (let i = 0; i < fields.length; i += 1){
+    fields[i].addEventListener("click", ()=>{
+        let colorSelected = document.querySelector(".selected").style.backgroundColor
+        fields[i].style.backgroundColor = colorSelected
+    })
+}
