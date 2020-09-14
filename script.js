788cdb507fb14d367693db4b9d20ceea16a95f88
Francisco <franciscoliratorres@gmail.com>
2020-09-14 17:10:02 -0300

    Ajustando box

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-//criei uma função que seleciona todas as linhas
-window.onload = function () {
-let baseQuadrado = 5;
-let controlPosition = baseQuadrado;
-let lines = document.querySelectorAll(".pixel");
-
-fillSquare(lines);
-
-//passa por todas as linhas e preenche o quadrado de pixels
-function fillSquare (lines) {
-    for (let index = 0; index <= lines.length; index += 1) {
-    fillLines(lines[index]);
-    controlPosition += 1
+window.onload = function() {
+    let baseSquare = 5;
+    let numberOfLines = baseSquare; // 5
+    let controlPosition = numberOfLines; // 5
+    let lines = document.querySelectorAll(".pixel");
+  
+    fillSquare(lines);
+    
+    // Passa por todos as linhas (div com class line) e preenche o triangulo
+    function fillSquare(lines) {
+      for(let index = 0; index <= lines.length; index += 1) {
+        fillLine(lines[index]);
+        controlPosition += 1;
+      }
     }
-}
-// é uma função que cria uma div, que criará uma caixa
-function createBox (className) {
-    let box = document.createElement("div");
-    box.className = className
-    return box;
-}
-
-function fillLines(divline) {
-    for (let lineColumn = 1; lineColumn <= baseQuadrado; lineColumn += 1) {
+    // Cria uma caixa com base nas diferentes classes
+    function createBox(className) {
+      let box = document.createElement("div");
+      box.className = className;
+      return box;
+    }
+    // Preenche uma linha
+    function fillLine(divLine) {
+      for (let lineColumn = 1; lineColumn < baseSquare; lineColumn += 1) {
+        if(lineColumn <= controlPosition) {
           let box = createBox("box");
           divLine.appendChild(box);
+        }
+      }
     }
-}
-}
+  }
