81f614f37f29ffb786591a4df6f1044fac597a40
Carol Andrade <a.carolg1@gmail.com>
2020-09-14 15:27:18 -0300

    codeCLimate

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // trata evento construir o tabuleiro
 
 // trata construção para 5
-  let pixelBoard = document.querySelector('#pixel-board');
-  for(let lIndex = 0; lIndex < 5; lIndex += 1){
-  let lPixelBoard = document.createElement('div');
-  lPixelBoard.className ='line';
+  const pixelBoard = document.querySelector('#pixel-board');
+  for (let lIndex = 0; lIndex < 5; lIndex += 1) {
+  const lPixelBoard = document.createElement('div');
+  lPixelBoard.className = 'line';
   pixelBoard.appendChild(lPixelBoard);
-  for(let pIndex = 0; pIndex < 5; pIndex +=1) {
-      let pixel = document.createElement('div');
-      pixel.className = 'pixel'
+  for (let pIndex = 0; pIndex < 5; pIndex += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
       lPixelBoard.appendChild(pixel);
   }
 }
