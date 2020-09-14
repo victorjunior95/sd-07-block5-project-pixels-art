c7678efeaff6e589ba618d3e93fd4a77f1c7e375
Fernando Antunes <fernando.antunes1@gmail.com>
2020-09-14 16:39:04 -0300

    Permite pintar com a cor que o usuario escolher

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ window.onload = function(){
     let corDois=document.getElementById('cor-2');
     let corTres=document.getElementById('cor-3');
     let corQuatro=document.getElementById('cor-4');
+    let corSelecionada='rgb(1,1,1)';
 
     for (i=0; i<linhaPixel.length; i+=1){
         for (j=1; j<=5; j+=1){
@@ -19,6 +20,8 @@ window.onload = function(){
         corDois.className='color';
         corTres.className='color';
         corQuatro.className='color';
+        let propriedadeCSS=window.getComputedStyle(corUm,null).getPropertyValue("background-color");
+        corSelecionada=propriedadeCSS;
     })
 
     corDois.addEventListener('click',function(){
@@ -26,6 +29,8 @@ window.onload = function(){
         corDois.className='color selected';
         corTres.className='color';
         corQuatro.className='color';
+        let propriedadeCSS=window.getComputedStyle(corDois,null).getPropertyValue("background-color");
+        corSelecionada=propriedadeCSS;
     })
 
     corTres.addEventListener('click',function(){
@@ -33,6 +38,8 @@ window.onload = function(){
         corDois.className='color';
         corTres.className='color selected';
         corQuatro.className='color';
+        let propriedadeCSS=window.getComputedStyle(corTres,null).getPropertyValue("background-color");
+        corSelecionada=propriedadeCSS;
     })
 
     corQuatro.addEventListener('click',function(){
@@ -40,11 +47,13 @@ window.onload = function(){
         corDois.className='color';
         corTres.className='color';
         corQuatro.className='color  selected';
+        let propriedadeCSS=window.getComputedStyle(corQuatro,null).getPropertyValue("background-color");
+        corSelecionada=propriedadeCSS;
     })
 
     document.querySelectorAll('.pixel').forEach(item => {
         item.addEventListener('click', event => {
-          item.style.backgroundColor='rgb(0,0,0)';
+          item.style.backgroundColor=corSelecionada;
         })
       })
 }
