15f19711e5a212f9fb33243d689f9448f435f7b5
Emanuelle Brasil <emanuelle.rbl@gmail.com>
2020-09-13 21:11:40 -0300

    adding Ids, classes and some properties

---
+++ script.js
@@ -0,0 +1,13 @@
+let color1 = document.getElementById('black');
+color1.style.backgroundColor = 'black';
+
+let color2 = document.getElementById('yellow');
+color2.style.backgroundColor = 'yellow';
+
+let color3 = document.getElementById('pink');
+color3.style.backgroundColor = 'pink';
+
+let color4 = document.getElementById('orange');
+color4.style.backgroundColor = 'orange';
+
+color1.classList.add('selected');
