b755b0b84d87f8fbfb0803fba902492a4a1d9044
Fernando Antunes <fernando.antunes1@gmail.com>
2020-09-14 15:33:59 -0300

    Setando a cor preta como inicial

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function(){
     let linhaPixel=document.getElementsByClassName('linha-pixel');
+    let corSelecionada='rgb(0,0,0)'
 
     for (i=0; i<linhaPixel.length; i+=1){
         for (j=1; j<=5; j+=1){
