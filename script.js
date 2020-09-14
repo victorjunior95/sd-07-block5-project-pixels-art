429d0549e5a8bc4b40f8ee89446068b9ecb77854
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-09-14 10:57:44 -0300

    requisito 7: selecionando as cores

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 window.onload = function(){
     //Declarando variáveis
-    let color = document.querySelectorAll(".color");
-    let listPixel = document.querySelector("#pixel-board");
+    let colorBlack = document.getElementById("black");
+    let colorBlue = document.getElementById("blue");
+    let colorGreen = document.getElementById("green");
+    let colorRed = document.getElementById("red");
+    
+    
+    //Adicionando 4 eventos para selecionar cor específica
+    colorBlue.addEventListener("click", function(){
+        colorBlue.classList.add("selected");
+        //removendo das outras 3 cores
+        colorBlack.classList.remove("selected");
+        colorGreen.classList.remove("selected");
+        colorRed.classList.remove("selected");
+    });
+    colorGreen.addEventListener("click", function(){
+        colorGreen.classList.add("selected");
+        //removendo das outras 3 cores
+        colorBlack.classList.remove("selected");
+        colorBlue.classList.remove("selected");
+        colorRed.classList.remove("selected");
+    });
+    colorRed.addEventListener("click", function(){
+        colorRed.classList.add("selected");
+        //removendo das outras 3 cores
+        colorBlack.classList.remove("selected");
+        colorGreen.classList.remove("selected");
+        colorBlue.classList.remove("selected");
+    });
+    colorBlack.addEventListener("click", function(){
+        colorBlack.classList.add("selected");
+        //removendo das outras 3 cores
+        colorBlue.classList.remove("selected");
+        colorGreen.classList.remove("selected");
+        colorRed.classList.remove("selected");
+    });
 
-
-    //Declarando as cores de fundo
-    document.getElementById("blue").style.backgroundColor = "blue";
-    document.getElementById("green").style.backgroundColor = "green";
-    document.getElementById("red").style.backgroundColor = "red";
-    document.getElementById("black").style.backgroundColor = "black";
     
 
 
