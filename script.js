1ecc5fe9f6c818673cf2e8e44665cc399a5b21a9
LAURO DE SA <itrafor@gmail.com>
2020-09-14 15:46:58 -0300

    Corrigindo Requisitos 01

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 window.onload = function() {
+  const pixelBoard = document.getElementById("pixel-board");
+  const qtMatrix = 5;
   
+ 
+  function createLine (){
+      
+      let pixel = document.createElement("div");
+      pixel.className="color";
+      for (let line = 0; line < qtMatrix; line +=1){
+        let pixelLine = document.createElement("div");
+        pixelBoard.appendChild(pixelLine);
+       for(let pixels = 0; pixels < qtMatrix; pixels+=1){
+            let pixel = document.createElement("div");
+            pixel.className="pixel";
+            pixelLine.appendChild(pixel);
+
+       }
+      }
+
+  }
+  createLine();
+
     
 
 }
