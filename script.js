51f865a76f3052854d34b004f666c831f9c01952
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-09-14 20:37:02 -0300

    fixing some code climate issues

--- script.js
+++ script.js
@@ -1,47 +1,46 @@
-//Clear button 
-const clearBtn = document.querySelector('#clear-board');
-const pixelTable = document.querySelectorAll('.pixel');
-
-//Clear button  
-clearBtn.addEventListener('click', function () {
-  for (let index = 0; index < pixelTable.length; index += 1) {
-      pixelTable[index].style.backgroundColor = 'white';
-    }
-});
-
-/*Select a color and add/remove 'selected' class from others elements. The 'selected' 
-class staying only on clicked element*/
+/* Select a color and add/remove 'selected' class from others elements. The 'selected' 
+class staying only on clicked element */
 const blackColor = document.querySelector('.black');
 const yellowColor = document.querySelector('.yellow');
 const blueColor = document.querySelector('.blue');
 const greenColor = document.querySelector('.green');
-let colorPallete = document.querySelector('.color');
+const colorPallete = document.querySelector('.color');
 
 greenColor.addEventListener('click', function () {
-    blackColor.className = 'black color';
-    yellowColor.className = 'yellow color';
-    blueColor.className = 'blue color';
-    greenColor.className = 'green color selected'
+  blackColor.className = 'black color';
+  yellowColor.className = 'yellow color';
+  blueColor.className = 'blue color';
+  greenColor.className = 'green color selected'
 });
 
 blueColor.addEventListener('click', function () {
-    blackColor.className = 'black color';
-    yellowColor.className = 'yellow color';
-    greenColor.className = 'green color';
-    blueColor.className = 'blue color selected';
-    
-})
+  blackColor.className = 'black color';
+  yellowColor.className = 'yellow color';
+  greenColor.className = 'green color';
+  blueColor.className = 'blue color selected';
+});
+
 yellowColor.addEventListener('click', function () {
-    blackColor.className = 'black color';
-    blueColor.className = 'blue color';
-    greenColor.className = 'green color';
-    yellowColor.className = 'yellow color selected';
+  blackColor.className = 'black color';
+  blueColor.className = 'blue color';
+  greenColor.className = 'green color';
+  yellowColor.className = 'yellow color selected';
 });
 
 blackColor.addEventListener('click', function () {
-    yellowColor.className = 'yellow color';
-    blueColor.className = 'blue color';
-    greenColor.className = 'green color';
-    blackColor.className = 'black color selected';
+  yellowColor.className = 'yellow color';
+  blueColor.className = 'blue color';
+  greenColor.className = 'green color';
+  blackColor.className = 'black color selected';
 });
 
+// Clear button 
+const clearBtn = document.querySelector('#clear-board');
+const pixelTable = document.querySelectorAll('.pixel');
+
+// Clear button  
+clearBtn.addEventListener('click', function () {
+  for (let index = 0; index < pixelTable.length; index += 1) {
+    pixelTable[index].style.backgroundColor = 'white';
+  }
+});
