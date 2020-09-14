1ee7b2f5bc03299b7f5014336a48bf1b119e9cc2
hvenses <hvenses@gmail.com>
2020-09-14 18:33:43 -0300

    Atualizando cores disponiveis na paleta.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//A cor preta deve ser a primeira na paleta de cores.
+let initialColor = document.querySelector(".selected");
+let red = document.getElemenquerySelector;
+let blue = document.getElementById("blueColor");
+let green = document.getElementById("greenColor");
+let clear = document.getElementById("clear-board");
+let box = [];
+
+window.onload = start;
+
+function start() {
+  for (let index = 1; index <= 25; index += 1) {
+    box[index] = document.querySelector("#px" + [index]);
+    box[index].addEventListener("click", function () {
+      box[index].style.backgroundColor = "black";
+    });
+  }
+}
