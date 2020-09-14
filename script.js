5df6a554573c73b1eca3c1ff7aa426e77f3702ae
Raul <afonsoafn@gmail.com>
2020-09-14 15:49:18 +0000

    adc de cores aleatorias e botao

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// -------------- GERAR COR ALEATÓRIA -------------- //
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  let paleta = document.querySelectorAll(".color")
+  for (let index = 0; index < paleta.length; index += 1) {
+      paleta[index].classList.add(getRandomColor())
+      paleta[index].style.backgroundColor = getRandomColor()
+  }
+
 // -------------- ESCOLHER A COR DA PALETA -------------- //
 
 let selected = document.getElementsByClassName("color")[0]
@@ -6,7 +22,6 @@ selected.addEventListener("click", function(){
     selected2.classList.remove('selected')
     selected3.classList.remove('selected')
     this.classList.add('selected')
-    console.log(selected)
 })
 let selected1 = document.getElementsByClassName("color")[1]
 selected1.addEventListener("click", function(){
@@ -14,7 +29,6 @@ selected1.addEventListener("click", function(){
     selected2.classList.remove('selected')
     selected3.classList.remove('selected')
     this.classList.add('selected')
-    console.log(selected1)
 })
 
 let selected2 = document.getElementsByClassName("color")[2]
@@ -23,7 +37,6 @@ selected2.addEventListener("click", function(){
     selected1.classList.remove('selected')
     selected3.classList.remove('selected')
     this.classList.add('selected')
-    console.log(selected2)
 })
 
 let selected3 = document.getElementsByClassName("color")[3]
@@ -32,7 +45,6 @@ selected3.addEventListener("click", function(){
     selected1.classList.remove('selected')
     selected2.classList.remove('selected')
     this.classList.add('selected')
-    console.log(selected3)
 })
 
 // -------------- MUDANDO A COR DOS PIXELS -------------- //
@@ -46,7 +58,16 @@ for (let index = 0; index < quadro.length; index += 1) {
 }
 
 function trocaCor() {
-    let selecionado = document.getElementsByClassName("selected")[0]
-    let cor = window.getComputedStyle(selecionado).backgroundColor
+    let corSelecionada = document.getElementsByClassName("selected")[0]
+    let cor = window.getComputedStyle(corSelecionada).backgroundColor
     return cor
 }
+
+// -------------- BOTÂO CLEAR -------------- //
+
+let button = document.getElementById("button")
+button.addEventListener("click", function() {
+    for (let index = 0; index < quadro.length; index += 1) {
+        quadro[index].style.backgroundColor = ""
+    }
+})
