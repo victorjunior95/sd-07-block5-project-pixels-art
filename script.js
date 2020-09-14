974730365bba3071b0b628bd428ead1fc8aa8468
cezar88augusto <cezar88augusto@gmail.com>
2020-09-14 19:03:41 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,66 +1,83 @@
 window.onload = function () {
-    let arrayColors = document.querySelectorAll(".color");
 
-    arrayColors[0].style.backgroundColor = "black";
-    arrayColors[1].style.backgroundColor = "red";
-    arrayColors[2].style.backgroundColor = "grey";
-    arrayColors[3].style.backgroundColor = "yellow";
+  //Referência dos elementos .color.
+  let arrayColors = document.querySelectorAll(".color");
 
-    let selectedColor = document.querySelectorAll(".color.selected")[0].style.backgroundColor;
+  //BackgroundColor de cada posição.
+  arrayColors[0].style.backgroundColor = "black";
+  arrayColors[1].style.backgroundColor = "red";
+  arrayColors[2].style.backgroundColor = "grey";
+  arrayColors[3].style.backgroundColor = "yellow";
 
-    for (let index = 0; index < arrayColors.length; index += 1) {
-      if (index == 0) {
-        arrayColors[index].addEventListener('click', function () {
-          selectedColor = arrayColors[index].style.backgroundColor;
-          arrayColors[index].classList.add("selected")
-          for (let index = 0; index < arrayColors.length; index++) {
-            if (index != 0) {
-              arrayColors[index].classList.remove("selected");
-            }
+  let selectedColor = document.querySelectorAll(".color.selected")[0].style.backgroundColor;
+
+  for (let index = 0; index < arrayColors.length; index += 1) {
+    if (index == 0) {
+      arrayColors[index].addEventListener('click', function () {
+        selectedColor = arrayColors[index].style.backgroundColor;
+        arrayColors[index].classList.add("selected")
+        for (let index = 0; index < arrayColors.length; index++) {
+          if (index != 0) {
+            arrayColors[index].classList.remove("selected");
           }
-        })
-      } else if (index == 1) {
-        arrayColors[index].addEventListener('click', function () {
-          selectedColor = arrayColors[1].style.backgroundColor;
-          arrayColors[index].classList.add("selected")
-          for (let index = 0; index < arrayColors.length; index++) {
-            if (index != 1) {
-              arrayColors[index].classList.remove("selected");
-            }
+        }
+      })
+    } else if (index == 1) {
+      arrayColors[index].addEventListener('click', function () {
+        selectedColor = arrayColors[1].style.backgroundColor;
+        arrayColors[index].classList.add("selected")
+        for (let index = 0; index < arrayColors.length; index++) {
+          if (index != 1) {
+            arrayColors[index].classList.remove("selected");
           }
-        })
-      } else if (index == 2) {
-        arrayColors[index].addEventListener('click', function () {
-          selectedColor = arrayColors[2].style.backgroundColor;
-          arrayColors[index].classList.add("selected");
-          for (let index = 0; index < arrayColors.length; index++) {
-            if (index != 2) {
-              arrayColors[index].classList.remove("selected");
-            }
+        }
+      })
+    } else if (index == 2) {
+      arrayColors[index].addEventListener('click', function () {
+        selectedColor = arrayColors[2].style.backgroundColor;
+        arrayColors[index].classList.add("selected");
+        for (let index = 0; index < arrayColors.length; index++) {
+          if (index != 2) {
+            arrayColors[index].classList.remove("selected");
           }
-        })
-      } else {
-        arrayColors[index].addEventListener('click', function () {
-          selectedColor = arrayColors[3].style.backgroundColor;
-          arrayColors[index].classList.add("selected");
-          for (let index = 0; index < arrayColors.length; index++) {
-            if (index != 3) {
-              arrayColors[index].classList.remove("selected");
-            }
+        }
+      })
+    } else {
+      arrayColors[index].addEventListener('click', function () {
+        selectedColor = arrayColors[3].style.backgroundColor;
+        arrayColors[index].classList.add("selected");
+        for (let index = 0; index < arrayColors.length; index++) {
+          if (index != 3) {
+            arrayColors[index].classList.remove("selected");
           }
-        })
-      }
+        }
+      })
     }
-
-    //Função para mudar a cor:
-    
-    let lines = document.querySelectorAll(".pixel");
-
-    function changeColor(event) {
-      event.target.style.backgroundColor = selectedColor;
-    }
-    for (let index = 0; index < lines.length; index++) { 
-      lines[index].addEventListener("click", changeColor);
-    }
-    
   }
+
+  //Função para mudar a cor:
+
+  let lines = document.querySelectorAll(".pixel");
+
+  function changeColor(event) {
+    event.target.style.backgroundColor = selectedColor;
+  }
+
+  for (let index = 0; index < lines.length; index++) {
+    lines[index].addEventListener("click", changeColor);
+  }
+
+  //Função para apagar as cores do quadro:
+
+  let button = document.getElementById("clear-board");
+
+  function eraseAll() {
+    for (let index = 0; index < lines.length; index++) {
+      lines[index].style.backgroundColor = "white";
+    }
+  }
+
+  button.addEventListener('click', eraseAll);
+
+
+}
