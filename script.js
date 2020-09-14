145afd7f1f8b0a498839626f899c06b87d109f9b
Thalys <thalysmatiascarrara@gmail.com>
2020-09-14 17:24:12 -0400

    resolvendo bug requisito 1 2 3

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+//getComputedStyles
 
-const colors = ['black', 'green', 'yellow','blue'];
-let paleta = document.getElementsByClassName("color")
+window.onload = function() {
 
-for (const index in paleta) {
-    paleta[index].classList.add(colors[index])
+    function CreatePalette(){
+        const colors = ['black', 'green', 'yellow','blue'];
+        let paleta = document.getElementsByClassName("color")
+        
+        for (const index in paleta) {
+            paleta[index].style.backgroundColor = colors[index]
+            console.log(paleta[index].style.backgroundColor)
+        }
+    }
+    
+    CreatePalette();
+    
 }
+
 
 
