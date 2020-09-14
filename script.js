5f3a0447820e5f8e57358b90eccac17f4d416f60
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-14 16:42:03 -0300

    4, 5 requisito

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-let cores = document.querySelectorAll(".color")
-cores[0].style.backgroundColor = "black"
-cores[1].style.backgroundColor = "blue"
-cores[2].style.backgroundColor = "red"
-cores[3].style.backgroundColor = "green"
+let cores = document.querySelectorAll('.color')
+cores[0].style.backgroundColor = 'black'
+cores[1].style.backgroundColor = 'blue'
+cores[2].style.backgroundColor = 'red'
+cores[3].style.backgroundColor = 'green'
+let pixelBoard = document.querySelector("#pixel-board")
+
+
+function stylePixel () { 
+    let caixa = document.createElement('div')
+    caixa.className = "pixel"
+    caixa.style.backgroundColor = "white"
+    caixa.style.height = "40px"
+    caixa.style.width = "40px"
+    caixa.style.border = "1px solid black"
+    caixa.style.display = "inline-block"
+    pixelBoard.appendChild(caixa)
+}
+
+for(index = 0; index < 20; index += 1){
+    stylePixel()
+}
