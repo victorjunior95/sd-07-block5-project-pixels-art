1c6f5fdfbee8d5354974d82695561616528fd4f1
Vanessa Oliveira <vanessanaara@gmail.com>
2020-09-14 20:16:07 -0300

    Arrumando o quadro de pixles para o tamanho correto de 40px e colocando a cor branca de fundo

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 //Criando as cores da paleta
 for (let i = 0; i < 4; i += 1) {
   let colorPalette = document.getElementById('color-palette');
-  let colorDiv = document.createElement('div');
-  
+  let colorDiv = document.createElement('div');  
   colorPalette.appendChild(colorDiv);
 
   let arrayOfColors = ['black', 'salmon', 'green', 'blue'];
   for (let j = 0; j < arrayOfColors.length; j += 1) {
     if (i === j) {
       colorDiv.className = `color ${arrayOfColors[j]}`;
-    }
-    
+    }    
     if (arrayOfColors[j] === 'black') {
       colorDiv.className = `color ${arrayOfColors[j]} selected`;
     }
@@ -24,8 +22,7 @@ window.onload = function() {
       let box = document.createElement('div');
       box.className = 'pixel';
       return box;
-    }
-  
+    }  
   for (let i = 0; i < 5; i += 1) {
     let board = document.getElementById('pixel-board');
     let lineBoardBox = document.createElement('div');
