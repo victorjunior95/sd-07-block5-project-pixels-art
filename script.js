ac544abf8fb49b1f87ba0760885e2e4f0b842066
MoisesSantana <moisaant@gmail.com>
2020-09-14 20:26:45 -0300

    cores aleatórias

--- script.js
+++ script.js
@@ -1,155 +1,122 @@
-const color = document.querySelectorAll('.color');
-const sizeBoard = document.querySelector('#size-board');
+const sizeBoard = document.querySelector('#board-size');
+const generatorButton = document.querySelector('#generate-board');
 const board = document.querySelector('#pixel-board');
-const clear = document.querySelector('#clear-board');
-const pixel = document.querySelectorAll('.pixel');
+const arrayColorOfPalette = document.querySelectorAll('.color');
 
+// const clear = document.querySelector('#clear-board');
+
+let size = null;
 
 let currentColor = 'black';
 
-sizeBoard.addEventListener('keydown', function (event) {
-  let size = sizeBoard.value;
-  if (event.keyCode == 13 && size > 4 && size < 51) {
-    createPixels(size);
+sizeBoard.addEventListener('keyup', function () {
+  size = sizeBoard.value;
+  if (size > 50) {
+    size = 50;
   }
+  if (size < 5) {
+    size = 5
+  }
+  returnOrClick ();
 });
 
-function createPixels (size) {
-  for (let index = 1; index <= size; index += 1) {
+function returnOrClick () {
+    sizeBoard.addEventListener('keydown', function (event) {
+      if (event.keyCode == 13) {
+        createPixels();
+      }
+    });
+    generatorButton.addEventListener('click', createPixels);
+}
+
+function createPixels () {
+  size = parseInt(size);
+
+  // https://developer.cdn.mozilla.net/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/pow - potencia;
+
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/repeat - (criando a quantidade suficiente pra preencher o grid) display grid mais no CSS;
+
+
+  for (let index = 1; index <= Math.pow(size, 2); index += 1) {
     let newPixel = document.createElement('div');
     newPixel.className = 'pixel';
     board.appendChild(newPixel);
   }
-  makeBoard (size);
+  makeBoard ();
 }
 
-function makeBoard (size) {
+function makeBoard () {
+
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/repeat - (preenchendo colunas e linhas com os elementos criados no passo acima) display grid mais no CSS;
+
   board.style.gridTemplateColumns = `repeat(${size}, 40px)`;
   board.style.gridTemplateRows = `repeat(${size}, 40px)`;
 } 
 
-color[0].addEventListener('click', function () {
-  color[0].className = 'color , first , selected';
-  currentColor = 'black';
-});
+function getRandom () {
+  return Math.floor(Math.random() * 100);
+}
 
-color[1].addEventListener('click', function () {
-  color.className = 'color , second , selected';
-  currentColor = 'red';
-});
-
-color[2].addEventListener('click', function () {
-  color.className = 'color , third , selected';
-  currentColor = 'green';
-});
-
-color[3].addEventListener('click', function () {
-  color.className = 'color , last , selected';
-  currentColor = 'blue';
-});
-
-// pixel[0].addEventListener('click', function () {
-//   pixel[0].style.backgroundColor = currentColor;
-// });
-
-// pixel[1].addEventListener('click', function () {
-//   pixel[1].style.backgroundColor = currentColor;
-// });
-
-// pixel[2].addEventListener('click', function () {
-//   pixel[2].style.backgroundColor = currentColor;
-// });
-
-// pixel[3].addEventListener('click', function () {
-//   pixel[3].style.backgroundColor = currentColor;
-// });
-
-// pixel[4].addEventListener('click', function () {
-//   pixel[4].style.backgroundColor = currentColor;
-// });
-
-// pixel[5].addEventListener('click', function () {
-//   pixel[5].style.backgroundColor = currentColor;
-// });
-
-// pixel[6].addEventListener('click', function () {
-//   pixel[6].style.backgroundColor = currentColor;
-// });
-
-// pixel[7].addEventListener('click', function () {
-//   pixel[7].style.backgroundColor = currentColor;
-// });
-
-// pixel[8].addEventListener('click', function () {
-//   pixel[8].style.backgroundColor = currentColor;
-// });
-
-// pixel[9].addEventListener('click', function () {
-//   pixel[9].style.backgroundColor = currentColor;
-// });
-
-// pixel[10].addEventListener('click', function () {
-//   pixel[10].style.backgroundColor = currentColor;
-// });
-
-// pixel[11].addEventListener('click', function () {
-//   pixel[11].style.backgroundColor = currentColor;
-// });
-
-// pixel[12].addEventListener('click', function () {
-//   pixel[12].style.backgroundColor = currentColor;
-// });
-
-// pixel[13].addEventListener('click', function () {
-//   pixel[13].style.backgroundColor = currentColor;
-// });
-
-// pixel[14].addEventListener('click', function () {
-//   pixel[14].style.backgroundColor = currentColor;
-// });
-
-// pixel[15].addEventListener('click', function () {
-//   pixel[15].style.backgroundColor = currentColor;
-// });
-
-// pixel[16].addEventListener('click', function () {
-//   pixel[16].style.backgroundColor = currentColor;
-// });
-
-// pixel[17].addEventListener('click', function () {
-//   pixel[17].style.backgroundColor = currentColor;
-// });
-
-// pixel[18].addEventListener('click', function () {
-//   pixel[18].style.backgroundColor = currentColor;
-// });
-
-// pixel[19].addEventListener('click', function () {
-//   pixel[19].style.backgroundColor = currentColor;
-// });
-
-// pixel[20].addEventListener('click', function () {
-//   pixel[20].style.backgroundColor = currentColor;
-// });
-
-// pixel[21].addEventListener('click', function () {
-//   pixel[21].style.backgroundColor = currentColor;
-// });
-
-// pixel[22].addEventListener('click', function () {
-//   pixel[22].style.backgroundColor = currentColor;
-// });
-
-// pixel[23].addEventListener('click', function () {
-//   pixel[23].style.backgroundColor = currentColor;
-// });
-
-// pixel[24].addEventListener('click', function () {
-//   pixel[24].style.backgroundColor = currentColor;
-// });
-
-clear.addEventListener('click', function () {
-  for (let index = 0; index < pixel.length; index += 1) {
-    pixel[index].style.backgroundColor = 'white';
+function selectColorsOfPalette () {
+  for (let index = 1; index < arrayColorOfPalette.length; index += 1) {
+    let randomRGBColor = `rgb(${getRandom ()} , ${getRandom ()} , ${getRandom()})`;
+    arrayColorOfPalette[index].style.backgroundColor = randomRGBColor;
+    arrayColorOfPalette[index].style.color = randomRGBColor;
   }
-});
+}
+
+selectColorsOfPalette ();
+
+window.onload = function() {
+  arrayColorOfPalette[0].classList.add('selected');
+  arrayColorOfPalette[0].style.backgroundColor = 'black';
+  arrayColorOfPalette[0].style.color = 'black';
+  currentColor = 'black';
+}
+
+function getTheColor () {
+  for (let index = 0; index < arrayColorOfPalette.length; index += 1) {
+    teste2 (index);
+    // arrayColorOfPalette[index].addEventListener('click', function (event) {
+    //   alert(event.target);
+    // });
+  }
+}
+
+function teste2 (index) {
+  arrayColorOfPalette[index].addEventListener('click', function () {
+    alert('ola');
+  });
+}
+
+function paint () {
+  alert('ola')   // <<<< Esse funciona
+  let pixels = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+  alert('ola') // <<<<< Esse não funciona
+    
+    teste(pixels[index]);
+    // alert('alo')
+    // pixels[index].addEventListener('click', function () {
+    //   pixels[index].style.backgroundColor = currentColor;
+    //   alert('alo')
+    // });
+  }
+}
+
+function teste (local) {
+  local.addEventListener('click', function () {
+    alert('alo')
+  })
+}
+
+getTheColor();
+paint();
+
+// clear.addEventListener('click', function () {
+//   for (let index = 0; index < pixel.length; index += 1) {
+//     pixel[index].style.backgroundColor = 'white';
+//   }
+// });
+
+// classList
