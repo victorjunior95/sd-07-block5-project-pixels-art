740e6b35c364bb33f5fee10356f27ca3926c8b8e
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-09-14 08:53:50 -0300

    segundo requisito: criando paleta de cores

---
+++ script.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+    //Declarando variáveis
+    let color = document.querySelectorAll(".color");
+
+
+    //Declarando as cores de fundo
+    document.getElementById("blue").style.backgroundColor = "blue";
+    document.getElementById("green").style.backgroundColor = "green";
+    document.getElementById("red").style.backgroundColor = "red";
+    document.getElementById("yellow").style.backgroundColor = "yellow";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
