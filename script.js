f1d4486e2a4a8bc3615bfd1a720f0ceca3f41c74
Raphael Caputo <faelelf2@gmail.com>
2020-09-13 21:22:50 -0300

    Tentando novamente

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-let button = document.getElementById('clear-board');
-button.addEventListener("click", function () {
-  document.querySelectorAll(".pixel").forEach(allToWhite);
-});
-
 function allToWhite(item) {
   item.style.backgroundColor = "white";
-}
+};
 
