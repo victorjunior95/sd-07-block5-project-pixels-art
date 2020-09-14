93f318b86db2f169844d72417523122656248c24
ericvini <ericcobain3@gmail.com>
2020-09-14 17:49:44 -0300

    Requisito 7-finalizado

--- script.js
+++ script.js
@@ -9,9 +9,42 @@ function quadroPixels() {
 
 quadroPixels();
 
-//function selectColorBlack() {
-  //const quadro = document.getElementById('color1');
-  //quadro.className = 'selected';
-//}
+const selectedColorBlack = document.getElementById('color1');
+const selectedColorRed = document.getElementById('color2');
+const selectedColorGreen = document.getElementById('color3');
+const selectedColorYellow = document.getElementById('color4');
 
-//selectColorBlack();
+function removeSelected() {
+  selectedColorBlack.classList.remove('selected');
+  selectedColorRed.classList.remove('selected');
+  selectedColorGreen.classList.remove('selected');
+  selectedColorYellow.classList.remove('selected');
+}
+
+selectedColorBlack.addEventListener('click', function () {
+  alert('black');
+
+  removeSelected();
+  selectedColorBlack.classList.add('selected');
+});
+
+selectedColorRed.addEventListener('click', function () {
+  alert('vermelha');
+
+  removeSelected();
+  selectedColorRed.classList.add('selected');
+});
+
+selectedColorGreen.addEventListener('click', function () {
+  alert('verde');
+
+  removeSelected();
+  selectedColorGreen.classList.add('selected');
+});
+
+selectedColorYellow.addEventListener('click', function () {
+  alert('yellow');
+
+  removeSelected();
+  selectedColorYellow.classList.add('selected');
+});
