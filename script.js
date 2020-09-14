ddf144fdb2e81fd3bf857d335d4ec9f6a1f194c4
Lfocosi <luccafocosi@gmail.com>
2020-09-14 19:17:51 -0300

    botao clear

---
+++ script.js
@@ -0,0 +1,60 @@
+let bot = document.getElementById('apagar');
+let pi1 = document.getElementById('p1');
+let pi2 = document.getElementById('p2');
+let pi3 = document.getElementById('p3');
+let pi4 = document.getElementById('p4');
+let pi5 = document.getElementById('p5');
+let pi6 = document.getElementById('p6');
+let pi7 = document.getElementById('p7');
+let pi8 = document.getElementById('p8');
+let pi9 = document.getElementById('p9');
+let pi10 = document.getElementById('p10');
+let pi11 = document.getElementById('p11');
+let pi12 = document.getElementById('p12');
+let pi13 = document.getElementById('p13');
+let pi14 = document.getElementById('p14');
+let pi15 = document.getElementById('p15');
+let pi16 = document.getElementById('p16');
+let pi17 = document.getElementById('p17');
+let pi18 = document.getElementById('p18');
+let pi19 = document.getElementById('p19');
+let pi20 = document.getElementById('p20');
+let pi21 = document.getElementById('p21');
+let pi22 = document.getElementById('p22');
+let pi23 = document.getElementById('p23');
+let pi24 = document.getElementById('p24');
+let pi25 = document.getElementById('p25');
+function deletar() {
+  
+  bot.addEventListener('click', function () {
+    for (let pixes = 1; pixes < 26; pixes += 1) {
+      pi1.style.backgroundColor = 'white'
+      pi2.style.backgroundColor = 'white'
+      pi3.style.backgroundColor = 'white'
+      pi4.style.backgroundColor = 'white'
+      pi5.style.backgroundColor = 'white'
+      pi6.style.backgroundColor = 'white'
+      pi7.style.backgroundColor = 'white'
+      pi8.style.backgroundColor = 'white'
+      pi9.style.backgroundColor = 'white'
+      pi10.style.backgroundColor = 'white'
+      pi11.style.backgroundColor = 'white'
+      pi12.style.backgroundColor = 'white'
+      pi13.style.backgroundColor = 'white'
+      pi14.style.backgroundColor = 'white'
+      pi15.style.backgroundColor = 'white'
+      pi16.style.backgroundColor = 'white'
+      pi17.style.backgroundColor = 'white'
+      pi18.style.backgroundColor = 'white'
+      pi19.style.backgroundColor = 'white'
+      pi20.style.backgroundColor = 'white'
+      pi21.style.backgroundColor = 'white'
+      pi22.style.backgroundColor = 'white'
+      pi23.style.backgroundColor = 'white'
+      pi24.style.backgroundColor = 'white'
+      pi25.style.backgroundColor = 'white'
+    }
+  })
+}
+
+window.onload = deletar;
