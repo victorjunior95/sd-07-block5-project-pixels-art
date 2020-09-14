51f24dc7241f4123d5c0ba7e953258f41f93a177
Raphael Caputo <faelelf2@gmail.com>
2020-09-13 23:22:52 -0300

    Tentando adicionar o for

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let selectedColor = document.getElementById("black-index");
-document.querySelectorAll(".color").forEach(clickConfigure);
-function clickConfigure(item) {
+document.querySelectorAll(".color").forEach(indexClick);
+function changeClass(item) {
+  item.classList.remove("selected");
+}
+
+function indexClick(item) {
   item.addEventListener("click", function () {
     document.querySelectorAll(".color").forEach(changeClass);
     item.classList.add("selected");
@@ -8,8 +12,12 @@ function clickConfigure(item) {
   });
 }
 
-let button = document.getElementById("clear-board");
-button.addEventListener("click", function () {
-  let pixel = document.querySelectorAll(".pixel");
-  pixel.style.backgroundColor = "white";
+let pixels = document.querySelectorAll(".pixel");
+for (let i = 0; i < pixels.length; i += 1) {
+  pixels[i].style.backgroundColor = "white";
+}
+document.querySelector("#clear-board").addEventListener("click", function () {
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = "white";
+  }
 });
