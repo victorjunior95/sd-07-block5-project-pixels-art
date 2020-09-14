cc5e78322c83cd99243928365227c2a907d07343
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-09-14 17:59:32 -0300

    Criado estrutura html, css e algoritmo js básico visando futura implementação.

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-window.onload = function() {
-    
+const linesCols = 5;
+const divPixelBoard = document.getElementById("pixel-board");
+
+function createLines(numberOfLines) {
+  for (let lines = 1; lines <= numberOfLines; lines += 1) {
+    let line = document.createElement("div");
+    line.id = `line ${lines}`;
+    line.className = 'line';
+    divPixelBoard.appendChild(line);
+  }
 }
+
+function fillLinesWithPixels(numberOfPixels) {
+  for (let lines = 1; lines <= numberOfPixels; lines += 1) {
+    for (let pixels = 1; pixels <= numberOfPixels; pixels += 1) {
+      let pixel = document.createElement("div");
+      pixel.className = "pixel";
+      let line = document.getElementById(`line ${pixels}`);
+      line.appendChild(pixel);
+    }
+  }
+}
+
+createLines(linesCols);
+fillLinesWithPixels(linesCols);
