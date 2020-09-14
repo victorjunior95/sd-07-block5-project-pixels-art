4a4d0cdf72fd1a3d3b2249232cc046206b50340d
moraisgabri <gabriel8azevedo@gmail.com>
2020-09-14 18:26:32 -0300

    code climate fixes(adding semicolon in everything)

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 window.onload = function() {
-  let selected = document.querySelector('.selected')
-  let button = document.querySelector('#clear-board')
+  let selected = document.querySelector('.selected');
+  const button = document.querySelector('#clear-board');
 
-  let selectColor = function() {
-    selected.classList.remove('selected')   
-    event.target.classList.add('selected')
-    selected = event.target
+  let selectColor = function () {
+    selected.classList.remove('selected');
+    event.target.classList.add('selected');
+    selected = event.target;
   }
-
 
   // https://flaviocopes.com/how-to-add-event-listener-multiple-elements-javascript/
   // paleta de cores
   document.querySelectorAll('.color').forEach(item => {
-    item.addEventListener('click', event => {
-      selectColor()
-      console.log(selected)
-    })
-  })
+    item.addEventListener('click', () => {
+      selectColor();
+      console.log(selected);
+    });
+  });
 
   //pixeis
   document.querySelectorAll('.pixel').forEach(item => {
-    item.addEventListener('click', event => {
-      event.target.style.backgroundColor = window.getComputedStyle(selected).backgroundColor
-    })
-  })
+    item.addEventListener('click', (event) => {
+      event.target.style.backgroundColor = window.getComputedStyle(selected).backgroundColor;
+    });
+  });
 
   button.addEventListener('click', function() {
-    let pixels = document.querySelectorAll('.pixel')
+    let pixels = document.querySelectorAll('.pixel');
     pixels.forEach(item => {
-      item.style.backgroundColor = 'White'
-    })
-  })
-
-}
+      item.style.backgroundColor = 'White';
+    });
+  });
+};
