7f9021c486366136ef6d2c30d739a3994642e3bf
Lenodeoliveira <lenodeoliveira@gmail.com>
2020-09-14 19:09:08 -0300

    Editando cores

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 window.onload = function(){
 
-  let color = document.querySelectorAll(".color");
-  let listPixel = document.querySelector("#pixel-board");
-
-  document.getElementById("blue").style.backgroundColor = "blue";
-  document.getElementById("green").style.backgroundColor = "green";
-  document.getElementById("red").style.backgroundColor = "red";
-
-  document.getElementById("black").style.backgroundColor = "black";
 }
