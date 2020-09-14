349f73af3cc9cc5d84a4aa623ab8ae0c22f77688
Pedro MF <pedromarques1011@gmail.com>
2020-09-14 15:27:10 -0300

    cor aleatoria

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ window.onload = function () {
   const mother = document.querySelector("#pixel-board");
 
   document.getElementsByClassName("color")[0].style.backgroundColor = "black";
-  document.getElementsByClassName("color")[1].style.backgroundColor = "red";
-  document.getElementsByClassName("color")[2].style.backgroundColor = "blue";
-  document.getElementsByClassName("color")[3].style.backgroundColor = "green";
+  document.getElementsByClassName(
+    "color"
+  )[1].style.backgroundColor = corAleatoria();
+  document.getElementsByClassName(
+    "color"
+  )[2].style.backgroundColor = corAleatoria();
+  document.getElementsByClassName(
+    "color"
+  )[3].style.backgroundColor = corAleatoria();
 
   function DefineSize() {
     for (let i = 0; i < BOX_SIZE; i += 1) {
@@ -55,3 +61,37 @@ window.onload = function () {
     }
   });
 };
+
+function aleatorio(inferior, superior) {
+  numPossibilidades = superior - inferior;
+  aleat = Math.random() * numPossibilidades;
+  aleat = Math.floor(aleat);
+  return parseInt(inferior) + aleat;
+}
+
+function corAleatoria() {
+  hexadecimal = new Array(
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F"
+  );
+  cor_aleatoria = "#";
+  for (i = 0; i < 6; i++) {
+    posarray = aleatorio(0, hexadecimal.length);
+    cor_aleatoria += hexadecimal[posarray];
+  }
+  return cor_aleatoria;
+}
