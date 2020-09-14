5e37887b393fba07860266f058bdad00e67cd046
TIAGO <tiagoesdrasvieira@gmail.com>
2020-09-14 19:03:30 -0300

    Fix name selected

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ let x = document.querySelectorAll('.color');
 
 for (let i = 0; i < x.length; i += 1) {
     x[i].addEventListener('click', function () {
-        let a = document.querySelector('.select');
-        a.classList.remove('select');
-        x[i].classList.add('select');
+        let a = document.querySelector('.selected');
+        a.classList.remove('selected');
+        x[i].classList.add('selected');
         console.log(x[i]);
     });
 }
@@ -20,7 +20,7 @@ let pixel = document.querySelectorAll('.pixel');
 for (let i = 0; i < line.length; i+= 1) {
     for (let j = 0; j < pixel.length; j += 1) {
         pixel[i, j].addEventListener('click', function () {
-            let a = document.querySelector('.select');
+            let a = document.querySelector('.selected');
             pixel[i,j].style.backgroundColor = window.getComputedStyle(a).backgroundColor; 
         });
         
