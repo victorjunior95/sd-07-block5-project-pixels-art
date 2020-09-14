9187e833ad6a945722a54ab391c1d3d3db3bd4c8
Melissa Gomes <mel.ge171201@gmail.com>
2020-09-14 18:35:08 -0300

    testando os requisitos que faltam

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-//eu crio varias lets e as que eu n usar eu apago 
-let paletaDeCores = document.querySelector("#color-palette");
-let corPreto = document.querySelector("#quadrado1")
-let corVermelho =document.querySelector("#quadrado2");
-let corAzul = document.querySelector("#quadrado3");
-let corVerde = document.querySelector("#quadrado4");
-let primeiraCorSelecionada = document.querySelector(".selected");
-let tabelaDePixels = document.querySelector("#pixel-board");
-let pixels = document.querySelector(".pixel");
+
+let quadrados = document.querySelectorAll(".pixel");
+let botaoDeLimpar = document.querySelector("#clear-board");
+let cores= document.querySelectorAll(".color");
+
+
+quadrados.forEach(function(item){
+    item.addEventListener("click", function(){
+        let selecionado = document.querySelector(".selected")
+        item.style.backgroundColor = window.getComputedStyle(selecionado).backgroundColor;
+    })
+})
+
+function selecionarCor (item){
+    item.addEventListener("click", function(){
+        let selecionado = document.querySelector(".selected");
+        selecionado.classList.remove("selected");
+        item.classList.add("selected")
+    })
+}
+cores.forEach(selecionarCor)
+
 
 
