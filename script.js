12ea382047b2cf4fe48a501f4f0b02e5f725a314
Carol Andrade <a.carolg1@gmail.com>
2020-09-14 18:07:04 -0300

    adiciona botoes para selecionar a cor

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
+//trata evento criar o board e limpa-lo para o usuário colocar o número que quiser
 window.onload = function () {
   makeBoard(5);
 }
 const pixelBoard = document.querySelector('#pixel-board');
 const numberInputBox = document.querySelector('#board-size');
 const buttonInput = document.querySelector('#generate-board');
-
 function makeBoard (number) {
 pixelBoard.innerHTML = '';
-for(let lIndex = 0; lIndex < number;lIndex +=1){
+for(let lIndex = 0; lIndex < number; lIndex +=1){
   let lPixelBoard = document.createElement('div');
   lPixelBoard.className ='line';
   pixelBoard.appendChild(lPixelBoard);
@@ -28,3 +28,28 @@ buttonInput.addEventListener('click', function () {
     makeBoard(number)
   }
 })};
+
+// trata evento selecionar elementos com cor
+    let btnBlack = document.getElementsByClassName('color')[0];
+    let btnOrange = document.getElementsByClassName('color')[1];
+    let btnOrchid = document.getElementsByClassName('color')[2];;
+    let btnBlue = document.getElementsByClassName('color')[3];;
+    let color = 'black';
+
+// cria botões para selecionar cor
+    btnBlack.addEventListener('click', function () {
+      btnBlack.classList.add("selected");
+      color = 'black';
+    });
+    btnOrange.addEventListener('click', function () {
+      btnOrange.classList.add("selected");
+      color = 'rgb(239 , 123 , 69)';
+    });
+    btnOrchid.addEventListener('click', function () {
+      btnOrchid.classList.add("selected");
+      color = 'rgb(94 , 177 , 191)';
+    });
+    btnBlue.addEventListener('click', function () {
+      btnBlue.classList.add("selected");
+      color = 'rgb(205 , 237 , 246)';
+    });
