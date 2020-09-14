c11595980306c7e6a1168701db5c5bd9cb785a86
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-09-14 18:36:54 -0300

    colocando botão clear

--- script.js
+++ script.js
@@ -13,15 +13,21 @@ for(let index = 0; index < selectPixel.length; index += 1) {
 let getColor = document.querySelectorAll('.color');
 
 function changeSelected(event){
-  console.log("estou aqui")
   let colorSelected = document.querySelector('.selected');
 
   colorSelected.classList.remove('selected');
   event.target.classList.add('selected');
 }
 
-for(let count = 0; count < getColor.length; count +=1) {
+for(let count = 0; count < getColor.length; count += 1) {
   getColor[count].addEventListener('click', changeSelected)
 }
 
+let clearButton = document.getElementById('clear-board');
 
+clearButton.addEventListener('click', function (){
+  for(let list = 0; list < selectPixel.length; list += 1){
+    let selectPixel = document.querySelectorAll('.pixel');
+  selectPixel[list].style.backgroundColor = 'white';
+  }
+})
