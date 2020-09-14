8ac362e9a656bf3cafb3bd9c0331ef1c1797931a
Ana Karine <anakarine.ti@gmail.com>
2020-09-14 17:23:27 -0300

    correçao requisito 7

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
-let selectBlackColor = document.querySelector('.selected');
-selectBlackColor.addEventListener('click', function() {
-  selectBlackColor.classList.add('.selected');
-  colorRed.classList.remove('.selected');
-  colorBlue.classList.remove('.selected');
-  colorGreen.classList.remove('.selected');
+let defaultColor = document.querySelector('.selected').style.backgroundColor;
+
+
+const selectColor = document.querySelector('.color-palette');
+selectColor.addEventListener('click', function (event) {
+  const select = document.querySelector('.selected');
+  select.classList.remove('selected');
+  select.classList.add('selected');
+  defaultColor = event.target.style.backgroundColor;
 });
 
-let colorRed = document.querySelector('.red');
-colorRed.addEventListener('click', function() {
-  colorRed.classList.add('.selected');
-  colorBlack.classList.remove('.selected');
-  colorBlue.classList.remove('.selected');
-  colorGreen.classList.remove('.selected');
-});
-
-let colorBlue = document.querySelector('.blue');
-colorBlue.addEventListener('click', function() {
-  colorBlue.classList.add('.selected');
-  colorBlack.classList.remove('.selected');
-  colorRed.classList.remove('.selected');
-  colorGreen.classList.remove('.selected');
-});
-
-let colorGreen = document.querySelector('.green');
-colorGreen.addEventListener('click', function() {
-  colorGreen.classList.add('.selected');
-  colorBlack.classList.remove('.selected');
-  colorRed.classList.remove('.selected');
-  colorBlue.classList.remove('.selected');
+const selectPixel = document.querySelector('.pixel-board');
+selectPixel.addEventListener('click', function (event) {
+  event.target.style.backgroundColor = defaultColor;
 });
