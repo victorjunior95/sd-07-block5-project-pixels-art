92a28b986587672d27b79dff4ce6c6bea5250ef2
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-14 15:18:18 -0300

    Adicionando a classe selected da cor clicada e removendo das demais em tempo real

--- script.js
+++ script.js
@@ -1,41 +1,56 @@
 let mouse;
-let blackPixel = document.querySelector(".selected");
+let colorPicked = document.querySelector(".selected");
+
+let blackPixel = document.querySelector(".black");
 let redPixel = document.querySelector(".red");
 let bluePixel = document.querySelector(".blue");
 let greenPixel = document.querySelector(".green");
 
 window.onload = function () {
   mouse = window
-    .getComputedStyle(blackPixel, null)
+    .getComputedStyle(colorPicked, null)
     .getPropertyValue("background-color");
 };
 
+//----------------------------------------------------------//
+
 blackPixel.addEventListener("click", function () {
+  colorPicked.classList.remove("selected");
+  blackPixel.classList.add("selected");
+  colorPicked = document.querySelector(".selected");
   mouse = window
-    .getComputedStyle(blackPixel, null)
+    .getComputedStyle(colorPicked, null)
     .getPropertyValue("background-color");
 });
 
 redPixel.addEventListener("click", function () {
+  colorPicked.classList.remove("selected");
+  redPixel.classList.add("selected");
+  colorPicked = document.querySelector(".selected");
   mouse = window
-    .getComputedStyle(redPixel, null)
+    .getComputedStyle(colorPicked, null)
     .getPropertyValue("background-color");
 });
 
 bluePixel.addEventListener("click", function () {
+  colorPicked.classList.remove("selected");
+  bluePixel.classList.add("selected");
+  colorPicked = document.querySelector(".selected");
   mouse = window
-    .getComputedStyle(bluePixel, null)
+    .getComputedStyle(colorPicked, null)
     .getPropertyValue("background-color");
 });
 
 greenPixel.addEventListener("click", function () {
+  colorPicked.classList.remove("selected");
+  greenPixel.classList.add("selected");
+  colorPicked = document.querySelector(".selected");
   mouse = window
-    .getComputedStyle(greenPixel, null)
+    .getComputedStyle(colorPicked, null)
     .getPropertyValue("background-color");
 });
 
-
-
+//---------------------------------------------------------------//
 
 let pixel = document.querySelector(".pixel");
 
