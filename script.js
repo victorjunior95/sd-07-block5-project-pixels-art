1cbd30eccaf61c3c05706bdf4035edb78b6bd923
Carol Andrade <a.carolg1@gmail.com>
2020-09-14 17:18:11 -0300

    arrumando problema de gerar 5 pixels no inicio

--- script.js
+++ script.js
@@ -1,45 +1,30 @@
-// trata evento construir o tabuleiro
+window.onload = function () {
+  makeBoard(5);
+}
+const pixelBoard = document.querySelector('#pixel-board');
+const numberInputBox = document.querySelector('#board-size');
+const buttonInput = document.querySelector('#generate-board');
 
-// trata construção para 5
-  const pixelBoard = document.querySelector('#pixel-board');
-  for (let lIndex = 0; lIndex < 5; lIndex += 1) {
-  const lPixelBoard = document.createElement('div');
-  lPixelBoard.className = 'line';
+function makeBoard (number) {
+pixelBoard.innerHTML = '';
+for(let lIndex = 0; lIndex < number;lIndex +=1){
+  let lPixelBoard = document.createElement('div');
+  lPixelBoard.className ='line';
   pixelBoard.appendChild(lPixelBoard);
-  for (let pIndex = 0; pIndex < 5; pIndex += 1) {
-      const pixel = document.createElement('div');
-      pixel.className = 'pixel';
+  for(let pIndex = 0; pIndex < number; pIndex +=1 ) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel'
       lPixelBoard.appendChild(pixel);
   }
 }
-//trata evento selecionar a cor
-btnBlack = document.querySelectorAll('.color')[0];
-btnOrange = document.querySelectorAll('.color')[1];
-btnOrchid = document.querySelectorAll('.color')[2];
-btnBlue = document.querySelectorAll('.color')[3];
-
-btnBlack.addEventListener('click', function {
-    selected.style.backgroundColor = 
-})
-// Trata construção dinâmica
-// const numberInputBox = document.querySelector('#board-size');
-// const buttonInput = document.querySelector('#generate-board');
-// buttonInput.addEventListener('click', function () {
-//   const numberInput = document.querySelector('#board-size').value;
-//   let number = numberInput;
-//   let pixelBoard = document.querySelector('#pixel-board');
-//   if(number === ''){
-//     alert('Board inválido!')}
-//   else {
-//   for(let lIndex = 0; lIndex < number;lIndex +=1){
-//   let lPixelBoard = document.createElement('div');
-//   lPixelBoard.className ='line';
-//   pixelBoard.appendChild(lPixelBoard);
-//   for(let pIndex = 0; pIndex < number; pIndex +=1 ) {
-//       let pixel = document.createElement('div');
-//       pixel.className = 'pixel'
-//       lPixelBoard.appendChild(pixel);
-//   }
-// }
-// }
-// });
+buttonInput.addEventListener('click', function () {
+  const numberInput = document.querySelector('#board-size').value;
+  let number = numberInput;
+  let pixelBoard = document.querySelector('#pixel-board');
+  if(number === '') {
+    alert('Board inválido!')
+  }
+  else {
+    makeBoard(number)
+  }
+})};
