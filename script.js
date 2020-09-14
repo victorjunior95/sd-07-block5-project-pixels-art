db8ace6fa695f478c72bbe2730f76d6b98c16eee
Fernando Antunes <fernando.antunes1@gmail.com>
2020-09-14 18:42:37 -0300

    Funcionalidade para o usuario escolher o tamanho do quadro de pixels

--- script.js
+++ script.js
@@ -6,15 +6,36 @@ window.onload = function(){
     let corQuatro=document.getElementById('cor-4');
     let corSelecionada='rgb(0,0,0)';
     let botãoLimpar=document.getElementById('clear-board');
+    let botaoCriaPixels=document.getElementById('generate-board');
+    let areaPixels=document.getElementById('pixel-board');
+    let qtdPixels=5;
+    let contaPixel=document.querySelectorAll('.pixel');
 
-    for (i=0; i<linhaPixel.length; i+=1){
-        for (j=1; j<=5; j+=1){
-            let criaDiv=document.createElement('div');
-            criaDiv.className='pixel';
-            criaDiv.innerText=`${i}+${j}`;
-            linhaPixel[i].appendChild(criaDiv);
+    
+    function criaGrid(quantidade){
+    if (contaPixel.length > 0){
+        for (i=0; i<contaPixel.length; i+=1){
+            contaPixel[i].remove();
         }
     }
+        for (i=1; i<=qtdPixels; i+=1){
+            let criaDivLinha=document.createElement('div');
+                areaPixels.appendChild(criaDivLinha);
+            for (j=1; j<=qtdPixels; j+=1){
+                let criaDiv=document.createElement('div');
+                criaDiv.className='pixel';
+                criaDiv.innerText=`${i}+${j}`;
+                areaPixels.appendChild(criaDiv);
+                }
+        }
+        document.querySelectorAll('.pixel').forEach(item => {
+            item.addEventListener('click', event => {
+            item.style.backgroundColor=corSelecionada;
+            })
+        })
+    }
+
+    criaGrid(qtdPixels);
 
     corUm.addEventListener('click',function(){
         corUm.className='color selected';
@@ -52,16 +73,17 @@ window.onload = function(){
         corSelecionada=propriedadeCSS;
     })
 
-    document.querySelectorAll('.pixel').forEach(item => {
-        item.addEventListener('click', event => {
-          item.style.backgroundColor=corSelecionada;
-        })
-      })
-
     botãoLimpar.addEventListener('click', function(){
         let selectDiv=document.querySelectorAll('.pixel');
         for (i=0; i<selectDiv.length; i+=1){
             selectDiv[i].style.backgroundColor='rgb(255,255,255';
         }
+    })
+
+    botaoCriaPixels.addEventListener('click', function(){
+        let qtd=document.getElementById('board-sizey');
+        contaPixel=document.querySelectorAll('.pixel');
+        qtdPixels=qtd.value;
+        criaGrid(qtdPixels);
     })
 }
