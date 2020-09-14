cd6e7f4c051e88e3761f86ca1287bd4caa9f4cca
josielvs <josielvs@gmail.com>
2020-09-14 00:36:01 -0300

    Requisito 2. Adicionando a paleta de cores.

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-function createReader() {
-  const headerPage = document.createElement('header');
-  headerPage.className = 'head-class';
-  document.body.appendChild(headerPage);
-  const titleProject = document.createElement('h1');
-  titleProject.innerHTML = 'Paleta de Cores';
-  titleProject.className = 'title';
-  titleProject.setAttribute('id', 'title')
-  headerPage.appendChild(titleProject);
+// Criando o header
+const headerPage = document.createElement('header');
+headerPage.className = 'head-class';
+document.body.appendChild(headerPage);
+// Criando H1
+const titleProject = document.createElement('h1');
+titleProject.innerHTML = 'Paleta de Cores';
+titleProject.className = 'title';
+titleProject.setAttribute('id', 'title');
+headerPage.appendChild(titleProject);
+// Criando o main
+const mainPage = document.createElement('main');
+mainPage.className = 'main-class';
+document.body.appendChild(mainPage);
+// criando o footer
+const footerPage = document.createElement('footer');
+footerPage.className = 'footer-class';
+footerPage.innerText = 'Josiel - Turma #7 - Trybe';
+document.body.appendChild(footerPage);
+
+function createPaletteColors(parent) {
+  const palette = document.createElement('section');
+  palette.className = 'color-palette';
+  palette.setAttribute('id', 'color-palette');
+  parent.appendChild(palette);
+
+  const colorsPalette = ['Black', 'OldLace', 'Tomato', 'MediumAquamarine', 'DodgerBlue'];
+  for (let index = 0; index < colorsPalette.length; index += 1) {
+    let colorOfPalette = document.createElement('span');
+    colorOfPalette.className = 'color';
+    colorOfPalette.setAttribute('id', colorsPalette[index]);
+    colorOfPalette.style.backgroundColor = colorsPalette[index];
+    palette.appendChild(colorOfPalette)
+  }
 }
-createReader();
+createPaletteColors(mainPage);
