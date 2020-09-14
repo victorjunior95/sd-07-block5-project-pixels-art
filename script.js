28437a186b4aac7d889d8580d57460c88b4bc741
alvaro <alvarovascon2@gmail.com>
2020-09-14 19:02:40 -0300

    code climate

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //  declarando variáveis
 const pixelBoard = document.getElementById('pixel-board');
 const colors = document.querySelectorAll('.color');
-let numberOfLines = 5;
+const numberOfLines = 5;
 const clearButton = document.getElementById('clear-board');
 const generateBoardBttn = document.getElementById('generate-board');
 
 //  remove board
-function removeBoard () {
+function removeBoard() {
   pixelBoard.innerHTML = '';
 }
 
 //  evento de clique no quadro
-function createEvent (pixelDiv) {
+function createEvent(pixelDiv) {
   pixelDiv.addEventListener('click', function () {
     const currentSelected = document.querySelector('.selected');
     pixelDiv.classList = 'pixel';
@@ -22,45 +22,45 @@ function createEvent (pixelDiv) {
 //  botão de limpar quadro
 clearButton.addEventListener('click', function () {
   const allPixels = document.querySelectorAll('.board div');
-  for (index = 0; index < allPixels.length; index += 1) {
+  for (let index = 0; index < allPixels.length; index += 1) {
     allPixels[index].classList = 'pixel white';
   }
 });
 
 //  função que enche a linha
-function fillLine (line) {
-  let pixelDiv = document.createElement('div');
+function fillLine(line) {
+  const pixelDiv = document.createElement('div');
   pixelDiv.classList.add('pixel', 'white');
   createEvent(pixelDiv);
   line.appendChild(pixelDiv);
 }
 
 //  função que cria linha
-function createLine (numberOfLines) {
-  let line = document.createElement('div');
-  for (column = 0; column < numberOfLines; column += 1) {
-    fillLine (line);
+function createLine(numberOfLines) {
+  const line = document.createElement('div');
+  for (let column = 0; column < numberOfLines; column += 1) {
+    fillLine(line);
   }
   pixelBoard.appendChild(line);
 }
 
 //  função que cria tabela
-function createBoard (numberOfLines) {
-  for (lineNumber = 0; lineNumber < numberOfLines; lineNumber += 1) {
+function createBoard(numberOfLines) {
+  for (let lineNumber = 0; lineNumber < numberOfLines; lineNumber += 1) {
     createLine(numberOfLines);
   }
 }
 
 //  função que remove cor selecionada
-function clearSelected () {
-  for (index = 0; index < colors.length; index += 1) {
+function clearSelected() {
+  for (let index = 0; index < colors.length; index += 1) {
     colors[index].classList.remove('selected');
   }
 }
 
 
 //  evento de clique na paleta de cores
-for (index = 0; index < colors.length; index += 1) {
+for (let index = 0; index < colors.length; index += 1) {
   colors[index].addEventListener('click', function (event) {
     clearSelected();
     event.target.classList.add('selected');
@@ -70,14 +70,14 @@ for (index = 0; index < colors.length; index += 1) {
 //  escolha de tamanho
 generateBoardBttn.addEventListener('click', function () {
   const resizer = document.getElementById('board-size');
-  if (resizer.value <= 0 ) {
+  if (resizer.value <= 0) {
     alert('Board inválido!');
-    return
+    return;
   }
-  if (resizer.value < 5 ) {
+  if (resizer.value < 5) {
     resizer.value = 5;
   }
-  if (resizer.value > 50 ) {
+  if (resizer.value > 50) {
     resizer.value = 50;
   }
   removeBoard();
