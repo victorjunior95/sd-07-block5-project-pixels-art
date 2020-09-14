5872e055874923f6606ca70f9f39eb7845dc3b50
Pedro MF <pedromarques1011@gmail.com>
2020-09-14 15:46:42 -0300

    selected

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 window.onload = function () {
   let BOX_SIZE = 5;
-  let DEFAULT_COLOR = "black";
-  const mother = document.querySelector("#pixel-board");
+  let DEFAULT_COLOR = 'black';
+  const mother = document.querySelector('#pixel-board');
 
-  document.getElementsByClassName("color")[0].style.backgroundColor = "black";
+  document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
   document.getElementsByClassName(
-    "color"
+    'color'
   )[1].style.backgroundColor = corAleatoria();
   document.getElementsByClassName(
-    "color"
+    'color'
   )[2].style.backgroundColor = corAleatoria();
   document.getElementsByClassName(
-    "color"
+    'color'
   )[3].style.backgroundColor = corAleatoria();
 
   function DefineSize() {
     for (let i = 0; i < BOX_SIZE; i += 1) {
-      const horizontalDiv = document.createElement("div");
-      horizontalDiv.classList.add("horiDiv");
+      const horizontalDiv = document.createElement('div');
+      horizontalDiv.classList.add('horiDiv');
       mother.appendChild(horizontalDiv);
       verticalPixel(horizontalDiv);
     }
@@ -26,37 +26,47 @@ window.onload = function () {
 
   function verticalPixel(motherDiv) {
     for (let u = 0; u < BOX_SIZE; u += 1) {
-      const singlePixel = document.createElement("div");
-      singlePixel.classList.add("pixel");
+      const singlePixel = document.createElement('div');
+      singlePixel.classList.add('pixel');
       motherDiv.appendChild(singlePixel);
-      singlePixel.addEventListener("click", () => {
+      singlePixel.addEventListener('click', () => {
         singlePixel.style.backgroundColor = DEFAULT_COLOR;
       });
     }
   }
 
   const amountPaleta = 4;
+  const removerSelected = () => {
+    for (let a = 0; a < amountPaleta; a += 1) {
+      const color = document.getElementsByClassName('color')[a];
+      color.addEventListener('click', () => {
+        color.classList.remove('selected');
+      });
+    }
+  };
   for (let a = 0; a < amountPaleta; a += 1) {
-    const color = document.getElementsByClassName("color")[a];
-    color.addEventListener("click", () => {
+    const color = document.getElementsByClassName('color')[a];
+    color.addEventListener('click', () => {
       DEFAULT_COLOR = color.style.backgroundColor;
+      color.classList.add('selected');
+      removerSelected();
     });
   }
 
-  document.querySelector(".clear").addEventListener("click", () => {
-    const element = document.getElementsByClassName("pixel");
+  document.querySelector('.clear').addEventListener('click', () => {
+    const element = document.getElementsByClassName('pixel');
     for (let a = 0; a < element.length; a += 1) {
-      element[a].style.backgroundColor = "white";
+      element[a].style.backgroundColor = 'white';
     }
   });
 
-  document.querySelector("#generate-board").addEventListener("click", () => {
-    const size = document.querySelector("#board-size").value;
-    if (size == "") {
-      return alert("Board inválido!");
+  document.querySelector('#generate-board').addEventListener('click', () => {
+    const size = document.querySelector('#board-size').value;
+    if (size == '') {
+      return alert('Board inválido!');
     } else {
       BOX_SIZE = size;
-      mother.innerHTML = "";
+      mother.innerHTML = '';
       DefineSize();
     }
   });
@@ -71,24 +81,24 @@ function aleatorio(inferior, superior) {
 
 function corAleatoria() {
   hexadecimal = new Array(
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F"
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F'
   );
-  cor_aleatoria = "#";
+  cor_aleatoria = '#';
   for (i = 0; i < 6; i++) {
     posarray = aleatorio(0, hexadecimal.length);
     cor_aleatoria += hexadecimal[posarray];
