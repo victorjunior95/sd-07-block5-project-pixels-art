471d074e5ece5f004d0edd3383312e61910a99b1
Muniz <vinicius.muniz.marin@gmail.com>
2020-09-14 18:04:51 -0300

    4. feito utilizando funcoes para criar elementos no html e formar os 25 pixels

---
+++ script.js
@@ -0,0 +1,26 @@
+let columns = 5;
+let lines = document.querySelectorAll(".line");
+
+// Cria pixels brancos
+function createPixel() {
+    let pixel = document.createElement("div");
+    pixel.className = "pixel";
+    return pixel;
+}
+
+// Preenche uma linha com 5 pixels dado a localização da line(divLine).
+function fillLine(divLine) {
+    for (let index = 0; index < columns; index += 1) {
+        let pixel = createPixel();
+        divLine.appendChild(pixel);
+    }
+}
+
+// Usando a função fillLines preenche os 25 pixels.
+function fillAll(arrayLines) {
+    for (let index = 0; index < arrayLines.length; index += 1) {
+        fillLine(arrayLines[index]);
+    }
+}
+
+fillAll(lines);
