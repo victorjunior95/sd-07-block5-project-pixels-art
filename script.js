3dbadef238016dfb4c6196bdb2559f38a7b9735e
Marcos R. Silva <marcos.1998226@gmail.com>
2020-09-14 19:37:26 -0300

    criando regra de negocio para class=selected

---
+++ script.js
@@ -0,0 +1,10 @@
+let blackColor = document.querySelector('#container').childNodes[3].children[0];
+let print = document.querySelector('#pixel-board').childNodes[1];
+
+
+if(blackColor.getAttribute('id') === 'black'){
+    blackColor.className = "selected";
+    print.style.backgroundColor = "black";
+}
+
+console.log(blackColor);
