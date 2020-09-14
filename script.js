7cd25643da3b2ec4cdfac0a5d63860e030bff51b
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-09-14 11:46:21 -0300

    ajuda js

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ let colorSelected = document.querySelector('.selected');
 
 function changeColor(event) {
   event.target.style.backgroundColor = colorSelected.style.backgroundColor;
+}
 
 // function changeColor(event) {
 //   event.target.style.backgroundColor = window.getComputedStyle(colorSelected, String).getPropertyValue("background-color");
+// }
 
 for(let index = 0; index < selectPixel.length; index += 1) {
   selectPixel[index].addEventListener('click', changeColor);
 }
+
