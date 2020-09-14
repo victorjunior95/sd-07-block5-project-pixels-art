5d2d2506be7daef251c7194392095ba78a7b4266
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-09-14 18:03:19 -0300

    quadrados

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 
-// let n = 5;
-// let lineIndex;
-// let symbol = '*';
-// let inputLine = '';
-
-// for (lineIndex = 0; lineIndex < n; lineIndex += 1) {
-//   inputLine = inputLine + symbol;
-// };
-// for (lineIndex = 0; lineIndex < n; lineIndex += 1) {
-//   console.log(inputLine);
-// };
 let board = document.getElementById('pixel-board')
 let amountOfSquares = 5;
 
@@ -19,6 +8,7 @@ for (counter = 0; counter < amountOfSquares;counter +=1){
         square.className += 'pixel';
         board.appendChild(square)
     }
+    board.innerHTML = board.innerHTML + '<br>'
 }
 
 
