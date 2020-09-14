80ab1a5c8fd73ce955e361b252217c6a332d003a
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-09-14 09:37:38 -0300

    requisito 3 e 4: criando tabela de pixels

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 window.onload = function(){
     //Declarando variáveis
     let color = document.querySelectorAll(".color");
+    let listPixel = document.querySelector("#pixel-board");
 
 
     //Declarando as cores de fundo
     document.getElementById("blue").style.backgroundColor = "blue";
     document.getElementById("green").style.backgroundColor = "green";
     document.getElementById("red").style.backgroundColor = "red";
-    document.getElementById("yellow").style.backgroundColor = "yellow";
-
+    document.getElementById("black").style.backgroundColor = "black";
+    
 
 
 
