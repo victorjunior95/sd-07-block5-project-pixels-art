e1359fa8250b93c449747780e809b7a026e2bf91
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2020-09-14 11:47:31 -0300

    requisito 7 envio 1

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const pixelBoard = document.querySelector("#pixel-board");
+const pixelBoard = document.querySelector('#pixel-board');
+const colors = document.querySelectorAll('.color')
 const numberOfLinesAndColunms = 5;
 
 function makeBoard() {
-  
   for (let index = 0; index < numberOfLinesAndColunms; index += 1) {
     let tr = document.createElement('div');
     tr.style.display.tr;
@@ -16,9 +16,21 @@ function makeBoard() {
   }
 }
 
+colors.forEach(item => {
+  item.addEventListener('click', function() {
+    event.target.classList.add('selected');
+     colors.forEach(item => {
+        if (item !== event.target) {
+          item.classList.remove('selected');
+        }
+      });
+  });
+});
+
 function classTD(td) {
   let tdClass = td.className = 'pixel';
   return tdClass;
 }
 
 makeBoard();
+
