d7cedd327a9a08d918f2efa7c829bcc1f12ae854
Melissa Gomes <mel.ge171201@gmail.com>
2020-09-14 13:02:43 -0300

    adicionando botão de limpar

---
+++ script.js
@@ -0,0 +1,11 @@
+//eu crio varias lets e as que eu n usar eu apago 
+let paletaDeCores = document.querySelector("#color-palette");
+let corPreto = document.querySelector("#quadrado1")
+let corVermelho =document.querySelector("#quadrado2");
+let corAzul = document.querySelector("#quadrado3");
+let corVerde = document.querySelector("#quadrado4");
+let primeiraCorSelecionada = document.querySelector(".selected");
+let tabelaDePixels = document.querySelector("#pixel-board");
+let pixels = document.querySelector(".pixel");
+
+
