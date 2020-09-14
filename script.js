b618bec0dbbcc092e7d46c71a4140b58256bfb41
richardfazeredo <richardcompletetour@gmail.com>
2020-09-14 18:04:51 -0300

    Criando quadrado de pixels

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 let colorPallete = document.getElementById('color-palette');
+let pixelBoard = document.getElementById('pixel-board');
+
+
+for (let indexLine = 0; indexLine < 5; indexLine += 1) {
+    let lineBoard = document.createElement('div');
+    pixelBoard.appendChild(lineBoard);
+  for (let indexColumn = 0; indexColumn < 5; indexColumn += 1){
+    let pixel = document.createElement('div');
+    pixel.setAttribute('class', 'pixel');
+    lineBoard.appendChild(pixel);
+  }
+}
