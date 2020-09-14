398efc3c1b1bace595c5dd94b52c57d50d8afdfd
Carol Andrade <a.carolg1@gmail.com>
2020-09-14 13:57:46 -0300

    requisito10 sub1

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 //  cria o quadro de pixels
-const numberInputBox = document.querySelector('#number-input');
-numberInputBox.addEventListener('keyup', function () {
-  const numberInput = document.querySelector('#number-input').value;
+const numberInputBox = document.querySelector('#board-size');
+const buttonInput = document.querySelector('#generate-board');
+buttonInput.addEventListener('click', function () {
+  const numberInput = document.querySelector('#board-size').value;
   let number = numberInput;
   let pixelBoard = document.querySelector('#pixel-board');
+  if(number === ''){
+      alert('Board inválido!')
+  }
+  else {
 for(let lIndex = 0; lIndex < number;lIndex +=1){
     let lPixelBoard = document.createElement('div');
     lPixelBoard.className ='line';
@@ -13,6 +18,7 @@ for(let lIndex = 0; lIndex < number;lIndex +=1){
         pixel.className = 'pixel'
         lPixelBoard.appendChild(pixel);
     }
+}
 }
 });
 
