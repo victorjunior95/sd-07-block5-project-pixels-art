95f89d572d6ad1d15880b9fdfd561edb76247199
Melissa Gomes <mel.ge171201@gmail.com>
2020-09-14 19:38:01 -0300

    adicionando comentarios explicando sobre o window.getComputedStyle pois recebi ajuda e fora e fui orientada a colocar nos comentarios

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ let quadrados = document.querySelectorAll(".pixel");
 let botaoDeLimpar = document.querySelector("#clear-board");
 let cores= document.querySelectorAll(".color");
 
-
+//essa variavel é encarregada de cuidar de que quando houver um click em um quadradinho usando o window.getComputedStyle ele manda um chamado para o meu DOM modificar a cor de fundo do meu quadradinho selecionado nessa função eu prcisei de ajuda do meu irmão, uma pessoa de fora da trybe para entender como usar e qual a estrutura de window.getComputedStyle pois eu realmente não havia entendido e ele me ajudou com isso
 quadrados.forEach(function(item){
     item.addEventListener("click", function(){
         let selecionado = document.querySelector(".selected")
         item.style.backgroundColor = window.getComputedStyle(selecionado).backgroundColor;
     })
 })
-
+// essa função é encarregada de resetar a let selecionado para atribuir a classe selected a outra div que sera a cor escolhida para pintar o quadradinho escolhido, usando o forEach eu caminho por todas as opções para que essa atualização seja feita
 function selecionarCor (item){
     item.addEventListener("click", function(){
         let selecionado = document.querySelector(".selected");
