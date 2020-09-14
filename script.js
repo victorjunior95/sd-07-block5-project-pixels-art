42f08ccf50bb73dc13a10119253035d93a8b0058
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-09-14 18:36:44 -0300

    botao de limpar adicionado

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let whiteBoard = document.querySelector("#pixel-board");
 let pixels = document.querySelectorAll(".pixel");
 let colors = document.querySelectorAll(".color");
 let selectedColor = "black";
+let leButton = document.querySelector("#clear-board");
 
 function updateColor(){
     for (let index = 0; index < colors.length; index +=1){
@@ -44,3 +45,9 @@ function pixelCasso(event){
 for (let index2 of pixels){
     index2.addEventListener("click", pixelCasso);
 }
+
+leButton.addEventListener("click", function(){
+    for (let index = 0; index < pixels.length; index += 1){
+        pixels[index].className = "pixel white";
+    }
+})
