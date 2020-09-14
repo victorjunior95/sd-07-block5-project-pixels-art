0420745da211ca674eb50c3709604e12fa16f09c
cezar88augusto <cezar88augusto@gmail.com>
2020-09-14 15:28:38 -0300

    Requisito 6: Try 2

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 window.onload = function () {
   let arrayColors = document.querySelectorAll(".color");
-  let selectedColor = document.querySelectorAll(".color")[0].style.backgroundColor;
 
   arrayColors[0].style.backgroundColor = "black";
   arrayColors[1].style.backgroundColor = "red";
   arrayColors[2].style.backgroundColor = "grey";
   arrayColors[3].style.backgroundColor = "yellow";
 
+  let selectedColor = arrayColors[0].style.backgroundColor;
+
+  localStorage.setItem("color", selectedColor)
 }
-
-
