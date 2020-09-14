d59384783918b511796c3906fe150867ddb9663c
Felipe Nascimento <flpnascto@gmail.com>
2020-09-14 10:57:30 -0300

    Adicionando requisito 7.

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 window.onload = function () {
-
   // criação da paleta de cores
   const paletaCores = ['black', 'red', 'green', 'blue'];
-
   const divColor = document.querySelectorAll('.color');
 
   for (let index = 0; index < divColor.length; index += 1) {
     divColor[index].style.backgroundColor = paletaCores[index];
+    divColor[index].addEventListener('click', selectColor);
   }
 
   // criação do quadro de pixels
@@ -19,4 +18,12 @@ window.onload = function () {
 
   // selecionando a cor inicial (black);
   let selectedColor = document.querySelector('.selected').style.backgroundColor;
-}
+
+  // aletera a classe selected conforme seleção da paleta de cores
+  function selectColor() {
+    for (let index = 0; index < divColor.length; index += 1) {
+      divColor[index].className = 'color';
+    }
+    this.className = 'color selected';
+  }
+};
