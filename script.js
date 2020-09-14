1e4e90547d4ba70665e317717d44b5ceccd0d0e5
brendalima <brenda.c.lima@poli.ufrj.br>
2020-09-14 17:24:12 -0300

    adding selected class to color black

--- script.js
+++ script.js
@@ -12,7 +12,33 @@ function boardGenerator(value) {
         line.appendChild(pixel);
       }
       board.appendChild(line);
+      attribute();
     }
   }
+
+
+let currentColor = document.querySelectorAll('.selected');
+function select(event) {
+  currentColor.classList.remove('selected');
+  event.target.classList.add('selected');
+  currentColor = event.target;
+}
+
+const color = document.getElementsByClassName('color');
+for (let i = 0; i < color.length; i += 1) {
+  color[i].addEventListener('click', select);
+}
+
+function newColor(event) {
+  event.target.style.backgroundColor = getComputedStyle(currentColor).backgroundColor;
+}
+
+function attribute() {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', newColor);
+  }
+}
+
 
 boardGenerator(5);
