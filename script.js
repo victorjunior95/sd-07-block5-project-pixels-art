ea64f04c6a027251fba2d03b6b5a8c3ffd270626
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-14 19:23:48 -0300

    agora vaai

--- script.js
+++ script.js
@@ -1,49 +1,46 @@
-
+window.onload = cores[0].className = "selected"
 let cores = document.querySelectorAll('.color');
 cores[0].style.backgroundColor = 'black';
 cores[1].style.backgroundColor = 'blue';
 cores[2].style.backgroundColor = 'red';
 cores[3].style.backgroundColor = 'green';
 let pixelBoard = document.querySelector('#pixel-board');
+
 for(index = 0; index < 25; index += 1){
     stylePixel();
 }
-cores[0].className += "selected"
+
 
 // Cor preta selecionada
 cores[0].addEventListener('click', function () {
     for(index = 0; index < 4; index += 1){
         cores[index].className = "color"
-        cores[index].className -= "selected"
     }
-    cores[0].className += "selected"
+    cores[0].className = "selected"
     pixelCor()
 })
 // Cor azul selecionada
 cores[1].addEventListener('click', function () {
     for(index = 0; index < 4; index += 1){
         cores[index].className = "color"
-        cores[index].className -= "selected"
     }
-    cores[1].className += "selected"
+    cores[1].className = "selected"
     pixelCor()
 })
 // Cor vermelha selecionada
 cores[2].addEventListener('click', function () {
     for(index = 0; index < 4; index += 1){
         cores[index].className = "color"
-        cores[index].className -= "selected"
     }
-    cores[2].className += "selected"
+    cores[2].className = "selected"
     pixelCor()
 })
 // Cor verde selecionada
 cores[3].addEventListener('click', function () {
     for(index = 0; index < 4; index += 1){
         cores[index].className = "color"
-        cores[index].className -= "selected"
     }
-    cores[3].className += "selected"
+    cores[3].className = "selected"
     pixelCor()
 })
 let pixel = document.querySelectorAll('.pixel')
