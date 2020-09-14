eb66d720643ab8b39d5cfd6f0d794b7afe7fb90a
Iury Nathan <iurynathan@gmail.com>
2020-09-14 18:14:28 -0300

    corrigindo defeito que pintava o fundo da pixel board

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 let paint = document.getElementsByClassName("selected")[0];
 let background = window.getComputedStyle(paint).backgroundColor;
 let selectedColor = document.getElementById("color-palette");
-let paintPixel = document.getElementById("pixel-board")
+let paintPixel = document.getElementById("pixel-board");
 
 selectedColor.addEventListener("click", function (event) {
-  if (event.target.classList.contains("selected") == false) {
+  if (
+    event.target.classList.contains("selected") == false &&
+    event.target.classList.contains("color") == true
+  ) {
     paint.classList.remove("selected");
     event.target.className += " selected";
-    paint = document.getElementsByClassName("selected")[0]
+    paint = document.getElementsByClassName("selected")[0];
     background = window.getComputedStyle(paint).backgroundColor;
   }
 });
 
-paintPixel.addEventListener("click", function(event) {
-  event.target.style.backgroundColor = background
-})
+paintPixel.addEventListener("click", function (event) {
+  if (event.target.classList.contains("pixel") == true) {
+    event.target.style.backgroundColor = background;
+  }
+});
