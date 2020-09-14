f278df2ca685ffbbdd5ddf97339c10475406b09a
moraisgabri <gabriel8azevedo@gmail.com>
2020-09-14 18:35:44 -0300

    code climate fixes 3.0

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-window.onload = function() {
+window.onload = function () {
   let selected = document.querySelector('.selected');
   const button = document.querySelector('#clear-board');
 
-  let selectColor = function () {
+  const selectColor = function () {
     selected.classList.remove('selected');
     event.target.classList.add('selected');
     selected = event.target;
-  }
+  };
 
   // https://flaviocopes.com/how-to-add-event-listener-multiple-elements-javascript/
   // paleta de cores
-  document.querySelectorAll('.color').forEach(item => {
+  document.querySelectorAll('.color').forEach((item) => {
     item.addEventListener('click', () => {
       selectColor();
-      console.log(selected);
     });
   });
 
   // pixeis
-  document.querySelectorAll('.pixel').forEach(item => {
+  document.querySelectorAll('.pixel').forEach((item) => {
     item.addEventListener('click', (event) => {
       event.target.style.backgroundColor = window.getComputedStyle(selected).backgroundColor;
     });
   });
 
   button.addEventListener('click', function () {
-    let pixels = document.querySelectorAll('.pixel');
-    pixels.forEach(item => {
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach((item) => {
       item.style.backgroundColor = 'White';
     });
   });
