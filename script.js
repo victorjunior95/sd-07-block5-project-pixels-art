4e106465e8c5e374396d5590b12b839cee5d5953
alvaro <alvarovascon2@gmail.com>
2020-09-14 16:36:51 -0300

    botão de limpar quadro

--- script.js
+++ script.js
@@ -2,12 +2,29 @@
 const pixelBoard = document.getElementById('pixel-board');
 const colors = document.querySelectorAll('.color');
 let numberOfLines = 5;
+const clearButton = document.getElementById('clear-board');
+
+//  evento de clique no quadro
+function createEvent (pixelDiv) {
+  pixelDiv.addEventListener('click', function () {
+    const currentSelected = document.querySelector('.selected');
+    pixelDiv.classList = currentSelected.classList;
+  })
+}
+
+//  botão de limpar quadro
+clearButton.addEventListener('click', function () {
+  const allPixels = document.querySelectorAll('.board div');
+  for (index = 0; index < allPixels.length; index += 1) {
+    allPixels[index].classList = 'pixel white';
+  }
+});
 
 //  função que enche a linha
 function fillLine (line) {
   let pixelDiv = document.createElement('div');
   pixelDiv.classList.add('pixel', 'white');
-
+  createEvent(pixelDiv);
   line.appendChild(pixelDiv);
 }
 
@@ -45,4 +62,5 @@ for (index = 0; index < colors.length; index += 1) {
   });
 }
 
-//  evento de clique no quadro
+
+
