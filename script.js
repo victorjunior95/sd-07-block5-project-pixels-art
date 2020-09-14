d2e751e1ca587359e681b7c8e57f1ed86ed10b4b
Renan OLiveira <ren4n.dev@gmail.com>
2020-09-14 15:14:48 -0300

    Criando funções de seleção das cores

---
+++ script.js
@@ -0,0 +1,23 @@
+//  Selecionando paleta
+const divColors = document.querySelector('#color-palette');
+const colors = document.querySelectorAll('.color');
+let selectedDiv = document.querySelector('.selected');
+
+//  Adicionando evento de clique nas cores da paleta
+divColors.addEventListener('click', function (event) {
+  clearSelection();
+  addSelection(event.target);
+});
+
+//  FUNCTIONS
+//  Função responsável por remover classe 'selected' da cor selecionada no momento
+function clearSelection () {
+  selectedDiv.classList.remove('selected');
+};
+
+//Função responsável por adicionar classe 'selected' ao elemento selecionado
+function addSelection (target) {
+  console.log(target);
+  target.classList.add('selected');
+  selectedDiv = document.querySelector('.selected');
+};
