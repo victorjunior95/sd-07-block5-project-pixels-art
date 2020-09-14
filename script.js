c1de4293077c92c1476c360a9d5e0d98bc187403
Renan OLiveira <ren4n.dev@gmail.com>
2020-09-14 15:33:57 -0300

    Adicionando evento de click no pixel-board

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+//  DEFINIÇÕES
 //  Selecionando paleta
 const divColors = document.querySelector('#color-palette');
 const colors = document.querySelectorAll('.color');
+const board = document.querySelector('#pixel-board');
 let selectedDiv = document.querySelector('.selected');
 let selectedColor = getColor(selectedDiv);
 
+//EVENTOS
 //  Adicionando evento de clique nas cores da paleta
 divColors.addEventListener('click', function (event) {
   clearSelection();
   addSelection(event.target);
   selectedColor = getColor(selectedDiv);
+});
+
+//  Adicionando evento de clique na pixel-board
+board.addEventListener('click', function (event) {
+  paint(event.target);
 });
 
 //  FUNCTIONS
@@ -17,15 +25,19 @@ function clearSelection () {
   selectedDiv.classList.remove('selected');
 };
 
-//Função responsável por adicionar classe 'selected' ao elemento selecionado
+//  Função responsável por adicionar classe 'selected' ao elemento selecionado
 function addSelection (target) {
-  console.log(target);
   target.classList.add('selected');
   selectedDiv = document.querySelector('.selected');
 };
 
-//Função Responsável por pegar a cor da div selecionada
+//  Função Responsável por pegar a cor da div selecionada
 function getColor (selectedDiv) {
   let property = window.getComputedStyle(selectedDiv, null).getPropertyValue('background-color');
   return property;
+};
+
+//  Função responsável por pintar pixel
+function paint (target) {
+  target.style.backgroundColor = selectedColor;
 };
