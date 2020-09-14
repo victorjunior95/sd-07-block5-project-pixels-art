6d82d9c00e1dfb8cec054df79255b6b5312c46ca
Felipe Nascimento <flpnascto@gmail.com>
2020-09-13 21:47:02 -0300

    Adicionando requisito 5 e 6.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
 
   // criação da paleta de cores
-  let paletaCores = ['black', 'red', 'green', 'blue'];
+  const paletaCores = ['black', 'red', 'green', 'blue'];
 
   const divColor = document.querySelectorAll('.color');
 
@@ -10,11 +10,13 @@ window.onload = function () {
   }
 
   // criação do quadro de pixels
-  let pixelBoard = document.querySelector('#pixel-board');
+  const pixelBoard = document.querySelector('#pixel-board');
   for (let pixel = 0; pixel < 25; pixel += 1) {
     const board = document.createElement('div');
     board.className = 'pixel';
     pixelBoard.appendChild(board);
   }
+
+  // selecionando a cor inicial (black);
+  let selectedColor = document.querySelector('.selected').style.backgroundColor;
 }
-  
