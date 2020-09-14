07d5d5400af89af60ee0ccf366e8a18cc1d660c6
kramersilva <kramerscs@gmail.com>
2020-09-14 10:39:43 -0300

    code climate reviw

--- script.js
+++ script.js
@@ -1,99 +1,99 @@
-window.onload = function () {
-  function generateRandom() {
-    const firstNumber = Math.ceil(Math.random() * 255);
-    const secondNumber = Math.ceil(Math.random() * 255);
-    const thirdNumber = Math.ceil(Math.random() * 255);
+function generateRandom() {
+  const firstNumber = Math.ceil(Math.random() * 255);
+  const secondNumber = Math.ceil(Math.random() * 255);
+  const thirdNumber = Math.ceil(Math.random() * 255);
 
-    const colorNumber = `rgb(${firstNumber}, ${secondNumber}, ${thirdNumber})`;
-    return colorNumber;
-  }
-  // Define color-pallete variables
-  const colorOne = document.getElementsByClassName('color')[0];
-  const colorTwo= document.getElementsByClassName('color')[1];
-  const colorThree = document.getElementsByClassName('color')[2];
-  const colorFour = document.getElementsByClassName('color')[3];
-  const clearButton = document.getElementById('clear-board');
+  const colorNumber = `rgb(${firstNumber}, ${secondNumber}, ${thirdNumber})`;
+  return colorNumber;
+}
 
-  colorOne.style.backgroundColor = 'black';
-  colorTwo.style.backgroundColor = generateRandom();
-  colorThree.style.backgroundColor = generateRandom();
-  colorFour.style.backgroundColor = generateRandom();
+// Define color-pallete variables
+const colorOne = document.getElementsByClassName('color')[0];
+const colorTwo = document.getElementsByClassName('color')[1];
+const colorThree = document.getElementsByClassName('color')[2];
+const colorFour = document.getElementsByClassName('color')[3];
+const clearButton = document.getElementById('clear-board');
 
-  // Define selected color with color selected class
-  const selectedColor = document.getElementsByClassName('color selected');
-  const pixelBoard = document.getElementById('pixel-board');
-  const generateTable = document.getElementById('generate-board');
-  const boardSizeInput = document.getElementById('board-size');
+colorOne.style.backgroundColor = 'black';
+colorTwo.style.backgroundColor = generateRandom();
+colorThree.style.backgroundColor = generateRandom();
+colorFour.style.backgroundColor = generateRandom();
 
-  // Array of all elements with class color
-  const paletteSelection = document.getElementsByClassName('color');
+// Define selected color with color selected class
+const selectedColor = document.getElementsByClassName('color selected');
+const pixelBoard = document.getElementById('pixel-board');
+const generateTable = document.getElementById('generate-board');
+const boardSizeInput = document.getElementById('board-size');
 
-  // Array of all elements in the pixel board
-  const pixelSelection = document.getElementsByClassName('pixel');
+// Array of all elements with class color
+const paletteSelection = document.getElementsByClassName('color');
 
-  // Loop to AddEventListener to all elements in color palette
-  for (let item = 0; item < paletteSelection.length; item += 1) {
-    paletteSelection[item].addEventListener('click', selectColor);
-  }
-
-  clearButton.addEventListener('click', function () {
-    for (let box = 0; box < pixelSelection.length; box += 1) {
-      pixelSelection[box].style.backgroundColor = 'white';
-    }
-  });
-
-  generateTable.addEventListener('click', function () {
-    removeTable();
-    const newBody = document.createElement('tbody');
-    pixelBoard.appendChild(newBody);
-    let size = boardSizeInput.value;
-    if (size === '') {
-      alert('Board inválido!');
-    } else if (size < 5) {
-      size = 5;
-    } else if (size > 50) {
-      size = 50;
-    }
-    for (let row = 1; row <= size; row += 1) {
-      const newRow = document.createElement('tr');
-      newBody.appendChild(newRow)
-      for (let i = 1; i <= size; i += 1) {
-        const newBox = document.createElement('td')
-        newBox.className = 'pixel';
-        newRow.appendChild(newBox);
-      }
-    }
-    eventListenerNewPixels();
-  })
-
-  // Loop to AddEventListener to all elements in the pixel board
-  for (let pixel = 0; pixel < pixelSelection.length; pixel += 1) {
-    let pixelBox = pixelSelection[pixel];
-    pixelBox.addEventListener('click', fillColor);
-  }
+// Array of all elements in the pixel board
+const pixelSelection = document.getElementsByClassName('pixel');
 
   // Function that pick a color (chosen element is tagged with a color selected class while previous selected color is changed to a color class name)
-  function selectColor(event) {
-    selectedColor[0].className = 'color';
-    event.target.className = 'color selected';
-  }
+function selectColor(event) {
+  selectedColor[0].className = 'color';
+  event.target.className = 'color selected';
+}
 
-  // function that fills a pixel and changes its background color to the selected one.
-  function fillColor(event) {
-    const color = selectedColor[0].style.backgroundColor;
-    event.target.style.backgroundColor = color;
-  }
+// Loop to AddEventListener to all elements in color palette
+for (let item = 0; item < paletteSelection.length; item += 1) {
+  paletteSelection[item].addEventListener('click', selectColor);
+}
 
-  // remove tabela
-  function removeTable() {
-    const tableContent = document.getElementsByTagName('tbody')[0];
-    pixelBoard.removeChild(tableContent);
+// colors all table to white
+clearButton.addEventListener('click', function () {
+  for (let box = 0; box < pixelSelection.length; box += 1) {
+    pixelSelection[box].style.backgroundColor = 'white';
   }
+});
 
-  function eventListenerNewPixels() {
-    for (let pixel = 0; pixel < pixelSelection.length; pixel += 1) {
-      const pixelBox = pixelSelection[pixel];
-      pixelBox.addEventListener('click', fillColor);
+function removeTable() {
+  const tableContent = document.getElementsByTagName('tbody')[0];
+  pixelBoard.removeChild(tableContent);
+}
+
+function eventListenerNewPixels() {
+  for (let pixel = 0; pixel < pixelSelection.length; pixel += 1) {
+    const pixelBox = pixelSelection[pixel];
+    pixelBox.addEventListener('click', fillColor);
+  }
+}  
+
+// generate table with N elements in height and width based on user input
+generateTable.addEventListener('click', function () {
+  removeTable();
+  const newBody = document.createElement('tbody');
+  pixelBoard.appendChild(newBody);
+  let size = boardSizeInput.value;
+  if (size === '') {
+    alert('Board inválido!');
+  } else if (size < 5) {
+    size = 5;
+  } else if (size > 50) {
+    size = 50;
+  }
+  for (let row = 1; row <= size; row += 1) {
+    const newRow = document.createElement('tr');
+    newBody.appendChild(newRow);
+    for (let i = 1; i <= size; i += 1) {
+      const newBox = document.createElement('td');
+      newBox.className = 'pixel';
+      newRow.appendChild(newBox);
     }
   }
+  eventListenerNewPixels();
+});
+
+// Loop to AddEventListener to all elements in the pixel board
+for (let pixel = 0; pixel < pixelSelection.length; pixel += 1) {
+  const pixelBox = pixelSelection[pixel];
+  pixelBox.addEventListener('click', fillColor);
+}
+
+// function that fills a pixel and changes its background color to the selected one.
+function fillColor(event) {
+  const color = selectedColor[0].style.backgroundColor;
+  event.target.style.backgroundColor = color;
 }
