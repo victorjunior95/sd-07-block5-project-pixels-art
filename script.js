6434fab6e0f6449871bbf325159307b2c84bdd19
Galaraz <jvictor.si.2013@gmail.com>
2020-09-14 15:44:19 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+
+ function createBox(className) {
+    let box = document.createElement("div");
+    box.className = className;
+    return box;
+  }
+
+  // vou estudar ainda essa trecho sujeito a modificaçoes 
+  function fillLine(divLine) {
+    for (let lineColumn = 1; lineColumn <= basePyramid; lineColumn += 1) {
+      if(lineColumn >= controlLeft && lineColumn <= controlRight) {
+        let box = createBox("box");
+        divLine.appendChild(box);
+      }
+    }
+            
