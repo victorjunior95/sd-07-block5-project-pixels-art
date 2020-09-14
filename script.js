92d3d85777935d7842d3ef1ca762e1939e3e2c87
Filipi Firmino <contatoic3web@gmail.com>
2020-09-14 18:10:55 -0300

    Pixel criado

--- script.js
+++ script.js
@@ -7,20 +7,31 @@ window.onload = function () {
 };
 
 const draw = document.getElementById('pixel-board'); 
+let size = document.querySelector('#linha');
+let sizePixel = document.querySelector('#coluna');
 
-let size = document.getElementsByName('size');
+window.onload = construtor(5, 5);
 
-  //Adcionando valor padrão
-  if (size.value == undefined) {
-   size = 5;
-  ;}
+size.addEventListener('change', function() {
+  size = size.value;
+  console.log(size)
+
+});
+sizePixel.addEventListener('change', function() {
+  sizePixel = sizePixel.value;
+  console.log(sizePixel)
+  construtor(size, sizePixel)
+})
+  
+
+function construtor (size , sizePixel){
   for (let index = 0; index < size ; index += 1) {
     //Criando Linha
     let line = document.createElement('div'); 
     line.className = 'line'
     //Inserindo linha a div container
     draw.appendChild(line);
-   for(let indexY = 0; indexY < size; indexY += 1) {
+   for(let indexY = 0; indexY < sizePixel; indexY += 1) {
     //Criando pixel
     let square = document.createElement('div');
     square.className = 'pixel';
@@ -28,4 +39,6 @@ let size = document.getElementsByName('size');
     line.appendChild(square);
    }
   }
+}
+  
 }
