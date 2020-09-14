91f97394f049d4c1664cbbd4320d74bac83a83eb
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-09-14 18:03:23 -0300

    colocado função para alterar a cor da paleta selecionada

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 let selectPixel = document.querySelectorAll('.pixel');
 
-let colorSelected = document.querySelector('.selected');
-
 function changeColor(event) {
-  event.target.style.backgroundColor = colorSelected.style.backgroundColor;
-}
+  let colorSelected = document.querySelector('.selected');
 
-// function changeColor(event) {
-//   event.target.style.backgroundColor = window.getComputedStyle(colorSelected, String).getPropertyValue("background-color");
-// }
+   event.target.className= "pixel td " +  colorSelected.classList[1];
+}
 
 for(let index = 0; index < selectPixel.length; index += 1) {
   selectPixel[index].addEventListener('click', changeColor);
 }
+
+let getColor = document.querySelectorAll('.color');
+
+function changeSelected(event){
+  console.log("estou aqui")
+  let colorSelected = document.querySelector('.selected');
+
+  colorSelected.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+for(let count = 0; count < getColor.length; count +=1) {
+  getColor[count].addEventListener('click', changeSelected)
+}
+
 
