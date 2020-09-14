761d190839d87aae0437f24bf6556250ac548db2
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-09-14 15:09:48 -0300

    testing requisition 9

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const pixelTable = document.querySelectorAll('.pixel');
 // Clear button  
 clearBtn.addEventListener('click', function () {
     for (let index = 0; index < pixelTable.length; index += 1) {
-    pixelTable[index].style.backgroundColor = 'white';
+    pixelTable[index].style.backgroundColor = 'rgb(255, 255, 255)';
 }
 });
-
