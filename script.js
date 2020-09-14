e151dbaa289bb0c6a8e058b74f8b57963a51a008
Lenodeoliveira <lenodeoliveira@gmail.com>
2020-09-14 17:46:13 -0300

    Criando tabela

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 window.onload = function() {
+  let pixelBoard = document.getElementById("pixel-board");
+
+function createTable() {
 
 
+  for (let index = 0; index < 5; index++) {
 
+      let line = document.createElement("div");
+      pixelBoard.appendChild(line).className = "line";
 
+      for (let index = 0; index < 5; index++) {
+
+          let pixel = document.createElement("div");
+          line.appendChild(pixel).className = "pixel";
+      }
+    }
+  }
+
+  createTable();
+
+  let cadaPixel = pixelBoard.querySelectorAll('.pixel');
+
+  for (let index = 0; index < cadaPixel.length; index++) {
+
+      let c = cadaPixel[index];
+      c.addEventListener('click', function(){
+
+      });
+  }
 }
-
