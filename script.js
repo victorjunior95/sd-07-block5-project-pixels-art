18f112d022a83b293bd2dfc06e8af1cd16209078
Andre Serudo <andreserudo@gmail.com>
2020-09-14 17:55:15 -0400

    criando o board dinamico

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-let palette = document.getElementsByClassName('color');
-let pixel = document.getElementsByClassName('pixel');
+let palleteColor = document.getElementsByClassName('color');
 let colorSelected = 'rgb(0, 0, 0)';
 let btnClear = document.getElementById('clear-board');
+let inputSize = document.getElementById('board-size');
+let btnCreate = document.getElementById('generate-board');
 
 const defaultColor =  'rgb(255, 255, 255)';
+const minValueBoard = 5;
+const maxValueBoard = 50;
 
-for(let i=0; i < pixel.length; i += 1){
-  pixel[i].addEventListener('click', changeColor);
+for(let i=0; i < palleteColor.length; i += 1){
+  palleteColor[i].addEventListener('click', addColor);
 }
 
-for(let i=0; i < palette.length; i += 1){
-  palette[i].addEventListener('click', addColor);
+function createPixelClickEvent(){
+  let pixel = document.getElementsByClassName('pixel');
+
+  for(let i=0; i < pixel.length; i += 1){
+    pixel[i].addEventListener('click', changeColor);
+  }
+
 }
 
 function changeColor(e){
@@ -35,6 +43,8 @@ function addColor(e){
 }
 
 function clearPixels(){
+  let pixel = document.getElementsByClassName('pixel');
+
   for(let i=0; i < pixel.length; i += 1){
     pixel[i].style.backgroundColor = defaultColor;
   }
@@ -58,8 +68,67 @@ function createPalletes(){
     palette[i].style.backgroundColor = colorArray[i];
   }
 
+  createPixelClickEvent();
+
+}
+
+function generateBoard(numberOfPixels){
+  let id = 0;
+  let boardPixel = document.getElementById('pixel-board');
+
+  for(let j = 0; j < numberOfPixels; j += 1){
+    for(let i = 0; i < numberOfPixels; i += 1){
+      let pixelElement = document.createElement('div');
+
+      pixelElement.id = `item-${id}`;
+      pixelElement.className = 'pixel';
+
+      id += 1;
+
+      boardPixel.appendChild(pixelElement);
+    }
+  }
+
+  createPixelClickEvent();
+}
+
+function clearPixelElements(){
+  let nodePixel = document.querySelectorAll('.pixel');
+
+  for (let i=0; i < nodePixel.length; i += 1){
+    nodePixel[i].remove();
+  }
+}
+
+function createBoard(){
+  let totalPixels = inputSize.value;
+  let board = document.getElementById('pixel-board');
+
+  if( totalPixels === ''){
+    alert('Board inválido!');
+    return null;
+  }
+
+  if( ( totalPixels >= 5) && ( totalPixels <= 50) ){
+    clearPixelElements();
+
+    board.style.gridTemplateColumns = `repeat(${totalPixels}, 40px)`;
+    generateBoard(totalPixels);
+  }else if(  totalPixels >= 50){
+    clearPixelElements();
+
+    board.style.gridTemplateColumns = `repeat(${maxValueBoard}, 40px)`;
+    generateBoard(maxValueBoard);
+  }else{
+    clearPixelElements();
+
+    board.style.gridTemplateColumns = `repeat(${minValueBoard}, 40px)`;
+    generateBoard(minValueBoard);
+  }
+
 }
 
 btnClear.addEventListener('click', clearPixels);
+btnCreate.addEventListener('click', createBoard);
 
 window.onload = createPalletes;
