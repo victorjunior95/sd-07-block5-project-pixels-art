fc5a3f36bb0d969b91bf1eba8362d5b795c16036
cezar88augusto <cezar88augusto@gmail.com>
2020-09-14 13:10:15 -0300

    Requisito 4: Try 2

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ arrayColors[0].style.backgroundColor = "black";
 arrayColors[1].style.backgroundColor = "red";
 arrayColors[2].style.backgroundColor = "grey";
 arrayColors[3].style.backgroundColor = "yellow";
+
