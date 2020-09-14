74d880eb34d6b39b72e383058483468d964295af
Bruno <bruno_grb@hotmail.com>
2020-09-14 17:56:38 -0300

    coloando cor aleatoriamente

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-let pixels = document.querySelectorAll('.pixel');
-let clear = document.getElementById('clear-board');
-
-pixels.forEach(function (index) {
-    index.addEventListener('click', function () {
-        index.style.backgroundColor = "rgb(0, 0, 0)";
-    });
-});
+window.onload = function () {
+    let pixels = document.querySelectorAll('.pixel');
+    let clear = document.getElementById('clear-board');
 
 
-clear.addEventListener('click', function () {
     pixels.forEach(function (index) {
-        index.style.backgroundColor = "rgb(255, 255, 255)";
-    })
-})
+        index.addEventListener('click', function () {
+            let selected = document.querySelector('.selected');
+            index.style.backgroundColor = window.getComputedStyle(selected).backgroundColor;
+        });
+    });
 
+    clear.addEventListener('click', function () {
+        pixels.forEach(function (index) {
+            index.style.backgroundColor = "rgb(255, 255, 255)";
+        })
+    })
+}
+function newSelected(newSelected) {
+    let color = document.querySelector('.selected');
+    document.querySelector('.selected').classList.remove('selected');
+    newSelected.classList.add('selected');
+    console.log(newSelected);   
+}
