7a8e09af35b113ac2fa4a6560aa8f563a95ee2ff
Strongreen <strongreen@live.com>
2020-09-14 10:19:06 -0300

    logica do grid para selecionar cores

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let limpar = document.getElementById("clear-board");
 
 let board = document.getElementsByClassName("pixel");
@@ -8,5 +9,65 @@ function limparBoard(){
     }
 }
 
-
 limpar.addEventListener('click', limparBoard);
+
+
+let paletBlack = document.getElementById('color-black');
+let paletRed = document.getElementById('color-red');
+let paletGreen = document.getElementById('color-green');
+let paletBlue = document.getElementById('color-blue');
+
+let select = 'black';
+
+function trocarCor(){
+    for(let index in board){
+        board[index].addEventListener("click", function() {
+			board[index].style.backgroundColor = select;
+            });
+    }  
+}
+
+paletRed.addEventListener('click',function(){
+    select = 'red';
+    for(let index in board){
+        board[index].addEventListener("click", function() {
+			board[index].style.backgroundColor = select;
+            });
+    }; 
+});
+
+
+function selecionaGreen(){
+    select = 'green';
+    trocarCor(select);
+};
+
+
+paletGreen.addEventListener('click',function(){
+    select = 'green';
+    for(let index in board){
+        board[index].addEventListener("click", function() {
+			board[index].style.backgroundColor = select;
+            });
+    }; 
+});
+
+paletBlue.addEventListener('click',function(){
+    select = 'blue';
+    for(let index in board){
+        board[index].addEventListener("click", function() {
+			board[index].style.backgroundColor = select;
+            });
+    }; 
+});
+
+paletBlack.addEventListener('click',function(){
+    select = 'black';
+    for(let index in board){
+        board[index].addEventListener("click", function() {
+			board[index].style.backgroundColor = select;
+            });
+    }; 
+});
+
+
