835d00855725d25222fe6a4b8ebe2032b013565f
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-14 16:03:17 -0300

    2 requisito

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+let cores = document.querySelectorAll(".color")
+cores[0].style.backgroundColor = "black"
+cores[1].style.backgroundColor = "blue"
+cores[2].style.backgroundColor = "red"
+cores[3].style.backgroundColor = "green"
