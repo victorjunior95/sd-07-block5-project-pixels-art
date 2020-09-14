286230aab844e2bc9882badcb09cbaaa8f8dc8ba
renatomak <renato.mark.silva@gmail.com>
2020-09-14 16:15:37 -0300

    mudança na função de troca de cor dos pixels

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-const BLACK = 0;
-const RED = 1;
-const BLUE = 2;
-const GREEN = 3;
+const COR = ['black', 'red', 'blue', 'green'];
+
 
 const botaoLimpar = document.getElementById('clear-board');
 const pixels = document.querySelectorAll('.pixel');
@@ -13,20 +11,20 @@ const colorGreen = document.querySelector('.colorGreen');
 
 
 const colors = document.querySelectorAll('.color');
-let colorSelected = BLACK;
+let posColor = 0;
 
 // Criar seleção de cores
 /** Passos do algoritimo: 
  * 1 - retiro qualquer ocorrencia da classe 'selected' com a função retiraSelected;
  * 2 - atribuo ao elemento clicado a classe 'selected';
- * 3 - atribuo o elemento clicado a variavel colorSelected;
+ * 3 - atribuo o elemento clicado a variavel posColor;
  */
 
 colors.forEach((element, index, array) => {
   element.addEventListener('click', event => {
     retiraSelected(array);
     element.className += ' selected';
-    colorSelected = index;
+    posColor = index;
   });
 })
 
@@ -47,15 +45,6 @@ botaoLimpar.addEventListener('click', function () {
 //Atribui cor aos pixels
 pixels.forEach((element) => {
   element.addEventListener('click', event => {
-    if(colorSelected === BLACK) {
-      element.style.backgroundColor = "black";
-    }else if(colorSelected === RED){
-      element.style.backgroundColor = "red";
-    }else if(colorSelected === BLUE){
-      element.style.backgroundColor = "blue";
-    }else{
-      element.style.backgroundColor = "green";
-    }
+      element.style.backgroundColor = COR[posColor];
   });
 });
-
