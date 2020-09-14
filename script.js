6977e96def1c987dd64a8348dadb363cf561fd4f
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-14 16:15:25 -0300

    Adicionando botao de resetar as cores

--- script.js
+++ script.js
@@ -1,58 +1,95 @@
 let mouse;
 let colorPicked = document.querySelector(".selected");
 
-let blackPixel = document.querySelector(".black");
-let redPixel = document.querySelector(".red");
-let bluePixel = document.querySelector(".blue");
-let greenPixel = document.querySelector(".green");
-
-let pixels = document.querySelectorAll(".pixel");
-
 window.onload = function () {
   mouse = window
     .getComputedStyle(colorPicked, null)
     .getPropertyValue("background-color");
 };
 
-//----------------------------------------------------------//
+//----------------------------------------------------------------------------
 
-blackPixel.addEventListener("click", function () {
-  colorPicked.classList.remove("selected");
-  blackPixel.classList.add("selected");
-  colorPicked = document.querySelector(".selected");
-  mouse = window
-    .getComputedStyle(colorPicked, null)
-    .getPropertyValue("background-color");
-});
+document.addEventListener(
+  "click",
+  function (event) {
+    if (event.target.classList.contains("black")) {
+      colorPicked.classList.remove("selected");
+      event.target.classList.add("selected");
+      colorPicked = event.target;
+      mouse = window
+        .getComputedStyle(colorPicked, null)
+        .getPropertyValue("background-color");
+    } else if (event.target.classList.contains("red")) {
+      colorPicked.classList.remove("selected");
+      event.target.classList.add("selected");
+      colorPicked = event.target;
+      mouse = window
+        .getComputedStyle(colorPicked, null)
+        .getPropertyValue("background-color");
+    } else if (event.target.classList.contains("blue")) {
+      colorPicked.classList.remove("selected");
+      event.target.classList.add("selected");
+      colorPicked = event.target;
+      mouse = window
+        .getComputedStyle(colorPicked, null)
+        .getPropertyValue("background-color");
+    } else if (event.target.classList.contains("green")) {
+      colorPicked.classList.remove("selected");
+      event.target.classList.add("selected");
+      colorPicked = event.target;
+      mouse = window
+        .getComputedStyle(colorPicked, null)
+        .getPropertyValue("background-color");
+    }
+  },
+  false
+);
 
-redPixel.addEventListener("click", function () {
-  colorPicked.classList.remove("selected");
-  redPixel.classList.add("selected");
-  colorPicked = document.querySelector(".selected");
-  mouse = window
-    .getComputedStyle(colorPicked, null)
-    .getPropertyValue("background-color");
-});
+//------------------------------------------------------------------------------
+//FAZ A MESMA COISA QUE O BLOCO ANTERIOR, MAS SEM EVENT BUBBLING
 
-bluePixel.addEventListener("click", function () {
-  colorPicked.classList.remove("selected");
-  bluePixel.classList.add("selected");
-  colorPicked = document.querySelector(".selected");
-  mouse = window
-    .getComputedStyle(colorPicked, null)
-    .getPropertyValue("background-color");
-});
+// let blackPixel = document.querySelector(".black");
+// let redPixel = document.querySelector(".red");
+// let bluePixel = document.querySelector(".blue");
+// let greenPixel = document.querySelector(".green");
 
-greenPixel.addEventListener("click", function () {
-  colorPicked.classList.remove("selected");
-  greenPixel.classList.add("selected");
-  colorPicked = document.querySelector(".selected");
-  mouse = window
-    .getComputedStyle(colorPicked, null)
-    .getPropertyValue("background-color");
-});
+// blackPixel.addEventListener("click", function () {
+//   colorPicked.classList.remove("selected");
+//   blackPixel.classList.add("selected");
+//   colorPicked = document.querySelector(".selected");
+//   mouse = window
+//     .getComputedStyle(colorPicked, null)
+//     .getPropertyValue("background-color");
+// });
 
-//---------------------------------------------------------------//
+// redPixel.addEventListener("click", function () {
+//   colorPicked.classList.remove("selected");
+//   redPixel.classList.add("selected");
+//   colorPicked = document.querySelector(".selected");
+//   mouse = window
+//     .getComputedStyle(colorPicked, null)
+//     .getPropertyValue("background-color");
+// });
+
+// bluePixel.addEventListener("click", function () {
+//   colorPicked.classList.remove("selected");
+//   bluePixel.classList.add("selected");
+//   colorPicked = document.querySelector(".selected");
+//   mouse = window
+//     .getComputedStyle(colorPicked, null)
+//     .getPropertyValue("background-color");
+// });
+
+// greenPixel.addEventListener("click", function () {
+//   colorPicked.classList.remove("selected");
+//   greenPixel.classList.add("selected");
+//   colorPicked = document.querySelector(".selected");
+//   mouse = window
+//     .getComputedStyle(colorPicked, null)
+//     .getPropertyValue("background-color");
+// });
+
+//----------------------------------------------------------------------------------
 
 document.addEventListener(
   "click",
@@ -63,3 +100,14 @@ document.addEventListener(
   },
   false
 );
+
+//---------------------------------------------------------------------------------
+
+let pixels = document.querySelectorAll(".pixel");
+let button = document.querySelector("#clear-board");
+
+button.addEventListener("click", function () {
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = "white";
+  }
+});
