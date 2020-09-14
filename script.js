e2f1b62b5029c0e3e1f71b7020db9ea72a661bc5
Raphael Caputo <faelelf2@gmail.com>
2020-09-13 21:56:54 -0300

    Tentando JS novamente

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+
+let selectedColor = document.getElementById('black-index');
+document.querySelectorAll('.color').forEach(clickConfigure);
+
+function changeClass(item) {
+    item.classList.remove('selected');
+}
+
+function clickConfigure(item) {
+    item.addEventListener("click", function () {
+        document.querySelectorAll('.color').forEach(changeClass);
+        item.classList.add('selected');
+        selectedColor = item;
+    });
+}
+
 let button = document.getElementById('clear-board');
 button.addEventListener("click", function () {
   document.querySelectorAll(".pixel").forEach(allToWhite);
