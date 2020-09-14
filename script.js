efa8754bd72a29da22dbcbf21bf22b9852007f42
Raphael Caputo <faelelf2@gmail.com>
2020-09-13 21:29:07 -0300

    Tentando botao com Onclick

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+let button = document.getElementById('clear-board');
+button.addEventListener("click", function () {
+  document.querySelectorAll(".pixel").forEach(allToWhite);
+});
+
 function allToWhite(item) {
   item.style.backgroundColor = "white";
 };
+
 
