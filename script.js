cc2785c19727032ffa3eceeb11e10c2e56aada48
cezar88augusto <cezar88augusto@gmail.com>
2020-09-14 12:28:19 -0300

    Requisito 2

---
+++ script.js
@@ -0,0 +1,6 @@
+let arrayColors = document.querySelectorAll(".color");
+
+arrayColors[0].style.backgroundColor = "black";
+arrayColors[1].style.backgroundColor = "red";
+arrayColors[2].style.backgroundColor = "grey";
+arrayColors[3].style.backgroundColor = "yellow";
