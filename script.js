7465165523ebe8a3a30bab27877e83a8384dd264
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-09-14 18:39:45 -0300

    Seguindo com construção de base e correções code climate

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const linesCols = 5;
-const divPixelBoard = document.getElementById("pixel-board");
+const divPixelBoard = document.getElementById('pixel-board');
 
 function createLines(numberOfLines) {
   for (let lines = 1; lines <= numberOfLines; lines += 1) {
-    let line = document.createElement("div");
+    const line = document.createElement('div');
     line.id = `line ${lines}`;
     line.className = 'line';
     divPixelBoard.appendChild(line);
@@ -13,9 +13,9 @@ function createLines(numberOfLines) {
 function fillLinesWithPixels(numberOfPixels) {
   for (let lines = 1; lines <= numberOfPixels; lines += 1) {
     for (let pixels = 1; pixels <= numberOfPixels; pixels += 1) {
-      let pixel = document.createElement("div");
-      pixel.className = "pixel";
-      let line = document.getElementById(`line ${pixels}`);
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      const line = document.getElementById(`line ${pixels}`);
       line.appendChild(pixel);
     }
   }
