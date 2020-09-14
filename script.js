76d0fd7ea312528ce4749b2547377205c3b90f8f
Andre Serudo <andreserudo@gmail.com>
2020-09-14 15:53:25 -0400

    adicionando evento de mudanca de cor

---
+++ script.js
@@ -0,0 +1,27 @@
+let palette = document.getElementsByClassName('color');
+let pixel = document.getElementsByClassName('pixel');
+let colorSelected = 'rgb(0, 0, 0)';
+
+//console.log(palette);
+
+for(let i=0; i < pixel.length; i += 1){
+  pixel[i].addEventListener('click', changeColor);
+}
+
+for(let i=0; i < palette.length; i += 1){
+  palette[i].addEventListener('click', addColor);
+}
+
+function changeColor(e){
+  let itemToChange = document.getElementById(e.srcElement.id);
+
+  itemToChange.style.backgroundColor = colorSelected;
+}
+
+function addColor(e){
+  let itemColor = document.getElementById(e.srcElement.id);
+  const style = window.getComputedStyle(itemColor, 'background-color');
+
+  colorSelected = style.backgroundColor;
+
+}
