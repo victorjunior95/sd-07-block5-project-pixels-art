1362371dd966d51434b481a3fc1b69e6e8ed7e57
Renan OLiveira <ren4n.dev@gmail.com>
2020-09-14 15:45:33 -0300

    Resolvendo problemas de sintaxe e identação

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-//  FUNCTIONS
-//  Função responsável por remover classe 'selected' da cor selecionada no momento
-function clearSelection() {
-  selectedDiv.classList.remove('selected');
-};
-
-//  Função responsável por adicionar classe 'selected' ao elemento selecionado
-function addSelection(target) {
-  target.classList.add('selected');
-  selectedDiv = document.querySelector('.selected');
-};
-
-//  Função Responsável por pegar a cor da div selecionada
-function getColor(selectedDiv) {
-  let property = window.getComputedStyle(selectedDiv, null).getPropertyValue('background-color');
-  return property;
-};
-
-//  Função responsável por pintar pixel
-function paint(target) {
-  target.style.backgroundColor = selectedColor;
-};
-
 //  DEFINIÇÕES
 //  Selecionando paleta
 const divColors = document.querySelector('#color-palette');
 const board = document.querySelector('#pixel-board');
 let selectedDiv = document.querySelector('.selected');
+
+//  FUNCTIONS
+//  Função responsável por remover classe 'selected' da cor selecionada no momento
+function clearSelection() {
+  selectedDiv.classList.remove('selected');
+}
+
+//  Função responsável por adicionar classe 'selected' ao elemento selecionado
+function addSelection(target) {
+  target.classList.add('selected');
+  selectedDiv = document.querySelector('.selected');
+}
+
+//  Função Responsável por pegar a cor da div selecionada
+function getColor(selectedDiv) {
+  const property = window.getComputedStyle(selectedDiv, null).getPropertyValue('background-color');
+  return property;
+}
+
+//  Função responsável por pintar pixel
+function paint(target) {
+  target.style.backgroundColor = selectedColor;
+}
+
+//  DEFINIÇÕES PÓS FUNÇÕES
 let selectedColor = getColor(selectedDiv);
 
 //  EVENTOS
@@ -40,4 +42,3 @@ divColors.addEventListener('click', function (event) {
 board.addEventListener('click', function (event) {
   paint(event.target);
 });
-
