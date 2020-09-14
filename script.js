1af03bf9954a9c9e3852f5e9949c6bd0111ff816
Cainan Coutinho <cainan6697@gmail.com>
2020-09-14 10:08:29 -0300

    Começando script

---
+++ script.js
@@ -0,0 +1,5 @@
+let colorPalette = document.querySelector('#color-palette');
+let pixelBoard = document.querySelector('#pixel-board');
+let colors = document.querySelectorAll('.color');
+let pixels = document.querySelectorAll('.color');
+
