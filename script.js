cedf03a4f9d7447571ae8764fae07662121bd5a2
Carol Andrade <a.carolg1@gmail.com>
2020-09-14 15:21:25 -0300

    codeClimate

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 // trata evento construir o tabuleiro
-const numberInputBox = document.querySelector('#board-size');
-const buttonInput = document.querySelector('#generate-board');
+
 // trata construção para 5
   let pixelBoard = document.querySelector('#pixel-board');
-  for(let lIndex = 0; lIndex < 5;lIndex +=1){
+  for(let lIndex = 0; lIndex < 5; lIndex += 1){
   let lPixelBoard = document.createElement('div');
   lPixelBoard.className ='line';
   pixelBoard.appendChild(lPixelBoard);
-  for(let pIndex = 0; pIndex < 5; pIndex +=1 ) {
+  for(let pIndex = 0; pIndex < 5; pIndex +=1) {
       let pixel = document.createElement('div');
       pixel.className = 'pixel'
       lPixelBoard.appendChild(pixel);
   }
 }
-
 //trata evento selecionar a cor
-
-
+// btnBlack = document.querySelectorAll('.color')[0];
+// btnOrange = document.querySelectorAll('.color')[1];
+// btnOrchid = document.querySelectorAll('.color')[2];
+// btnBlue = document.querySelectorAll('.color')[3];
 // Trata construção dinâmica
+// const numberInputBox = document.querySelector('#board-size');
+// const buttonInput = document.querySelector('#generate-board');
 // buttonInput.addEventListener('click', function () {
 //   const numberInput = document.querySelector('#board-size').value;
 //   let number = numberInput;
