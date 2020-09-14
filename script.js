2567926f1f731359a553389a798a412bc23166e6
jpaula <paula.jfe@gmail.com>
2020-09-14 18:22:03 -0300

    Projeto pixels art primeiros requisitos

---
+++ script.js
@@ -0,0 +1,57 @@
+let board = document.querySelectorAll(".pixel");
+let clearButton = document.querySelector("#clear-board");
+let setNumberTable = document.querySelector("#generate-board");
+let numberTable = document.querySelector("#board-size");
+let selectedColor = document.querySelector(".selected");
+let color1 = document.querySelector("#black");
+let color2 = document.querySelector("#orange");
+let color3 = document.querySelector("#pink");
+let color4 = document.querySelector("#blueviolet");
+
+/* Mudar a cor selecionada */
+color1.addEventListener("click", setSelectedColor);
+color2.addEventListener("click", setSelectedColor);
+color3.addEventListener("click", setSelectedColor);
+color4.addEventListener("click", setSelectedColor);
+
+function setSelectedColor() {
+    let setColor = event.target;
+    if (selectedColor != setColor) {
+        selectedColor.className = "color";
+        selectedColor.classList.remove("selected")
+        setColor.classList.add("selected");
+    }
+}
+
+/* Pintar quadro de pixels */
+function selectPixel () {
+    for (index = 0; index < board.length; index += 1) {
+        board[index].addEventListener('click', paintPixels);
+    }
+}
+selectPixel()
+
+function paintPixels() {
+    let setPixel = event.target;
+    let paintColor = document.querySelector('.selected').style.backgroundColor;  
+    setPixel.style.backgroundColor = paintColor;
+}
+
+for (index = 0; index < board.length; index += 1) {
+    let paintColor = board[index].addEventListener('click', paintPixels);
+    function paintPixels() {
+        let setPixel = document.querySelector('.selected').style.backgroundColor;  
+        /* let setPixel = event.target; */
+        paintColor.style.backgroundColor = window.getComputedStyle(setPixel).backgroundColor;
+        /* setPixel.style.backgroundColor = paintColor; */
+    }
+}
+
+/* 
+function setNumberPixels() {
+    if (numberTable.value < 5) {
+        numberTable = 5;
+    } else if {
+
+    }
+} */
