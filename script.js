eb2b3ab4c008e0689c796ca5c0477aa919e8bd45
TIAGO <tiagoesdrasvieira@gmail.com>
2020-09-14 18:48:05 -0300

    Requisito 8 e 9

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 let x = document.querySelectorAll('.color');
 
 for (let i = 0; i < x.length; i += 1) {
@@ -15,11 +20,18 @@ let pixel = document.querySelectorAll('.pixel');
 for (let i = 0; i < line.length; i+= 1) {
     for (let j = 0; j < pixel.length; j += 1) {
         pixel[i, j].addEventListener('click', function () {
-            let a = document.querySelector('.select');     
-            
-            pixel[i,j].style.backgroundColor = window.getComputedStyle(a).backgroundColor;            
-            
+            let a = document.querySelector('.select');
+            pixel[i,j].style.backgroundColor = window.getComputedStyle(a).backgroundColor; 
         });
         
     }
 }
+
+let clearAll = document.querySelector('#clear-board');
+clearAll.addEventListener('click', function () { 
+    for (let i = 0; i < line.length; i+= 1) {
+        for (let j = 0; j < pixel.length; j += 1) { 
+            pixel[i,j].style.backgroundColor = 'white'; 
+        }
+    }
+})
