021029777a4a90471f34c06e912d8266d05369ab
Rogerio-feitor <roger.feitor@gmail.com>
2020-09-14 17:18:55 -0300

    Acrescentando cores diferentes na paleta de cores e a colocando abaixo da tag <h1>.

---
+++ script.js
@@ -0,0 +1,5 @@
+const colorDefined = document.querySelectorAll('.color');
+colorDefined[0].style.backgroundColor = 'black';
+colorDefined[1].style.backgroundColor = 'red';
+colorDefined[2].style.backgroundColor = 'green';
+colorDefined[3].style.backgroundColor = 'blue';
