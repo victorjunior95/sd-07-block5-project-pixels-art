f2969c659b057bbbd3351693fec1a0d3e7ebf708
cezar88augusto <cezar88augusto@gmail.com>
2020-09-14 13:54:29 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let arrayColors = document.querySelectorAll(".color");
+window.onload = function () {
+  let arrayColors = document.querySelectorAll(".color");
+  let selectedColor = document.querySelectorAll(".color")[0].style.backgroundColor;
 
-arrayColors[0].style.backgroundColor = "black";
-arrayColors[1].style.backgroundColor = "red";
-arrayColors[2].style.backgroundColor = "grey";
-arrayColors[3].style.backgroundColor = "yellow";
+  arrayColors[0].style.backgroundColor = "black";
+  arrayColors[1].style.backgroundColor = "red";
+  arrayColors[2].style.backgroundColor = "grey";
+  arrayColors[3].style.backgroundColor = "yellow";
+
+}
+
 
