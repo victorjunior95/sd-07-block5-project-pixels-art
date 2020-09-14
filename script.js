0c7e15da00af929b9ceebefc18f16cb93d561c1a
Carol Andrade <a.carolg1@gmail.com>
2020-09-14 13:40:47 -0300

    quadro dinamico

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 //  cria o quadro de pixels
-let pixelBoard = document.querySelector('#pixel-board');
-
-for(let lIndex = 0; lIndex < 5;lIndex +=1){
+const numberInputBox = document.querySelector('#number-input');
+numberInputBox.addEventListener('keyup', function () {
+  const numberInput = document.querySelector('#number-input').value;
+  let number = numberInput;
+  let pixelBoard = document.querySelector('#pixel-board');
+for(let lIndex = 0; lIndex < number;lIndex +=1){
     let lPixelBoard = document.createElement('div');
     lPixelBoard.className ='line';
     pixelBoard.appendChild(lPixelBoard);
-    for(let pIndex = 0; pIndex < 5; pIndex +=1 ) {
+    for(let pIndex = 0; pIndex < number; pIndex +=1 ) {
         let pixel = document.createElement('div');
         pixel.className = 'pixel'
         lPixelBoard.appendChild(pixel);
     }
 }
+});
+
 
 
