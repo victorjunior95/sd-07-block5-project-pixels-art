1a937cc410f9bfbacde6e0b35d707b07fb981206
Fernando Antunes <fernando.antunes1@gmail.com>
2020-09-14 15:58:58 -0300

    Colocado regra para selecionar a cor

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 window.onload = function(){
     let linhaPixel=document.getElementsByClassName('linha-pixel');
-    let corSelecionada='rgb(0,0,0)'
+    let corUm=document.getElementById('cor-1');
+    let corDois=document.getElementById('cor-2');
+    let corTres=document.getElementById('cor-3');
+    let corQuatro=document.getElementById('cor-4');
 
     for (i=0; i<linhaPixel.length; i+=1){
         for (j=1; j<=5; j+=1){
@@ -10,4 +13,32 @@ window.onload = function(){
             linhaPixel[i].appendChild(criaDiv);
         }
     }
+
+    corUm.addEventListener('click',function(){
+        corUm.className='color selected';
+        corDois.className='color';
+        corTres.className='color';
+        corQuatro.className='color';
+    })
+
+    corDois.addEventListener('click',function(){
+        corUm.className='color';
+        corDois.className='color selected';
+        corTres.className='color';
+        corQuatro.className='color';
+    })
+
+    corTres.addEventListener('click',function(){
+        corUm.className='color';
+        corDois.className='color';
+        corTres.className='color selected';
+        corQuatro.className='color';
+    })
+
+    corQuatro.addEventListener('click',function(){
+        corUm.className='color';
+        corDois.className='color';
+        corTres.className='color';
+        corQuatro.className='color  selected';
+    })
 }
