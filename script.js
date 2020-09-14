52a77b02be924250671bf121f2d2d21fe54a304b
Carol Andrade <a.carolg1@gmail.com>
2020-09-14 14:50:42 -0300

    baby steps

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-//  cria o quadro de pixels
 const numberInputBox = document.querySelector('#board-size');
 const buttonInput = document.querySelector('#generate-board');
-buttonInput.addEventListener('click', function () {
-  const numberInput = document.querySelector('#board-size').value;
-  let number = numberInput;
+// trata construção para 5
   let pixelBoard = document.querySelector('#pixel-board');
-  if(number === ''){
-      alert('Board inválido!')
+  for(let lIndex = 0; lIndex < 5;lIndex +=1){
+  let lPixelBoard = document.createElement('div');
+  lPixelBoard.className ='line';
+  pixelBoard.appendChild(lPixelBoard);
+  for(let pIndex = 0; pIndex < 5; pIndex +=1 ) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel'
+      lPixelBoard.appendChild(pixel);
   }
-  else {
-for(let lIndex = 0; lIndex < number;lIndex +=1){
-    let lPixelBoard = document.createElement('div');
-    lPixelBoard.className ='line';
-    pixelBoard.appendChild(lPixelBoard);
-    for(let pIndex = 0; pIndex < number; pIndex +=1 ) {
-        let pixel = document.createElement('div');
-        pixel.className = 'pixel'
-        lPixelBoard.appendChild(pixel);
-    }
 }
-}
-});
 
 
-
+// Trata construção dinâmica
+// buttonInput.addEventListener('click', function () {
+//   const numberInput = document.querySelector('#board-size').value;
+//   let number = numberInput;
+//   let pixelBoard = document.querySelector('#pixel-board');
+//   if(number === ''){
+//     alert('Board inválido!')}
+//   else {
+//   for(let lIndex = 0; lIndex < number;lIndex +=1){
+//   let lPixelBoard = document.createElement('div');
+//   lPixelBoard.className ='line';
+//   pixelBoard.appendChild(lPixelBoard);
+//   for(let pIndex = 0; pIndex < number; pIndex +=1 ) {
+//       let pixel = document.createElement('div');
+//       pixel.className = 'pixel'
+//       lPixelBoard.appendChild(pixel);
+//   }
+// }
+// }
+// });
