64f1ada8f30fa093865802e392e1df91656dda2d
Renan OLiveira <ren4n.dev@gmail.com>
2020-09-14 15:38:50 -0300

    Resolvendo problemas de sintaxe e identação

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
+//  FUNCTIONS
+//  Função responsável por remover classe 'selected' da cor selecionada no momento
+function clearSelection() {
+  selectedDiv.classList.remove('selected');
+};
+
+//  Função responsável por adicionar classe 'selected' ao elemento selecionado
+function addSelection(target) {
+  target.classList.add('selected');
+  selectedDiv = document.querySelector('.selected');
+};
+
+//  Função Responsável por pegar a cor da div selecionada
+function getColor(selectedDiv) {
+  let property = window.getComputedStyle(selectedDiv, null).getPropertyValue('background-color');
+  return property;
+};
+
+//  Função responsável por pintar pixel
+function paint(target) {
+  target.style.backgroundColor = selectedColor;
+};
+
 //  DEFINIÇÕES
 //  Selecionando paleta
 const divColors = document.querySelector('#color-palette');
-const colors = document.querySelectorAll('.color');
 const board = document.querySelector('#pixel-board');
 let selectedDiv = document.querySelector('.selected');
 let selectedColor = getColor(selectedDiv);
 
-//EVENTOS
+//  EVENTOS
 //  Adicionando evento de clique nas cores da paleta
 divColors.addEventListener('click', function (event) {
   clearSelection();
@@ -19,25 +41,3 @@ board.addEventListener('click', function (event) {
   paint(event.target);
 });
 
-//  FUNCTIONS
-//  Função responsável por remover classe 'selected' da cor selecionada no momento
-function clearSelection () {
-  selectedDiv.classList.remove('selected');
-};
-
-//  Função responsável por adicionar classe 'selected' ao elemento selecionado
-function addSelection (target) {
-  target.classList.add('selected');
-  selectedDiv = document.querySelector('.selected');
-};
-
-//  Função Responsável por pegar a cor da div selecionada
-function getColor (selectedDiv) {
-  let property = window.getComputedStyle(selectedDiv, null).getPropertyValue('background-color');
-  return property;
-};
-
-//  Função responsável por pintar pixel
-function paint (target) {
-  target.style.backgroundColor = selectedColor;
-};
