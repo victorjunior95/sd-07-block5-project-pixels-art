59b3f78770b15c34149116ee15c81fec26c55698
Vanessa Oliveira <vanessanaara@gmail.com>
2020-09-14 20:04:40 -0300

    Criando o quadro de pixles

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//Criando as cores da paleta
 for (let i = 0; i < 4; i += 1) {
   let colorPalette = document.getElementById('color-palette');
   let colorDiv = document.createElement('div');
@@ -8,7 +8,30 @@ for (let i = 0; i < 4; i += 1) {
   let arrayOfColors = ['black', 'salmon', 'green', 'blue'];
   for (let j = 0; j < arrayOfColors.length; j += 1) {
     if (i === j) {
-        colorDiv.className = `color ${arrayOfColors[j]}`;
+      colorDiv.className = `color ${arrayOfColors[j]}`;
     }
+    
+    if (arrayOfColors[j] === 'black') {
+      colorDiv.className = `color ${arrayOfColors[j]} selected`;
+    }
+  }
+}
+
+//Criando o quadro de pixels
+window.onload = function() {
+
+    function createBox() {
+      let box = document.createElement('div');
+      box.className = 'pixel';
+      return box;
+    }
+  
+  for (let i = 0; i < 5; i += 1) {
+    let board = document.getElementById('pixel-board');
+    let lineBoardBox = document.createElement('div');
+    for (let j = 0; j < 5 ; j += 1) {
+      lineBoardBox.appendChild(createBox());
+    }
+    board.appendChild(lineBoardBox).className = 'pixel-line';
   }
 }
