be683d40a6cf2107a344d4cb616865793d4a5c4d
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-09-14 15:39:09 -0300

    requisito 7: adicionando a cor selecionada no pixel em branco

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.onload = function(){
     let colorBlue = document.getElementById("blue");
     let colorGreen = document.getElementById("green");
     let colorRed = document.getElementById("red");
-    
+    let colorSelect = "black";
     
     //Adicionando 4 eventos para selecionar cor específica
     colorBlue.addEventListener("click", function(){
@@ -13,6 +13,7 @@ window.onload = function(){
         colorBlack.classList.remove("selected");
         colorGreen.classList.remove("selected");
         colorRed.classList.remove("selected");
+        colorSelect = "blue";
     });
     colorGreen.addEventListener("click", function(){
         colorGreen.classList.add("selected");
@@ -20,6 +21,7 @@ window.onload = function(){
         colorBlack.classList.remove("selected");
         colorBlue.classList.remove("selected");
         colorRed.classList.remove("selected");
+        colorSelect = "green";
     });
     colorRed.addEventListener("click", function(){
         colorRed.classList.add("selected");
@@ -27,6 +29,7 @@ window.onload = function(){
         colorBlack.classList.remove("selected");
         colorGreen.classList.remove("selected");
         colorBlue.classList.remove("selected");
+        colorSelect = "red";
     });
     colorBlack.addEventListener("click", function(){
         colorBlack.classList.add("selected");
@@ -34,25 +37,14 @@ window.onload = function(){
         colorBlue.classList.remove("selected");
         colorGreen.classList.remove("selected");
         colorRed.classList.remove("selected");
+        colorSelect = "black"
     });
-
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //Usando estrutura de repetição para pintar os pixels
+    for(let index = 0; index <= 24; index += 1){
+        let pixelValor = document.querySelectorAll("div .pixel")[index];
+        pixelValor.addEventListener("click", function(){
+        pixelValor.style.backgroundColor = colorSelect;
+        });
+    } 
 }
