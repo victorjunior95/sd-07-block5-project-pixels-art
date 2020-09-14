05b43dec6ab374b63afefa40bed28396e6e287b3
Cainan Coutinho <cainan6697@gmail.com>
2020-09-14 10:58:46 -0300

    Tentando atender requisito 7 e 8

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const colorPalette = document.querySelector('#color-palette');
 const pixelBoard = document.querySelector('#pixel-board');
-const colors = document.querySelectorAll('.color');
-const pixels = document.querySelectorAll('.color');
-const selectedColor = 'black';
 
-colorPalette.addEventListener('color', function (event) {
+let selectedColor = 'black';
+
+colorPalette.addEventListener('click', function (event) {
   const selected = document.querySelector('.selected');
   selected.classList.remove('selected');
-  event.target.className += 'selected';
+  event.target.className += ' selected';
   selectedColor = event.target.style.backgroundColor;
+});
+
+pixelBoard.addEventListener('click', function (event) {
+  event.target.style.backgroundColor = selectedColor;
 });
