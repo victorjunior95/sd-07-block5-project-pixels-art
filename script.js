d39d084f11a63b53028744682bca2635e33e2dd7
cezar88augusto <cezar88augusto@gmail.com>
2020-09-14 17:07:27 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -6,12 +6,53 @@ window.onload = function () {
   arrayColors[2].style.backgroundColor = "grey";
   arrayColors[3].style.backgroundColor = "yellow";
 
-  //let selectedColor = arrayColors[0].style.backgroundColor;
-
-  //localStorage.setItem("color", selectedColor)
-
-
   let selectedColor = document.querySelectorAll(".color.selected")[0].style.backgroundColor;
 
-  
+  for (let index = 0; index < arrayColors.length; index += 1) {
+    if (index == 0) {
+      arrayColors[index].addEventListener('click', function () {
+        this.selectedColor = arrayColors[index].style.backgroundColor;
+        arrayColors[index].classList.add("selected")
+        for (let index = 0; index < arrayColors.length; index++) {
+          if (index != 0) {
+            arrayColors[index].classList.remove("selected");
+          }
+        }
+      })
+    } else if (index == 1) {
+      arrayColors[index].addEventListener('click', function () {
+        selectedColor = arrayColors[1].style.backgroundColor;
+        arrayColors[index].classList.add("selected")
+        for (let index = 0; index < arrayColors.length; index++) {
+          if (index != 1) {
+            arrayColors[index].classList.remove("selected");
+          }
+        }
+      })
+    } else if (index == 2) {
+      arrayColors[index].addEventListener('click', function () {
+        selectedColor = arrayColors[2].style.backgroundColor;
+        arrayColors[index].classList.add("selected");
+        for (let index = 0; index < arrayColors.length; index++) {
+          if (index != 2) {
+            arrayColors[index].classList.remove("selected");
+          }
+        }
+      })
+    } else {
+      arrayColors[index].addEventListener('click', function () {
+        selectedColor = arrayColors[3].style.backgroundColor;
+        arrayColors[index].classList.add("selected");
+        for (let index = 0; index < arrayColors.length; index++) {
+          if (index != 3) {
+            arrayColors[index].classList.remove("selected");
+          }
+        }
+      })
+    }
+  }
+
+
+
+
 }
