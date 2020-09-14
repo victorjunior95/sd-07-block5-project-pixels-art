8b5eecbf852e657a0b90f6decc5791e6468c298d
Cainan Coutinho <cainan6697@gmail.com>
2020-09-14 10:29:36 -0300

    Tentativa requisito 7, selecionar uma cor da paleta

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ let colorPalette = document.querySelector('#color-palette');
 let pixelBoard = document.querySelector('#pixel-board');
 let colors = document.querySelectorAll('.color');
 let pixels = document.querySelectorAll('.color');
+let selectedColor = 'black';
 
+colorPalette.addEventListener('color', function (event) {
+  let selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  event.target.className += 'selected';
+  selectedColor = event.target.style.backgroundColor;
+});
