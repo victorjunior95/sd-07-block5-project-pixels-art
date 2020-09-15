74cb0949872ac73b880db0db8c25a0e7a0022c61
Arnaelcio <arnaelcio.gomes@gmail.com>
2020-09-14 22:07:17 -0300

    iniciando o projeto. VAMOS COM TUDO

--- script.js
+++ script.js
@@ -23,10 +23,10 @@ function createBoard() {
     container.className = 'container';
     divPixelBoard.appendChild(container);
     for (let index2 = 0; index2 < myArray[index].length; index2 += 1) {
-        const divPixel = document.createElement('div');
-        divPixel.className = 'pixel';
-        container.appendChild(divPixel);
-      }
+      const divPixel = document.createElement('div');
+      divPixel.className = 'pixel';
+      container.appendChild(divPixel);
+    }
   }
   function currentColorToBlack() {
     currentColor = window.getComputedStyle(document.querySelector('#color1')).getPropertyValue('background-color');
@@ -63,14 +63,14 @@ function createBoard() {
 
   for (let e = 0; e < classPixel.length; e += 1) {
     classPixel[e].addEventListener('click', function () {
-    classPixel[e].style.background = currentColor;
-  });
+      classPixel[e].style.background = currentColor;
+    });
   }
 
   function clearBoard() {
     for (let e = 0; e < classPixel.length; e += 1) {
-    classPixel[e].style.background = 'white';
-  }
+      classPixel[e].style.background = 'white';
+    }
   }
   clear.addEventListener('click', clearBoard);
 
@@ -81,59 +81,58 @@ function createBoard() {
 }
 createBoard();
 
-  function randomColorOne() {
-    let randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
-    let r = randomBetween(0, 255);
-    let g = randomBetween(0, 255);
-    let b = randomBetween(0, 255);
-    let rgb = `rgb(${r},${g},${b})`;
-    colorOne.style.background = rgb
+function randomColorOne() {
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * ((max - min) + 1));
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);
+    const rgb = `rgb(${r},${g},${b})`;
+    colorOne.style.background = rgb;
 }
-randomColorOne()
+randomColorOne();
 
-  function randomColorTwo() {
-    let randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
-    let r = randomBetween(0, 255);
-    let g = randomBetween(0, 255);
-    let b = randomBetween(0, 255);
-    let rgb = `rgb(${r},${g},${b})`;
-    colorTwo.style.background = rgb
-}
-randomColorTwo()
+function randomColorTwo() {
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * ((max - min) + 1));
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);
+    const rgb = `rgb(${r},${g},${b})`;
+    colorTwo.style.background = rgb;
+  }
+randomColorTwo();
 
-  function randomColorThree() {
-    let randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
-    let r = randomBetween(0, 255);
-    let g = randomBetween(0, 255);
-    let b = randomBetween(0, 255);
-    let rgb = `rgb(${r},${g},${b})`;
+function randomColorThree() {
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * ((max - min) + 1));
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);
+    const rgb = `rgb(${r},${g},${b})`;
     colorThree.style.background = rgb;
 }
-randomColorThree()
+randomColorThree();
 
 
-  function ArrayNew(){
-    myArray = []
-    for (let index = 0; index < boardSize.value ; index += 1){
-        myArray.push([])
-        for (let index2 = 0; index2 < boardSize.value; index2 += 1){
-            myArray[index].push(3)
-            }
-            }
-boardSize.value = ''
-currentColor = window.getComputedStyle(document.querySelector('#color1')).getPropertyValue('background-color')
-createBoard()
-}
+function ArrayNew() {
+    myArray = [];
+    for (let index = 0; index < boardSize.value; index += 1) {
+      myArray.push([]);
+      for (let index2 = 0; index2 < boardSize.value; index2 += 1) {
+          myArray[index].push(3);
+        }
+    }
+    boardSize.value = '';
+    currentColor = window.getComputedStyle(document.querySelector('#color1')).getPropertyValue('background-color')
+    createBoard();
+  }
 
-  function checkBoardSize(){
-    if (boardSize.value === ''){
-        boardSize.value = 5
-        alert('Board inválido!')
-        }else if (boardSize.value < 5){
-            boardSize.value = 5
-        }else if (boardSize.value > 50){
-            boardSize.value = 50
-
+function checkBoardSize() {
+    if (boardSize.value === '') {
+      boardSize.value = 5
+      alert('Board inválido!');
+        } else if (boardSize.value < 5) {
+          boardSize.value = 5;
+        } else if (boardSize.value > 50) {
+          boardSize.value = 50;
         }
 return boardSize.value
 }
