7d7a59673c0a2eb7e3fc26b37f3dacf6e50db657
Rogerio-feitor <roger.feitor@gmail.com>
2020-09-15 19:05:59 -0300

    Implementando o botão gerador de pixels

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const colorDefined = document.getElementsByClassName('color');
 const cleanTheBoard = document.querySelector('#clear-board');
+const generateBoard = document.getElementById('generate-board');
+const pixelBoard = document.getElementById('pixel-board');
 let pixelElements = document.getElementsByClassName('pixel');
 let firstMainColor = document.getElementsByClassName('selected');
+let boardSize = document.getElementById('board-size').value;
+let numberOfColumns = 5;
+let numberOfLines = 5;
+let pixelWidth = 40;
+let pixelHeight = 40;
 
-function randomNumber(number) {
-  return Math.round(Math.random() * number);
+function randomNumber() {
+  return Math.round(Math.random() * 255);
 }
 
 for (let index = 0; index < Object.keys(colorDefined).length; index += 1) {
@@ -14,6 +21,7 @@ for (let index = 0; index < Object.keys(colorDefined).length; index += 1) {
     firstMainColor = document.getElementsByClassName('selected')
   });
 };
+function listenerToPixels() {
 pixelElements = document.getElementsByClassName('pixel');
 for (let index = 0; index < Object.keys(pixelElements).length; index += 1) {
   pixelElements[index].addEventListener('click', function () {
@@ -21,16 +29,62 @@ for (let index = 0; index < Object.keys(pixelElements).length; index += 1) {
     event.target.style.backgroundColor = firstMainColor[0].style.backgroundColor;
   });
 }
+}
+
+function test() {
+  boardSize = document.getElementById('board-size').value;
+  if (boardSize === '') {
+    alert('Board inválido!');
+  } else {
+    scaleBoardSizeOnRange();
+  }
+}
+function scaleBoardSizeOnRange() {
+  if (boardSize >= 5 && boardSize <= 50) {
+    assignSizeForGenerateBoard();
+  } else if (boardSize < 5) {
+      boardSize = 5;
+      assignSizeForGenerateBoard();
+    } else {
+      boardSize = 50;
+      assignSizeForGenerateBoard();
+    }
+  }
+
+function assignSizeForGenerateBoard() {
+  numberOfColumns = boardSize;
+  numberOfLines = boardSize;
+  boardCreator();
+  listenerToPixels();
+  pixelBoard.style.width = numberOfColumns * pixelWidth;
+  pixelBoard.style.width += 'px';
+}
+function boardCreator() {
+  pixelBoard.innerHTML = '';
+  for (let line = 0; line < numberOfLines; line += 1) {
+    let elementCreator = document.createElement('section');
+    elementCreator.className = 'line';
+    pixelBoard.appendChild(elementCreator);
+    for (let column = 0; column < numberOfColumns; column += 1) {
+      elementCreator = document.createElement('div');
+      elementCreator.className = 'pixel';
+      document.getElementsByClassName('line')[line].appendChild(elementCreator);
+    }
+  }
+}
+colorDefined[0].style.backgroundColor = 'black'
+colorDefined[1].style.backgroundColor = `rgb(${randomNumber()} , ${randomNumber()} , ${randomNumber()})`;
+colorDefined[2].style.backgroundColor = `rgb(${randomNumber()} , ${randomNumber()} , ${randomNumber()})`;
+colorDefined[3].style.backgroundColor = `rgb(${randomNumber()} , ${randomNumber()} , ${randomNumber()})`;
+listenerToPixels();
 cleanTheBoard.addEventListener('click', function ()  {
   for (let index = 0; index < Object.keys(pixelElements).length; index += 1) {
     pixelElements[index].style.backgroundColor = 'white';
   }
 });
+generateBoard.addEventListener('click', test);
 
-colorDefined[0].style.backgroundColor = 'black'
-colorDefined[1].style.backgroundColor = `rgb(${randomNumber(255)} , ${randomNumber(255)} , ${randomNumber(255)})`;
-colorDefined[2].style.backgroundColor = `rgb(${randomNumber(255)} , ${randomNumber(255)} , ${randomNumber(255)})`;
-colorDefined[3].style.backgroundColor = `rgb(${randomNumber(255)} , ${randomNumber(255)} , ${randomNumber(255)})`;
+
 
 
 
