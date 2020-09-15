b3c178082de193cf975d526d3ef7bd78cc796919
Rodrigo <rd64103@gmail.com>
2020-09-15 18:28:07 -0300

    colocando cada pixel

---
+++ script.js
@@ -0,0 +1,16 @@
+const tabelaDePixel = document.getElementById('pixel-board')
+let pixel = [];
+let linha = [];
+let numeroDeLinhas = 5
+let contaPixel = 0
+for (let i = 0; i < numeroDeLinhas; i += 1) {
+    linha[i] = document.createElement('div');
+    linha[i].className = 'linha';
+    tabelaDePixel.appendChild(linha[i])
+    for (let j = 0; j < 5; j += 1) {
+        pixel[contaPixel] = document.createElement('div');
+        pixel[contaPixel].className = 'coluna'
+        linha[i].appendChild(pixel[contaPixel]);
+        contaPixel += 1;
+    }
+}
