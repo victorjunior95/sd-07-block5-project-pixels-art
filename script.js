e748fe79ffe2b91212d4ba365c997b506e226b50
vini-lima <vini20.henrique@gmail.com>
2020-09-15 15:26:16 -0300

    Estilizando o Projeto

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const colors = document.querySelectorAll('.color');
 const grid = document.querySelectorAll('.pixel');
-let colorSelected = document.querySelector('.selected');
+let colorSelected = document.querySelector('.selected').style.backgroundColor;
 
-colors.addEventListener('click', function (event) {
-  if ( event.target) {
-    grid.style.backgroundColor = colors;  
-  }
-}, false);
+
+// let corAtual = "black"
+// function pintar(){ event.target.style.backgroundColor = corAtual}
+// colors.addEventListener('click', function (event) {
+//   if (event.target.className === 'selected') {
+//   }
+// }, false);
