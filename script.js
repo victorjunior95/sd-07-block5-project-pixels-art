ee721c9bec40caa756d582db9a734267ed4df0a3
alexandrefaustino <alexandrefauldade@gmail.com>
2020-09-15 07:43:13 -0300

    commit: 0.0.4

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-window.onload = function() {
+window.onload = function () {
   const numberOfLine = 5;
   const numberOfColumn = 5;
 
   for (let line = 1; line <= numberOfLine; line += 1) {
-    let divLine = document.createElement('div');
+    const divLine = document.createElement('div');
     divLine.className = 'tr';
     document.getElementById('pixel-board').appendChild(divLine);
 
-    for (let column = 1; column <= numberOfColumn; column += 1 ) {
-      let divColumn = document.createElement('div')
+    for (let column = 1; column <= numberOfColumn; column += 1) {
+      const divColumn = document.createElement('div');
       divColumn.className = 'pixel td';
       divLine.appendChild(divColumn);
     }
   }
-}
+};
