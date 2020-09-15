cc521c632b5de816535b2fb39af757b04dba2ae4
Marcos R. Silva <marcos.1998226@gmail.com>
2020-09-15 12:29:35 -0300

    teste para selected class

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let color = document.querySelectorAll('.color');
 let selected = document.querySelectorAll('.pixel');
 let clear = document.querySelector('#clear-board');
+let div = document.querySelector('#black');
 
 clearAll();
 setClass();
@@ -13,7 +14,6 @@ function clearAll() {
     });
 }
 function setClass(){
-    let div = document.querySelector('#black');
     div.classList.add('selected');
     console.log(div);
 }
