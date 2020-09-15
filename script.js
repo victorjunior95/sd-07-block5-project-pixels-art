372a36c9853e4a6f1b4435073bf98c7a0a1312c9
luc-zago <luc.zago@hotmail.com>
2020-09-15 01:20:15 -0300

    Coloadas as cores da paleta de cores lado à lado

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ window.onload = function() {
   // Muda a cor do pixel selecionado
   function changeColor() {
     const pixel = event.target;
-    for (let checker = 0; checker < document.getElementsByClassName('color').length; checker + = 1) {
-      for (let searcher = 0; searcher < document.getElementsByClassName('color')[checker].classList.length; searcher + = 1) {
+    for (let checker = 0; checker < document.getElementsByClassName('color').length; checker += 1) {
+      for (let searcher = 0; searcher < document.getElementsByClassName('color')[checker].classList.length; searcher += 1) {
         if ((document.getElementsByClassName('color')[checker].classList[searcher]) === 'selected') {
           pixel.classList.add(document.getElementsByClassName('color')[checker].classList[1]);
         }
@@ -15,11 +15,11 @@ window.onload = function() {
   // Cria o quadro de pixels
   function createBoard() {
     const boardHeight = 5;
-    for (let boardColumn = 0; boardColumn < boardHeight; boardColumn + = 1) {
+    for (let boardColumn = 0; boardColumn < boardHeight; boardColumn += 1) {
       const baseLine = document.createElement('div');
       baseLine.className = 'line';
       document.getElementById('pixel-board').appendChild(baseLine);
-      for (let index = 0; index < boardHeight; index + = 1) {
+      for (let index = 0; index < boardHeight; index += 1) {
         const pixel = document.createElement('div');
         pixel.className = 'pixel';
         pixel.addEventListener('click', changeColor);
@@ -31,14 +31,14 @@ window.onload = function() {
   createBoard();
 
   function selectedColor() {
-    for (let checker = 0; checker < document.getElementsByClassName('color').length; checker + = 1) {
+    for (let checker = 0; checker < document.getElementsByClassName('color').length; checker += 1) {
       document.getElementsByClassName('color')[checker].classList.remove('selected');
     }
     const brush = event.target;
     brush.classList.add('selected');
   }
 
-  for (let index = 0; index < document.getElementsByClassName('color').length; index + = 1) {
+  for (let index = 0; index < document.getElementsByClassName('color').length; index += 1) {
     document.getElementsByClassName('color')[index].addEventListener('click', selectedColor);
   }
 }
