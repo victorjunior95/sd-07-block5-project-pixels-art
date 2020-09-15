8f66abdd59a9c651a83f7bb7106b9f441640d4e7
Israel Menon <zezomenon@gmail.com>
2020-09-15 15:40:24 -0300

    Requisito 4 e 5, criando tabela de pixels em branco

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+  let boxPixelBoard = document.getElementById('pixel-board');
+  let onePixelBox = document.querySelector('.pixel');
+  
+  // for (let index = 0; index < 5; index += 1) {
+  //   let box = document.createElement('div');
+  //   for (let jindex = 0; jindex < 5; jindex += 1) {
+  //     onePixelBox.appendChild(box).className = 'box-pixel';
+  //   }
+  // }
+}
