eae29a075202ed74eb8fd6f58ea0d90aa3193e2a
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-09-15 20:16:14 -0300

    deleting comments

--- script.js
+++ script.js
@@ -1,50 +1,5 @@
 /* Select a color and add/remove 'selected' class from others elements. The 'selected' 
 class staying only on clicked element */
-// const blackColor = document.querySelector('.black');
-// const yellowColor = document.querySelector('.yellow');
-// const blueColor = document.querySelector('.blue');
-// const greenColor = document.querySelector('.green');
-
-// greenColor.addEventListener('click', function () {
-//   blackColor.className = 'black color';
-//   yellowColor.className = 'yellow color';
-//   blueColor.className = 'blue color';
-//   greenColor.className = 'green color selected';
-// });
-
-// blueColor.addEventListener('click', function () {
-//   blackColor.className = 'black color';
-//   yellowColor.className = 'yellow color';
-//   greenColor.className = 'green color';
-//   blueColor.className = 'blue color selected';
-// });
-
-// yellowColor.addEventListener('click', function () {
-//   blackColor.className = 'black color';
-//   blueColor.className = 'blue color';
-//   greenColor.className = 'green color';
-//   yellowColor.className = 'yellow color selected';
-// });
-
-// blackColor.addEventListener('click', function () {
-//   yellowColor.className = 'yellow color';
-//   blueColor.className = 'blue color';
-//   greenColor.className = 'green color';
-//   blackColor.className = 'black color selected';
-// });
-
-// // Clear button 
-// const clearBtn = document.querySelector('#clear-board');
-// const pixelTable = document.querySelectorAll('.pixel');
-
-// // Clear button  
-// clearBtn.addEventListener('click', function () {
-//   for (let index = 0; index < pixelTable.length; index += 1) {
-//     pixelTable[index].style.backgroundColor = 'white';
-//   }
-// });
-
-
 let color = document.querySelectorAll('.color');
 
 for (let index = 0; index < color.length; index += 1) {
@@ -54,3 +9,13 @@ for (let index = 0; index < color.length; index += 1) {
     event.target.className += ' selected';
   })
 }
+
+// // Clear button 
+// const clearBtn = document.querySelector('#clear-board');
+// const pixelTable = document.querySelectorAll('.pixel');
+ 
+// clearBtn.addEventListener('click', function () {
+//   for (let index = 0; index < pixelTable.length; index += 1) {
+//     pixelTable[index].style.backgroundColor = 'white';
+//   }
+// });
