a525355114cd1afc455766dabddcbfbc0089d781
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-09-15 18:22:08 -0300

    inicio de inserindo pixel-board com todos os pixel vai JS

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 let pixelBoard = document.getElementbyId("pixel-board");
+let numberLines = 5;
+let pixel = [];
+let linha = [];
+
+
+
+for (let i = 0; i < numberLines; i++) {
+    linha[i] = document.createElement("div");
+    linha[i].className = "linha";
+    pixelBoard.appendChild(linha[i]);
+
+    for (let j = 0; j < numberLines; j += 1) {
+
+    }
+};
