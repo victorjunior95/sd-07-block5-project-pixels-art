2dca935185a53465e4b2775bc92740680064d88c
Vanessa Oliveira <vanessanaara@gmail.com>
2020-09-15 20:37:47 -0300

    Corrigindo a função que reseta a cor branca dos pixels

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+// Seleciona a cor para pintar o pixel
+
+let color = 'black';
+function selectedColor(event) {
+  const divSelected = document.querySelector('.selected');
+  color = window.getComputedStyle(event.target).getPropertyValue('background-color');
+  divSelected.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+// Pinta o pixel
+
+function paintPixel(event) {
+  event.target.style.backgroundColor = color;
+}
+
 // Cria as cores da paleta
 for (let i = 0; i < 4; i += 1) {
   const colorPalette = document.getElementById('color-palette');
   const colorDiv = document.createElement('div');
   colorPalette.appendChild(colorDiv);
-  
 
   const arrayOfColors = ['black', 'salmon', 'green', 'blue'];
   for (let j = 0; j < arrayOfColors.length; j += 1) {
     if (i === j) {
       colorDiv.className = `color ${arrayOfColors[j]}`;
-    } 
+    }
   }
 
   if (colorDiv.classList.contains('black')) {
@@ -17,10 +32,10 @@ for (let i = 0; i < 4; i += 1) {
   }
 
   colorDiv.addEventListener('click', selectedColor);
-};
+}
 
 // Cria o quadro de pixels
-window.onload = function () {
+window.onload = function() {
 
   function createBox() {
     const box = document.createElement('div');
@@ -38,26 +53,12 @@ window.onload = function () {
   }
 };
 
-// Seleciona a cor para pintar o pixel
-
-let color = 'black';
-function selectedColor(event) {
-  let divSelected = document.querySelector('.selected');
-  color = window.getComputedStyle(event.target).getPropertyValue('background-color');
-  divSelected.classList.remove('selected');
-  event.target.classList.add('selected');
-}
-
-// Pinta o pixel
-
-function paintPixel(event) {
-  event.target.style.backgroundColor = color;
-}
-
 // Reseta a cor branca dos pixels
 function turnWhite() {
   const paintedPixel = document.querySelectorAll('.pixel');
-  paintedPixel.style.backgroundColor = 'white';
+  for (let i = 0; i < paintedPixel.length; i += 1) {
+    paintedPixel[i].style.backgroundColor = 'white';
+  }
 }
 const clearButton = document.getElementById('clear-board');
 clearButton.addEventListener('click', turnWhite);
