600207a79a739a2d27f8f1421438fc081efc49b6
Camila Valentim <camilaifpb@hotmail.com>
2020-09-15 16:21:38 -0300

    clear board

---
+++ script.js
@@ -0,0 +1,11 @@
+let paleta = document.getElementById('color-palette');
+
+let button = documente.getElementById('clear-board');
+button.addEventListener('click', clear);
+
+function limpar() {
+  let pixels = documente.querySelectorAll('.pixel');
+  for(let index = 0; index < pixels.length; index += 1){
+    pixels[index].style.backgroundColor = 'white';
+  }
+}
