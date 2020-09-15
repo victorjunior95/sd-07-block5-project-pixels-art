3c6ba20be3c9f3cfb1092710f64b988321a5923a
Leonardo <lecastro16@gmail.com>
2020-09-15 17:48:35 -0300

    Iniciando o Projeoto. VAMOS COM TUDO: rocket: Leonardo Sardinha v4

--- script.js
+++
@@ -1,8 +0,0 @@
-let pixel = document.getElementsByClassName("pixel");
-
-document.onload = (function () {
-  for (let indice=0; indice<=25; indice +=1){
-    pixel[indice].style.background-color = red;
-  }
-    
-});
