6509b27c8b94d1e29e0aa2af8e94eadfc9ba07ec
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-09-15 20:32:35 -0300

    atualização até requisito 7

--- script.js
+++ script.js
@@ -1,48 +1,38 @@
-window.onload = function (){
+/* window.onload = function (){
     let colorBlack = document.querySelector(".black");
     let color = window.getComputedStyle(colorBlack).backgroundColor;
   //  console.log(color);
-}
+}*/
 
-let black = document.querySelector(".black");
+let numberLines = 5;
+let pixelBoard = document.querySelector("#pixel-board");
 
-console.log(black)
-console.log(document.getElementsByClassName("black"))
-let green = document.querySelector(".green");
-let pink = document.querySelector(".pink");
-let yellow = document.querySelector(".yellow");
-
-let pixels = document.querySelector(".pixel");
-let palette = document.querySelectorAll(".color");
-console.log(palette)
-palette.addEventListener("click", function(){
-})
-
-
-
-
-
-// armazenar a cor selecionada em uma função ou variável
-// deletar a classe selected da cor atual 
-// adicionar a classe selected na cor selecionada
-
-   //for para criar a tabela dinamicamente - finalizar apenas quando o restante estiver ok 
-    /*let pixelBoard = document.querySelector("#pixel-board");
-    let colunasELinhas = 10;
-    let lines = []
-    for(index = 0; index < colunasELinhas; index += 1){
-        for(index = 0; index < colunasELinhas; index += 1){
-            let pixel = document.createElement("div");
-            pixel.className = "pixel";
-            lines.push(pixel)
-        };
-        console.log(lines)
-        pixelBoard.appendChild(lines)
-        // lines é um array - fazer outro for passando por cada elemento para add ele no appendChild
+let lines = [];
+let pixel = [];
+let k = 0;
+for(index = 0; index < numberLines; index += 1){
+    lines[index] = document.createElement("div");
+    lines[index].className = "linha";
+    pixelBoard.appendChild(lines[index]);
+    
+    for(let j = 0; j < numberLines; j += 1){
+        pixel[k] = document.createElement("div");
+        pixel[k].className = "pixel";
+        lines[index].appendChild(pixel[k]);
+        k = k + 1;   
     }
 }
-console.log("teste");
+let body = document.querySelector("body");
+console.log(body)
+let color = document.querySelectorAll(".color");
 
-function selectedColor(){
-
-}*/ 
+for(let index = 0; index < color.length; index += 1){
+    color[index].addEventListener("click", function(){
+        let selected = document.querySelector(".selected");
+        console.log(selected)
+        selected.classList.remove("selected");
+        // poderia ser passado como parâmetro da função um "event" e na linha abaixo: "event.target.className += " selected"""
+        color[index].classList.add("selected");
+        console.log(color[index]);          
+    })
+}
