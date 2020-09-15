ecaf0e14f30a3aef4fc5a07fadf2b00bde28e372
murilopereira006 <muriloedu006@gmail.com>
2020-09-15 19:07:58 -0300

    preciso dar um jeito de n ter que especificar a cor

---
+++ script.js
@@ -0,0 +1,58 @@
+var black = document.querySelector(".black");
+var green = document.querySelector(".green");
+var red = document.querySelector(".red");
+var yellow = document.querySelector(".yellow");
+var colors = document.getElementsByClassName("color");
+
+function selectorB () {
+    //encontrar elemento com 'selected'
+    for (let i = 0; i < colors.length; i += 1) {
+        colors[i].classList.remove("selected");
+        console.log(colors[i])
+
+        if(colors[i] === black){
+            colors[i].classList.add("selected");
+        }
+    }
+}
+
+function selectorG () {
+    //encontrar elemento com 'selected'
+    for (let i = 0; i < colors.length; i += 1) {
+        colors[i].classList.remove("selected");
+        console.log(colors[i])
+
+        if(colors[i] === green){
+            colors[i].classList.add("selected");
+        }
+    }
+}
+
+function selectorR () {
+    //encontrar elemento com 'selected'
+    for (let i = 0; i < colors.length; i += 1) {
+        colors[i].classList.remove("selected");
+        console.log(colors[i])
+
+        if(colors[i] === red){
+            colors[i].classList.add("selected");
+        }
+    }
+}
+
+function selectorY () {
+    //encontrar elemento com 'selected'
+    for (let i = 0; i < colors.length; i += 1) {
+        colors[i].classList.remove("selected");
+        console.log(colors[i])
+
+        if(colors[i] === yellow){
+            colors[i].classList.add("selected");
+        }
+    }
+}
+
+black.addEventListener('click', selectorB);
+green.addEventListener('click', selectorG);
+red.addEventListener('click', selectorR);
+yellow.addEventListener('click', selectorY);
