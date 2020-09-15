9ea33e12a8d4cfb6f4ed2b9e59c25383cd4cbb4f
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-09-15 01:12:03 -0300

    code climate bactério do caralho

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-
-let cores = document.querySelectorAll(".color");
-let selectedColor = document.querySelector(".selected");
+const cores = document.querySelectorAll('.color');
+const selectedColor = document.querySelector('.selected');
 let background = getComputedStyle(selectedColor).backgroundColor;// porque tem que separar em dois?
-for (let i=0; i<cores.length; i+= 1) {
-    cores[i].addEventListener("click",pegaCor);
-    function pegaCor() {
-        let selectedColor = document.querySelector(".selected");
-        selectedColor.classList.remove("selected");
-        cores[i].classList.add("selected");
-        selectedColor = document.querySelector(".selected");
-        background = getComputedStyle(selectedColor).backgroundColor;
-    }
+for (let i=0; i<cores.length; i += 1) {
+cores[i].addEventListener('click', pegaCor);
+function pegaCor() {
+    selectedColor = document.querySelector('.selected');
+    selectedColor.classList.remove('selected');
+    cores[i].classList.add('selected');
+    selectedColor = document.querySelector('.selected');
+    background = getComputedStyle(selectedColor).backgroundColor;
+}
 }
 
-let caixas = document.querySelectorAll(".pixel");
+const caixas = document.querySelectorAll('.pixel');
 
-for (let i=0; i<caixas.length; i+= 1) {
-    caixas[i].addEventListener("click",trocaCor);
-    function trocaCor() {
-        caixas[i].style.backgroundColor = background;
-    }
+for (let i=0; i<caixas.length; i += 1) {
+    caixas[i].addEventListener('click', trocaCor);
+function trocaCor() {
+    caixas[i].style.backgroundColor = background;// don't make function with a loop, mas como eu faço pra ele ver o i?
+}
 }
 
 function cleanBoard() {
-    for (let i=0; i<caixas.length; i+= 1) {
-        caixas[i].style.backgroundColor = document.querySelector("#pixel-board").style.backgroundColor;
-        //porque aqui não teve que separar?
+    for (let i=0; i<caixas.length; i += 1) {
+        caixas[i].style.backgroundColor = document.querySelector('#pixel-board').style.backgroundColor;
+        // porque aqui não teve que separar?
         // porque ele não aceita: caixas[i].style.backgroundColor = white;?
     }
 }
