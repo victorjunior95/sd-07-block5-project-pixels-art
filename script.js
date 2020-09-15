9f5ec81fc710c857187fcf24e3b1997b8876e6fc
Filipi Firmino <contatoic3web@gmail.com>
2020-09-15 16:51:48 -0300

    Board e color prontos

--- script.js
+++ script.js
@@ -1,37 +1,20 @@
+// All consts
 const colorPalet = document.getElementById('color-palette');
 const draw = document.getElementById('pixel-board');
 const btnCreate = document.querySelector('#generate-board');
-const hexaDecimal = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+const hexaDecimal = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+const clear = document.querySelector('#clear-bord');
 
-window.onload = function () {
+// Created pallet color
+function createdPaleta () {
   for (let indice = 0; indice < 4 ; indice += 1) {
     let square = document.createElement('div');
     square.className = 'color';
     colorPalet.appendChild(square);
   };
-
-  if(draw.childElementCount == 0 ){
-    construtor(5, 5);
-  }
-
-function construtor (size , sizePixel){
-  for (let index = 0; index < size ; index += 1) {
-    //Criando Linha
-    let line = document.createElement('div');
-    line.className = 'line'
-    //Inserindo linha a div container
-    draw.appendChild(line);
-  for(let indexY = 0; indexY < sizePixel; indexY += 1) {
-    //Criando pixel
-    let square = document.createElement('div');
-    square.className = 'pixel';
-    //Adcionando pixel a linha
-    line.appendChild(square);
-  }
-  }
 }
-colorPalet.children[0].style.backgroundColor = 'black';
 
+// Color random
 function colorRandom(){
   let color = "#";
   for (let index = 0; index < 6; index += 1) {
@@ -40,25 +23,32 @@ function colorRandom(){
   return color;
 }
 
-for (let cont = 0; cont < colorPalet.children.length; cont +=1){
-  console.log(colorPalet.children[cont])
-  if(cont == 0){
-    colorPalet.children[cont].style.backgroundColor = 'black';
-  }else{
-    colorPalet.children[cont].style.backgroundColor = colorRandom();
+function boardConstructor (size , sizePixel){
+  for (let index = 0; index < size ; index += 1) {
+    // Criando Linha
+    let line = document.createElement('div');
+    line.className = 'line'
+    // Inserindo linha a div container
+    draw.appendChild(line);
+  for(let indexY = 0; indexY < sizePixel; indexY += 1) {
+    // Criando pixel
+    let square = document.createElement('div');
+    square.className = 'pixel';
+    // Adcionando pixel a linha
+    line.appendChild(square);
+  }
   }
 }
 
+// Remove 
+function remove() {
+  let filho = draw.childNodes;
+  for (let indice = filho.length - 1; indice >= 0; indice -= 1){
+  draw.removeChild(filho[indice]);
+  }
 }
 
-btnCreate.addEventListener('click', function () {
-  let line = document.querySelector('#board-size');
-  let colun = document.querySelector('#coluna');
-  line = line.value
-  colun = colun.value
-  reload(line, colun)
-})
-
+//Reload Board
 function reload (line, colun) {
   if(line == 0 || colun == 0){
     alert("Board inválido!")
@@ -67,28 +57,40 @@ function reload (line, colun) {
       colun = 5;
       line = 5;
     }
-
     if(line > 50 && colun > 50 || line > 50 || colun > 50) {
       line = 50;
+      colun = 50;
     }
     remove()
-  for (let index = 0; index < line ; index += 1) {
-    let line = document.createElement('div'); 
-    line.className = 'line'
-    draw.appendChild(line);
-  for(let indexY = 0; indexY < colun; indexY += 1){
-    let square = document.createElement('div');
-    square.className = 'pixel';
-    line.appendChild(square);
-  }
-  }
+    boardConstructor(line, colun)
   }
   
 }
 
-function remove() {
-  let filho = draw.childNodes;
-  for (let indice = filho.length - 1; indice >= 0; indice -= 1){
-  draw.removeChild(filho[indice]);
+createdPaleta();
+boardConstructor(5,5);
+
+// Condition
+for (let cont = 0; cont < colorPalet.children.length; cont +=1){
+    if(cont == 0){
+    colorPalet.children[cont].style.backgroundColor = 'black';
+  }else{
+    colorPalet.children[cont].style.backgroundColor = colorRandom();
   }
 }
+
+// Events
+btnCreate.addEventListener('click', function () {
+  let line = document.querySelector('#board-size');
+  let colun = document.querySelector('#coluna');
+  line = line.value
+  colun = colun.value
+  reload(line, colun)
+})
+
+clear.addEventListener('click', function (){
+   const pixel = document.querySelectorAll('.pixel');
+   for (let indexPixel = 0; indexPixel < pixel.length; indexPixel += 1){
+      pixel[indexPixel].style.backgroundColor = 'white';
+   }
+});
