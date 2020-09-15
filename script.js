0ae36743470b40daae1c7dc5284e23645ff3341c
Victor Junior <victorjr.muchacho@gmail.com>
2020-09-15 17:28:46 -0400

    Cumprido ate o Requisito 7

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// documents
+let numberLines = 5; // para dar como alternativa editar o numero de linhas nos requisitos bonus
+let pixelBoard = document.getElementById("pixel-board");
+
+let pixel = [];
+let linha = [];
+
+let k = 0; // pra ir do 0 até o 24
+for (let index = 0; index < numberLines; index += 1) {
+    linha[index] = document.createElement("div"); // document.createElement("div").className = "linha"
+    linha[index].className = "linha"; // ^ forma alternativa
+    pixelBoard.appendChild(linha[index]);
+
+    for (let j = 0; j < numberLines; j += 1) {
+        //
+        pixel[k] = document.createElement("div");
+        pixel[k].className = "pixel";
+        linha[i].appendChild(pixel[k]);
+        k = k + 1;
+    }
+}
+console.log(linha);
+
