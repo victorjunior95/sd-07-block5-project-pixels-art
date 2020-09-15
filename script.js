b2813ed97c0c28e56f9abe041bc99ef03e744c6a
Marcos R. Silva <marcos.1998226@gmail.com>
2020-09-14 23:31:59 -0300

    refluxo

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let blackColor = document.querySelector('#container').childNodes[3].children[0];
 let print = document.querySelector('#pixel-board').childNodes[1];
 
 
-if(blackColor.getAttribute('id') === 'black'){
-    blackColor.className = "selected";
-    print.style.backgroundColor = "black";
-}
+// if (blackColor.getAttribute('id') === 'black') {
+//     blackColor.className = "selected";
+//     // print.style.backgroundColor = "black";
+// }
 
-console.log(blackColor);
+//console.log(blackColor);
