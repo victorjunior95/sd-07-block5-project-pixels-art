04be6f9ace59c3ca94bc2fb1756ce00f8d2e0b79
Marcos R. Silva <marcos.1998226@gmail.com>
2020-09-15 17:00:07 -0300

    codigo para setar a classe: 'selected

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let clear = document.querySelector('#clear-board');
 let div = document.querySelector('#black');
 
 clearAll();
-setClass();
 
 function clearAll() {
     clear.addEventListener('click', function () {
@@ -14,21 +13,21 @@ function clearAll() {
     });
 }
 
-    function setClass() {
-        div.classList.add('selected'); // aqui a palleta preta ja inicia com a "selected"
-    }
+    // function setClass() {
+    //     div.classList.add('selected'); // aqui a palleta preta ja inicia com a "selected"
+    // }
 
-    for (let index = 0; index < color.length; index += 1) { // efetuou uma busca pra ver se o elemento esta sem a classe "selected"
-        color[index].addEventListener('click', function () {
-            if (color[index].className !== 'selected') { //se não tiver insere
-                color[index].classList.add('selected');
-            } else {
-                for (let index = 0; index < color.length; index += 1) { // se tiver remove
-                    if (color[index].className === 'selected') {
-                        color[index].classList.remove('selected');
-                    }
-                }
+
+color.forEach(item => { // para cada
+    item.addEventListener('click', function (event) {
+        event.target.classList.add('selected');
+        color.forEach(item => {
+            if (item !== event.target) {
+                item.classList.remove('selected');
             }
         });
-    }
+    });
+});
+
+
 
