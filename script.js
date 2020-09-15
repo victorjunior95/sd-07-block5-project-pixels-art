bbb702d2675218038304f0f682071cb0de16e1b0
Anderson Santana <anderslash@gmail.com>
2020-09-14 21:11:27 -0300

    Refazendo a parte para trocar de cor

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-let pixel = document.getElementsByClassName("pixel")
-for (let index in pixel) {
-    let selectedColor = document.getElementsByClassName("selected")[0]
-    pixel[index].addEventListener("click", function () {
-        pixel[index].style.backgroundColor = getComputedStyle(selectedColor).backgroundColor
-    })
+let pixels = document.getElementsByClassName("pixel");
+
+// Looping para adicionar Event Listener em todos os pixels
+for (let index in pixels) {
+    pixels[index].addEventListener("click", paintPixel);
 }
+
+// Função para pintar o pixel
+function paintPixel() {
+    let selectedColor = document.getElementsByClassName("selected")[0];
+    this.style.backgroundColor = getComputedStyle(selectedColor).backgroundColor;
+}
+
+// Trocar de cor
+// let seletorDeCores = document.getElementsByClassName("color");
+// for (let index in seletorDeCores) {
+//     seletorDeCores[index].addEventListener("click", function () {
+//         console.log("Cor selecionada");
+//     })
+// }
