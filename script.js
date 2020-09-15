976741568af020201122793c92e6107d800b24a8
Lucas Miranda <lucasmirandadev@gmail.com>
2020-09-15 19:27:46 -0300

    fazendo o item numero  07

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const numberLines = 5;
 const pixelBoard = document.getElementById("pixel-board");
+const color = document.querySelectorAll(".color");
 
 const pixel = []; 
 const linha = [];
@@ -7,17 +8,30 @@ const linha = [];
 let k = 0;
 for (let i = 0; i < numberLines; i += 1) {
     linha[i] = document.createElement("div");
-    linha[i].className = "linha"
+    linha[i].className = "linha";
     pixelBoard.appendChild(linha[i]);
 
 
     for(let j = 0; j < numberLines; j += 1) {
     pixel[k] = document.createElement("div");
-    pixel[k].className = "pixel"
-    linha[i].appendChild(pixel[k])
+    pixel[k].className = "pixel";
+    linha[i].appendChild(pixel[k]);
     k = k + 1;
     }
  
 }
 
-console.log(pixel);
+
+
+
+for (let i = 0; i < color.length; i += 1) {
+color[i].addEventListener("click", function() {
+    
+
+    let selected = document.querySelector(".selected");
+    selected.classList.remove("selected");
+    color[i].classList.add('selected');
+   
+    
+    })
+}
