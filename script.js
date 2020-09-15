42fe81426684c9ee43099179eead94b99a4d5229
Rogerio-feitor <roger.feitor@gmail.com>
2020-09-15 10:45:54 -0300

    Implementando as cores aleatórias quando carregada a página

--- script.js
+++ script.js
@@ -3,37 +3,34 @@ const cleanTheBoard = document.querySelector('#clear-board');
 let pixelElements = document.getElementsByClassName('pixel');
 let firstMainColor = document.getElementsByClassName('selected');
 
-function ClickPalette() {
-  for (let index = 0; index < Object.keys(colorDefined).length; index += 1) {
-    colorDefined[index].addEventListener('click', function () { 
-      firstMainColor[0].classList.remove('selected');
-      event.target.classList.add('selected');
-      firstMainColor = document.getElementsByClassName('selected')
-    });
-  };
+function randomNumber(number) {
+  return Math.round(Math.random() * number);
 }
 
-function ClickPixels() {
-  pixelElements = document.getElementsByClassName('pixel');
-  for (let index = 0; index < Object.keys(pixelElements).length; index += 1) {
-    pixelElements[index].addEventListener('click', function () {
-      firstMainColor = document.getElementsByClassName('selected');
-      event.target.style.backgroundColor = firstMainColor[0].style.backgroundColor;
-    });
-  }
+for (let index = 0; index < Object.keys(colorDefined).length; index += 1) {
+  colorDefined[index].addEventListener('click', function () { 
+    firstMainColor[0].classList.remove('selected');
+    event.target.classList.add('selected');
+    firstMainColor = document.getElementsByClassName('selected')
+  });
+};
+pixelElements = document.getElementsByClassName('pixel');
+for (let index = 0; index < Object.keys(pixelElements).length; index += 1) {
+  pixelElements[index].addEventListener('click', function () {
+    firstMainColor = document.getElementsByClassName('selected');
+    event.target.style.backgroundColor = firstMainColor[0].style.backgroundColor;
+  });
 }
-colorDefined[0].style.backgroundColor = 'black';
-colorDefined[1].style.backgroundColor = 'red';
-colorDefined[2].style.backgroundColor = 'green';
-colorDefined[3].style.backgroundColor = 'blue';
-ClickPalette();
-ClickPixels();
-
 cleanTheBoard.addEventListener('click', function ()  {
   for (let index = 0; index < Object.keys(pixelElements).length; index += 1) {
     pixelElements[index].style.backgroundColor = 'white';
   }
 });
+
+colorDefined[0].style.backgroundColor = 'black'
+colorDefined[1].style.backgroundColor = `rgb(${randomNumber(255)} , ${randomNumber(255)} , ${randomNumber(255)})`;
+colorDefined[2].style.backgroundColor = `rgb(${randomNumber(255)} , ${randomNumber(255)} , ${randomNumber(255)})`;
+colorDefined[3].style.backgroundColor = `rgb(${randomNumber(255)} , ${randomNumber(255)} , ${randomNumber(255)})`;
 
 
 
