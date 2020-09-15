8542080a0b788e364bf71c6d7b9d52bf25a72e20
Francisco <franciscoliratorres@gmail.com>
2020-09-14 22:10:30 -0300

    Resolvendo pendências parte 8

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ window.onload = function () {
     let green = document.getElementById("color-green");
     let pourple = document.getElementById("color-pourple");
     let orange = document.getElementById("color-orange");
-    let pixel = document.getElementsByClassName("pixel")
-    let choosenColor = black;
+    let pixel1 = document.getElementsByClassName("pixel")
+    let choosenColor = "color-black";
 
     green.addEventListener('click', function () {
         green.classList.add("slected");
         black.classList.remove("slected");
         pourple.classList.remove("slected");
         orange.classList.remove("slected");
-        let choosenColor = green;
+        let choosenColor = "color-green";
 
     })
     pourple.addEventListener('click', function () {
@@ -19,24 +19,24 @@ window.onload = function () {
         black.classList.remove("slected");
         green.classList.remove("slected");
         orange.classList.remove("slected");
-        let choosenColor = pourple;
+        let choosenColor = "color-pourple";
     })
     orange.addEventListener('click', function () {
         orange.classList.add("slected");
         black.classList.remove("slected");
         green.classList.remove("slected");
         pourple.classList.remove("slected");
-        let choosenColor = orange
+        let choosenColor = "color-orange"
     })
     black.addEventListener('click', function () {
         black.classList.add("slected");
         orange.classList.remove("slected");
         green.classList.remove("slected");
         pourple.classList.remove("slected");
-        let choosenColor = black;
+        let choosenColor = "color-black";
     })
-    for(let index = 0; index <= 24; index += 1){
-        let pixelValor = document.querySelectorAll("div .pixel")[index];
+    for(let index = 0; pixel1.length <= 24; index += 1){
+        let pixelValor = document.getElementsByClassName("pixel")[index];
         pixelValor.addEventListener("click", function(){
         pixelValor.style.backgroundColor = choosenColor;
         })
