11668a46022e57fb1729168f221025b1ae5e23ee
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-09-14 22:52:16 -0300

    mudança de cor

--- script.js
+++ script.js
@@ -11,13 +11,23 @@ for (counter = 0; counter < amountOfSquares;counter +=1){
     board.innerHTML = board.innerHTML + '<br>'
 }
 
+let paletteColors = document.getElementsByClassName('color');
 
-
-document.addEventListener('click', function (event) {
-    if ( event.target.classList.contains( 'color' ) ) {
+document.addEventListener('click', function(event){
+    if(event.target.classList.contains('color')){
         let selectColor = document.querySelector('.selected');
         selectColor.classList.remove('selected');
-        event.target.classList.add('selected');   
+        event.target.classList.add('selected')
     }
 })
+
+let pixelBoard = document.getElementsByClassName('pixel')
+for (let i = 0; i < pixelBoard.length; i++) {
+    pixelBoard[i].addEventListener('click', function (event) {
+    let colors = document.getElementsByClassName('selected')[0];
+    let pixelColor = event.target;
+    pixelColor.style.backgroundColor = getComputedStyle(colors).backgroundColor
+  });
+}
+
 
