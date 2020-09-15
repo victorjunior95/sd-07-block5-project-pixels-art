972e24240a2d93a8d47a7489373a6aaabf9ea745
Iury Nathan <iurynathan@gmail.com>
2020-09-15 15:15:26 -0300

    corrigindo requisito bonus 1 e adicionando requisito bonus 2

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let paint = document.getElementsByClassName("selected")[0];
 let background = window.getComputedStyle(paint).backgroundColor;
 let selectedColor = document.getElementById("color-palette");
 let paintPixel = document.getElementById("pixel-board");
-let resetPixel = document.getElementById("clear-board")
-let buttonVqv = document.getElementById("generate-board")
-let sizeBoard = document.getElementById("board-size")
+let resetPixel = document.getElementById("clear-board");
+let buttonVqv = document.getElementById("generate-board");
+let sizeBoard = document.getElementById("board-size");
 
 selectedColor.addEventListener("click", function (event) {
-  console.log(paintPixel)
+  console.log(paintPixel);
   if (
     event.target.classList.contains("selected") == false &&
     event.target.classList.contains("color") == true
@@ -25,32 +25,38 @@ paintPixel.addEventListener("click", function (event) {
   }
 });
 
-resetPixel.addEventListener("click", function() {
-  let reset = document.getElementsByClassName("pixel")
+resetPixel.addEventListener("click", function () {
+  let reset = document.getElementsByClassName("pixel");
   for (let index = 0; index < reset.length; index += 1) {
-    reset[index].style.backgroundColor = "white"
+    reset[index].style.backgroundColor = "white";
   }
-})
+});
 
-buttonVqv.addEventListener("click", function() {
-  // paintPixel.remove(this.children)
-  // console.log(paintPixel)
-  // paintPixel = document.createElement("table")
-  // paintPixel.setAttribute("id", "pixel-board")
-  // document.querySelector("section").appendChild(paintPixel)
-  // paintPixel = document.getElementById("pixel-board");
-  while (document.querySelector('#pixel-board').firstChild) {
-    document.querySelector('#pixel-board').removeChild(document.querySelector('#pixel-board').firstChild);
+buttonVqv.addEventListener("click", function () {
+  // console.log(sizeBoard.value)
+  if (sizeBoard.value == "") {
+    alert("Board inválido");
+  } else if (sizeBoard.value <= 5) {
+    sizeBoard.value = 5;
+  } else if (sizeBoard.value >= 50) {
+    sizeBoard.value = 50;
+  }
+  console.log(sizeBoard.value)
+
+  while (document.querySelector("#pixel-board").firstChild) {
+    document
+      .querySelector("#pixel-board")
+      .removeChild(document.querySelector("#pixel-board").firstChild);
   }
   for (let index = 0; index < sizeBoard.value; index += 1) {
-    let createTable = document.createElement("tr")
-    document.getElementById("pixel-board").appendChild(createTable)
-    for (let indexPixel = 0; indexPixel < sizeBoard.value; indexPixel +=1) {
-      let createPixel = document.createElement("td")
-      createPixel.className = "pixel"
-      createTable.appendChild(createPixel)
+    let createTable = document.createElement("tr");
+    document.getElementById("pixel-board").appendChild(createTable);
+    for (let indexPixel = 0; indexPixel < sizeBoard.value; indexPixel += 1) {
+      let createPixel = document.createElement("td");
+      createPixel.className = "pixel";
+      createTable.appendChild(createPixel);
     }
   }
   // paintPixel = document.getElementById("pixel-board");
-  console.log(paintPixel)
+  console.log(paintPixel);
 });
