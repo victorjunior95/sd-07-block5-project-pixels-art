59e2cf2cb56b748e0a6eaa82daf7ac50720882f3
hvenses <hvenses@gmail.com>
2020-09-15 20:52:16 -0300

    Alterando a tabela por div craida pelo javaScript.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let initialColor = document.querySelector("#blackColor");
+/*let initialColor = document.querySelector("#blackColor");
 let red = document.getElementById("redColor");
 let blue = document.getElementById("blueColor");
 let green = document.getElementById("greenColor");
@@ -14,4 +14,36 @@ function inicio() {
       board[i].style.backgroundColor = "black";
     });
   }
+}
+*/
+let numLinhas = 5;
+let pixelBoard = document.getElementById("pixel-board");
+
+let pixel = [];
+let linha = [];
+
+let k = 0;
+for (let i = 0; i < numLinhas; i += 1) {
+  linha[i] = document.createElement("div");
+  linha[i].className = "linha";
+  pixelBoard.appendChild(linha[i]);
+
+  for (let j = 0; j < numLinhas; j += 1) {
+    pixel[k] = document.createElement("div");
+    pixel[k].className = "pixel";
+    linha[i].appendChild(pixel[k]);
+    k = k + 1;
+  }
+}
+
+let selectedColor = document.querySelectorAll(".color");
+
+for (let i = 0; i < selectedColor.length; i += 1) {
+  selectedColor[i].addEventListener("click", function () {
+    let selected = document.querySelector(".selected");
+    console.log(selected);
+    selected.classList.remove("selected");
+
+    selectedColor[i].classList.add("selected");
+  });
 }
