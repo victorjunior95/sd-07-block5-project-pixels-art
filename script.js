98d7cf4d6e645074055a8ca28af5bc42c099738c
LAURO DE SA <itrafor@gmail.com>
2020-09-15 12:45:44 -0300

    alterações 7-150920

--- script.js
+++ script.js
@@ -1,87 +1,95 @@
 window.onload = function() {
-  const pixelBoard = document.getElementById("pixel-board"); 
+  //const pixelBoard = document.getElementById("pixel-board"); 
   const qtMatrix = 5;
   const widthPixels = 42;
-  createLine(); 
+  createBoard(qtMatrix); 
 
   const btBlack = document.getElementById("black");
   const btRed = document.getElementById("red");
   const btBlue = document.getElementById("blue");
   const btGreen = document.getElementById("green");
   const btClear = document.getElementById("clear-board");
-  const selectedClasse = document.getElementsByClassName("color black selected");
-  selectedClasse.className="color black"; 
-  /*xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx*/  
-  function createLine (){
-    pixelBoard.style.width = qtMatrix*widthPixels+"px";    
-       for(let pixels = 0; pixels < qtMatrix*5; pixels+=1){
-            let pixel = document.createElement("div");
-            pixel.className="pixel";
-            pixel.addEventListener('click', changeBackground);
-            pixelBoard.appendChild(pixel);
-       }    
-  }
- /*xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx*/  
-  function changeBackground(){
-     this.className=selectedClasse.className;
-     console.log(this.className); 
+ 
+  /*colorindo*/
+    let colorPalette = document.querySelectorAll('.color')
+    let colors = ['black', 'red', 'green', 'blue']
+
+  for (let index = 0; index < colors.length; index += 1) {
+    colorPalette[index].style.backgroundColor = colors[index]
+  } 
+  /*xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx*/ 
+  function createBoard(qtMatrix) {
+    const pixelBoard = document.querySelector('#pixel-board');
+    pixelBoard.innerHTML = '';
+    const matrixSize = qtMatrix ;
+    for (let column = 0; column < matrixSize; column += 1) {
+      // Creating row for the pixels
+      const pixelsRow = document.createElement('div');
+      pixelsRow.className = 'pixels-row';
+      for (let row = 0; row < matrixSize; row += 1) {
+        // Creating the pixel
+        const pixel = document.createElement('div');
+        pixel.className = 'pixel';
   
-  }  
-/*xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx*/  
-  btBlack.addEventListener('click', function(){
-    let parent = this.parentNode; // pega o elemento pai     
-        for(let index = 0; index < parent.children.length; index +=1 ){
-            parent.children[index].classList.remove('selected');           
-        }
-        selectedClasse.className = this.className;
-        this.classList.add('selected');
-        return selectedClasse; // retorna a classe com a cor selecionada
-  });
-/*---------------------------------------------------------------------------*/
-  btRed.addEventListener('click', function(){
-    let parent = this.parentNode; // pega o elemento pai     
-    for(let index = 0; index < parent.children.length; index +=1 ){
-      parent.children[index].classList.remove('selected');  // remove a classe 'selected' de todos os elementos         
+        // Event for fill pixel with selected color
+          pixel.addEventListener('click', function (event) {         
+          const selected = document.querySelector('.selected');          
+          event.target.style.backgroundColor = selected.style.backgroundColor;
+         
+        });
+        pixelsRow.appendChild(pixel);
+      }
+      pixelBoard.appendChild(pixelsRow);
     }
-    selectedClasse.className = this.className;// muda a classe do pixels clicado para a mesma classe da seleção de cor
-    this.classList.add('selected');// adiciona a classe selected ao seletor de cor
-    return selectedClasse; // retorna a classe com a cor selecionada  
+  }
+
+/*xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx*/  
+
+btBlack.addEventListener('click', function(){
+    let selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    this.classList.add('selected');              
+   
+      
+  });
+/*--------------------------------------------------------------------------*/
+  btRed.addEventListener('click', function(){
+    let selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    this.classList.add('selected');              
+  
+    
         
   });
-  /*---------------------------------------------------------------------------*/
+  /*-----------------------------------------------------------------------*/
   
-  btBlue.addEventListener('click', function(){
-    let parent = this.parentNode; // pega o elemento pai     
-    for(let index = 0; index < parent.children.length; index +=1 ){
-      parent.children[index].classList.remove('selected');  // remove a classe 'selected' de todos os elementos         
-    }
-    selectedClasse.className = this.className;// muda a classe do pixels clicado para a mesma classe da seleção de cor
-    this.classList.add('selected');// adiciona a classe selected ao seletor de cor
-    return selectedClasse; // retorna a classe com a cor selecionada  
+  btBlue.addEventListener('click', function(e){
+   let selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    this.classList.add('selected');              
+   
         
   });
 
-  /*---------------------------------------------------------------------------*/
+  /*-----------------------------------------------------------------------*/
   
-  btGreen.addEventListener('click', function(){
-    let parent = this.parentNode; // pega o elemento pai     
-    for(let index = 0; index < parent.children.length; index +=1 ){
-      parent.children[index].classList.remove('selected');  // remove a classe 'selected' de todos os elementos         
-    }
-    selectedClasse.className = this.className;// muda a classe do pixels clicado para a mesma classe da seleção de cor
-    this.classList.add('selected');// adiciona a classe selected ao seletor de cor
-    return selectedClasse; // retorna a classe com a cor selecionada  
+  btGreen.addEventListener('click', function(e){
+    let selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    this.classList.add('selected');              
+    
         
   });
 
   /*---------------------------------------------------------------------------*/
   
   btClear.addEventListener('click', function(){
-    
-    let parent = pixelBoard; // pega o elemento pai         
-    for(let index = 0; index < parent.children.length; index +=1 ){
-      parent.children[index].classList.remove();  // remove as classes de todos os elementos         
-      parent.children[index].classList.add('white');
+   
+    const pixels = document.querySelectorAll('.pixel');
+   
+    for (let filledPixel = 0; filledPixel < pixels.length; filledPixel += 1) {
+        
+       pixels[filledPixel].style.backgroundColor="white";
     }    
        
   });
