7f9df9bbb619762f2450b7812d235c4b42e4af7a
Eduardo Pizorno <pizorno@gmail.com>
2020-09-15 13:49:43 -0300

    Pixel Art 0.0.8 - Botão limpar background-color ajustado

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function () {
   document.getElementById("clear-board").addEventListener("click",function () {
     const elements = document.getElementsByClassName("pixel");
     for (let key = 0; key < elements.length; key += 1) {
-      elements[key].style.backgroundColor = "rgb(255, 0, 0)";
+      elements[key].style.backgroundColor = "rgb(255, 255, 255)";
     }
   });
 }
