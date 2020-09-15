09e6a475694c84b3e39ce6a6f66d7c2483c402d0
Lucas Andrade <lafdesouza2002@outlook.com>
2020-09-15 15:37:34 -0300

    Corrigindo erro

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let colors = document.querySelectorAll(".color")
 let boardSize = document.querySelector("#board-size").value
 let generateBoard = document.querySelector("#generate-board")
 let options = ["red", "blue", "green", "aliceblue", "brown", "purple", "gray", "violet", "pink", "yellow", "coral", "orange", "gold", "darkkhaki", "peachpuff", "springgreen", "mediumspringgreen", "cyan", "teal", "slateblue", "indigo", "deeppink", "seagreen", "seashell", "darkslategray", "sandybrown", "chocolate", "peru", "wheat", "blueviolet", "magenta"]
+let randoms = []
+let number = 0
 
 palette.addEventListener("click", (event)=>{
     let selected = document.querySelector(".selected")
@@ -13,7 +15,12 @@ palette.addEventListener("click", (event)=>{
 })
 
 for (let i = 1; i < colors.length; i = i + 1){
-    colors[i].style.backgroundColor = options[Math.floor(Math.random() * colors.length) + 1]
+    number = Math.floor(Math.random() * colors.length) + 1
+    while (randoms.indexOf(number) != -1){
+        number = Math.floor(Math.random() * colors.length) + 1 
+    }
+    randoms.push(number)
+    colors[i].style.backgroundColor = options[number]
 }
 
 for (let i = 0; i < fields.length; i += 1){
