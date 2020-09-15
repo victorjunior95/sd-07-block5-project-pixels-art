0cdfc09611cf22d66b2bb43f2087b3b6da82a653
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-09-15 18:56:25 -0300

    JS faz a 'tabela'

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-let pixelBoard = document.getElementbyId("pixel-board");
 let numberLines = 5;
+let pixelBoard = document.getElementById("pixel-board");
+
 let pixel = [];
 let linha = [];
 
-
-
-for (let i = 0; i < numberLines; i++) {
+let k = 0
+for (let i = 0; i < numberLines; i += 1) {
     linha[i] = document.createElement("div");
-    linha[i].className = "linha";
+    linha[i].className = "linha"
     pixelBoard.appendChild(linha[i]);
 
     for (let j = 0; j < numberLines; j += 1) {
-
+        pixel[k] = document.createElement("div")
+        pixel[k].className = "pixel"
+        linha[i].appendChild(pixel[k])
+        k = k + 1;
     }
-};
+
+}
