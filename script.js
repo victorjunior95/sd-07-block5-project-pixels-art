bc0889e71c9aab41b68e3bc96cb1739d04e7b0bf
LAURO DE SA <itrafor@gmail.com>
2020-09-15 09:59:38 -0300

    alterações 3-150920

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 window.onload = function() {
   const pixelBoard = document.getElementById("pixel-board"); 
   const qtMatrix = 5;
-  createLine();
-   
- 
+  const widthPixels = 40;
+  createLine(); 
+
   const btBlack = document.getElementById("black");
   const btRed = document.getElementById("red");
   const btBlue = document.getElementById("blue");
   const btGreen = document.getElementById("green");
   const selectedClasse = document.getElementsByClassName("color black selected");
+  selectedClasse.className="color black"; 
   /*xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx*/  
- 
   function createLine (){
-    pixelBoard.style.width = qtMatrix*40+"px";    
+    pixelBoard.style.width = qtMatrix*widthPixels+"px";    
        for(let pixels = 0; pixels < qtMatrix*5; pixels+=1){
             let pixel = document.createElement("button");
             pixel.className="pixel";
             pixel.addEventListener('click', changeBackground);
             pixelBoard.appendChild(pixel);
-       }
+       }    
   }
  /*xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx*/  
   function changeBackground(){
-    this.className=selectedClasse.className;
-    console.log(this.className);
-  }
+     this.className=selectedClasse.className;
+     console.log(this.className); 
   
+  }  
 /*xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx*/  
   btBlack.addEventListener('click', function(){
     let parent = this.parentNode; // pega o elemento pai     
@@ -34,6 +34,7 @@ window.onload = function() {
         }
         selectedClasse.className = this.className;
         this.classList.add('selected');
+        return selectedClasse; // retorna a classe com a cor selecionada
   });
 /*---------------------------------------------------------------------------*/
   btRed.addEventListener('click', function(){
