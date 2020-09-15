fbab8f84b3cb0a991499628b9f2c4dea50e78aad
Galaraz <jvictor.si.2013@gmail.com>
2020-09-15 17:45:24 -0300

    codigo da piramid copiada para entender logica

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
-window.onload= function(){
-    let tamanhoLinha=5;
-    let lines = document.querySelectorAll('.pixel');
+window.onload = function() {
+    let base = 5;
+    let numberOfLines = (base + 1) / 2; // 5
+    let controlLeft = numberOfLines; // 5
+    let controlRight = numberOfLines; // 5
+    let lines = document.querySelectorAll(".pixel");
+  
+      
+    fill(lines);
+    
    
- function createBox(className) {
-    let box = document.createElement('div');
-    box.className = className;
-    return box;
+    
+  
+    // Passa por todos as linhas (div com class line) e preenche o triangulo
+    function fill(lines) {
+      for(let index = 0; index < lines.length; index += 1) {
+        fillLine(lines[index]);
+        controlRight += 1;
+        controlLeft -= 1;
+      }
+    }
+  
+    // Cria uma caixa com base nas diferentes classes
+    function createBox(className) {
+      let box = document.createElement("div");
+      box.className = className;
+      return box;
+    }
+  
+    // Preenche uma linha
+    function fillLine(divLine) {
+      for (let lineColumn = 1; lineColumn <= base; lineColumn += 1) {
+        if(lineColumn >= controlLeft && lineColumn <= controlRight) {
+          let box = createBox("box");
+          divLine.appendChild(box);
+        }
+        else{
+          let box = createBox("box");
+          divLine.appendChild(box);
+
+        }
+      }
+    }
   }
 
-  // vou estudar ainda essa trecho sujeito a modificaçoes 
-  function criarlinhas(lines) {
-    for (let lineColumn = 1; lineColumn <= tamanhoLinha; lineColumn += 1) {
-      //mais uma condiçao para a proximas de baixo
-      for (let index = 0; index < lines.length; index++) {
-          
-        let box = createBox("box");
-        lines.appendChild(box);
-      }
-      }  
-     
-    }
-}       
-
-function colorir(){
+  function colorir(){
 
     console.log('coloriu')
    // document.getElementById();
 
 
 }
-
---
+++ script1.js
@@ -0,0 +1,27 @@
+window.onload= function(){
+    let tamanhoLinha=5;
+    let pixelBoard = document.querySelectorAll('pixel-board');
+
+     
+  const pixel = [];
+  const linha =[];
+
+
+ 
+
+  for (let i = 0; i < tamanhoLinha; i+= 1) {
+    linha[i] = document.createElement('div');
+    linha[i].className ='linha';
+    console.log(linha[i]);
+    
+  }
+
+
+function colorir(){
+
+    console.log('coloriu')
+   // document.getElementById();
+
+}
+
+}
