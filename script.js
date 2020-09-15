92976e514824cba1f4e1909857e37ab0d14442db
Iury Nathan <iurynathan@gmail.com>
2020-09-15 14:48:58 -0300

    adicionado a opção que quse me matou de adicionar tabelas pelo usuário

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ let background = window.getComputedStyle(paint).backgroundColor;
 let selectedColor = document.getElementById("color-palette");
 let paintPixel = document.getElementById("pixel-board");
 let resetPixel = document.getElementById("clear-board")
+let buttonVqv = document.getElementById("generate-board")
+let sizeBoard = document.getElementById("board-size")
 
 selectedColor.addEventListener("click", function (event) {
+  console.log(paintPixel)
   if (
     event.target.classList.contains("selected") == false &&
     event.target.classList.contains("color") == true
@@ -28,3 +31,26 @@ resetPixel.addEventListener("click", function() {
     reset[index].style.backgroundColor = "white"
   }
 })
+
+buttonVqv.addEventListener("click", function() {
+  // paintPixel.remove(this.children)
+  // console.log(paintPixel)
+  // paintPixel = document.createElement("table")
+  // paintPixel.setAttribute("id", "pixel-board")
+  // document.querySelector("section").appendChild(paintPixel)
+  // paintPixel = document.getElementById("pixel-board");
+  while (document.querySelector('#pixel-board').firstChild) {
+    document.querySelector('#pixel-board').removeChild(document.querySelector('#pixel-board').firstChild);
+  }
+  for (let index = 0; index < sizeBoard.value; index += 1) {
+    let createTable = document.createElement("tr")
+    document.getElementById("pixel-board").appendChild(createTable)
+    for (let indexPixel = 0; indexPixel < sizeBoard.value; indexPixel +=1) {
+      let createPixel = document.createElement("td")
+      createPixel.className = "pixel"
+      createTable.appendChild(createPixel)
+    }
+  }
+  // paintPixel = document.getElementById("pixel-board");
+  console.log(paintPixel)
+});
