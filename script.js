742edf2a70418c20e1a8f4fa5f33307739a2cf16
berchon <berchon@gmail.com>
2020-09-16 02:54:12 -0300

    Bônus

--- script.js
+++ script.js
@@ -1,27 +1,67 @@
-let pixelBoard = document.getElementById('pixel-board');
-let boardSize = 5 // document.getElementById("board-size").value;
-
-let boardLines = boardSize;
-let divLines = [];
-let pixel = [];
-for (let i = 0; i < boardSize; i += 1) {
-  divLines[i] = document.createElement("div");
-  divLines[i].className = "boardLines"
-  pixelBoard.appendChild(divLines[i]);
-
-  for (let  j = 0; j < boardSize; j += 1) {
-    pixel[i * boardSize + j] = document.createElement("div");
-    pixel[i * boardSize + j].className = "pixel"
-    divLines[i].appendChild(pixel[i * boardSize + j]);
-  }
+function rndRGB() {
+  return (Math.floor(parseInt(Math.random() * 255)));
 }
 
+function randRGB() {
+  return (`rgb(${rndRGB()} , ${rndRGB()} , ${rndRGB()})`);
+}
+
+function pixelGenerator() {
+  // console.log(pixelBoard.childElementCount)
+  // console.log(pixelBoard.innerHTML)
+  // console.log(pixelBoard.children[0])
+  // for (let i; i < pixelBoard.childElementCount; i += 1) {
+  //   pixelBoard.removeChild(pixelBoard.children[0])
+  // }
+
+  pixelBoard.innerHTML = ''
+  for (let i = 0; i < boardSize.value; i += 1) {
+    divLines[i] = document.createElement("div");
+    divLines[i].className = "boardLines"
+
+    pixelBoard.appendChild(divLines[i]);
+
+    for (let  j = 0; j < boardSize.value; j += 1) {
+      pixel[i * boardSize.value + j] = document.createElement("div");
+      pixel[i * boardSize.value + j].className = "pixel"
+      divLines[i].appendChild(pixel[i * boardSize.value + j]);
+    }
+  }
+  console.log(pixelBoard.children[0])
+}
+
+let pixelBoard = document.getElementById('pixel-board');
+let boardSize = document.getElementById("board-size");
+
+let boardLines = boardSize.value;
+let divLines = [];
+let pixel = [];
+
+pixelGenerator();
+// for (let i = 0; i < boardSize.value; i += 1) {
+//   divLines[i] = document.createElement("div");
+//   divLines[i].className = "boardLines"
+//   pixelBoard.appendChild(divLines[i]);
+
+//   for (let  j = 0; j < boardSize.value; j += 1) {
+//     pixel[i * boardSize.value + j] = document.createElement("div");
+//     pixel[i * boardSize.value + j].className = "pixel"
+//     divLines[i].appendChild(pixel[i * boardSize.value + j]);
+//   }
+// }
+
 let colorSelected = document.getElementsByClassName("color");
+colorSelected[1].style.backgroundColor = randRGB();
+colorSelected[2].style.backgroundColor = randRGB();
+colorSelected[3].style.backgroundColor = randRGB();
 
 for (let i = 0; i < 4; i += 1) {
   colorSelected[i].addEventListener("click", function() {
     document.querySelector(".selected").classList.remove("selected");
     colorSelected[i].classList.add("selected")
+
+    let fontColor = document.getElementsByClassName('colorFont');
+    fontColor[0].style.backgroundColor = window.getComputedStyle(colorSelected[i]).backgroundColor;
   });
 }
 
@@ -31,7 +71,6 @@ for (let i = 0; i < pixel.length; i += 1) {
     let cor = document.querySelector(".selected");
     let selected = document.querySelectorAll(".selected")
     pixel[i].style.backgroundColor = getComputedStyle(cor).backgroundColor;
-    console.log(getComputedStyle(cor).backgroundColor)
   })
 
   let buttonClear= document.getElementById("clear-board");
@@ -40,5 +79,26 @@ for (let i = 0; i < pixel.length; i += 1) {
     for (let i = 0; i < pixel.length; i += 1) {
       pixel[i].style.backgroundColor = "white"
     }
-  })
+  });
 }
+
+boardSize.addEventListener("blur", function() {
+  //console.log(boardSize.value)
+  if (boardSize.value == '') {
+    alert('Board inválido!');
+  }
+  // else {
+  //   console.log(boardSize.max)
+  //   if (boardSize.value < boardSize.min){
+  //     boardSize.value = boardSize.min;
+  //   }
+  //   if (boardSize.value > boardSize.max){
+  //     boardSize.value = boardSize.max;
+  //   }
+  // }
+});
+
+let buttonGenerator = document.getElementById('generate-board');
+buttonGenerator.addEventListener('click', function() {
+  pixelGenerator();
+})
