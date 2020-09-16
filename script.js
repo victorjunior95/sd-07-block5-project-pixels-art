482460abf6fd41946e577826a66995c8084000b4
Emanuelle Brasil <emanuelle.rbl@gmail.com>
2020-09-15 23:36:41 -0300

    adicionando os eventos

--- script.js
+++ script.js
@@ -1,13 +1,69 @@
-let color1 = document.getElementById('black');
-color1.style.backgroundColor = 'black';
+let colors = ['black', 'yellow', 'pink', 'orange'];
+let arrayPaletteColor = paletteColor(colors);
+let numberPixelsLine = 5;
 
-let color2 = document.getElementById('yellow');
-color2.style.backgroundColor = 'yellow';
+// criando os elementos da paleta de cores
+function paletteColor(arrayColors) {
+    let paletteColor = [];
+    for (index = 0; index < arrayColors.length; index += 1) {
+        let color = document.getElementById(arrayColors[index]);
+        color.style.backgroundColor = arrayColors[index];
+        paletteColor.push(color);
+    }
+    paletteColor[0].classList.add('selected');
 
-let color3 = document.getElementById('pink');
-color3.style.backgroundColor = 'pink';
+    return paletteColor;
+}
 
-let color4 = document.getElementById('orange');
-color4.style.backgroundColor = 'orange';
+createPixelBoard(numberPixelsLine);
+addEventClick(arrayPaletteColor);
 
-color1.classList.add('selected');
+// adicionando evento click na paleta de cores
+function addEventClick(arrayPaletteColor) {
+    for (index = 0; index < arrayPaletteColor.length; index += 1) {
+        arrayPaletteColor[index].addEventListener('click', function (event) {
+            let selected = document.querySelector('.selected'); // pega a cor que está selecionada
+            selected.classList.remove('selected');
+            event.target.classList.add('selected');
+        });
+    }
+}
+
+// função para criar um pixel
+function createPixel(className) {
+    let pixel = document.createElement('div');
+    pixel.classList.add(className);
+    return pixel;
+}
+// função criar a div de cada linha de pixels
+function createLine(numberPixels) {
+    let divLine = document.createElement('div');
+    for (let index = 0; index < numberPixels; index += 1) {
+        let pixel = createPixel("pixel");
+        divLine.appendChild(pixel);
+    }
+    return divLine;
+}
+// função para criar board de tamanho nxn
+function createPixelBoard(numberPixels) { // recebe numero pixels
+    let board = document.getElementById('pixel-board'); // pega a div do HTML
+    for (let line = 1; line <= numberPixels ; line += 1) { // add a qntd de linhas
+        let divLine = createLine(numberPixels); // cria a linha
+        board.appendChild(divLine);
+    }
+}
+
+// código criado em conjuno no grupo de estudos Trybe Turma-07 ------
+const pixelBoard = document.getElementById("pixel-board");
+pixelBoard.addEventListener('click', function(event) {
+    const selected = document.querySelector('.selected');
+    event.target.style.backgroundColor = window.getComputedStyle(selected).backgroundColor;
+});
+
+const buttonClear = document.getElementById('clear-board');
+buttonClear.addEventListener('click', function() {
+    const pixel = document.querySelectorAll('.pixel');
+    for (let i = 0; i < pixel.length; i += 1) {
+        pixel[i].style.backgroundColor = 'white';
+    }
+});
