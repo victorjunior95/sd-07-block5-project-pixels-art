bc2e18646f056f128e78bd2287807fdd50643102
alexandrefaustino <alexandrefauldade@gmail.com>
2020-09-16 19:34:44 -0300

    commit: 0.0.18

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 
-  const numberOfLine = 5;
-  const numberOfColumn = 5;
+const numberOfLine = 5;
+const numberOfColumn = 5;
 
-  for (let line = 1; line <= numberOfLine; line += 1) {
-    const divLine = document.createElement('div');
-    divLine.className = 'tr';
-    document.getElementById('pixel-board').appendChild(divLine);
+for (let line = 1; line <= numberOfLine; line += 1) {
+  const divLine = document.createElement('div');
+  divLine.className = 'tr';
+  document.getElementById('pixel-board').appendChild(divLine);
 
-    for (let column = 1; column <= numberOfColumn; column += 1) {
-      const divColumn = document.createElement('div');
-      divColumn.className = 'pixel td';
-      divLine.appendChild(divColumn);
-    }
+  for (let column = 1; column <= numberOfColumn; column += 1) {
+    const divColumn = document.createElement('div');
+    divColumn.className = 'pixel td';
+    divLine.appendChild(divColumn);
   }
+}
 
-  const getPixelList = document.getElementsByClassName('color');
-
-  function randomColorPalette() {
-    for (let index = 1; index < getPixelList.length; index += 1) {
-      getPixelList[index].style.backgroundColor = generationRandomColorRGB();
-    }
-  }
-
-randomColorPalette();
+const getPixelList = document.getElementsByClassName('color');
 function generationRandomColorRGB() {
   const maxNumberRGB = 256;
   const decimalNumber = () => Math.floor(Math.random() * maxNumberRGB);
   return `rgb(${decimalNumber()},${decimalNumber()},${decimalNumber()})`;
 }
 
+function randomColorPalette() {
+  for (let index = 1; index < getPixelList.length; index += 1) {
+    getPixelList[index].style.backgroundColor = generationRandomColorRGB();
+  }
+}
+
+randomColorPalette();
 function findIndexPaletteSelected() {
   let searchElement;
   for (let index = 0; index < getPixelList.length; index += 1) {
@@ -108,5 +107,5 @@ document.getElementById('generate-board').addEventListener('click', function () 
   const inputSize = borderSizeValidation(inputSizeValue);
   if (inputSize === '') {
     alert('Board inválido!');
-  } else updateSizePixel(inputSize);
+  } else updateSizePixel(inputSize * inputSize);
 });
