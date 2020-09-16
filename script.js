2697ef4e5e4b2355c97dbfc96ecac3182346ab34
ericvini <ericcobain3@gmail.com>
2020-09-15 23:10:58 -0300

    Requisito 8-finalizado

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ function quadroPixels() {
     const divQuadro = document.createElement('div');
     divQuadro.id = `pixel${i + 1}`;
     divQuadro.className = 'pixel';
+    divQuadro.addEventListener('click', function () {
+      const getElement = document.getElementById(`${returnColor()}`);
+      if (getElement.id === 'color1') {
+        divQuadro.style.backgroundColor = 'black';
+      } else if (getElement.id === 'color2') {
+        divQuadro.style.backgroundColor = 'red';
+      } else if (getElement.id === 'color3') {
+        divQuadro.style.backgroundColor = 'green';
+      } else if (getElement.id === 'color4') {
+        divQuadro.style.backgroundColor = 'yellow';
+      }
+    });
     quadro.appendChild(divQuadro);
   }
 }
@@ -23,29 +35,21 @@ function removeSelected() {
 }
 
 selectedColorBlack.addEventListener('click', function () {
-  alert('black');
-
   removeSelected();
   selectedColorBlack.classList.add('selected');
 });
 
 selectedColorRed.addEventListener('click', function () {
-  alert('vermelha');
-
   removeSelected();
   selectedColorRed.classList.add('selected');
 });
 
 selectedColorGreen.addEventListener('click', function () {
-  alert('verde');
-
   removeSelected();
   selectedColorGreen.classList.add('selected');
 });
 
 selectedColorYellow.addEventListener('click', function () {
-  alert('yellow');
-
   removeSelected();
   selectedColorYellow.classList.add('selected');
 });
@@ -56,6 +60,22 @@ btn.addEventListener('click', function () {
   const y = x.getElementsByTagName('*');
   let i;
   for (i = 0; i < y.length; i += 1) {
-    y[i].style.backgroundColor = 'rgb(255, 255, 255)';
+    y[i].style.backgroundColor = 'white';
   }
 });
+
+const x = document.getElementById('color-palette');
+const y = x.getElementsByTagName('*');
+function returnColor() {
+  let i;
+  let idSelected = '';
+
+  for (i = 0; i < y.length; i += 1) {
+    if (y[i].classList.contains('selected')) {
+      idSelected = y[i].id;
+    }
+  }
+  return `${idSelected}`;
+}
+
+returnColor();
