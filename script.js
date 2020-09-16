4578e9a1e6b49bcb58ccf5715e0079b47a4c7b52
Camila Valentim <camilaifpb@hotmail.com>
2020-09-16 18:07:13 -0300

    commit -m 0.0.15

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let paleta = document.getElementById('color-palette');
-let pixelBoard = document.getElementById('pixel-board');
-let pixels = document.querySelectorAll('.pixel');
 
 
 
-function limpar() {
+
+document.getElementById('clear-board').addEventListener('click', function(){
+  console.log('eu estou aqui');
   let pixels = document.querySelectorAll('.pixel');
   for(let index = 0; index < pixels.length; index += 1){
     pixels[index].style.backgroundColor = 'white';
   }
-}
+});
 
 paleta.addEventListener('click', function() {
   pixels.style = paleta;
-});
+})
 
 
 
