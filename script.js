b9a5a0ef358b6385ece355c5666b621db5eb8db5
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2020-09-15 22:00:21 -0300

    Add a funcionalidade do requisito 8

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 let colors = document.querySelectorAll('.color');
 
 for (let i = 0; i < colors.length; i += 1) {
-    colors[i].addEventListener('click', function() {
-        let selected = document.querySelector('.selected')
+  colors[i].addEventListener('click', function () {
+    let selected = document.querySelector('.selected')
 
-        selected.classList.remove('selected');
+    selected.classList.remove('selected');
 
-        colors[i].classList.add('selected')
-    })
+    colors[i].classList.add('selected')
+  })
+}
+
+
+let pixels = document.querySelectorAll('.pixel');
+
+for (let i = 0; i < pixels.length; i += 1) {
+  pixels[i].addEventListener('click', function () {
+
+    let selected = document.querySelector('.selected');
+    pixels[i].style.backgroundColor = window.getComputedStyle(selected).backgroundColor
+  })
 }
