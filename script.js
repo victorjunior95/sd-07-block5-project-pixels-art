357a72a35f3ec46296626381128f6a2804c2a373
Galaraz <jvictor.si.2013@gmail.com>
2020-09-16 00:41:01 -0300

    mensao honrosa ao grupo de estudos XD

--- script.js
+++ script.js
@@ -1,52 +1,54 @@
-window.onload = function() {
-    let base = 5;
-    let numberOfLines = (base + 1) / 2; // 5
-    let controlLeft = numberOfLines; // 5
-    let controlRight = numberOfLines; // 5
-    let lines = document.querySelectorAll(".pixel");
-  
-      
-    fill(lines);
-    
-   
-    
-  
-    // Passa por todos as linhas (div com class line) e preenche o triangulo
-    function fill(lines) {
-      for(let index = 0; index < lines.length; index += 1) {
-        fillLine(lines[index]);
-        controlRight += 1;
-        controlLeft -= 1;
-      }
-    }
-  
-    // Cria uma caixa com base nas diferentes classes
-    function createBox(className) {
-      let box = document.createElement("div");
-      box.className = className;
-      return box;
-    }
-  
-    // Preenche uma linha
-    function fillLine(divLine) {
-      for (let lineColumn = 1; lineColumn <= base; lineColumn += 1) {
-        if(lineColumn >= controlLeft && lineColumn <= controlRight) {
-          let box = createBox("box");
-          divLine.appendChild(box);
-        }
-        else{
-          let box = createBox("box");
-          divLine.appendChild(box);
+let numberLines = 5;
+let pixelBoard = document.getElementById("pixel-board");
 
-        }
-      }
-    }
+let pixel = [];
+let linha = [];
+
+let k = 0
+for (let i = 0; i < numberLines; i += 1) {
+  linha[i] = document.createElement("div");
+  linha[i].className = "linha";
+  pixelBoard.appendChild(linha[i]);
+
+  for(let j = 0; j < numberLines; j += 1) {
+    pixel[k] = document.createElement("div");
+    pixel[k].className = "pixel";
+    linha[i].appendChild(pixel[k]);
+    k = k + 1;
   }
 
-  function colorir(){
-
-    console.log('coloriu')
-   // document.getElementById();
-
-
 }
+
+let color = document.querySelectorAll(".color");
+
+
+for (let i = 0; i < color.length; i += 1) {
+  color[i].addEventListener("click", function(event) {
+    let selected = document.querySelector(".selected");
+
+    selected.classList.remove('selected');
+
+    //color[i].classList.add('selected');
+    event.target.className += " selected";
+
+  })
+}
+
+pixelBoard.addEventListener("click", function(event) {
+  let selected = document.querySelector(".selected");
+  event.target.style.backgroundColor =  window.getComputedStyle(selected).backgroundColor;
+})
+
+
+
+let buttonClear = document.getElementById("clear-board");
+
+buttonClear.addEventListener("click", function() {
+  for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].style.backgroundColor = "white";
+  }
+})
+
+
+/* 
+agradecimento espcial ao grupo de estudos  */
--- script1.js
+++ script1.js
@@ -7,13 +7,46 @@ window.onload= function(){
   const linha =[];
 
 
- 
+ let k =0 ;
 
   for (let i = 0; i < tamanhoLinha; i+= 1) {
     linha[i] = document.createElement('div');
     linha[i].className ='linha';
-    console.log(linha[i]);
+    pixelBoard.appendChild(linha[i]);
     
+  for (let j = 0; j < tamanhoLinha; j +=1) {
+    pixel[j] = document.createElement('div');
+    pixel[j].className ='pixel';
+    linha[i].appendChild(pixel[j])
+    k = k+1;
+    }
+   
+  }
+
+
+
+  function selecionarPaleta(event) {
+    let mudaBorda = '';
+    switch (event.target.id) {
+      case 'black':
+        mudaBorda = 'color selected';
+        break;
+      case 'red':
+        mudaBorda = 'color selected';
+        break;
+      case 'green':
+        mudaBorda = 'meme-image-container earth-border';
+        break;
+      default:
+        mudaBorda = 'meme-image-container default-border';
+    }
+
+    document.getElementById('meme-image-container').className = mudaBorda;
+  }
+
+ const buttonElementos = document.getElementsByTagName('button');
+  for (let index = 0; index < buttonElementos.length; index+=1) {
+    buttonElementos[index].addEventListener('click',editarBorda);
   }
 
 
