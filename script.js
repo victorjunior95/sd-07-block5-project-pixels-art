d571614f47bf416ac484ea06e3403ed4d4d7be47
Thays Costa <thayscosta3@gmail.com>
2020-09-15 22:56:19 -0300

    Requisito obrigatório 9.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let colorOptions = document.querySelectorAll('.color');
 let pixels = document.querySelectorAll('.pixel');
-let clearButton = document.querySelector('#clear-button');
+let clearButton = document.querySelector('#clear-board');
 
 for (let index = 0; index < colorOptions.length; index += 1){
   colorOptions[index].addEventListener('click', function(event){
@@ -16,3 +16,9 @@ for (let index = 0; index < pixels.length; index += 1) {
     pixels[index].style.backgroundColor = window.getComputedStyle(classSelected).backgroundColor
   })
 }
+
+clearButton.addEventListener('click', function(){
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].style.backgroundColor = 'white';
+  }
+});
