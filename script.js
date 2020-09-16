57f8b12b8b8a48fee64851cf3552f1e86f245037
Rodrigo <rd64103@gmail.com>
2020-09-16 17:53:30 -0300

    erros do css

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ for (let i = 0; i < numeroDeLinhas; i += 1) {
     tabelaDePixel.appendChild(linha[i])
     for (let j = 0; j < 5; j += 1) {
         pixel[contaPixel] = document.createElement('div');
-        pixel[contaPixel].className = 'coluna'
+        pixel[contaPixel].className = 'pixel'
         linha[i].appendChild(pixel[contaPixel]);
         contaPixel += 1;
     }
