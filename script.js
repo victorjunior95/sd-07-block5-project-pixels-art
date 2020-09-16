bc45f73c40bda65b92ad3ad7ad5aca5bce778ff4
Lets <letsferreiratx@gmail.com>
2020-09-15 22:01:53 -0300

    funçoes

--- script.js
+++ script.js
@@ -9,10 +9,36 @@ function fillBoard(boardpixel) {
     pixelLine.className = 'pixelLine';
     board.appendChild(pixelLine);
       for (let column = 0; column < boardpixel; column += 1) {
-          let pixel = document.createElement("div");
+          let pixel = document.createElement("input");
           pixel.className = 'pixel';
+          pixel.type = 'button';
           pixelLine.appendChild(pixel);
       }
   }
 }
+
+let palet = document.querySelectorAll('.color');
+for (let index = 0; index < palet.length; index += 1){
+    palet[index].addEventListener('click', function(){
+      let selected = document.querySelector('.selected');
+      selected.classList.remove('selected');
+      palet[index].classList.add('selected');
+  })
+}
+
+let changeColorPixel = document.querySelectorAll('.pixel');
+for (let index = 0; index < changeColorPixel.length; index += 1){
+changeColorPixel[index].addEventListener('click',  function(){
+  let selectedColor = document.querySelector('.selected');
+  changeColorPixel[index].style.backgroundColor = window.getComputedStyle(selectedColor).backgroundColor;
+})
+}
+
+let limpar = document.getElementById('clear-board');
+limpar.addEventListener('click', function(){
+  let clearColorPixel = document.querySelectorAll('.pixel');
+for (let index = 0; index < clearColorPixel.length; index += 1){
+ clearColorPixel[index].style.backgroundColor = 'white';
+}
+})
 }
