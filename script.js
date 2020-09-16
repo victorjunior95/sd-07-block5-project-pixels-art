840da8f88124c0ec035ac808a61084d55ac57506
vini-lima <vini20.henrique@gmail.com>
2020-09-16 07:39:50 -0300

    Input do usuario

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-let lineNumbers = document.querySelector('#lineNumbers');
+const initialGrid = 5;
 const pixelBoard = document.getElementById('pixel-board');
-
 let line = [];
 let pixel = [];
 let count = 0;
 
-for (let index = 0; index < lineNumbers; index += 1) {
-  line[index] = document.createElement('div');
-  line[index].className = 'line';
-  pixelBoard.appendChild(line[index]);
-
-  for (let j = 0; j < lineNumbers; j += 1) {
-    pixel[count] = document.createElement('div');
-    pixel[count].className = 'pixel';
-    line[index].appendChild(pixel[count]);
-    count += 1;
-  }
+for (let index = 0; index < initialGrid; index += 1) {
+    line[index] = document.createElement('div');
+    line[index].className = 'line';
+    pixelBoard.appendChild(line[index]);
+  
+    for (let j = 0; j < initialGrid; j += 1) {
+      pixel[count] = document.createElement('div');
+      pixel[count].className = 'pixel';
+      line[index].appendChild(pixel[count]);
+      count += 1;
+    }
 }
 
 const colors = document.querySelectorAll('.color');
@@ -42,5 +41,27 @@ const buttonClear = document.getElementById('clear-board');
 buttonClear.addEventListener('click', function () {
   for (let index = 0; index < pixel.length; index += 1) {
     pixel[index].style.backgroundColor = 'white';
+  }
+});
+
+const boardSize = document.querySelector('#board-size').value;
+const vqv = document.querySelector('#generate-board');
+k = 0;
+
+vqv.addEventListener('click', function() {
+  // console.log(boardSize);
+  pixel = [];
+  line = [];
+  for (let index = 0; index < boardSize; index += 1) {
+    line[index] = document.createElement('div');
+    line[index].className = 'line';
+    pixelBoard.appendChild(line[index]);
+  
+    for (let j = 0; j < boardSize; j += 1) {
+      pixel[k] = document.createElement('div');
+      pixel[k].className = 'pixel';
+      line[index].appendChild(pixel[k]);
+      k += 1;
+    }
   }
 });
