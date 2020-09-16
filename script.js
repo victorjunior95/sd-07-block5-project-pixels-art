400b6e432810fbefc3b50ba5eba7d565667f8d21
wellington <profwellington17@gmail.com>
2020-09-16 19:31:18 -0300

    ativiade 1-3 tc

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let divtotal=document.getElementById("pixel-board");
-let nLinha=5;
-
+let nLinha = 5;
+let pixel =[];
 let linha = [];
-
+let k=0;
 /* criação da div em linhas*/
 for(let i=0; i < nLinha; i += 1){
 
-    linha[i]=document.createElement("div");
-    linha[i]=className = "linha";
+    linha[i]= document.createElement("div");
+    linha[i].className = "linha";
     divtotal.appendChild(linha[i]);
     console.log(linha[i]);
+
+        for(let j=0; j < nLinha; j += 1){
+
+            pixel[k]= document.createElement("div");
+            pixel[k].className="pixel";
+            linha[i].appendChild(pixel[k]);
+            k=k+1;
+        }
 }
