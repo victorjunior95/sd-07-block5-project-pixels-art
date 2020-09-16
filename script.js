d4fdb088bc4e59d7acd6808a7dbbf6619ee0992d
Leonardo <lecastro16@gmail.com>
2020-09-15 22:36:49 -0300

    Iniciando o Projeoto. VAMOS COM TUDO: rocket: Leonardo Sardinha v11

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const preto = document.getElementById("preto");
 const color2 = document.getElementById("color2");
 const color3 = document.getElementById("color3");
 const color4 = document.getElementById("color4");
-
+const pixel = document.getElementsByClassName("pixel");
 // Definindo a cor selecionada a partir do preto
 let corSelecionada = preto;
 preto.addEventListener("click", function () {
@@ -29,12 +29,19 @@ color4.addEventListener("click", function () {
   color4.classList.add("selected");
 });
 
-// O comando window.getComputedStyle() 
-document.addEventListener("click", function(event){
-    if (event.target.classList.contains("pixel")){
-        let selecionado = window.getComputedStyle(corSelecionada).backgroundColor;
-        event.target.style.backgroundColor = selecionado;
-        console.log(window.getComputedStyle(corSelecionada).backgroundColor)
-    }
+// O comando window.getComputedStyle()
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("pixel")) {
+    let selecionado = window.getComputedStyle(corSelecionada).backgroundColor;
+    event.target.style.backgroundColor = selecionado;
+    console.log(window.getComputedStyle(corSelecionada).backgroundColor);
+  }
 });
 
+let limpar = document.getElementById("clear-board");
+
+limpar.addEventListener("click", function(){
+    for (let index = 0; index <=pixel.length; index += 1) {
+        pixel[index].style.backgroundColor= "white";
+    }
+});
