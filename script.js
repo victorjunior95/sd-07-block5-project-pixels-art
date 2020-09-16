593871f7fccbc239740c81801ac77cd71deed95c
Matheus Beaumord <matheusbeaumord@gmail.com>
2020-09-16 16:58:09 -0300

    selecionando a cor e preenchendo o quadro

--- script.js
+++ script.js
@@ -11,14 +11,48 @@ function removeSelectedColor(){
   }
 }
 
+let selectedColor = "black";
+
 function getBlackColor(e){
   let element = e.target;
-  removeClassSelected();
+  removeSelectedColor();
   element.classList.add('selected');
   selectedColor = 'black'
 }
 color1.addEventListener('click', getBlackColor);
 
+function getGreenColor(e){
+  let element = e.target;
+  removeSelectedColor();
+  element.classList.add('selected');
+  selectedColor = 'green'
+}
+color2.addEventListener('click', getGreenColor);
+
+function getRedColor(e){
+  let element = e.target;
+  removeSelectedColor();
+  element.classList.add('selected');
+  selectedColor = 'red'
+}
+color3.addEventListener('click', getRedColor);
+
+function getBlueColor(e){
+  let element = e.target;
+  removeSelectedColor();
+  element.classList.add('selected');
+  selectedColor = 'blue'
+}
+color4.addEventListener('click', getBlueColor);
+
+let pickPixel = document.querySelector('#pixel-board');
+
+function changePixelColor(e){
+    let pixel = e.target;
+    pixel.style.backgroundColor = selectedColor;
+}
+
+pickPixel.addEventListener('click', changePixelColor);
 
 // for (let i = 0 ; i < colors.length ; i += 1) {
 //   list[i].addEventListener(TouchEvent , changeSelectedColor(list[i]));
