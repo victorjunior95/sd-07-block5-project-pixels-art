b62bfd8a9536b0c9162e4060f8a1391098e16d6a
Lucas Andrade <lafdesouza2002@outlook.com>
2020-09-16 00:18:04 -0300

    Consertando issues codeclimate

--- script.js
+++ script.js
@@ -1,79 +1,79 @@
-let palette = document.querySelector("#color-palette")
-let colors = document.querySelectorAll(".color")
-let generateBoard = document.querySelector("#generate-board")
-const clearBoard = document.querySelector("#clear-board")
-const board = document.querySelector("#pixel-board")
-let options = ["red", "blue", "green", "aliceblue", "brown", "purple", "gray", "violet", "pink", "yellow", "coral", "orange", "gold", "darkkhaki", "peachpuff", "springgreen", "mediumspringgreen", "cyan", "teal", "slateblue", "indigo", "deeppink", "seagreen", "seashell", "darkslategray", "sandybrown", "chocolate", "peru", "wheat", "blueviolet", "magenta"]
-let randoms = []
-let number = 0
+const palette = document.querySelector('#color-palette');
+const colors = document.querySelectorAll('.color');
+const generateBoard = document.querySelector('#generate-board');
+const clearBoard = document.querySelector('#clear-board');
+const board = document.querySelector('#pixel-board');
+const options = ['red', 'blue', 'green', 'aliceblue', 'brown', 'purple', 'gray', 'violet', 'pink', 'yellow', 'coral', 'orange', 'gold', 'darkkhaki', 'peachpuff', 'springgreen', 'mediumspringgreen', 'cyan', 'teal', 'slateblue', 'indigo', 'deeppink', 'seagreen', 'seashell', 'darkslategray', 'sandybrown', 'chocolate', 'peru', 'wheat', 'blueviolet', 'magenta'];
+const randoms = [];
+let number = 0;
 
-for (let i = 0; i < 5; i++){
-    let div = document.createElement("div")
-    for (let j = 0; j < 5; j++){
-        let pixel = document.createElement("div")
-        pixel.classList.add("pixel")
-        div.appendChild(pixel)
-}
-board.appendChild(div)
-}
-
-palette.addEventListener("click", (event)=>{
-    let selected = document.querySelector(".selected")
-    selected.classList.remove("selected")
-    color = event.target
-    color.classList.add("selected")
-})
-
-for (let i = 1; i < colors.length; i = i + 1){
-    number = Math.floor(Math.random() * options.length) + 1
-    while (randoms.indexOf(number) != -1){
-        number = Math.floor(Math.random() * options.length) + 1 
+for (let i = 0; i < 5; i+=1) {
+  const div = document.createElement('div');
+  for (let j = 0; j < 5; j++) {
+      const pixel = document.createElement('div');
+      pixel.classList.add('pixel');
+      div.appendChild(pixel);
     }
-    randoms.push(number)
-    colors[i].style.backgroundColor = options[number]
+  board.appendChild(div);
+}
+
+palette.addEventListener('click', (event) => {
+  let selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  color = event.target;
+  color.classList.add('selected');
+});
+
+for (let i = 1; i < colors.length; i+=1) {
+  number = Math.floor(Math.random() * options.length) + 1;
+  while (randoms.indexOf(number) !== -1) {
+      number = Math.floor(Math.random() * options.length) + 1;
+    }
+  randoms.push(number);
+  colors[i].style.backgroundColor = options[number];
 }
 
 
-clearBoard.addEventListener("click", ()=>{
-  let fields = document.querySelectorAll(".pixel")
+clearBoard.addEventListener('click', () => {
+  const fields = document.querySelectorAll('.pixel');
   for (let i = 0; i < fields.length; i += 1) {
-    fields[i].style.backgroundColor = "rgb(255, 255, 255)"
-  }  
-})
+    fields[i].style.backgroundColor = 'rgb(255, 255, 255)';
+    }
+});
 
-generateBoard.addEventListener("click", ()=>{
-    let boardSize = document.querySelector("#board-size").value
-    if (parseInt(boardSize) < 5){
-        boardSize = 5
-    }
-    else if (parseInt(boardSize) > 50){
-        boardSize = 50
-    }
-    else if (boardSize == ""){
-        alert("Board inválido!")
-    }
-    board.innerHTML = ""
-    for (let i = 0; i < parseInt(boardSize); i++){
-        let div = document.createElement("div")
-        for (let j = 0; j < parseInt(boardSize); j++){
-            let pixel = document.createElement("div")
-            pixel.classList.add("pixel")
-            div.appendChild(pixel)
-    }
+generateBoard.addEventListener('click', () => {
+  let boardSize = document.querySelector('#board-size').value;
+  if (parseInt(boardSize) < 5) {
+      boardSize = 5;
+  }
+  else if (parseInt(boardSize) > 50) {
+      boardSize = 50;
+  }
+  else if (boardSize === '') {
+      alert('Board inválido!');
+  }
+  board.innerHTML = '';
+  for (let i = 0; i < parseInt(boardSize); i+=1){
+      const div = document.createElement('div')
+      for (let j = 0; j < parseInt(boardSize); j++) {
+          let pixel = document.createElement('div')
+          pixel.classList.add('pixel')
+          div.appendChild(pixel)
+        }
     board.appendChild(div)
     }
-    PixelClick()
-})
+  PixelClick();
+});
 
 
-function PixelClick(){
-    let fields = document.querySelectorAll(".pixel")
-    for (let i = 0; i < fields.length; i += 1){
-        fields[i].addEventListener("click", ()=>{
-            let colorSelected = document.querySelector(".selected").style.backgroundColor
-            fields[i].style.backgroundColor = colorSelected
-        })
+function PixelClick() {
+  let fields = document.querySelectorAll('.pixel');
+  for (let i = 0; i < fields.length; i += 1) {
+      fields[i].addEventListener('click', () => {
+          let colorSelected = document.querySelector('.selected').style.backgroundColor;
+          fields[i].style.backgroundColor = colorSelected;
+      });
     }
 }
 
-PixelClick()
+PixelClick();
