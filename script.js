288a648d7ed53611c0343b958c209e956c81fa03
Strongreen <strongreen@live.com>
2020-09-16 18:06:32 -0300

    Isaac novamente salvando, disponibilizando a opçao do usuario definir o tamanho do board

--- script.js
+++ script.js
@@ -1,33 +1,8 @@
 
 let limpar = document.querySelector("#clear-board");
-let board = document.querySelectorAll(".pixel");
+let board = document.querySelector("#pixel-board");
 let tamanhoBoard = document.querySelector('#board-size');
 let btnVqv = document.querySelector('#generate-board');
-
-
-function validaInput(){
-    if(tamanhoBoard.value != ""){
-        console.log('Deu certo');
-    }
-    else{
-        alert('Board inválido!');
-    };
-};
-
-
-btnVqv.addEventListener('click', validaInput)
-
-
-
-function limparBoard(){
-    for(let index = 0; index < board.length ; index ++){
-        board[index].style.backgroundColor = "white";
-    }
-}
-
-limpar.addEventListener('click', limparBoard);
-
-
 let paletBlack = document.querySelector('#color-black');
 let paletRed = document.querySelector('#color-red');
 let paletGreen = document.querySelector('#color-green');
@@ -35,65 +10,114 @@ let paletBlue = document.querySelector('#color-blue');
 
 let select = 'black';
 
+let tamanhoPixel = 40;
+
+criaInicial(tamanhoPixel);
+
+function criaInicial(tamanhoPixel){
+    let pixelInicial = 25;
+    let criaPixel = [];
+
+    for(let index = 0; index < pixelInicial; index++ ){
+        criaPixel[index] = document.createElement('div');
+        criaPixel[index].classList.add('pixel');
+        criaPixel[index].style.width = tamanhoPixel + 'px';
+        criaPixel[index].style.height = tamanhoPixel + 'px';
+        board.style.maxWidth = (tamanhoPixel * 5 + tamanhoPixel)+ 'px';
+        board.style.maxHeight = (tamanhoPixel * 5 + tamanhoPixel)+ 'px';
+        console.log(tamanhoPixel);
+        board.appendChild(criaPixel[index]);
+        criaPixel[0].id = 'primeiroPixel';
+    }
+    trocarCor();
+}
+
+function criaBoard(){
+    if(tamanhoBoard.value != "" ){
+        board.innerHTML = "";
+        criaInicial(tamanhoBoard.value * tamanhoBoard.value);
+    }
+    else{
+        alert('Board inválido!');
+    };
+};
+
+
+function limparBoard(){
+    let pixelBoard = document.querySelectorAll(".pixel");
+    for(let index = 0; index < pixelBoard.length ; index ++){
+        pixelBoard[index].style.backgroundColor = "white";
+    }
+}
+
+
 function trocarCor(){
-    for(let index = 0; index < board.length ; index ++){
-        board[index].addEventListener("click", function() {
-			board[index].style.backgroundColor = select;
+    let pixelBoard = document.querySelectorAll(".pixel");
+    for(let index = 0; index < pixelBoard.length ; index ++){
+        pixelBoard[index].addEventListener("click", function() {
+			pixelBoard[index].style.backgroundColor = select;
             });
     }  
 }
 
 paletRed.addEventListener('click',function(){
+    let pixelBoard = document.querySelectorAll(".pixel");
     select = 'red';
     paletBlack.classList.remove('selected');
     paletBlue.classList.remove('selected');
     paletGreen.classList.remove('selected');
     paletRed.classList.add('selected');
-    for(let index = 0; index < board.length ; index ++){
-        board[index].addEventListener("click", function() {
-			board[index].style.backgroundColor = select;
+    for(let index = 0; index < pixelBoard.length ; index ++){
+        pixelBoard[index].addEventListener("click", function() {
+			pixelBoard[index].style.backgroundColor = select;
             });
     }; 
 });
 
 
 paletGreen.addEventListener('click',function(){
+    let pixelBoard = document.querySelectorAll(".pixel");
     select = 'green';
     paletBlack.classList.remove('selected');
     paletBlue.classList.remove('selected');
     paletRed.classList.remove('selected');
     paletGreen.classList.add('selected');
-    for(let index = 0; index < board.length ; index ++){
-        board[index].addEventListener("click", function() {
-			board[index].style.backgroundColor = select;
+    for(let index = 0; index < pixelBoard.length ; index ++){
+        pixelBoard[index].addEventListener("click", function() {
+			pixelBoard[index].style.backgroundColor = select;
             });
     }; 
 });
 
 paletBlue.addEventListener('click',function(){
+    let pixelBoard = document.querySelectorAll(".pixel");
     select = 'blue';
     paletBlack.classList.remove('selected');
     paletBlue.classList.add('selected');
     paletRed.classList.remove('selected');
     paletGreen.classList.remove('selected');
-    for(let index = 0; index < board.length ; index ++){
-        board[index].addEventListener("click", function() {
-			board[index].style.backgroundColor = select;
+    for(let index = 0; index < pixelBoard.length ; index ++){
+        pixelBoard[index].addEventListener("click", function() {
+			pixelBoard[index].style.backgroundColor = select;
             });
     }; 
 });
 
+
 paletBlack.addEventListener('click',function(){
+    let pixelBoard = document.querySelectorAll(".pixel");
     select = 'black';
     paletBlack.classList.add('selected');
     paletBlue.classList.remove('selected');
     paletRed.classList.remove('selected');
     paletGreen.classList.remove('selected');    
-    for(let index = 0; index < board.length ; index ++){
-        board[index].addEventListener("click", function() {
-			board[index].style.backgroundColor = select;
+    for(let index = 0; index < pixelBoard.length ; index ++){
+        pixelBoard[index].addEventListener("click", function() {
+			pixelBoard[index].style.backgroundColor = select;
             });
     }; 
 });
 
 trocarCor();
+btnVqv.addEventListener('click', criaBoard);
+limpar.addEventListener('click', limparBoard);
