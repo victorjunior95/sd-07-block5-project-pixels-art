9e04c0e17b15f4b2a797a8e602f3e6a417891ea7
Camila Valentim <camilaifpb@hotmail.com>
2020-09-16 19:04:58 -0300

    commit 0.018

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-
-
-
-
+let mouse;
+let colorPicked = document.querySelector(".selected");
 
 document.getElementById('clear-board').addEventListener('click', function(){
   console.log('eu estou aqui');
@@ -11,13 +9,49 @@ document.getElementById('clear-board').addEventListener('click', function(){
   }
 });
 
+document.addEventListener(
+  "click",
+  function (event) {
+    if (event.target.classList.contains("black")) {
+      colorPicked.classList.remove("selected");
+      event.target.classList.add("selected");
+      colorPicked = event.target;
+      mouse = window
+        .getComputedStyle(colorPicked, null)
+        .getPropertyValue("background-color");
+    } else if (event.target.classList.contains("red")) {
+      colorPicked.classList.remove("selected");
+      event.target.classList.add("selected");
+      colorPicked = event.target;
+      mouse = window
+        .getComputedStyle(colorPicked, null)
+        .getPropertyValue("background-color");
+    } else if (event.target.classList.contains("blue")) {
+      colorPicked.classList.remove("selected");
+      event.target.classList.add("selected");
+      colorPicked = event.target;
+      mouse = window
+        .getComputedStyle(colorPicked, null)
+        .getPropertyValue("background-color");
+    } else if (event.target.classList.contains("green")) {
+      colorPicked.classList.remove("selected");
+      event.target.classList.add("selected");
+      colorPicked = event.target;
+      mouse = window
+        .getComputedStyle(colorPicked, null)
+        .getPropertyValue("background-color");
+    }
+  },
+  false
+);
 
-document.getElementById('color-palette').addEventListener('click', function(){
-  console.log("estou aqui");
-  let paletaDeCores = document.querySelectorAll('.color');
-  for(let index = 0; index < paletaDeCores.length; index += 1){
-
-  }
-
-})
+document.addEventListener(
+  "click",
+  function (event) {
+    if (event.target.classList.contains("pixel")) {
+      event.target.style.backgroundColor = mouse;
+    }
+  },
+  false
+);
 
