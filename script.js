12e1a0764bc4da11b927c560f7ba28b6498de64b
Israel Menon <zezomenon@gmail.com>
2020-09-16 18:51:43 -0300

    Requisito 7 e 8, colorindo os pixels

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 window.onload = function () {
-  let boxPixelBoard = document.getElementById('pixel-board');
-  let onePixelBox = document.querySelector('.pixel');
+  colorPaletSelected();
+}
+
+function colorPaletSelected () {
+  let colorPalet = document.querySelector('.selected');
+  let selectedColor = document.getElementsByClassName('color');
+
+  for (let index = 0; index < selectedColor.length; index += 1) {
+    selectedColor[index].addEventListener('click', function (event) {
+      colorPalet.classList.remove('selected');
+      event.target.classList.add('selected');
+      colorPalet = event.target;
+    });
+  }
+
+  let pixel = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixel.length; index += 1) {
+    let pixelSelected = pixel[index];
+    pixelSelected.addEventListener('click', function (event) {
+      event.target.style.backgroundColor = getComputedStyle(colorPalet).backgroundColor;
+    });
+  }
+  // Consultei vários repo dos colegas, mas o que me ajudou a entender melhor, e usei de base foi do
+  // colega Lennon de Oliveira - Turma 7
+  // https://github.com/tryber/sd-07-block5-project-pixels-art/blob/lenodeoliveira-pixel-art/script.js
   
-  // for (let index = 0; index < 5; index += 1) {
-  //   let box = document.createElement('div');
-  //   for (let jindex = 0; jindex < 5; jindex += 1) {
-  //     onePixelBox.appendChild(box).className = 'box-pixel';
-  //   }
-  // }
+  // source: https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
 }
