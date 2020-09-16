ed5cc35d80ad038c98df04850a43a4675f280ea0
lisvalver <lisvalver.atte@gmail.com>
2020-09-16 09:46:11 -0300

    requesito 7 - refatorando funcao para atribuir class 'selected' a apenas um elemento por vez

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-// Adicionando class 'selected' na cor selecionada 
+// Adicionando evento click aos elementos da paleta
 let paletteColor = document.getElementsByClassName('color')
 
 for (let index = 0; index < paletteColor.length; index += 1) {
     paletteColor[index].addEventListener('click', selectedColor)
 }
 
+// Função para remover e adiocionar a class 'selected'
+
 function selectedColor () {
-    for(let index = 0; index < paletteColor.length; index += 1) {
-        if(paletteColor[index].classList.contains('selected')) {
-            return getComputedStyle(paletteColor[index])
-        } else {
-            let removeSelectedClass = document.querySelector('.selected')
-            removeSelectedClass.classList.remove('selected')
-            paletteColor[index].classList.add('selected')
-            return getComputedStyle(paletteColor[index])
-        }
+    let elementTarget = this
+    if (elementTarget.classList.contains('selected')) {
+    } else {
+        let removeSelectedClass = document.querySelector('.selected')
+        removeSelectedClass.classList.remove('selected')
+        elementTarget.classList.add('selected')
     }
 }
 
