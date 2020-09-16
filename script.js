f62bd96780162178c535ac9ef631bcb89e14bbbe
Israel Menon <zezomenon@gmail.com>
2020-09-16 20:49:29 -0300

    Fazendo correções para passar no codeclimate

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   let colorPalet = document.querySelector('.selected');
-  let selectedColor = document.getElementsByClassName('color');
+  const selectedColor = document.getElementsByClassName('color');
 
   for (let index = 0; index < selectedColor.length; index += 1) {
     selectedColor[index].addEventListener('click', function (event) {
@@ -10,14 +10,15 @@ window.onload = function () {
     });
   }
 
-  let pixel = document.querySelectorAll('.pixel');
+  const pixel = document.querySelectorAll('.pixel');
   for (let index = 0; index < pixel.length; index += 1) {
-    let pixelSelected = pixel[index];
+    const pixelSelected = pixel[index];
     pixelSelected.addEventListener('click', function (event) {
       event.target.style.backgroundColor = getComputedStyle(colorPalet).backgroundColor;
     });
   }
-  /* Consultei vários repo dos colegas, mas o que me ajudou a entender melhor, e usei de base foi do colega Lennon de Oliveira - Turma 7
+  /* Consultei vários repo dos colegas, mas o que me ajudou a entender melhor, e usei de base foi do
+    colega Lennon de Oliveira - Turma 7
     https://github.com/tryber/sd-07-block5-project-pixels-art/blob/lenodeoliveira-pixel-art/script.js */
   // source: https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
 
@@ -27,5 +28,4 @@ window.onload = function () {
       pixel[index].style.backgroundColor = 'white';
     }
   });
-
-}
+};
