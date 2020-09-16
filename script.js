0ec3980191c603574d33cc8fad77ad3fef677959
berchon <berchon@gmail.com>
2020-09-15 22:34:09 -0300

    Sexto commit 100%

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ let linha = [];
 let k = 0
 for (let i = 0; i < numberLines; i += 1) {
   linha[i] = document.createElement("div");
-  linha[i].className = "linha"
+  linha[i].className = "linha";
   pixelBoard.appendChild(linha[i]);
 
   for(let j = 0; j < numberLines; j += 1) {
-    pixel[k] = document.createElement("div")
-    pixel[k].className = "pixel"
-    linha[i].appendChild(pixel[k])
+    pixel[k] = document.createElement("div");
+    pixel[k].className = "pixel";
+    linha[i].appendChild(pixel[k]);
     k = k + 1;
   }
 
@@ -22,31 +22,34 @@ for (let i = 0; i < numberLines; i += 1) {
 let color = document.querySelectorAll(".color");
 
 //usar o indexOf é um método de busca dentro do array
-
 for (let i = 0; i < color.length; i += 1) {
   color[i].addEventListener("click", function(event) {
-    let selected = document.querySelector(".selected")
+    let selected = document.querySelector(".selected");
 
     selected.classList.remove('selected');
 
     //color[i].classList.add('selected');
-    event.target.className += " selected"
+    event.target.className += " selected";
 
   })
 }
 
-for (let i = 0; i < pixel.length; i += 1) {
-  pixel[i].addEventListener("click", function() {
-    let selected = document.querySelector(".selected")
-    // https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
-    pixel[i].style.backgroundColor = window.getComputedStyle(selected).backgroundColor
-    //console.log(window.getComputedStyle(selected).backgroundColor)
+pixelBoard.addEventListener("click", function(event) {
+  let selected = document.querySelector(".selected");
+  event.target.style.backgroundColor =  window.getComputedStyle(selected).backgroundColor;
+})
 
-  })
-}
+// for (let i = 0; i < pixel.length; i += 1) {
+//   pixel[i].addEventListener("click", function() {
+//     let selected = document.querySelector(".selected")
+//     // https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+//     pixel[i].style.backgroundColor = window.getComputedStyle(selected).backgroundColor
+//     //console.log(window.getComputedStyle(selected).backgroundColor)
+
+//   })
+// }
 
 let buttonClear = document.getElementById("clear-board");
-
 
 buttonClear.addEventListener("click", function() {
   for (let i = 0; i < pixel.length; i += 1) {
