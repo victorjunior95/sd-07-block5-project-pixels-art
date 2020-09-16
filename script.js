842857289ab7e7c44570c7eb4fa32167597e07da
Eduardo Pizorno <pizorno@gmail.com>
2020-09-16 12:03:41 -0300

    Pixel Art 0.0.9 - Evento click em cada pixel

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 window.onload = function () {
+
   document.getElementById("clear-board").addEventListener("click",function () {
     const elements = document.getElementsByClassName("pixel");
     for (let key = 0; key < elements.length; key += 1) {
       elements[key].style.backgroundColor = "rgb(255, 255, 255)";
     }
   });
+
+  let element;
+  let origin;
+  let pixel = document.querySelectorAll(".pixel");
+
+  for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].addEventListener("click", function (e) {
+      element = e.target;
+      origin = document.querySelector(".selected");
+      element.style.backgroundColor = origin.classList[1];
+    });
+  }
 }
