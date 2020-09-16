0dc5966aea12925e21015eec0d7473925f3e2a34
berchon <berchon@gmail.com>
2020-09-15 22:08:24 -0300

    Quinto commit item 9

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ let color = document.querySelectorAll(".color");
 for (let i = 0; i < color.length; i += 1) {
   color[i].addEventListener("click", function(event) {
     let selected = document.querySelector(".selected")
-    //console.log(selected)
+
     selected.classList.remove('selected');
 
     //color[i].classList.add('selected');
@@ -35,8 +35,21 @@ for (let i = 0; i < color.length; i += 1) {
   })
 }
 
+for (let i = 0; i < pixel.length; i += 1) {
+  pixel[i].addEventListener("click", function() {
+    let selected = document.querySelector(".selected")
+    // https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+    pixel[i].style.backgroundColor = window.getComputedStyle(selected).backgroundColor
+    //console.log(window.getComputedStyle(selected).backgroundColor)
+
+  })
+}
+
+let buttonClear = document.getElementById("clear-board");
 
 
-
-
-
+buttonClear.addEventListener("click", function() {
+  for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].style.backgroundColor = "white";
+  }
+})
