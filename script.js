9f6188f82f24106a2ed7afa7b12bc53e454ecfdb
Thays Costa <thayscosta3@gmail.com>
2020-09-15 22:45:42 -0300

    Requisito obrigatório 8.

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let colorOptions = document.querySelectorAll('.color');
+let pixels = document.querySelectorAll('.pixel');
+let clearButton = document.querySelector('#clear-button');
 
 for (let index = 0; index < colorOptions.length; index += 1){
   colorOptions[index].addEventListener('click', function(event){
     let selectedColor = document.querySelector('.selected');
     selectedColor.classList.remove('selected');
     event.target.classList.add('selected');
-  })
+  });
 } // Abstração facilitada pela resolução em grupo feita por parte da turma.
+  
+for (let index = 0; index < pixels.length; index += 1) {
+  pixels[index].addEventListener('click', function (){
+    let classSelected = document.querySelector('.selected');
+    pixels[index].style.backgroundColor = window.getComputedStyle(classSelected).backgroundColor
+  })
+}
