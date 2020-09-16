8264d5ac99896cc9fea9db12b34a6a2525838274
FabricioS <fab.silva.x2@gmail.com>
2020-09-15 23:31:34 -0300

    finalizado

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ window.onload = function () {
 };
 
 const tabelaLinha = [1, 5];
-const tipoTag = ['tr', 'tr'];
-const tipoTag2 = ['th', 'td'];
+const tabelaCelula = [4, 5];
+const tipoTag = ['div', 'tr'];
+const tipoTag2 = ['div', 'td'];
 const qualIdVincular = ['color-palette', 'pixel-board'];
 const nomeClasse = ['color', 'pixel'];
-const tabelaCelula = [4, 5];
 const classeVinculo = ['paletaCoresLinha', 'pixelLinha'];
+const btVqv = document.getElementById('generate-board');
+const inputBoard = document.getElementById('board-size');
+const tableId = document.getElementById('pixel-board');
 
 function linhaTabela(tpTag, idVinculo, classVinculo) {
   const quadradinho = document.createElement(tpTag);
@@ -40,18 +43,28 @@ function selecionarCor() {
   this.classList.add('selected');
 }
 
-function coresEscolhas() {
-  const arrayCores = ['black', 'yellow', 'red', 'green'];
+function coresEscolhas(coresDoRandon) {
+  const arrayCores = coresDoRandon;
   for (let i = 0; i < arrayCores.length; i += 1) {
     document.getElementsByClassName('color')[i].style.backgroundColor = arrayCores[i];
     document.getElementsByClassName('color')[i].addEventListener('click', selecionarCor);
   }
 }
 
+function randonColors() {
+  const coresRandon = ['black'];
+  const cores = ['yellow', 'red', 'green', 'purple', 'aqua', 'peachpuff', 'pink', 'indigo', 'darkred', 'lightgreen'];
+  for (let contagem = 0; contagem < 3; contagem += 1) {
+    const randonCor = Math.ceil(Math.random() * 7);
+    coresRandon.push(cores.splice(randonCor, 1));
+  }
+  coresEscolhas(coresRandon);
+}
+
 function botaoLimpar() {
   const pixels = document.getElementsByClassName('pixel');
   for (let index = 0; index < pixels.length; index += 1) {
-    document.getElementsByClassName('pixel')[index].style.backgroundColor = 'white';
+    pixels[index].style.backgroundColor = 'white';
   }
 }
 document.getElementById('clear-board').addEventListener('click', botaoLimpar);
@@ -65,7 +78,47 @@ function criaPaletaDeEscolhas(i) {
   }
 }
 
-for (let i = 0; i < 2; i += 1) {
-  criaPaletaDeEscolhas(i);
+function createBoard(qntCell) {
+  tabelaLinha.pop();
+  tabelaLinha.push(qntCell);
+  tabelaCelula.pop();
+  tabelaCelula.push(qntCell);
+  const tableSize = ((qntCell * 40) + 1);
+  tableId.style.width = `${tableSize}px`;
+  tableId.style.height = `${tableSize}px`;
+  criaPaletaDeEscolhas(1);
 }
-coresEscolhas();
+
+function minManRange() {
+  const qntCell1 = inputBoard.value;
+  if (qntCell1 < 5) {
+    createBoard(5);
+  } else if (qntCell1 > 50) {
+    createBoard(50);
+  } else {
+    createBoard(qntCell1);
+  }
+}
+
+function eraseBoard() {
+  if (tableId.childElementCount > 0) {
+    do {
+      tableId.removeChild(tableId.children[0]);
+    }
+    while (tableId.childElementCount > 0);
+  }
+  minManRange();
+}
+
+function emptyCheck() {
+  if (inputBoard.value === '') {
+    alert('Board inválido!');
+  } else {
+    eraseBoard();
+  }
+}
+
+criaPaletaDeEscolhas(0);
+randonColors();
+
+btVqv.addEventListener('click', emptyCheck);
