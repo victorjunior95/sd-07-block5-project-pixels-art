2f4ae7cd1019ead8a212e7f8f6d735125a4fb35a
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-09-16 20:18:32 -0300

    issues code climate

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
 
-let board = document.getElementById('pixel-board')
-let amountOfSquares = 5;
+const board = document.getElementById('pixel-board');
+const amountOfSquares = 5;
 
-for (counter = 0; counter < amountOfSquares;counter +=1){
-    for (index = 0; index < amountOfSquares;index +=1){
-        let square = document.createElement('div');
-        square.className += 'pixel';
-        board.appendChild(square)
+for (let counter = 0; counter < amountOfSquares; counter += 1) {
+    for (let index = 0; index < amountOfSquares; index += 1) {
+      const square = document.createElement('div');
+      square.className += 'pixel';
+      board.appendChild(square);
     }
-    board.innerHTML = board.innerHTML + '<br>'
+  board.innerHTML += '<br>';
 }
 
-let paletteColors = document.getElementsByClassName('color');
-
-document.addEventListener('click', function(event){
-    if(event.target.classList.contains('color')){
-        let selectColor = document.querySelector('.selected');
-        selectColor.classList.remove('selected');
-        event.target.classList.add('selected')
-    }
+document.addEventListener('click', function (event) {
+  if (event.target.classList.contains('color') ) {
+      const selectColor = document.querySelector('.selected');
+      selectColor.classList.remove('selected');
+      event.target.classList.add('selected');
+    } 
 })
-//baseado em https://app.betrybe.com/course/fundamentals/javascript/dom-manipulation/js-part-6
-let pixelBoard = document.getElementsByClassName('pixel')
-for (let i = 0; i < pixelBoard.length; i+= 1) {
-    pixelBoard[i].addEventListener('click', function (event) {
-    let colors = document.getElementsByClassName('selected')[0];
-    let pixelColor = event.target;
-    pixelColor.style.backgroundColor = getComputedStyle(colors).backgroundColor
-  });
+// baseado em https://app.betrybe.com/course/fundamentals/javascript/dom-manipulation/js-part-6
+const pixelBoard = document.getElementsByClassName('pixel');
+for (let i = 0; i < pixelBoard.length; i += 1) {
+  pixelBoard[i].addEventListener('click', function (event) {
+      const colors = document.getElementsByClassName('selected')[0];
+      const pixelColor = event.target;
+      pixelColor.style.backgroundColor = getComputedStyle(colors).backgroundColor
+    });
 }
 
-
-let clear = document.getElementById('clear-board')
+const clear = document.getElementById('clear-board')
 clear.addEventListener('click', function () {
     for (let index = 0; index < pixelBoard.length; index += 1) {
         pixelBoard[index].style.backgroundColor = 'white'
