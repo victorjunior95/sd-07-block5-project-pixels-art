71ee4ee8d0095e88a49b6ef1b5cd6ab3dfbcc6b9
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-16 20:49:58 -0400

    função para ver cor selecionada criada

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-const selecao = document.getElementById('color-palette').className;
-console.log(selecao);
+const selecionandoCor = document.getElementById('color-palette').children;
+for (let index = 0; index < selecionandoCor.length; index += 1) {
+  selecionandoCor[index].addEventListener('click', function () {
+    document.getElementsByClassName('color selected')[0].classList.remove('selected');
+    this.classList.add('selected');
+  });
+}
