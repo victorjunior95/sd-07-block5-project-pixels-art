c1ec5b02bed709725269720a4c3ec74ff36e2cfd
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-09-17 13:30:42 -0300

    adjusts

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ const red = document.getElementById('red');
 const pixels = document.getElementsByClassName('pixel');
 const clear = document.getElementById('clear-board');
 const board = document.getElementById('pixel-board');
-const colums = document.getElementsByClassName('tr');
 const inputNumber = document.getElementById('board-size');
 const renderNewBoard = document.getElementById('generate-board');
 
-document.onload = newColors();
+// document.onload = newColors();
 
 function getSelectedColor() {
   return document.getElementsByClassName('selected')[0].className.split(' ')[1];
@@ -93,15 +92,15 @@ function addColums(number) {
 
 renderNewBoard.addEventListener('click', addColums);
 
-function generateColor() {
-  const first = Math.floor(Math.random() * 256);
-  const second = Math.floor(Math.random() * 256);
-  const third = Math.floor(Math.random() * 256);
-  return `rgb(${first} , ${second} , ${third})`;
-}
+// function generateColor() {
+//   const first = Math.floor(Math.random() * 256);
+//   const second = Math.floor(Math.random() * 256);
+//   const third = Math.floor(Math.random() * 256);
+//   return `rgb(${first} , ${second} , ${third})`;
+// }
 
-function newColors() {
-  blue.style.backgroundColor = generateColor();
-  red.style.backgroundColor = generateColor();
-  green.style.backgroundColor = generateColor();
-}
+// function newColors() {
+//   blue.style.backgroundColor = generateColor();
+//   red.style.backgroundColor = generateColor();
+//   green.style.backgroundColor = generateColor();
+// }
