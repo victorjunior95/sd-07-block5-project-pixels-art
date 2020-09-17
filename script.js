06688c5818c1ddcbf67af2b7b6aef4d9a149642c
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-09-17 00:28:12 -0300

    tentativa 2 função pintar

--- script.js
+++ script.js
@@ -7,13 +7,20 @@ for (let index = 0; index < color.length; index += 1) {
     colorSelected.classList.remove("selected");
 
     color[index].classList.add("selected");
-
   });
 }
 
 let pixel = document.querySelectorAll(".pixel");
 
-for (let index = 0; index < pixel.length; index += 1) {
-  let colorSelected = document.querySelectorAll(".selected");
-pixel[index].style.backgroundColor = window.getComputedStyle(colorSelected).backgroundColor;
-}
+pixelBoard.addEventListener("click", function(event){
+  let colorSelected = document.querySelector(".selected");
+  event.target.style.backgroundColor = window.getComputedStyle(colorSelected).backgroundColor;
+})
+
+let clear = document.getElementsById("clear-board");
+
+clear.addEventListener("click", function () {
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[i].style.backgroundColor = "white";
+  }
+});
