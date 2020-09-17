e9cf8c3f61c073fba5017ce14c841e3087b24752
Rodrigo <rd64103@gmail.com>
2020-09-17 13:55:27 -0300

    projeto obrigatorio todo? tlvz?

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let pixel = [];
 let linha = [];
 let numeroDeLinhas = 5
 let contaPixel = 0
+
 for (let i = 0; i < numeroDeLinhas; i += 1) {
     linha[i] = document.createElement('div');
     linha[i].className = 'linha';
@@ -14,4 +15,22 @@ for (let i = 0; i < numeroDeLinhas; i += 1) {
         contaPixel += 1;
     }
 }
-console.log(pixel);
+let color = document.querySelectorAll('.color')
+for (let i = 0; i < color.length; i += 1) {
+    color[i].addEventListener("click", function(){
+       let selected = document.querySelector('.selected')
+       selected.classList.remove('selected')
+       color[i].classList.add('selected')
+    })
+    
+}
+
+for (let i = 0; i < contaPixel; i += 1) {
+    pixel[i].addEventListener("click", function(){
+        let pintar = document.querySelector('.selected')
+        pintar = getComputedStyle(pintar)
+        corDoPixel = getComputedStyle(pixel[i])
+        pixel[i].style.backgroundColor = pintar.backgroundColor
+
+    })
+}
