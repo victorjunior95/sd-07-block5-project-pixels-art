5c812579a48df6642ed5376edf9dd35c0b3f8156
Rodolfo <rodolfojr94@gmail.com>
2020-09-17 09:45:35 -0300

    Refatorado JS... Agora vai

--- script.js
+++ script.js
@@ -1,80 +1,138 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', () => { 
 
-    tableCreate();
-    tableEvent();
-    paleteColorEvent();
-    clearTableEvent();
-
+    creatColorsPalette();
+    createPixelBoard();
+    creatEvents();
 });
 
-let colorSelec = "black";
-var pixelBoard = document.getElementById("pixel-board");
-var paleteColor = document.querySelectorAll("#color-palette div"); 
+let colorPalette = document.getElementById("color-palette"),
+    colors = ['black', 'blue', 'pink', 'green'];
 
-function tableCreate() { 
+function creatColorsPalette() {
+
+    for( let i = 0 ; i < 4 ; i++ ) {
+        let color = document.createElement('div');
+        color.id = `paletteColor${i+1}`;
+        color.className = "color";
+        color.style.backgroundColor = colors[i];
+        colorPalette.appendChild(color);
+    };
+};
+
+let pixelBoard = document.getElementById("pixel-board");
+
+function createPixelBoard() {
+
+    let cont = 0;
 
     for( let i = 0 ; i < 5 ; i++ ) {
 
         let linePixel = document.createElement('div');
-        //tentar link com css
+
         linePixel.style.height = '40px' ;
         pixelBoard.appendChild(linePixel);
-        
-
+            
         for( let j = 0 ; j < 5 ; j++ ) {
 
-            let pixelElement = document.createElement('div');
+        let pixelElement = document.createElement('div');
+            cont++;
             pixelElement.className = "pixel";
+            pixelElement.id = `pixel${cont}`;
             linePixel.appendChild(pixelElement);
-
         };
-    
     };
-    
+
 };
 
-function paleteColorEvent() {
+let buttonClear = document.getElementById('clear-board'),
+    colorSelected = "black",
+    lastColorId;
 
-    for( let i = 0 ; i < paleteColor.length ; i++) {
+function selectColor(element) {
 
-        paleteColor[i].addEventListener('click', () => {
+    let color = element
+        n = color.id.replace("paletteColor","");
 
-            colorSelec = paleteColor[i].id.replace("Color", "");
+    if(lastColorId === undefined ) {
 
-        });
+        lastColorId = color;
+
+    }
+    else {
+
+        document.querySelector(`#${lastColorId.id}`).className = 'color';
 
     };
+
+    colorSelected = colors[n-1];
+    lastColorId = color;
+    color.className = 'selected';
+
 };
 
-function tableEvent() {
+function selectPixel(element) {
 
-    let pixels = pixelBoard.querySelectorAll('div div');
+    let pixel = element;
+    pixel.style.backgroundColor = colorSelected;
+
+};
+
+function selectClear() {
+    
+    let pixel = document.querySelectorAll('.pixel');
+
+    for( let i = 0 ; i < pixel.length ; i++) {
+
+        pixel[i].style.backgroundColor = "white";
+
+    };
+
+};
+
+function  whichElement(id) {
+
+    let element = document.querySelector(`#${id}`);
+
+    if( id.indexOf('pixel') !== -1 ){
+        
+        selectPixel(element);
+
+    }
+    else if( id.indexOf('paletteColor') !== -1 ){
+
+        selectColor(element);
+
+    }
+    else {
+
+        selectClear();
+
+    };
+
+};
+
+function creatEvents() {
+
+    let pixels = pixelBoard.querySelectorAll('.pixel'),
+        col = document.querySelectorAll(".color"), 
+        getClick = e => {
+        whichElement(e.target.id);        
+        };
 
     for( let i = 0 ; i < pixels.length ; i++) {
 
-        pixels[i].addEventListener('click', () => {
+        let pixel = pixels[i],
+            color = col[i];
 
-            pixels[i].style.backgroundColor = colorSelec ;
+        pixel.addEventListener('click',getClick);
 
-        });
+        if ( i < col.length ) { 
 
+            color.addEventListener('click',getClick);
+
+        };
     };
 
-};
-
-function clearTableEvent() {
-
-    document.getElementById('clear-board').addEventListener('click', () => {
-
-        let pixels = pixelBoard.querySelectorAll('div div');
-
-        for( let i = 0 ; i < pixels.length ; i++) {
-
-            pixels[i].style.backgroundColor = 'white' ;
-    
-        };
-
-    })
+    buttonClear.addEventListener('click',getClick);
 
 };
-
