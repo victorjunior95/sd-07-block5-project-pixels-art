2a07d68824f36b2dde4817aa6ba72e6c86cf2172
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-09-17 01:47:07 -0300

    Tentativa requisitos obrigatórios

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-let color = document.querySelectorAll(".color");
+let initialColor = document.querySelectorAll(".color");
 
-for (let index = 0; index < color.length; index += 1) {
-  color[index].addEventListener("click", function (event) {
+for (let index = 0; index < initialColor.length; index += 1) {
+  initialColor[index].addEventListener("click", function (event) {
     let colorSelected = document.querySelector(".selected");
 
     colorSelected.classList.remove("selected");
 
-    color[index].classList.add("selected");
+    initialColor[index].classList.add("selected");
   });
 }
 
 let pixelContainer = document.querySelectorAll(".pixel");
-console.log(pixelContainer)
-for(let index = 0; index < pixelContainer.length; index += 1){
-pixelContainer[index].addEventListener("click", function (event) {
-  let colorSelected = document.querySelector(".selected");
+console.log(pixelContainer);
+for (let index = 0; index < pixelContainer.length; index += 1) {
+  pixelContainer[index].addEventListener("click", function (event) {
+    let colorSelected = document.querySelector(".selected");
 
-  event.target.style.backgroundColor = window.getComputedStyle(
-    colorSelected
-  ).backgroundColor;
-});
+    event.target.style.backgroundColor = window.getComputedStyle(
+      colorSelected
+    ).backgroundColor;
+  });
 }
 
 let clear = document.getElementById("clear-board");
