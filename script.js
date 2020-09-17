de64f29a8076cc9683a951fc5c632cbdbe83f11c
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-09-17 00:07:18 -0300

    Tentativa 4 função selecionar a cor

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let color = document.querySelectorAll("color");
+
 for (let index = 0; index < color.length; index += 1) {
   color[index].addEventListener("click", function (event) {
-    let colorSelected = document.querySelector(".selected")
+    let colorSelected = document.querySelector(".selected");
 
     colorSelected.classList.remove("selected");
 
-    event.target.className += "selected";
-
+    color[index].classList.add("selected")
   });
-
 }
 
+let pixel = document.querySelectorAll(".pixel");
+
+for (let index = 0; index < pixel.length; index += 1) {
+  let colorSelected = document.querySelectorAll(".selected")
+}
