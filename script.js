7649d629a68c3bf92412a604be0ee4b36fdb7e0b
barrrosomarcio <barrrosomarcio@gmail.com>
2020-09-18 16:30:54 -0300

    correcoes

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ document.onload
     for (let count = 1; count < collorPallet.length ; count +=1 ){
         collorPallet[count].style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);     
     }
-    collorPallet[0].className = 'Selected';
 }
 
+let selectedCollor = collorPallet[0].style.backgroundColor;
 
 
 
