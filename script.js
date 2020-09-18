73a6d8f50c9e8360797c5c2745a509c041badc7a
Eduardo Souza <dado.eletrons@gmail.com>
2020-09-18 20:30:37 -0300

    requisito 3°, 4°, 5°

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
-window.onload = () => {
-
-  makeColorsPalette(3);
-
-}
-
 const makeColorsPalette = (number) => {
-  makeColorSinglePalette("black");
+  const makeColorSinglePalette = (color) => {
+    const colorPaletteDiv = document.querySelector('#color-palette');
+    const colorDiv = document.createElement('div');
+    colorDiv.className = 'color';
+    colorDiv.style.backgroundColor = color;
+    colorPaletteDiv.appendChild(colorDiv);
+  }
+
+  const colorGeneratorRandom = () => {
+    return `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
+  }
+
+  makeColorSinglePalette('black');
   for (let index = 1; index <= number; index += 1) {
     makeColorSinglePalette(colorGeneratorRandom());
   }
 }
 
-const makeColorSinglePalette = (color) => {
-  const colorPaletteDiv = document.querySelector('#color-palette');
-  const colorDiv = document.createElement('div');
-  colorDiv.className = 'color';
-  colorDiv.style.backgroundColor = color;
-  colorPaletteDiv.appendChild(colorDiv);
+
+const makeCanvasBoard = (heightNumber, widthNumber) => {
+
+  const createLinePixel = () => {
+    const linePixelBoard = document.createElement('div');
+    linePixelBoard.className = "line-pixel-board";
+    return linePixelBoard;
+  }
+
+  const addPixelInLine = () => {
+    const pixelDiv = document.createElement('div');
+    pixelDiv.className = 'pixel'
+    pixelDiv.style.backgroundColor = 'white';
+    return pixelDiv;
+  }
+
+  const pixelBoard = document.querySelector('#pixel-board');
+  for (let line = 1; line <= heightNumber; line += 1) {
+    const line = createLinePixel();
+    for (let pixel = 1; pixel <= widthNumber; pixel += 1) {
+      line.appendChild(addPixelInLine());
+    }
+    pixelBoard.appendChild(line);
+  }
 }
 
-const colorGeneratorRandom = () => {
-  return `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`
+window.onload = () => {
+
+  makeColorsPalette(3);
+  makeCanvasBoard(5, 5)
+
 }
