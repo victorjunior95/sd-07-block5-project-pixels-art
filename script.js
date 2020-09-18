1ef5d93219491ff8c9346c4bd10bc013144abd9b
Eduardo Souza <dado.eletrons@gmail.com>
2020-09-18 15:51:31 -0300

    realizado 2°

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+window.onload = () => {
 
+  makeColorsPalette(3);
+
+}
+
+const makeColorsPalette = (number) => {
+  makeColorSinglePalette("black");
+  for (let index = 1; index <= number; index += 1) {
+    makeColorSinglePalette(colorGeneratorRandom());
+  }
+}
+
+const makeColorSinglePalette = (color) => {
+  const colorPaletteDiv = document.querySelector('#color-palette');
+  const colorDiv = document.createElement('div');
+  colorDiv.className = 'color';
+  colorDiv.style.backgroundColor = color;
+  colorPaletteDiv.appendChild(colorDiv);
+}
+
+const colorGeneratorRandom = () => {
+  return `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`
+}
