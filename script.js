8b4f0f9203af03adafe215477c31e91b398bca39
Geziel <geziel.fernandez@pucp.pe>
2020-09-18 20:31:51 -0300

    more javascript contribution

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let box1 = document.querySelectorAll('.color')[0];
 let box2 = document.querySelectorAll('.color')[1]; 
 let box3 = document.querySelectorAll('.color')[2];
 let box4 = document.querySelectorAll('.color')[3];
+const pixelBoard = document.getElementById('pixel-board');
 
 box1.style.backgroundColor = 'black';
 
@@ -20,11 +21,7 @@ function randomColors () {
   box4.style.backgroundColor = cor4;
 }
 
-// Select black when the page load
-window.addEventListener('load', function () {
-  box1.classList.add('selected');
-  randomColors();
-});
+
 
 for (let linha = 0; linha < 5; linha += 1) {
   // criar 1 linha
@@ -36,4 +33,22 @@ for (let linha = 0; linha < 5; linha += 1) {
       elementCell.className = 'pixel';
       elementLine.appendChild(elementCell);
     }
+}
+
+
+// Select black when the page load
+window.addEventListener('load', function () {
+  box1.classList.add('selected');
+  randomColors();
+})
+
+let color = document.querySelectorAll('.color');
+for (let i = 0; i < color.length; i += 1) {
+  color[i].addEventListener('click', function () {
+    let selected = document.querySelector('.selected');
+      selected.classList.remove('selected');
+      // event.target.className += ' selected';
+      color[i].classList.add('selected');
+})
+    
 }
