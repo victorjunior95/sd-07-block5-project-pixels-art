861e2067a90e21a23799a5de3284a861c56ca739
lukeCruz <enio.cruz@yahoo.com.br>
2020-09-19 13:52:25 -0300

    teste de Js

--- script.js
+++ script.js
@@ -2,39 +2,40 @@ let numberLines = 5;
 let pixelBoard = document.getElementById("pixel-board");
 let pixel = [];
 let linha = [];
-let kIndex = 0
-for (let index = 0; index < numberLines; index += 1) {
-  linha[index] = document.createElement("div");
+let k = 0
 
-  linha[index].className = "line";
-  pixelBoard.appendChild(linha[index]);
+for (let i = 0; i < numberLines; i += 1) {
+  linha[i] = document.createElement("div");
+  linha[i].className = "linha";
+  pixelBoard.appendChild(linha[i]);
 
-  for(let jIndex = 0; jIndex < numberLines; jIndex += 1) {
-    pixel[kIndex] = document.createElement("div");
-    pixel[kIndex].className = "pixel";
-    linha[index].appendChild(pixel[kIndex]);
-    kIndex = kIndex + 1;
+for(let j = 0; j < numberLines; j += 1) {
+
+    pixel[k] = document.createElement("div");
+    pixel[k].className = "pixel";
+    linha[i].appendChild(pixel[k]);
+    k = k + 1;
   }
-
 }
 let color = document.querySelectorAll(".color");
-for (let index = 0; index < color.length; index += 1) {
-    color[index].addEventListener("click", function(event) {
+
+for (let i = 0; i < color.length; i += 1) {
+    color[i].addEventListener("click", (event) => {
         let selected = document.querySelector(".selected");
 
-    selected.classList.remove('selected');
-    event.target.className += " selected";
+        selected.classList.remove('selected');
+        event.target.className += " selected";
 
-})
-}
-pixelBoard.addEventListener("click", function(event) {
+})}
+
+
+pixelBoard.addEventListener("click", (event) => {
     let selected = document.querySelector(".selected");
     event.target.style.backgroundColor =  window.getComputedStyle(selected).backgroundColor;
-  })
+})
   let buttonClear = document.getElementById("clear-board");
 
-buttonClear.addEventListener("click", function(event) {
-  for (let index = 0; index < pixel.length; index += 1) {
-    pixel[index].style.backgroundColor = "white";
-  }
-})
+  buttonClear.addEventListener("click",(event) => {
+    for (let i = 0; i < pixel.length; i += 1) {
+      pixel[i].style.backgroundColor = "white";
+}})  
