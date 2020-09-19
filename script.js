6c394e166931f09e335e9b0e20beec927629d0cf
Alstent <rafaelasltent@gmail.com>
2020-09-20 00:02:41 +0200

    adicionando pixel board

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const colorPalette = document.querySelector('#color-palette')
 
-let colorSelected
+let colorSelected = 'black'
 
 let r
 let g
@@ -32,6 +32,47 @@ window.onload = function palette() {
 			})
 		}
 	}
+	generatePixelBoard()
 }
 
+const pixelBoard = document.querySelector('#pixel-board')
+const wholeBoard = document.querySelector('#whole-board')
+const generateBoard = document.querySelector('#generate-board')
 
+let i = 0
+let rowMax = 5
+let columnMax = 5
+generateBoard.addEventListener('click', function () {
+	const boardSize = document.querySelector('#board-size').value
+	boardSizeN = Number(boardSize)
+	if (boardSizeN > 7 || boardSizeN <= 2) {
+		window.alert('Board Inválido!')
+	} else {
+		rowMax = boardSizeN
+		columnMax = boardSizeN
+		removePixelBoard()
+		generatePixelBoard()
+	}
+})
+
+function generatePixelBoard() {
+	for (let row = 0; row < rowMax; row += 1) {
+		const pixelRow = document.createElement('div')
+		pixelRow.setAttribute('id', 'pixel-row' + i)
+		pixelRow.setAttribute('class', 'pixel-row')
+		document.querySelector('#whole-board').appendChild(pixelRow)
+		for (let column = 0; column < columnMax; column += 1) {
+			const pixel = document.createElement('div')
+			pixel.setAttribute('id', 'pixel')
+			pixel.setAttribute('class', 'pixel')
+			document.querySelector('#pixel-row' + i).appendChild(pixel)
+		}
+		i += 1
+	}
+}
+
+function removePixelBoard() {
+	while (wholeBoard.firstChild) {
+	  wholeBoard.removeChild(wholeBoard.firstChild);
+	}
+}
