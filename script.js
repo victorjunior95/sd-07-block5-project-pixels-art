6a33741836469b4e0f2c6f2fd73387056253edf2
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-19 11:45:29 -0300

    code climate 3

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 const cores = document.querySelectorAll('.color');
 cores[0].style.backgroundColor = 'black';
-RgbAlea();
-const pixelBoard = document.querySelector('#pixel-board');
-window.onload = function () {
-  cores[0].classList.add('selected');
-  pixelCor();
-};
-generatePixel(25);
-function generatePixel(quadrados) {
-  for (index = 0; index < quadrados; index += 1) {
-    stylePixel();
-  }
-}
-
 function pixelCor() {
   pixel = document.querySelectorAll('.pixel');
   pixel.forEach((item) => {
@@ -25,7 +12,7 @@ function pixelCor() {
 }
 
 function stylePixel() {
-  let caixa = document.createElement('div');
+  const caixa = document.createElement('div');
   caixa.className = 'pixel';
   caixa.style.backgroundColor = 'white';
   caixa.style.height = '40px';
@@ -42,7 +29,7 @@ function apagao() {
 }
 
 function inputSize() {
-  let inputValue = document.querySelector('#board-size').value;
+  const inputValue = document.querySelector('#board-size').value;
   if (inputValue > 50) {
     return (inputValue = 50);
   } else if (inputValue < 5) {
@@ -76,30 +63,43 @@ function nulll() {
     alert('Board inválido!');
   }
 }
+RgbAlea();
+const pixelBoard = document.querySelector('#pixel-board');
+window.onload = function () {
+  cores[0].classList.add('selected');
+  pixelCor();
+};
+generatePixel(25);
+function generatePixel(quadrados) {
+  for (let index = 0; index < quadrados; index += 1) {
+    stylePixel();
+  }
+}
+
 // Cor preta selecionada
 cores[0].addEventListener('click', function () {
-  for (index = 0; index < 4; index += 1) {
+  for (let index = 0; index < 4; index += 1) {
     cores[index].className = 'color';
   }
   cores[0].classList.add('selected');
 });
 // Cor azul selecionada
 cores[1].addEventListener('click', function () {
-  for (index = 0; index < 4; index += 1) {
+  for (let index = 0; index < 4; index += 1) {
     cores[index].className = 'color';
   }
   cores[1].classList.add('selected');
 });
 // Cor vermelha selecionada
 cores[2].addEventListener('click', function () {
-  for (index = 0; index < 4; index += 1) {
+  for (let index = 0; index < 4; index += 1) {
     cores[index].className = 'color';
   }
   cores[2].classList.add('selected');
 });
 // Cor verde selecionada
 cores[3].addEventListener('click', function () {
-  for (index = 0; index < 4; index += 1) {
+  for (let index = 0; index < 4; index += 1) {
     cores[index].className = 'color';
   }
   cores[3].classList.add('selected');
