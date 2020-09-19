57319bdacb7a0384ba44942052786174f0725743
barrrosomarcio <barrrosomarcio@gmail.com>
2020-09-19 09:33:40 -0300

    commit de correções

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let collorPallet = document.getElementsByClassName("color")
+let collorPallet = document.querySelectorAll('.selected,.color');
 collorPallet[0].style.backgroundColor = "rgb(0, 0, 0)"
 for (let count = 1; count < collorPallet.length ; count +=1 ){
     collorPallet[count].style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);     
