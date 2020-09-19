a8442e56376ec5c87b7cedf7681e70c5987ae1c2
menezesandre <dehhmenezes87@gmail.com>
2020-09-19 16:15:47 -0300

    Adicionado funçao pintar pixels e apagar

--- script.js
+++ script.js
@@ -18,49 +18,109 @@ function createPixelsBoard(number) {
   }
 }
 
-// Predefinições ao carregar a página
-window.onload = function () {
-  createPixelsBoard(5);
-}
 
-let color = "";
 
-// Adiciona a classe selected à cor preta
-let black = document.querySelector(".black");
-black.addEventListener("click", function () {
+const black = document.querySelector(".black");
+const red = document.querySelector(".red");
+const green = document.querySelector(".green");
+const blue = document.querySelector(".blue");
+let selected = document.querySelector(".selected");
+
+// Preto
+function blackSelect() {
   black.classList.add("selected");
   red.classList.remove("selected");
   green.classList.remove("selected");
   blue.classList.remove("selected");
-  color = window.getComputedStyle(black).getPropertyValue("background-color");
-});
+  selected = black;
+  const colorBlack = window.getComputedStyle(black).getPropertyValue("background-color");
+  return colorBlack;
+}
 
-// Adiciona a classe selected à cor vermelha
-let red = document.querySelector(".red");
-red.addEventListener("click", function () {
+// Vermelho
+function redSelect() {
   black.classList.remove("selected");
   red.classList.add("selected");
   green.classList.remove("selected");
   blue.classList.remove("selected");
-  color = window.getComputedStyle(red).getPropertyValue("background-color");
-});
+  selected = red;
+  const colorRed = window.getComputedStyle(red).getPropertyValue("background-color");
+  return colorRed;
+}
 
-// Adiciona a classe selected à cor verde
-let green = document.querySelector(".green");
-green.addEventListener("click", function () {
+// Verde
+function greenSelect() {
   black.classList.remove("selected");
   red.classList.remove("selected");
   green.classList.add("selected");
   blue.classList.remove("selected");
-  color = window.getComputedStyle(green).getPropertyValue("background-color");
-});
+  selected = green;
+  const colorGreen = window.getComputedStyle(green).getPropertyValue("background-color");
+  return colorGreen;
+}
 
-// Adiciona a classe selected à cor azul
-let blue = document.querySelector(".blue");
-blue.addEventListener("click", function () {
+// Azul
+function blueSelect() {
   black.classList.remove("selected");
   red.classList.remove("selected");
   green.classList.remove("selected");
   blue.classList.add("selected");
-  color = window.getComputedStyle(blue).getPropertyValue("background-color");
+  selected = blue;
+  const colorBlue = window.getComputedStyle(blue).getPropertyValue("background-color");
+  return colorBlue;
+}
+
+// Evento de click das cores selecionadas
+black.addEventListener("click", blackSelect);
+red.addEventListener("click", redSelect);
+green.addEventListener("click", greenSelect);
+blue.addEventListener("click", blueSelect);
+
+// Função pintar os píxels
+function paintBoard() {
+  let pixelBoard = document.querySelectorAll(".pixel");
+  for (let indexPixel = 0; indexPixel < pixelBoard.length; indexPixel += 1) {
+    pixelBoard[indexPixel].addEventListener("click", function () {
+      pixelBoard[indexPixel].style.backgroundColor = window.getComputedStyle(selected).backgroundColor;
+    });
+  }
+}
+
+
+
+
+// Botão para limpar o quadro
+let btnErase = document.querySelector("#erase-board");
+btnErase.addEventListener("click", function () {
+  let numberPixels = document.querySelectorAll(".pixel");
+  for (let indexBoard = 0; indexBoard < numberPixels.length; indexBoard += 1) {
+    numberPixels[indexBoard].style.backgroundColor = "white";
+  };
 });
+
+// Botão para gerar o quadro com o tamanho definido pelo usuário
+let buttonGenerate = document.querySelector("#generate-board");
+buttonGenerate.addEventListener("click", function () {
+  let size = document.querySelector("#board-size");
+  let erase = document.querySelector("#pixel-board");
+  erase.innerHTML = "";
+  if (size.value == "") {
+    alert("Board inválido");
+  }
+  if (size.value < 5) {
+    size.value = 5;
+    createPixelsBoard(size.value);
+  } else if (size.value > 50) {
+    size.value = 50;
+    createPixelsBoard(size.value)
+  } else {
+    createPixelsBoard(size.value);
+  };
+  paintBoard();
+});
+
+// Predefinições ao carregar a página
+window.onload = function () {
+  createPixelsBoard(5);
+  paintBoard();
+}
