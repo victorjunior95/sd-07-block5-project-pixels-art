3685692e391ae395ea0f291f5fa3f2bdec055806
Ana Karine <anakarine.ti@gmail.com>
2020-09-19 00:53:54 -0300

    codeclimate

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 // so it's applied to all spec files
 // cypress/support/index.js
 
-Cypress.on('uncaught:exception', (err, runnable) => {
+//Cypress.on('uncaught:exception', (err, runnable) => {
   // returning false here prevents Cypress from
   // failing the test
-  return false;
-});
+//  return false;
+//});
 
 
 // https://github.com/tryber/sd-07-block5-project-pixels-art/tree/Cainan6697-project-pixels-art
@@ -45,8 +45,7 @@ for (let index = 0; index < colors.length; index += 1) {
 
 // github.com/susanschen/Pixel-Art-Maker/blob/master/index.html
 const canvas = document.getElementById('pixel-board');
-const height = document.getElementById('board-size');
-const width = document.getElementById('board-size');
+const size = document.getElementById('board-size');
 const pixelBoard = document.getElementById('custom-board');
 
 pixelBoard.onsubmit = function (event) {
@@ -62,13 +61,13 @@ function clearGrid() {
 }
 
 function fillSquare() {
-  this.setAttribute('style', `background-color: ${color.value}`);
+  this.setAttribute('style', `background-color: ${colors.value}`);
 }
 
 function makeGrid() {
-  for (let r = 0; r < height.value; r += 1) {
+  for (let r = 0; r < size.value; r += 1) {
     const row = canvas.insertRow(r);
-    for (let c = 0; c < width.value; c += 1) {
+    for (let c = 0; c < size.value; c += 1) {
       const cell = row.insertCell(c);
       cell.addEventListener('click', fillSquare);
       cell.className = 'pixel';
@@ -77,18 +76,24 @@ function makeGrid() {
 }
 
 window.onload = function () {
-  makeGrid();
-};
+  for (let r = 0; r < 5; r += 1) {
+    const row = canvas.insertRow(r);
+    for (let c = 0; c < 5; c += 1) {
+      const cell = row.insertCell(c);
+      cell.addEventListener('click', fillSquare);
+      cell.className = 'pixel';
+    }
+  }
+}
 // ---------------------------------------------------------------------------------------------
 
 
-const alertVQV = document.querySelector('#board-size');
 document.querySelector('#generate-board').addEventListener('click', function () {
-  if (alertVQV.value === '') {
+  if (size.value === '') {
     alert('Board inválido!');
-  } else if (alertVQV.value < 5 && alertVQV.value !== '') {
-    alertVQV.value = 5;
-  } else if (alertVQV.value > 50 && alertVQV.value !== '') {
-    alertVQV.value = 50;
+  } else if (size.value > 50) {
+    size.value = 50;
+  } else if (size.value < 5) {
+    size.value = 5;
   }
 });
