ca0a7bfd3628202891f49cf090c5b872da9b1168
barrrosomarcio <barrrosomarcio@gmail.com>
2020-09-19 07:56:55 -0300

    pixels recebendo cor

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-document.onload
-{
-    let collorPallet = document.getElementsByClassName("color")
-    collorPallet[0].style.backgroundColor = "rgb(0, 0, 0)"
-    for (let count = 1; count < collorPallet.length ; count +=1 ){
-        collorPallet[count].style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);     
-    }
+let collorPallet = document.getElementsByClassName("color")
+collorPallet[0].style.backgroundColor = "rgb(0, 0, 0)"
+for (let count = 1; count < collorPallet.length ; count +=1 ){
+    collorPallet[count].style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);     
 }
 
-let selectedCollor = collorPallet[0].style.backgroundColor;
+let pixels = document.getElementsByClassName("pixel");
+for (let count = 0; count < pixels.length; count+=1){
+    pixels[count].style.backgroundColor = "rgb(255, 255, 255)";
+}
+
+
+let selected = collorPallet[0];
+selected.className = "selected";
+document.addEventListener("click", function(event){
+    if(event.target.className == "pixel"){
+        event.target.style.backgroundColor = selected.style.backgroundColor;
+    }
+    else if (event.target.className == "color"){
+        selected.className = "color";
+        selected = event.target;
+        selected.className = "selected";
+        }
+
+
+})
+
 
 
 
