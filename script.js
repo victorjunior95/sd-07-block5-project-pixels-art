1f5a25b6878e0b061b8f18d96e6134c25ac907e4
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-19 11:38:32 -0300

    code climate 2

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const cores = document.querySelectorAll(".color");
-cores[0].style.backgroundColor = "black";
+const cores = document.querySelectorAll('.color');
+cores[0].style.backgroundColor = 'black';
 RgbAlea();
-const pixelBoard = document.querySelector("#pixel-board");
+const pixelBoard = document.querySelector('#pixel-board');
 window.onload = function () {
-  cores[0].classList.add("selected");
+  cores[0].classList.add('selected');
   pixelCor();
 };
 generatePixel(25);
@@ -12,79 +12,37 @@ function generatePixel(quadrados) {
     stylePixel();
   }
 }
-// Cor preta selecionada
-cores[0].addEventListener("click", function () {
-  for (index = 0; index < 4; index += 1) {
-    cores[index].className = "color";
-  }
-  cores[0].classList.add("selected");
-});
-// Cor azul selecionada
-cores[1].addEventListener("click", function () {
-  for (index = 0; index < 4; index += 1) {
-    cores[index].className = "color";
-  }
-  cores[1].classList.add("selected");
-});
-// Cor vermelha selecionada
-cores[2].addEventListener("click", function () {
-  for (index = 0; index < 4; index += 1) {
-    cores[index].className = "color";
-  }
-  cores[2].classList.add("selected");
-});
-// Cor verde selecionada
-cores[3].addEventListener("click", function () {
-  for (index = 0; index < 4; index += 1) {
-    cores[index].className = "color";
-  }
-  cores[3].classList.add("selected");
-});
-let pixel = document.querySelectorAll(".pixel");
-const apaga = document.querySelector(".clear");
-apaga.addEventListener("click", apagao);
-const inputButton = document.querySelector("#generate-board");
-
-inputButton.addEventListener("click", function () {
-  nulll();
-  let inputValue = inputSize();
-  pixelBoard.style.height = inputValue * 42 + "px";
-  pixelBoard.style.width = inputValue * 42 + "px";
-  removeAll();
-  generatePixel(inputValue * inputValue);
-  pixelCor();
-});
 
 function pixelCor() {
-  pixel = document.querySelectorAll(".pixel");
+  pixel = document.querySelectorAll('.pixel');
   pixel.forEach((item) => {
-    item.addEventListener("click", function () {
+    item.addEventListener('click', function () {
       item.style.backgroundColor = document.querySelector(
-        ".selected"
+        '.selected'
       ).style.backgroundColor;
     });
   });
 }
 
 function stylePixel() {
-  let caixa = document.createElement("div");
-  caixa.className = "pixel";
-  caixa.style.backgroundColor = "white";
-  caixa.style.height = "40px";
-  caixa.style.width = "40px";
-  caixa.style.border = "1px solid black";
-  caixa.style.display = "inline-block";
+  let caixa = document.createElement('div');
+  caixa.className = 'pixel';
+  caixa.style.backgroundColor = 'white';
+  caixa.style.height = '40px';
+  caixa.style.width = '40px';
+  caixa.style.border = '1px solid black';
+  caixa.style.display = 'inline-block';
   pixelBoard.appendChild(caixa);
 }
 
 function apagao() {
   pixel.forEach((item) => {
-    item.style.backgroundColor = "white";
+    item.style.backgroundColor = 'white';
   });
 }
 
 function inputSize() {
-  let inputValue = document.querySelector("#board-size").value;
+  let inputValue = document.querySelector('#board-size').value;
   if (inputValue > 50) {
     return (inputValue = 50);
   } else if (inputValue < 5) {
@@ -113,8 +71,50 @@ function RgbAlea() {
 }
 
 function nulll() {
-  let inputValue = document.querySelector("#board-size").value;
-  if (inputValue == "") {
-    alert("Board inválido!");
+  let inputValue = document.querySelector('#board-size').value;
+  if (inputValue == '') {
+    alert('Board inválido!');
   }
 }
+// Cor preta selecionada
+cores[0].addEventListener('click', function () {
+  for (index = 0; index < 4; index += 1) {
+    cores[index].className = 'color';
+  }
+  cores[0].classList.add('selected');
+});
+// Cor azul selecionada
+cores[1].addEventListener('click', function () {
+  for (index = 0; index < 4; index += 1) {
+    cores[index].className = 'color';
+  }
+  cores[1].classList.add('selected');
+});
+// Cor vermelha selecionada
+cores[2].addEventListener('click', function () {
+  for (index = 0; index < 4; index += 1) {
+    cores[index].className = 'color';
+  }
+  cores[2].classList.add('selected');
+});
+// Cor verde selecionada
+cores[3].addEventListener('click', function () {
+  for (index = 0; index < 4; index += 1) {
+    cores[index].className = 'color';
+  }
+  cores[3].classList.add('selected');
+});
+let pixel = document.querySelectorAll('.pixel');
+const apaga = document.querySelector('.clear');
+apaga.addEventListener('click', apagao);
+const inputButton = document.querySelector('#generate-board');
+
+inputButton.addEventListener('click', function () {
+  nulll();
+  let inputValue = inputSize();
+  pixelBoard.style.height = inputValue * 42 + 'px';
+  pixelBoard.style.width = inputValue * 42 + 'px';
+  removeAll();
+  generatePixel(inputValue * inputValue);
+  pixelCor();
+});
