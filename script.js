e25da687bc4f5c351941e3355286e35939f15861
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-09-19 21:38:57 -0300

    adicionando input para ser alterado o tamanho do quadrado

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //função para escolher uma cor aleatória para a paleta de cores (cria um numero rgb)
+
 let secondColor = document.querySelector('.second-color');
 let thirdColor = document.querySelector('.third-color');
 let fourthColor = document.querySelector('.fourth-color');
@@ -13,6 +14,44 @@ function randomPalette() {
 }
 
 window.onload = randomPalette;
+
+//função para definir tamanho do quadrado
+
+function createTable() {
+  let pixelBoard = document.getElementById('pixel-board');
+
+  while (pixelBoard.firstChild){
+    pixelBoard.removeChild(pixelBoard.firstChild);
+  }
+
+  let input = document.getElementById('board-size');
+
+  if(input.value === ''){
+    alert("Board inválido!");
+  }
+  else if(input.value < 5){
+    input.value = 5;
+  }
+  else if (input.value > 50){
+    input.value = 50;
+  }
+
+  for(i = 0; i < input.value; i += 1){
+    let tr = document.createElement('div');
+    tr.className = "tr";
+    pixelBoard.appendChild(tr);
+
+    for(j = 0; j < input.value; j += 1){
+      let td = document.createElement('div');
+      td.className = "pixel td";
+      tr.appendChild(td);
+    }
+  }
+}
+
+let generateTable = document.getElementById('generate-board');
+
+generateTable.addEventListener('click', createTable)
 
 // função para colorir pixels de acordo com a cor da paleta escolhida
 
@@ -53,5 +92,3 @@ clearButton.addEventListener('click', function (){
     selectPixel[list].style.backgroundColor = 'white';
   }
 })
-
-
