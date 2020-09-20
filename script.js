81c1d3672a3e9cafbb6b7ba49adee3fac06ecf4b
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-09-20 15:10:52 -0300

    Ajustando code climate 05 - 20/09/2020

--- script.js
+++ script.js
@@ -1,60 +1,29 @@
 // MY VARIABLES AND CONSTANTS
-const pixelBoard = document.querySelector("#pixel-board");
-const inputSizeBoard = document.querySelector("#board-size");
-const buttonSizeBoard = document.querySelector("#generate-board");
-let getColor = "black";
+const pixelBoard = document.querySelector('#pixel-board');
+const inputSizeBoard = document.querySelector('#board-size');
+const buttonSizeBoard = document.querySelector('#generate-board');
+let getColor = 'black';
 let numberLines = 5;
 let numberElements = 5;
 
-function createNewBoard() {
-  buttonSizeBoard.addEventListener("click", function () {
-    if (inputSizeBoard.value === "") {
-      alert("Board inválido!");
-    } else if (inputSizeBoard.value <= 5) {
-      numberLines = 5;
-      numberElements = 5;
-      getColor = "black";
-    } else if (inputSizeBoard.value >= 50) {
-      numberLines = 50;
-      numberElements = 50;
-      getColor = "black";
-    } else if (inputSizeBoard.value > 5 && inputSizeBoard.value < 50) {
-      numberElements = inputSizeBoard.value;
-      numberLines = inputSizeBoard.value;
-      getColor = "black";
-      // console.log(numberElements);
-      // console.log(numberLines);
-    }
-    executeAll();
-  });
-}
-
+// FUNCTION TO 
 function clearBoard() {
-  pixelBoard.innerHTML = "";
+  pixelBoard.innerHTML = '';
 }
 
 // FUNCTION TO CREATE DIVS WITH CLASS 'LINE'
 function createLine() {
   for (let i = 0; i < numberLines; i += 1) {
-    const line = document.createElement("div");
-    line.classList.add("line");
+    const line = document.createElement('div');
+    line.classList.add('line');
     pixelBoard.appendChild(line);
   }
 }
 createLine();
 
-// LOOP TO EXECUTE IN ALL LINES THE FUNCTION FILL LINE
-function createPixels() {
-  for (let i = 0; i < numberLines; i += 1) {
-    const line = document.querySelectorAll(".line");
-    fillLine(line[i]);
-  }
-}
-createPixels();
-
 // FUNCTION TO CREATE MY PIXEL ELEMENTS AND THE CLASSES
 function createElement(classList) {
-  const elementPixel = document.createElement("div");
+  const elementPixel = document.createElement('div');
   elementPixel.classList = classList;
   return elementPixel;
 }
@@ -62,16 +31,25 @@ function createElement(classList) {
 // FUNCTION TO INPUT THE PIXEL ELEMENTS IN THE LINE
 function fillLine(divLine) {
   for (let i = 0; i < numberElements; i += 1) {
-    const elementPixel = createElement("pixel");
+    const elementPixel = createElement('pixel');
     divLine.appendChild(elementPixel);
   }
 }
 
+// LOOP TO EXECUTE IN ALL LINES THE FUNCTION FILL LINE
+function createPixels() {
+  for (let i = 0; i < numberLines; i += 1) {
+    const line = document.querySelectorAll('.line');
+    fillLine(line[i]);
+  }
+}
+createPixels();
+
 // LOOP TO UPDATE MY LET GET COLOR
 function updateColors() {
-  const colorSelected = document.querySelectorAll(".color");
+  const colorSelected = document.querySelectorAll('.color');
   for (let i = 0; i < colorSelected.length; i += 1) {
-    colorSelected[i].addEventListener("click", function (event) {
+    colorSelected[i].addEventListener('click', function (event) {
       let divs = event.target;
       getColor = divs.style.backgroundColor;
       // getColor = divs.classList[1];
@@ -83,9 +61,9 @@ updateColors();
 
 // LOOP TO SELECTED MY PIXEL ELEMENT AND CHANGE HIS BACKGROUND COLOR
 function changeColorPixel() {
-  const myPixels = document.querySelectorAll(".pixel");
+  const myPixels = document.querySelectorAll('.pixel');
   for (let i = 0; i < myPixels.length; i += 1) {
-    myPixels[i].addEventListener("click", function (event) {
+    myPixels[i].addEventListener('click', function (event) {
       let onePixel = event.target;
       onePixel.style.backgroundColor = getColor;
       // console.log(getColor);
@@ -95,30 +73,30 @@ function changeColorPixel() {
 changeColorPixel();
 
 // LOOP TO ADD AND REMOVE NEW CLASS 'SELECTED'
-const colorSelected = document.querySelectorAll(".color");
+const colorSelected = document.querySelectorAll('.color');
 for (let i = 0; i < colorSelected.length; i += 1) {
-  colorSelected[i].addEventListener("click", function (event) {
+  colorSelected[i].addEventListener('click', function (event) {
     let divs = event.target;
-    let checkClassName = document.querySelector(".selected");
+    let checkClassName = document.querySelector('.selected');
     // If the class selected existed, it will be removed
     if (checkClassName) {
-      checkClassName.classList.remove("selected");
+      checkClassName.classList.remove('selected');
     }
     // When the div be clicked, the class selected will be created in the div;
-    divs.classList.add("selected");
+    divs.classList.add('selected');
     // console.log(document.querySelectorAll('.selected'));
   });
 }
 
 // BUTTON CREATED TO CLEAR MY BACKGROUND
 function clearColors() {
-  const buttonClear = document.querySelector("#clear-board");
-  buttonClear.addEventListener("click", function () {
-    const myPixels = document.querySelectorAll(".pixel");
+  const buttonClear = document.querySelector('#clear-board');
+  buttonClear.addEventListener('click', function () {
+    const myPixels = document.querySelectorAll('.pixel');
     for (let i = 0; i < myPixels.length; i += 1) {
-      myPixels[i].style.backgroundColor = "white";
+      myPixels[i].style.backgroundColor = 'white';
     }
-    pixelBoard.style.backgroundColor = "white";
+    pixelBoard.style.backgroundColor = 'white';
   });
 }
 clearColors();
@@ -127,11 +105,11 @@ function paletteRandom() {
   let randomNumber1 = Math.round(Math.random() * 1000000);
   let randomNumber2 = Math.round(Math.random() * 1000000);
   let randomNumber3 = Math.round(Math.random() * 1000000);
-  let palette = document.querySelectorAll(".color");
-  palette[0].style.backgroundColor = "black";
-  palette[1].style.backgroundColor = "#" + randomNumber1;
-  palette[2].style.backgroundColor = "#" + randomNumber2;
-  palette[3].style.backgroundColor = "#" + randomNumber3;
+  let palette = document.querySelectorAll('.color');
+  palette[0].style.backgroundColor = 'black';
+  palette[1].style.backgroundColor = '#' + randomNumber1;
+  palette[2].style.backgroundColor = '#' + randomNumber2;
+  palette[3].style.backgroundColor = '#' + randomNumber3;
 }
 paletteRandom();
 
@@ -143,6 +121,29 @@ function executeAll() {
   updateColors();
   changeColorPixel();
   clearColors();
+}
+
+function createNewBoard() {
+  buttonSizeBoard.addEventListener('click', function () {
+    if (inputSizeBoard.value === '') {
+      alert('Board inválido!');
+    } else if (inputSizeBoard.value <= 5) {
+      numberLines = 5;
+      numberElements = 5;
+      getColor = 'black';
+    } else if (inputSizeBoard.value >= 50) {
+      numberLines = 50;
+      numberElements = 50;
+      getColor = 'black';
+    } else if (inputSizeBoard.value > 5 && inputSizeBoard.value < 50) {
+      numberElements = inputSizeBoard.value;
+      numberLines = inputSizeBoard.value;
+      getColor = 'black';
+      // console.log(numberElements);
+      // console.log(numberLines);
+    }
+    executeAll();
+  });
 }
 
 window.onload = function () {
