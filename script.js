02972c82e299e41e8f696c577464fabb5466b3d9
Diego F Affonso <diegofernandesaffonso@gmail.com>
2020-09-20 23:25:18 -0300

    construção dinamica

--- script.js
+++ script.js
@@ -12,16 +12,34 @@ let cores = document.getElementsByClassName("color");
 let pixelBoard = document.getElementById("pixel-board")
 let botaoVqv = document.getElementById("generate-board");
 
-botaoVqv.addEventListener ("click", teste)
+botaoVqv.addEventListener ("click", function () {
+  let numeroPixel = document.getElementById("board-size").value
+  if (numeroPixel < 5 && numeroPixel > 1) {
+    numeroPixel = 5
+  } else if (numeroPixel > 50) {
+    numeroPixel = 50
+  } else if (numeroPixel >= 5 || numeroPixel <= 50) {
+    numeroPixel = numeroPixel
+  } else {
+      alert("Board inválido!")
+  }
+  while (pixelBoard.firstChild) {
+      pixelBoard.removeChild(pixelBoard.firstChild)
+  }
+    for (let index = 0; index < numeroPixel; index += 1) {
+        let linha = document.createElement("div");
+        pixelBoard.appendChild(linha);
+            for (index2 = 0; index2 < numeroPixel; index2 += 1){
+                let pixelsFilhos = document.createElement("div");
+                linha.appendChild(pixelsFilhos);
+                linha.lastElementChild.className = "pixel"
+            }
+    }
+  pintarPreta()
+})
 
 function teste() {
-  let numeroPixel = document.getElementById("board-size").value
-  if (numeroPixel < 5) {
-    numeroPixel = 5
-  }
-  if (numeroPixel > 50) {
-    numeroPixel = 50
-  }
+    let numeroPixel = 5
   for (let index = 0; index < numeroPixel; index += 1) {
     let linha = document.createElement("div");
     pixelBoard.appendChild(linha);
@@ -35,34 +53,33 @@ function teste() {
   pintarPreta()
 };
 
-botaoCorPreta.addEventListener("click", pintarPreta);
+botaoCorPreta.addEventListener('click', pintarPreta);
 
 function pintarPreta () {
   for (index = 0; index < cores.length; index += 1) {
-    cores[index].classList.remove("selected")
-  }
-  botaoCorPreta.classList.add("selected")
+    cores[index].classList.remove('selected');
+  };
+  botaoCorPreta.classList.add('selected');
   let corPreta = window.getComputedStyle(botaoCorPreta, null).getPropertyValue("background-color");
-  document.querySelectorAll(".pixel").forEach(item => {
-    item.addEventListener("click", function() {
-      item.style.backgroundColor = corPreta
-    })
-  })
-}
+  document.querySelectorAll('.pixel').forEach(item => {
+    item.addEventListener('click', function () {
+      item.style.backgroundColor = corPreta;
+    });
+  });
+};
 
-botaoCorAzul.addEventListener("click", function(){
+botaoCorAzul.addEventListener('click', function () {
   for (index = 0; index < cores.length; index += 1) {
-    cores[index].classList.remove("selected")
-  }
-  botaoCorAzul.classList.add("selected")
-  let corAzul = window.getComputedStyle(botaoCorAzul, null).getPropertyValue("background-color");
-  document.querySelectorAll(".pixel").forEach(item => {
-    item.addEventListener("click", function() {
-        item.style.backgroundColor = corAzul
-
-})
-})
-})
+    cores[index].classList.remove('selected');
+  };
+  botaoCorAzul.classList.add('selected');
+  let corAzul = window.getComputedStyle(botaoCorAzul, null).getPropertyValue('background-color');
+  document.querySelectorAll('.pixel').forEach(item => {
+    item.addEventListener('click', function() {
+      item.style.backgroundColor = corAzul;
+    });
+  });
+});
 
 botaoCorVerde.addEventListener("click", function(){
     for (index = 0; index < cores.length; index += 1) {
@@ -88,15 +105,15 @@ document.querySelectorAll(".pixel").forEach(item => {
     item.addEventListener("click", function() {
         item.style.backgroundColor = corCinza
 
-})
-})
-})
+});
+});
+});
 
-let limpar = document.getElementById("clear-board");
+let limpar = document.getElementById('clear-board');
 
-limpar.addEventListener("click", function(){
-    let pixels = document.querySelectorAll(".pixel");
-    for (index = 0; index < pixels.length; index += 1) {
-        pixels[index].style.backgroundColor = "rgb(255, 255, 255)"
-    }
-})
+limpar.addEventListener("click", function () {
+  let pixels = document.querySelectorAll('.pixel');
+  for (index = 0; index < pixels.length; index += 1) {
+    pixels[index].style.backgroundColor = 'rgb(255, 255, 255)'
+  }
+});
