ff0e6b1c18535ee93c52be7909040a22b4b4504f
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2020-09-21 02:26:06 -0300

    Commit 01 - Pixels Art Project

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
- 
+window.onload = function() {
+    let numberOfLines = 5
+    let lines = document.querySelectorAll(".line");
+}
+
+fillSquare(lines);
+
+function fillSquare(lines) {
+    for(let index = 0; index < lines.length; index += 1) {
+      fillLine(lines[index]);
+    }
+  }
+
+function createBox(className) {
+    let box = document.createElement("div");
+    box.className = className;
+    return box;
+}
+
+function fillLine(divLine) {
+    for (let lineColumn = 1; lineColumn <= 5; lineColumn += 1) {
+      let box = createBox("box");
+      divLine.appendChild(box);
+      divLine.appendChild(createBox("box-empty"));
+    }
+  }
+  
+
