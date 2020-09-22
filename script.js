58ea81031969eab532a99cc0ebd16b6ff7e9fc66
victorvalim <victor.valim.c@gmail.com>
2020-09-22 14:53:56 -0300

    Falta aprimorar a selecao do selected, resolvi mas pode ter uma solucao melhor

--- script.js
+++ script.js
@@ -13,26 +13,48 @@ let pixel = document.querySelectorAll(".pixel");
 selected = window.getComputedStyle(selected, null).getPropertyValue("background-color");
 
 black.addEventListener("click", function (){
-    selected = window.getComputedStyle(black, null).getPropertyValue("background-color");
-    
+    selected = window.getComputedStyle(black, null).getPropertyValue("background-color"); 
+    black.classList.remove('selected'); /*https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList add e remove classList, para retirar caso clicque duas vezes no mesmo item da paleta e add para adicionar  */
+    black.classList.add('selected');
+    blue.classList.remove('selected');
+    red.classList.remove('selected');
+    green.classList.remove('selected');
+      
 })
+
 blue.addEventListener("click", function (){
-    selected = window.getComputedStyle(blue, null).getPropertyValue("background-color");
+    blue.classList.remove('selected');
+    blue.classList.add('selected');
+    selected = window.getComputedStyle(blue, null).getPropertyValue("background-color"); 
+    black.classList.remove('selected');
+    red.classList.remove('selected');
+    green.classList.remove('selected');
     
 })
+
+
 red.addEventListener("click", function (){
-    selected = window.getComputedStyle(red, null).getPropertyValue("background-color");
-    
+    selected = window.getComputedStyle(red, null).getPropertyValue("background-color");  
+    red.classList.remove('selected');
+    red.classList.add('selected');
+    blue.classList.remove('selected');
+    black.classList.remove('selected');
+    green.classList.remove('selected');
 })
+
 green.addEventListener("click", function (){
-    selected = window.getComputedStyle(green, null).getPropertyValue("background-color");
-    
+    selected = window.getComputedStyle(green, null).getPropertyValue("background-color");  
+    green.classList.remove('selected');
+    green.classList.add('selected');
+    blue.classList.remove('selected');
+    red.classList.remove('selected');
+    black.classList.remove('selected');
 })
 
 
-for (let i = 0; i < pixel.length; i++) {
-    pixel[i].addEventListener("click", function() {
-      pixel[i].style.backgroundColor = selected;
+for(let i = 0; i < pixel.length; i++) {
+    pixel[i].addEventListener("click", function(){
+        pixel[i].style.backgroundColor = selected;
     });
 }
 
