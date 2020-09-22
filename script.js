a0ec41c1b0d8b0a3fe0aa90cd02e91b1763f19a4
vicoliveir <victorhugoqxd@gmail.com>
2020-09-22 05:21:44 -0300

    tentando requisitos finais

---
+++ script.js
@@ -0,0 +1,29 @@
+document.querySelectorAll(".color").forEach(colorSelect);
+    function colorSelect(element) {
+    element.addEventListener("click", function () {
+        document.querySelectorAll(".color");
+    element.classList.add("selected");
+        selectedColor = element;
+    element.classList.remove("selected");
+  });
+}
+
+let paintColor = document.querySelectorAll(".pixel");
+
+document.querySelectorAll(".pixel").forEach(changeColor);
+    function changeColor(element) {
+   element.addEventListener("click", () => {
+    color = window
+  .getComputedStyle(selectedColor)
+  .getPropertyValue("background-color");
+    element.style.backgroundColor = color;
+  });
+}
+let table = document.querySelectorAll(".pixel");
+let clearBoard = document.getElementById("clear-board");
+
+clearBoard.addEventListener("click", () => {
+  for (let i = 0; i < table.length; i += 1) {
+    table[i].style.backgroundColor = "white";
+  }
+});
