7b27eeff755785850a79357d6317699873f968ac
Demistocles <demi.treze@gmail.com>
2020-09-21 23:07:39 -0300

    incluindo a matriz

---
+++ script.js
@@ -0,0 +1,19 @@
+// let numberLines = 5;
+// let pixelBoard = document.getElementById("pixel-board");
+
+// let pixel = [];
+// let linha = [];
+
+// let k = 0
+// for(let i = 0; i < numberLines;i +=1) {
+//     linha[i] = document.createElement("div");
+//     linha[i].classname = "linha"
+//     pixelBoard.appendChild(linha[i]);
+
+//     for (let j = 0; j < numberLines;j += 1) {
+//         pixel[k] = document.createElement("div")
+//         pixel[k].classname = "pixel"
+//         linha[i].appendChild(pixel[k]);
+//         k += 1;
+//     }
+// }
