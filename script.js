d694f778cb8b03e5f0c6c5892e87227a9053c2ef
paulohp7 <ph.pereira.7@gmail.com>
2020-09-22 16:30:59 -0300

    Sixth objective try2

--- script.js
+++ script.js
@@ -5,10 +5,8 @@ for (let index = 1; index < 26; index += 1) {
     myPixel.className = "pixel";
     pixelBoard.appendChild(myPixel);
     if (index % 5 === 0) {
-        console.log(index);
-        console.log("entrei no if"); 
         pixelBoard.appendChild(document.createElement('BR'));
     }
 }
 
-document.querySelector('.color .black').className = "color black selected";
+document.querySelector('.black').className = "color black selected";
