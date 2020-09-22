e2250e193790dc215691a73ad833bb3fe68879d5
Breenolf <blf@ic.ufal.br>
2020-09-22 19:34:44 -0300

    Corrigindo requisito 09

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 window.onload = function() {
+    
     let pixel = document.querySelectorAll('.pixel');
-    for (let i in pixel) {
-        pixel[i].innerText = '';
-    }
-}
-let pixel = document.querySelectorAll('.pixel');
-let button = document.querySelector('#clear-board');
-button.addEventListener('click', function() {
+    let button = document.querySelector('#clear-board');
+    button.addEventListener('click', function() {
     for (let i in pixel) {
         pixel[i].style.backgroundColor = "white";
     }
 })
+}
