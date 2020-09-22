4344fc1778f33748c7a342823dbed2fb1d9d2fa1
Wendrick <wendrick.b.c1@gmail.com>
2020-09-22 15:34:08 -0300

    Depois do recomeço

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-let colorPaletts = document.querySelectorAll(".color");
-
 window.onload = function() {
-    colorPaletts[0].className = "selected"
+    defineColorsBackground();
+}
+
+function getRandomColors() {
+    return ((Math.floor(Math.random()*255) + 1) + ", " + (Math.floor(Math.random()*255) + 1) + ", " + (Math.floor(Math.random()*255) + 1));
+}
+function defineColorsBackground() {
+    let buttons = document.querySelectorAll(".color");
+    buttons[0].style.backgroundColor = "black";
+    let helper = ["0, 0, 0"];
+    for (let i = 1; i < buttons.length; i++) {
+        helper[i] = getRandomColors();
+        for (let j = 0; j < helper.length; j++) {
+            if (helper[j] === helper[i]) {
+                helper[i] = getRandomColors();
+            }
+        }
+        buttons[i].style.backgroundColor = "rgb(" + getRandomColors() + ")";
+    }
 }
