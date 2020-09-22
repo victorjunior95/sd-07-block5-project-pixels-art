ded172903d141731cf13af2d052d3e27bbdaf96f
Renato Silva <renatosdev@gmail.com>
2020-09-22 00:15:25 -0300

    Item 9

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// let buttonClearBoard = document.getElementById("clear-board");
+let buttonClearBoard = document.getElementById("clear-board");
 
-// buttonClearBoard.addEventListener('click', function clearBoard() {
+buttonClearBoard.addEventListener('click', function () {
 //   for (index = 0; index < pixel.length; index =+ 1)
-//     document.getElementsByClassName("pixel").style.backgroundColor = 'white';
-// });
-
-function clearBoard() {
     document.getElementsByClassName("pixel").style.backgroundColor = 'white';
-};
+});
+
+// function clearBoard() {
+//     document.getElementsByClassName("pixel").style.backgroundColor = 'white';
+// };
