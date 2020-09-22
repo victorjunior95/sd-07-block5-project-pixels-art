1ffc273a1483e152771eeb4c2f11599e78bbb1d4
Breenolf <blf@ic.ufal.br>
2020-09-22 19:29:06 -0300

    Requisito 09

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    let pixel = document.querySelectorAll('.pixel');
+    for (let i in pixel) {
+        pixel[i].innerText = '';
+    }
+}
+let pixel = document.querySelectorAll('.pixel');
+let button = document.querySelector('#clear-board');
+button.addEventListener('click', function() {
+    for (let i in pixel) {
+        pixel[i].style.backgroundColor = "white";
+    }
+})
