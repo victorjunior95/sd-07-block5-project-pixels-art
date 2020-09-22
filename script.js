cd00bfb863f2deb853c253bddb2c29c9e6875fa3
Renato Silva <renatosdev@gmail.com>
2020-09-21 23:58:30 -0300

    Item 9

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let buttonClearBoard = document.getElementById("clear-board");
+// let buttonClearBoard = document.getElementById("clear-board");
 
-buttonClearBoard.addEventListener('click', function clearBoard() {
-  pixel.style.backgroundColor = 'white';
-});
+// buttonClearBoard.addEventListener('click', function clearBoard() {
+//   for (index = 0; index < pixel.length; index =+ 1)
+//     document.getElementsByClassName("pixel").style.backgroundColor = 'white';
+// });
+
+function clearBoard() {
+    document.getElementsByClassName("pixel").style.backgroundColor = 'white';
+};
