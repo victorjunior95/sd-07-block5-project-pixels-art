92f22e51ca8474ead55023eccbf6cdc553556ef6
paulohp7 <ph.pereira.7@gmail.com>
2020-09-22 17:25:52 -0300

    Eighth objective

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ for (let index = 1; index < 26; index += 1) {
     let myPixel = document.createElement('DIV');
     myPixel.className = "pixel";
     pixelBoard.appendChild(myPixel);
+    myPixel.addEventListener("click", function() {pintarPixel(myPixel);});
     if (index % 5 === 0) {
         pixelBoard.appendChild(document.createElement('BR'));
     }
@@ -20,11 +21,15 @@ document.querySelector('.black').className = "color black selected";
 
 function selecionaCor(cor){
     for (let index = 0; index < paletaCores.length; index += 1) {
-        console.log("entrei no for do selecionaCor");
         if(paletaCores[index].classList.contains('selected')) {
-            console.log("removi um selected");
             paletaCores[index].classList.remove('selected');
         }
     }
     cor.classList.add('selected');
+}
+
+function pintarPixel(pixel) {
+    console.log("pintei um pixel");
+    let corSelecionada = document.getElementsByClassName('selected');
+    pixel.style.backgroundColor = corSelecionada[0].classList[1];
 }
