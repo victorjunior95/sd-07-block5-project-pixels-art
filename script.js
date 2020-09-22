a96b7a7a489bfcc6a533773643d5d0bda411e7bc
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-22 09:48:21 -0400

    criada função para o usuário selecionar o tamanho do quadro

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function criaQuadro(tamanho) {  
+  for (let index = 0; index < tamanho; index += 1) {
+    document.querySelector('#pixel-board').appendChild(document.createElement('div'));
+    document.querySelector('#pixel-board').children[index].classList.add('linha');
+    for (let index2 = 0; index2 < tamanho; index2 += 1) {
+      document.querySelectorAll('.linha')[index].appendChild(document.createElement('div'));
+      document.querySelectorAll('.linha')[index].children[index2].classList.add('pixel');
+    }
+  }
+  trocaCor();
+}
 const selecionandoCor = document.getElementById('color-palette').children;
 for (let index = 0; index < selecionandoCor.length; index += 1) {
   selecionandoCor[index].addEventListener('click', function () {
@@ -5,16 +16,30 @@ for (let index = 0; index < selecionandoCor.length; index += 1) {
     this.classList.add('selected');
   });
 }
-const selecionandoPixel = document.getElementsByClassName('pixel');
-for (let index = 0; index < selecionandoPixel.length; index += 1) {
-  selecionandoPixel[index].addEventListener('click', function () {
-    this.style.backgroundColor = window.getComputedStyle(document.getElementsByClassName('color selected')[0]).backgroundColor;
-  });
-};
-
+function trocaCor() {
+  const selecionandoPixel = document.getElementsByClassName('pixel');
+  for (let index = 0; index < selecionandoPixel.length; index += 1) {
+    selecionandoPixel[index].addEventListener('click', function () {
+      this.style.backgroundColor = window.getComputedStyle(document.getElementsByClassName('color selected')[0]).backgroundColor;
+    });
+  }
+}
 const botaoLimpaTela = document.getElementById('clear-board');
+const selecionandoPixel = document.getElementsByClassName('pixel');
 botaoLimpaTela.addEventListener('click', function () {
-  for (index = 0; index < selecionandoPixel.length; index += 1) {
+  for (let index = 0; index < selecionandoPixel.length; index += 1) {
     selecionandoPixel[index].style.backgroundColor = 'white';
-  };
+  }
+  document.getElementById('board-size').value = '';
+})
+const botaoVqv = document.querySelector('#generate-board');
+botaoVqv.addEventListener('click', function () {
+  let tamanhoQuadro = document.querySelector('#board-size').value;
+  if (tamanhoQuadro < 5) {
+    tamanhoQuadro = 5;
+  } else if (tamanhoQuadro > 50) {
+    tamanhoQuadro = 50;
+  }
+  criaQuadro(tamanhoQuadro);
 });
+
