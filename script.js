352d1c955db2a69ababc3aa301ddadf080f19d6d
Wendrick <wendrick.b.c1@gmail.com>
2020-09-22 11:17:04 -0300

    Testando aqui

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-let buttonSelected = document.querySelector(".color");
-buttonSelected.className = "selected";
+let buttonSelected = document.querySelectorAll(".color");
+
+for (let i = 0; i < buttonSelected.length; i += 1) {
+    buttonSelected[i].addEventListener("click", function() {
+        buttonSelected[i].className = "selected"
+    });
+}
