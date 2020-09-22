5623c8d4893eda50aaad63c89abaa8f589131288
Wendrick <wendrick.b.c1@gmail.com>
2020-09-22 16:46:26 -0300

    Depois do recomeço

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ window.onload = function() {
 }
 
 function getRandomColors() {
-    return ((Math.floor(Math.random()*255) + 1) + ", " + (Math.floor(Math.random()*255) + 1) + ", " + (Math.floor(Math.random()*255) + 1));
+    return (Math.floor(Math.random()*256) + ", " + Math.floor(Math.random()*256) + ", " + Math.floor(Math.random()*256));
 }
+
 function defineColorsBackground() {
     let buttons = document.querySelectorAll(".color");
     buttons[0].style.backgroundColor = "black";
@@ -19,3 +20,12 @@ function defineColorsBackground() {
         buttons[i].style.backgroundColor = "rgb(" + getRandomColors() + ")";
     }
 }
+// let selectedButton = document.querySelector(".selected");
+// let buttons = document.querySelectorAll(".color");
+
+// for (let i of buttons) {
+//     i.addEventListener("click", function() {
+//         i.classList.add("selected")
+//         selectedButton.classList.remove("selected")
+//     });
+// }
