c9149a6d69ab09f528cf078f185935d7900168e7
v-icto-r <victor130496@gmail.com>
2020-09-22 00:59:56 -0300

    Oitavo requisito

--- script.js
+++ script.js
@@ -4,29 +4,40 @@ let pixelBoard = document.getElementById("pixel-board");
 let pixel = [];
 let linha = [];
 
+let k = 0
 for (let i = 0; i < numberLines; i += 1) {
-    linha[i] = document.createElement("div");
-    linha[i].className = "linha"
-    pixelBoard.appendChild(linha[i]);
-    
-    let k = 0;
-    for(let j = 0; j < numberLines; j += 1) {
-        pixel[k] = document.createElement("div");
-        pixel[k].className = "pixel"
-        linha[i].appendChild(pixel[k]);
-        k = k + 1;
-    }
+  linha[i] = document.createElement("div");
+  linha[i].className = "linha"
+  pixelBoard.appendChild(linha[i]);
+
+  for(let j = 0; j < numberLines; j += 1) {
+    pixel[k] = document.createElement("div")
+    pixel[k].className = "pixel"
+    linha[i].appendChild(pixel[k])
+    k = k + 1;
+  }
 }
 
 let color = document.querySelectorAll(".color");
+//usar o indexOf é um método de busca dentro do array
+for (let i = 0; i < color.length; i += 1) {
+  color[i].addEventListener("click", function(event) {
+    let selected = document.querySelector(".selected")
+    //console.log(selected)
 
-for(let i = 0; i < color.length; i += 1){
-    color[i].addEventListener("click", function(event) {
-        let selected = document.querySelector(".selected");
+    selected.classList.remove('selected');
 
-        selected.classList.remove('selected');
+    //color[i].classList.add('selected');
+    event.target.className += " selected"
+  })
+}
 
-        //color[i].classList.add('selected');
-        event.target.className += " selected"
-    })
+for (let i = 0; i < pixel.length; i += 1) {
+  pixel[i].addEventListener("click", function() {
+    let selected = document.querySelector(".selected")
+    // https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+    pixel[i].style.backgroundColor = window.getComputedStyle(selected).backgroundColor
+    //console.log(window.getComputedStyle(selected).backgroundColor)
+
+  })
 }
