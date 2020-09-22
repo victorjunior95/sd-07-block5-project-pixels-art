5702a70124501041f3bc4e04e8a1ea52dfd51838
richardfazeredo <richardcompletetour@gmail.com>
2020-09-22 15:16:07 -0300

    Adicionando funcionalidade de atribuir cor ao pixel ao clicar sobre ele, copiando a cor que foi previamente selecionada na paleta

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ let colorPallete = document.getElementById('color-palette');
 let pixelBoard = document.getElementById('pixel-board');
 let colors = document.querySelectorAll('.color');
 
-window.onload = function () {
-  colorPallete.children[0].classList.add('selected');
-};
-
 for (let indexLine = 0; indexLine < 5; indexLine += 1) {
   let lineBoard = document.createElement('div');
   pixelBoard.appendChild(lineBoard);
@@ -13,15 +9,27 @@ for (let indexLine = 0; indexLine < 5; indexLine += 1) {
     let pixel = document.createElement('div');
     pixel.setAttribute('class', 'pixel');
     lineBoard.appendChild(pixel);
+    pixel.addEventListener('click', function() {
+      let elementSelected = document.querySelector('.selected');
+      pixel.style.backgroundColor = getBgColor(elementSelected);  
+    });
   }
+}
+
+function getBgColor(element) {
+  let colorOfElementPallete = window.getComputedStyle(element, null)
+  .getPropertyValue("background-color");
+  console.log(colorOfElementPallete);
+  return colorOfElementPallete;
 }
 
 colors.forEach(element => {
   element.addEventListener('click', function (){
-    for (let color of colors) {
+    for(let color of colors){
       color.classList.remove('selected');
     }
     element.classList.add('selected');
   });
 });
+
 
