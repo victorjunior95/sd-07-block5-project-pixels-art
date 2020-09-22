38ed1f0f76e5794184bc381d93da9160122925bd
paulohp7 <ph.pereira.7@gmail.com>
2020-09-22 16:12:06 -0300

    Fourth and Fifith objective

---
+++ script.js
@@ -0,0 +1,12 @@
+let pixelBoard = document.querySelector('#pixel-board');
+
+for (let index = 1; index < 26; index += 1) {
+    let myPixel = document.createElement('DIV');
+    myPixel.className = "pixel";
+    pixelBoard.appendChild(myPixel);
+    if (index % 5 === 0) {
+        console.log(index);
+        console.log("entrei no if"); 
+        pixelBoard.appendChild(document.createElement('BR'));
+    }
+}
