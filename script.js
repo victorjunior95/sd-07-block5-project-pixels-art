daae4fac0660714137b63ac643c52f77a8bf03e1
Jorge Alves <alvesdjorge@gmail.com>
2020-09-21 22:00:23 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

---
+++ script.js
@@ -0,0 +1,42 @@
+let numberLines = 5;
+let pixelBoard = document.getElementById("pixel-board");
+
+let pixel = [];
+let linha = [];
+
+let k = 0
+for (let i = 0; i < numberLines; i += 1) {
+    linha[i] = document.createElement("div");
+    linha[i].className = "linha"
+    pixelBoard.appendChild(linha[i]);
+    
+    for (let j = 0; j < numberLines; j += 1) {
+        pixel[k] = document.createElement("div")
+        pixel[k].className = "pixel"
+        linha[i].appendChild(pixel[k])
+        k = k + 1;
+    }
+
+}
+
+let color = document.querySelectorAll(".color");
+
+//usar o indexOf é um método de busca dentro do array
+
+for (let i = 0; i < color.length; i +=1) {
+    color[i].addEventListener("click", function (event) {
+    // remover select
+    //acrescentrar o select na cor certa
+    //.REMOVE
+    let selected = document.querySelector(".selected")
+    console.log(selectd)
+    selected.classList.remove("selected");
+    
+    // color[i].classList.add("selected");
+    event.target.className += " selected"
+    //console.log(selected)
+
+    })
+}
+
+
