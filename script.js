d174070a0691432daa173b0e4beaa87db3e5db9d
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2020-09-22 15:54:22 -0300

    Commit 02 - Pixels Art Project

--- script.js
+++ script.js
@@ -1,28 +1,58 @@
-window.onload = function() {
-    let numberOfLines = 5
-    let lines = document.querySelectorAll(".line");
-}
+window.onload = function() 
+{
+  let numberOfLines = 5;
+  var lines = document.querySelectorAll(".line");
+  let className="box-empty";
+  let color = document.getElementById("")
 
-fillSquare(lines);
+  fillSquare(lines);
 
-function fillSquare(lines) {
-    for(let index = 0; index < lines.length; index += 1) {
+  function fillSquare(lines)
+  {
+    for (let index = 0; index < lines.length; index++)
+    {
       fillLine(lines[index]);
     }
   }
 
-function createBox(className) {
+  function createBox()
+  {
     let box = document.createElement("div");
     box.className = className;
-    return box;
-}
+    return(box);
+  }
 
-function fillLine(divLine) {
-    for (let lineColumn = 1; lineColumn <= 5; lineColumn += 1) {
-      let box = createBox("box");
+  function fillLine(divLine)
+  {
+    for (let lineColumn = 0; lineColumn < numberOfLines; lineColumn += 1)
+    {
+      let box = createBox();
       divLine.appendChild(box);
-      divLine.appendChild(createBox("box-empty"));
     }
   }
+}
+
+
+// fillSquare(lines);
+
+// function fillSquare(lines) {
+//     for(let index = 0; index < lines; index += 1) {
+//       fillLine(lines[index]);
+//     }
+//   }
+
+// function createBox(className) {
+//     let box = document.createElement("div");
+//     box.className = className;
+//     return box;
+// }
+
+// function fillLine(divLine) {
+//     for (let lineColumn = 1; lineColumn <= 5; lineColumn += 1) {
+//       let box = createBox("box");
+//       divLine.appendChild(box);
+//       divLine.appendChild(createBox("box-empty"));
+//     }
+//   }
   
 
