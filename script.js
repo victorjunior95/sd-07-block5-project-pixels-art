59fcd23307b7dd856cf4e8c4e2c94114ba782c1c
Thays Costa <thayscosta3@gmail.com>
2020-09-23 12:59:41 -0300

    Mudança na criação no board e requisitos bônus 10 e 11.

--- script.js
+++ script.js
@@ -1,41 +1,57 @@
 let colorOptions = document.querySelectorAll('.color');
 let pixels = document.querySelectorAll('.pixel');
 let clearButton = document.querySelector('#clear-board');
+let board = document.querySelector('#pixel-board');
 
-for (let index = 0; index < colorOptions.length; index += 1){
-  colorOptions[index].addEventListener('click', function(event){
+for (let index = 0; index < colorOptions.length; index += 1) {
+  colorOptions[index].addEventListener('click', function (event) {
     let selectedColor = document.querySelector('.selected');
     selectedColor.classList.remove('selected');
     event.target.classList.add('selected');
   });
 } // Abstração facilitada pela resolução em grupo feita por parte da turma.
-  
-for (let index = 0; index < pixels.length; index += 1) {
-  pixels[index].addEventListener('click', function (){
-    let classSelected = document.querySelector('.selected');
-    pixels[index].style.backgroundColor = window.getComputedStyle(classSelected).backgroundColor
-  });
+
+function paintPixel () {
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener('click', function () {
+      let classSelected = document.querySelector('.selected');
+      pixels[index].style.backgroundColor = window.getComputedStyle(classSelected).backgroundColor;
+    });
+  }
 }
 
-clearButton.addEventListener('click', function(){
+clearButton.addEventListener('click', function () {
   for (let index = 0; index < pixels.length; index += 1) {
     pixels[index].style.backgroundColor = 'white';
   }
 });
 
 //requisitos bônus #VQV
+function createBoard (input) {
+  for (let index = 0; index < input; index += 1) {
+    const linhas = document.createElement('div');  
+    for (let index = 0; index < input; index += 1) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      linhas.appendChild(pixel);
+    }
+  board.appendChild(linhas);
+  }
+}
+createBoard(5);
 
-let boardSize = document.querySelector('#board-size');
-let vqvBtn = document.querySelector('#generate-board');
-
+const vqvBtn = document.querySelector('#generate-board');
 vqvBtn.addEventListener('click', function () {
-  if (boardSize.value < 5) {
-    boardSize.value == 5;
-  }
-  if (boardSize.value > 50) {
-    boardSize.value == 50;
-  }
+  const boardSize = document.querySelector('#board-size');
+  const board = document.querySelector('#pixel-board');
+  board.innerHTML = '';
   if (boardSize.value === '') {
     alert('Board inválido!');
+  } else if (boardSize.value < 5) {
+    createBoard(5);
+  } else if (boardSize.value > 50) {
+    createBoard(50);
+  } else { 
+    createBoard(boardSize.value)
   }
 });
