c459400fff1b7153ff7f44b0dc89d7acb5adf462
wellington <profwellington17@gmail.com>
2020-09-26 20:04:56 -0300

    Atividade11.

--- script.js
+++ script.js
@@ -23,49 +23,71 @@ function createGrid(nLinha){
 }
 
 createGrid(nLinha);
-
+function selectColor(){
 let color =document.querySelectorAll(".color");
 
-for (let i = 0; i < color.length; i += 1) {
-    
-    color[i].addEventListener("click", function() {
+    for (let i = 0; i < color.length; i += 1) {
         
-        let selected = document.querySelector(".selected");
-        selected.classList.remove('selected');
-        color[i].classList.add('selected');
-
-     }); 
-}
-
-for(let i=0; i < pixel.length; i += 1){
-    pixel[i].addEventListener("click", function(){
-        let selected=document.querySelector(".selected");
-        console.log(selected);
-        pixel[gi].style.backgroundColor = window.getComputedStyle(selected).backgroundColor;
+        color[i].addEventListener("click", function() {
             
+            let selected = document.querySelector(".selected");
+            selected.classList.remove('selected');
+            color[i].classList.add('selected');
+
+        }); 
+    }
+}
+selectColor();
+function changeColor(){
+    for(let i=0; i < pixel.length; i += 1){
+        pixel[i].addEventListener("click", function(){
+            let selected=document.querySelector(".selected");
+            console.log(selected);
+            pixel[i].style.backgroundColor = window.getComputedStyle(selected).backgroundColor;
+                
+
+        });
+    }
+}
+changeColor();
+
+function clear(){
+    let limpapx=document.getElementById("clear-board")
+
+    limpapx.addEventListener("click", function(){ 
+        
+        for(let i=0; i < pixel.length; i += 1){
+
+            pixel[i].style.backgroundColor= "white";
+        }
+
 
     });
 }
+clear();
 
-let limpapx=document.getElementById("clear-board")
-
-limpapx.addEventListener("click", function(){ 
-    
-    for(let i=0; i < pixel.length; i += 1){
-
-        pixel[i].style.backgroundColor= "white";
-    }
-
-
-});
 let btn=document.querySelector("#generate-board")
     btn.addEventListener("click", function() {
     let numberImput = document.querySelector("#board-size").value;
     if(numberImput === null || numberImput === "" ){
         alert("Board inválido!")
     }
+
+    else if(numberImput < 5){
+
+        numberImput =5;
+
+    }else if(numberImput >50){
+        numberImput =50;
+
+    }
+
+
     deleteGrid();
     createGrid(numberImput);
+    clear();
+    selectColor();
+    changeColor();
 });
 
 function deleteGrid(){
