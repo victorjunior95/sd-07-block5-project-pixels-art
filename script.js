320f5b1f3ad3305b89f8b21bd142e5f3a5b5e95a
Breenolf <blf@ic.ufal.br>
2020-09-28 18:13:35 -0300

    Implementando funcao que pinta o pixel com a cor requerida (08)

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const colorBlack = document.getElementById('black');
+colorBlack.style.backgroundColor = 'black';
+
+const colorRed = document.getElementById('red');
+colorRed.style.backgroundColor = 'red';
+
+const colorBlue = document.getElementById('blue');
+colorBlue.style.backgroundColor = 'blue';
+
+const colorGreen = document.getElementById('green');
+colorGreen.style.backgroundColor = 'green';
+
 document.getElementById('color-palette').addEventListener('click', function(event) {
   let colors = document.getElementsByClassName('color');
   for (let i = 0; i < colors.length; i += 1) {
@@ -5,6 +17,10 @@ document.getElementById('color-palette').addEventListener('click', function(even
   }
   let selectedColor = event.target;
   selectedColor.classList.add('selected');
-  let teste = document.getElementById('color-palette');
-  console.log(teste);
+})
+
+document.getElementById('pixel-board').addEventListener('click', function (event) {
+  let selectedPixel = event.target;
+  let selectedColor = document.querySelector('.selected');
+  selectedPixel.style.backgroundColor = selectedColor.style.backgroundColor; 
 })
