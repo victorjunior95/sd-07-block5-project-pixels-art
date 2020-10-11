0da61bca3a93c544b3d6950fac8c4d5dc8d9fd27
Bruno Nogueira <nugobruno@gmail.com>
2020-10-11 06:38:07 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const firstColor = document.querySelector('.color');
+const colors = document.querySelectorAll('.color');
 
-firstColor.className += ' selected';
+colors[0].className = 'color selected';
+
+const setColor = (selectedColor) => {
+  colors.forEach((element, index) => {
+    if (index !== selectedColor) {
+      element.className = 'color';
+    } else {
+      element.className = 'color selected';
+    }
+  });
+};
+
+colors[0].addEventListener('click', () => setColor(0));
+colors[1].addEventListener('click', () => setColor(1));
+colors[2].addEventListener('click', () => setColor(2));
+colors[3].addEventListener('click', () => setColor(3));
