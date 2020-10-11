c81de9e493c1a6789bb9e9c08bfae66d9cd0c698
Bruno Nogueira <nugobruno@gmail.com>
2020-10-11 07:12:27 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const colors = document.querySelectorAll('.color');
-
 colors[0].className = 'color selected';
 
-const setColor = (selectedColor) => {
+const setColor = (thisColor) => {
   colors.forEach((element, index) => {
-    if (index !== selectedColor) {
+    if (index !== thisColor) {
       element.className = 'color';
     } else {
       element.className = 'color selected';
@@ -16,3 +15,13 @@ colors[0].addEventListener('click', () => setColor(0));
 colors[1].addEventListener('click', () => setColor(1));
 colors[2].addEventListener('click', () => setColor(2));
 colors[3].addEventListener('click', () => setColor(3));
+
+const pixels = document.querySelectorAll('.pixel');
+
+//  https://stackoverflow.com/questions/25238153/how-to-get-background-color-property-value-in-javascript
+pixels.forEach((eachPixel) => {
+  eachPixel.addEventListener('click', () => {
+    const selectedColor = window.getComputedStyle(document.querySelector('.selected')).getPropertyValue('background-color');
+    eachPixel.style.backgroundColor = selectedColor;
+  });
+});
