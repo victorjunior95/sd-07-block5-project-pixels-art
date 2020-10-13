72f328f98d26331f0371024c468c4fa5b8876c89
Bruno Nogueira <nugobruno@gmail.com>
2020-10-13 18:17:46 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const color = document.querySelectorAll('.color');
 color[0].classList.add('selected');
 
+
 const colorPalette = document.querySelector('.color-palette');
 colorPalette.addEventListener('click', (event) => {
   const selectedColor = document.querySelector('.selected');
@@ -9,4 +10,16 @@ colorPalette.addEventListener('click', (event) => {
     selectedColor.classList.toggle('selected');
     event.target.classList.toggle('selected');
   }
+});
+
+const pixelBoard = document.querySelector('#pixel-board');
+
+const pixels = document.querySelectorAll('.pixel');
+pixels.forEach((eachPixel) => {
+  eachPixel.addEventListener("click", () => {
+    const selectedColor = window
+      .getComputedStyle(document.querySelector(".selected"))
+      .getPropertyValue("background-color");
+    eachPixel.style.backgroundColor = selectedColor;
+  });
 });
